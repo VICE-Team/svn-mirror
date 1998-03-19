c84038f872d726b168764dd9fe9bcf111d749cde
trikalio <Spiro.Trikaliotis@gmx.de>
1998-03-19 21:23:09 +0000

    This commit was generated by cvs2svn to compensate for changes in r132, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@133 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/include/patchlevel.h
@@ -0,0 +1,18 @@
+/*
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ */
+
+/* Global VICE release version.  */
+#define VERSION		"0.14.0"
+
+/* Define this for unstable, untested versions for developers only.  */
+/* #define UNSTABLE */
+
+/* Petcat version. */
+#define PETCATVERSION	2.02
+#define PETCATLEVEL	1
+
+/* Petlp version. */
+#define PETLPVERSION	1.00
+#define PETLPLEVEL	0
