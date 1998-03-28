689c7bcb191d0661ece0fddf55158547cc8fb687
trikalio <Spiro.Trikaliotis@gmx.de>
1998-03-28 11:47:15 +0000

    This commit was generated by cvs2svn to compensate for changes in r182, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@183 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/include/6510core.h
+++ vice/src/include/6510core.h
@@ -2,7 +2,7 @@
  * 6510core.h - Core definitions for the MOS 6510 CPU emulation.
  *
  * Written by
- *   Ettore Perazzoli (ettore@comm2000.it)
+ *  Ettore Perazzoli (ettore@comm2000.it)
  *
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
