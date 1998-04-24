f68bf3e0909f51e764165d39c6a92b7040c0f3cb
trikalio <Spiro.Trikaliotis@gmx.de>
1998-04-24 18:07:39 +0000

    This commit was generated by cvs2svn to compensate for changes in r304, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@305 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/include/tapeunit.h
+++ vice/src/include/tapeunit.h
@@ -28,68 +28,16 @@
 #define _TAPEUNIT_H
 
 #include "tape.h"
-
-#define  CAS_BUFFER_OFFSET  (ram[0xB2] | (ram[0xB3] << 8))  /* C64,128 TAPE1 */
-
-#define  VERCK			0x93
-
-
-#ifdef PET
-#define FIND_RET		0xF
-#define WRITE_RET		0xF
-#define TRANSFER_RET		0xF	/* BASIC 1: Restore Normal IRQ */
-
-#define FIND_2_RET		0xF
-#define WRITE_2_RET		0xF
-#define TRANSFER_2_RET		0xFC7B	/* BASIC 2: Restore Normal IRQ */
-
-#define FIND_4_RET		0xF
-#define WRITE_4_RET		0xF
-#define TRANSFER_4_RET		0xFCC0	/* BASIC 4: Restore Normal IRQ */
-#endif
-
-
-#ifdef VIC20
-#define  IRQTMP			0x2A0
-
-#define FIND_RET		0xF7B5
-#define WRITE_RET		0xF83E
-#define TRANSFER_RET		0xFCCF	/* VIC20: Restore Normal IRQ */
-#endif
-
-#ifdef CBM64
-#define  IRQTMP			0x2A0
-
-#define FIND_RET		0xF732
-#define WRITE_RET		0xF7C1
-#define TRANSFER_RET		0xFC93	/* C64: Restore Normal IRQ */
-#endif
-
-#ifdef C128
-#define  IRQTMP			0xA0A
-
-#define FIND_RET		0xE8D6
-#define WRITE_RET		0xE971
-#define TRANSFER_RET		0xEE57	/* C128: Restore Normal IRQ */
-#endif
-
-#ifdef PET
-#define SET_ST(b)	STORE(0x96, (LOAD(0x96) | b))
-#define BSOUR			0xA5	/* Buffered Character for IEEE Bus */
-#define TMP_IN			0xA5
-#define IRQTMP                  0x000   /* FIXME... */
-#else
-#define SET_ST(b)	STORE(0x90, (LOAD(0x90) | b))
-#define BSOUR			0x95	/* Buffered Character for Serial Bus */
-#define TMP_IN			0xA4	/* Temp Data Area */
-#endif
-
-/* ------------------------------------------------------------------------- */
+#include "traps.h"
 
 /* Global functions */
-extern void  detach_tape_image(TAPE *tape);
-extern int   attach_tape_image(TAPE *tape, const char *name, int mode);
-extern int   check_tape(void);
-extern int   initialize_tape(int number);
+extern void detach_tape_image(TAPE *tape);
+extern int attach_tape_image(TAPE *tape, const char *name, int mode);
+extern int check_tape(void);
+extern int initialize_tape(const trap_t *tape);
+extern void findheader (void);
+extern void writeheader (void);
+/* extern void checkplay (void); */
+extern void tapereceive (void);
 
 #endif /* ndef _TAPEUNIT_H */
