d6d97dac59795f8ad6d76e11f9968747f9ade527
trikalio <Spiro.Trikaliotis@gmx.de>
1998-04-24 20:10:17 +0000

    This commit was generated by cvs2svn to compensate for changes in r306, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@307 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/include/mos6510.h
@@ -0,0 +1,61 @@
+/*
+ * mos6510.h - Definitions for emulation of the 6510 processor.
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _MOS6510_H
+#define _MOS6510_H
+
+#include "types.h"
+
+/* Struct for the 6510 registers.  */
+typedef struct _mos6510_regs {
+    ADDRESS pc;
+    BYTE a;
+    BYTE x;
+    BYTE y;
+    BYTE sp;
+    struct {
+        int z:1;
+        int n:1;
+        int v:1;
+        int b:1;
+        int d:1;
+        int i:1;
+        int c:1;
+    } psp;
+} mos6510_regs_t;
+
+/* These define the position of the status flags in the P (`status')
+   register.  */
+#define S_SIGN		0x80
+#define S_OVERFLOW	0x40
+#define S_UNUSED	0x20
+#define S_BREAK		0x10
+#define S_DECIMAL	0x08
+#define S_INTERRUPT	0x04
+#define S_ZERO		0x02
+#define S_CARRY		0x01
+
+#endif
