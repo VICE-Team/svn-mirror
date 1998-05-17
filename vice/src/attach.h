d88b807ae0d37b9fbb3d8c742d6db3c46ec30f5b
trikalio <Spiro.Trikaliotis@gmx.de>
1998-05-17 13:46:19 +0000

    This commit was generated by cvs2svn to compensate for changes in r426, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@427 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/attach.h
@@ -0,0 +1,42 @@
+
+/*
+ * attach.h - file system attach management
+ *
+ * Written by
+ *  Andreas Boose (boose@unixserv.rz.fh-hannover.de)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _ATTACH_H
+#define _ATTACH_H
+
+#include "types.h"
+#include "drive.h"
+
+int file_system_init_resources(void);
+void file_system_init(void);
+int file_system_set_hooks(int unit,
+                          drive_attach_func_t attach_func,
+                          drive_detach_func_t detach_func);
+int file_system_attach_disk(int unit, char *filename);
+void file_system_detach_disk(int unit);
+
+#endif
