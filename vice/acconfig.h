339bb8d04d38b98822f115682a44766545dbcf18
trikalio <Spiro.Trikaliotis@gmx.de>
1998-05-19 08:13:27 +0000

    This commit was generated by cvs2svn to compensate for changes in r456, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@457 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/acconfig.h
@@ -0,0 +1,23 @@
+/* Define to your X11 display depth.  */
+#undef X_DISPLAY_DEPTH
+
+/* Define if you want to use Rober W. McMullen's TextField widget.  */
+#undef ENABLE_TEXTFIELD
+
+/* The number of bytes in an unsigned long.  */
+#undef SIZEOF_UNSIGNED_LONG
+
+/* The number of bytes in an unsigned int.  */
+#undef SIZEOF_UNSIGNED_INT
+
+/* The number of bytes in an unsigned short.  */
+#undef SIZEOF_UNSIGNED_SHORT
+
+/* Does libXext have XShmPutImage?  */
+#undef HAVE_XSHMPUTIMAGE
+
+/* Package name.  */
+#define PACKAGE ""
+
+/* Version number.  */
+#define VERSION ""
