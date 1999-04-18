dc469c11e2de5223ab2575a59d3a9ab2dfef30d7
trikalio <Spiro.Trikaliotis@gmx.de>
1999-04-18 17:29:12 +0000

    This commit was generated by cvs2svn to compensate for changes in r712, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@713 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/os2/joystick.h
@@ -0,0 +1,62 @@
+/*
+ * joystick.h - Joystick support for MS-DOS.
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _VICE_JOYSTICK_H
+#define _VICE_JOYSTICK_H
+
+#include "kbd.h"
+
+typedef enum {
+    JOYDEV_NONE,
+    JOYDEV_NUMPAD,
+    JOYDEV_KEYSET1,
+    JOYDEV_KEYSET2,
+    JOYDEV_HW1,
+    JOYDEV_HW2
+} joystick_device_t;
+
+typedef enum {
+    KEYSET_NW,
+    KEYSET_N,
+    KEYSET_NE,
+    KEYSET_E,
+    KEYSET_SE,
+    KEYSET_S,
+    KEYSET_SW,
+    KEYSET_W,
+    KEYSET_FIRE
+} joystick_direction_t;
+
+extern void joystick_init(void);
+extern void joystick_close(void);
+extern int  joystick_init_resources(void);
+extern int  joystick_init_cmdline_options(void);
+extern void joystick_update(void);
+extern int  joystick_handle_key(kbd_code_t kcode, int pressed);
+extern const char *joystick_direction_to_string(joystick_direction_t
+                                                direction);
+
+#endif
