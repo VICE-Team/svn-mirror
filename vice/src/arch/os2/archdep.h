74ba400389920e841d47dd8cbae90a61ccba3833
trikalio <Spiro.Trikaliotis@gmx.de>
1999-05-04 20:28:54 +0000

    This commit was generated by cvs2svn to compensate for changes in r758, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@759 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/os2/archdep.h
@@ -0,0 +1,71 @@
+/*
+ * archdep.h - Miscellaneous system-specific stuff.
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _ARCHDEP_H
+#define _ARCHDEP_H
+
+#include "ui.h"  // ui_drive_enable_t
+//#include <stdio.h>
+
+/* Filesystem dependant operators.  */
+#define FSDEVICE_DEFAULT_DIR   "."
+#define FSDEV_DIR_SEP_STR      "/"
+#define FSDEV_DIR_SEP_CHR      '/'
+#define FSDEV_EXT_SEP_STR      "."
+#define FSDEV_EXT_SEP_CHR      '.'
+
+/* Modes for fopen().  */
+#define MODE_READ   "rb"
+#define MODE_WRITE  "wb"
+#define MODE_APPEND "ab"
+
+/* ------------------------------------------------------------------------- */
+
+void ui_display_speed(float spd, float fps, int sec);
+void ui_display_drive_led(int drive_number, int status);
+void ui_display_drive_track(int drive_number, double track_number);
+void ui_enable_drive_status(ui_drive_enable_t state,
+                            int *drive_led_color);
+
+// -------------------------- archdependent stuff -----------------------
+
+int archdep_startup(int *argc, char **argv);
+const char *archdep_program_name(void);
+const char *archdep_boot_path(void);
+const char *archdep_default_sysfile_pathlist(const char *emu_id);
+const char *archdep_default_resource_file_name(void);
+const char *archdep_default_save_resource_file_name(void);
+FILE *archdep_open_default_log_file(void);
+int archdep_num_text_lines(void);
+int archdep_num_text_columns(void);
+int archdep_default_logger(const char *level_string, const char *format,
+                                                                va_list ap);
+
+void archdep_setup_signals(int do_core_dumps);
+int archdep_path_is_relative(const char *path);
+
+#endif
+
