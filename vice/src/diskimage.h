75e521081235d6c04057d3d5689812ce7ba22ff6
trikalio <Spiro.Trikaliotis@gmx.de>
1999-06-06 17:52:54 +0000

    This commit was generated by cvs2svn to compensate for changes in r778, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@779 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/diskimage.h
+++ vice/src/diskimage.h
@@ -31,6 +31,15 @@
 
 #include <types.h>
 
+#define D64_FILE_SIZE_35  174848        /* D64 image, 35 tracks */
+#define D64_FILE_SIZE_35E 175531        /* D64 image, 35 tracks with errors */
+#define D64_FILE_SIZE_40  196608        /* D64 image, 40 tracks */
+#define D64_FILE_SIZE_40E 197376        /* D64 image, 40 tracks with errors */
+#define D71_FILE_SIZE     349696        /* D71 image, 70 tracks */
+#define D81_FILE_SIZE     819200        /* D81 image, 80 tracks */
+#define D80_FILE_SIZE     533248        /* D80 image, 77 tracks */
+#define D82_FILE_SIZE    1066496        /* D82 image, 77 tracks */
+
 #define DISK_IMAGE_TYPE_X64 0
 #define DISK_IMAGE_TYPE_GCR 100
 #define DISK_IMAGE_TYPE_D64 1541
@@ -47,12 +56,16 @@ typedef struct {
     int tracks;
 } disk_image_t;
 
+extern void disk_image_init(void);
+
 extern int disk_image_open(disk_image_t *image);
 extern int disk_image_close(disk_image_t *image);
 extern int disk_image_read_sector(disk_image_t *image, BYTE *buf, int track,
                                   int sector);
 extern int disk_image_write_sector(disk_image_t *image, BYTE *buf, int track,
                                    int sector);
+extern int disk_image_check_sector(int format, int track, int sector);
+extern int disk_image_sector_per_track(int format, int track);
 
 #endif
 
