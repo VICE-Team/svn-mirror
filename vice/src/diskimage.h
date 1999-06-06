bc5c7b85ce8bdb5c56e7910615a598cb5dab198a
trikalio <Spiro.Trikaliotis@gmx.de>
1999-06-06 00:46:24 +0000

    This commit was generated by cvs2svn to compensate for changes in r774, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@775 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/diskimage.h
@@ -0,0 +1,58 @@
+/*
+ * diskimage.h - Common low-level disk image access.
+ *
+ * Written by
+ *  Andreas Boose       <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _DISKIMAGE_H
+#define _DISKIMAGE_H
+
+#include <stdio.h>
+
+#include <types.h>
+
+#define DISK_IMAGE_TYPE_X64 0
+#define DISK_IMAGE_TYPE_GCR 100
+#define DISK_IMAGE_TYPE_D64 1541
+#define DISK_IMAGE_TYPE_D71 1571
+#define DISK_IMAGE_TYPE_D81 1581
+#define DISK_IMAGE_TYPE_D80 8050
+#define DISK_IMAGE_TYPE_D82 8250
+
+typedef struct {
+    FILE *fd;
+    char *name;
+    int read_only;
+    int type;
+    int tracks;
+} disk_image_t;
+
+extern int disk_image_open(disk_image_t *image);
+extern int disk_image_close(disk_image_t *image);
+extern int disk_image_read_sector(disk_image_t *image, BYTE *buf, int track,
+                                  int sector);
+extern int disk_image_write_sector(disk_image_t *image, BYTE *buf, int track,
+                                   int sector);
+
+#endif
+
