28c158df819446524066e5c29822c256c7a99bbd
trikalio <Spiro.Trikaliotis@gmx.de>
1999-11-14 00:42:27 +0000

    This commit was generated by cvs2svn to compensate for changes in r838, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@839 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/vdrive/vdrive-command.h
@@ -0,0 +1,74 @@
+/*
+ * vdrive-command.h - Virtual disk-drive implementation. Command interpreter.
+ *
+ * Written by
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _VDRIVE_COMMAND_H
+#define _VDRIVE_COMMAND_H
+
+#include "types.h"
+#include "vdrive.h"
+
+/* Input Processor Error Codes.  */
+#define IPE_OK                          0
+#define IPE_DELETED                     1
+#define IPE_SEL_PARTN                   2       /* 1581 */
+#define IPE_UNIMPL                      3
+#define IPE_MEMORY_READ                 4
+
+#define IPE_WRITE_PROTECT_ON            26
+#define IPE_SYNTAX                      30
+#define IPE_INVAL                       31
+#define IPE_LONG_LINE                   32
+#define IPE_BAD_NAME                    33
+#define IPE_NO_NAME                     34
+
+#define IPE_NOT_WRITE                   60
+#define IPE_NOT_OPEN                    61
+#define IPE_NOT_FOUND                   62
+#define IPE_FILE_EXISTS                 63
+#define IPE_BAD_TYPE                    64
+#define IPE_NO_BLOCK                    65
+#define IPE_ILLEGAL_TRACK_OR_SECTOR     66
+
+#define IPE_NO_CHANNEL                  70
+#define IPE_DISK_FULL                   72
+#define IPE_DOS_VERSION                 73
+#define IPE_NOT_READY                   74
+#define IPE_BAD_PARTN                   77      /* 1581 */
+
+#define IPE_NOT_EMPTY                   80      /* dir to remove not empty */
+#define IPE_PERMISSION                  81      /* permission denied */
+
+extern void vdrive_command_init(void);
+extern int vdrive_command_execute(vdrive_t *vdrive, BYTE *buf,
+                                  unsigned int length);
+extern int vdrive_command_validate(vdrive_t *vdrive);
+extern void vdrive_command_set_error(vdrive_t *vdrive, int code,
+                                     unsigned int track, unsigned int sector);
+extern int vdrive_command_memory_read(vdrive_t *vdrive, ADDRESS addr,
+                                      unsigned int length);
+
+#endif
+
