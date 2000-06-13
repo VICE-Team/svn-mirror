8be3466f669b37d4c9ea378d9ee59ddb1a1d9e74
trikalio <Spiro.Trikaliotis@gmx.de>
2000-06-13 17:16:18 +0000

    This commit was generated by cvs2svn to compensate for changes in r890, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@891 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/screendrv/png.c
+++ vice/src/screendrv/png.c
@@ -2,6 +2,7 @@
  * png.c - Create a PNG file.
  *
  * Written by
+ *  Daniel Sladic <dsladic@cs.cmu.edu>
  *  Andreas Boose <boose@linux.rz.fh-hannover.de>
  *
  * This file is part of VICE, the Versatile Commodore Emulator.
@@ -28,31 +29,66 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <png.h>
 
+#include "archdep.h"
 #include "screenshot.h"
 #include "types.h"
 #include "utils.h"
 
 int png_save(screenshot_t *screenshot, const char *filename)
 {
-    unsigned int line, x;
+    unsigned int line;
     BYTE *data;
+    FILE *fd;
+    png_structp png_ptr = NULL;
+    png_infop info_ptr = NULL;
+
+    png_ptr = png_create_write_struct (PNG_LIBPNG_VER_STRING,
+                                       (void *)NULL, NULL, NULL);
+
+    if (!png_ptr)
+        return -1;
+
+    info_ptr = png_create_info_struct (png_ptr);
+    if (!info_ptr) {
+        png_destroy_write_struct (&png_ptr, (png_infopp)NULL);
+        return -1;
+    }
+
+    if (setjmp (png_ptr->jmpbuf)) {
+        png_destroy_write_struct (&png_ptr, &info_ptr);
+        return -1;
+    }
+
+    fd = fopen(filename, MODE_WRITE);
+
+    if (fd == NULL)
+        return -1;
 
     data = (BYTE *)xmalloc(screenshot->width * 4);
 
+    png_init_io (png_ptr, fd);
+    png_set_compression_level (png_ptr, Z_BEST_COMPRESSION);
+
+    info_ptr->width = screenshot->width;
+    info_ptr->height= screenshot->height;
+    info_ptr->bit_depth = 8;
+    info_ptr->color_type = PNG_COLOR_TYPE_RGB_ALPHA;
+    png_write_info (png_ptr, info_ptr);
+    png_set_invert_alpha(png_ptr);
+
     for (line = 0; line < screenshot->height; line++) {
-
-        printf("LINE %03i\n", line);
-
         screenshot_line_data(screenshot, data, line, SCREENSHOT_MODE_RGB32);
-
-        for (x = 0; x < screenshot->width * 4; x++) {
-            printf("%02x ", data[x]);
-        }
-        printf("\n");
+	png_write_row (png_ptr, (png_bytep)data);
     }
 
+    png_write_end (png_ptr, info_ptr);
+
+    png_destroy_write_struct (&png_ptr, &info_ptr);
     free(data);
+    fclose(fd);
+
     return 0;
 }
 
