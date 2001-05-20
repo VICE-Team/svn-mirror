d7cb721f21b03de0389470ff138e0bfb5e2f7d41
trikalio <Spiro.Trikaliotis@gmx.de>
2001-05-20 14:34:58 +0000

    This commit was generated by cvs2svn to compensate for changes in r934, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@935 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/monitor/mon_command.h
@@ -0,0 +1,37 @@
+/*
+ * mon_command.h - The VICE built-in monitor command table.
+ *
+ * Written by
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _MON_COMMAND_H
+#define _MON_COMMAND_H
+
+extern int mon_command_lookup_index(const char *str, int *push_back);
+extern int mon_command_get_token(int mon_index);
+extern int mon_command_get_next_state(int mon_index);
+
+extern void mon_command_print_help(const char *cmd);
+
+#endif
+
