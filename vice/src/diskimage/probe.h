f32da752a8e753e7a630ea6b68d3d8e04bf6365d
trikalio <Spiro.Trikaliotis@gmx.de>
2002-04-21 15:59:18 +0000

    This commit was generated by cvs2svn to compensate for changes in r1120, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@1121 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/diskimage/probe.h
@@ -0,0 +1,34 @@
+/*
+ * probe.h - Probe disk images.
+ *
+ * Written by
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _PROBE_H
+#define _PROBE_H
+
+extern int disk_image_probe(disk_image_t *image);
+extern void disk_image_probe_init(void);
+
+#endif
+
