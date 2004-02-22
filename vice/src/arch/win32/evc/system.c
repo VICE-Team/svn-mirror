7a7f53b40db8036ddc48869f5b0d20c55d9e2b05
trikalio <Spiro.Trikaliotis@gmx.de>
2004-02-22 13:34:16 +0000

    This commit was generated by cvs2svn to compensate for changes in r1378, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@1379 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/win32/evc/system.c
@@ -0,0 +1,57 @@
+/*
+ * system.c - System dependant functions.
+ *
+ * Written by
+ *  Markus Brenner <markus@brenner.de>
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <windows.h>
+#include <prsht.h>
+#include <tchar.h>
+
+#include <stdlib.h>
+
+#include "system.h"
+
+
+void system_init_dialog(HWND hwnd)
+{
+}
+
+void system_psh_settings(PROPSHEETHEADER* ppsh)
+{
+}
+
+size_t system_wcstombs(char *mbs, const wchar_t *wcs, size_t len)
+{
+    return wcstombs(mbs, wcs, len);
+}
+
+size_t system_mbstowcs(wchar_t *wcs, const wchar_t *mbs, size_t len)
+{
+    _tcsnccpy(wcs, mbs, len);
+    return _tcsclen(wcs);
+}
+
