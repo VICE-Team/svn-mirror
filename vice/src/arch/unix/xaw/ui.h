86efc7c550d39a01ffd1f422a90224f18395553a
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 14:58:26 +0000

    This commit was generated by cvs2svn to compensate for changes in r2009, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@2010 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/unix/xaw/ui.h
+++ vice/src/arch/unix/xaw/ui.h
@@ -35,44 +35,74 @@
 #include "xdebug.h"
 #include "palette.h"
 
-typedef Widget UiWindow;
-typedef XtCallbackProc UiMenuCallback;
-typedef void (*UiExposureHandler) (unsigned int width, unsigned int height);
-typedef enum { Button_None, Button_Close, Button_Ok, Button_Cancel,
-	       Button_Yes, Button_No, Button_Reset, Button_HardReset,
-               Button_Mon,Button_Debug, Button_Contents, Button_Autostart,
-             } UiButton;
-typedef XtPointer UiCallbackDataPtr;
+typedef Widget ui_window_t;
 
-typedef enum { UI_JAM_RESET, UI_JAM_HARD_RESET,
-               UI_JAM_MONITOR, UI_JAM_DEBUG } UiJamAction;
+typedef void (*ui_exposure_handler_t) (unsigned int width,
+                                       unsigned int height);
+
+typedef enum {
+    UI_BUTTON_NONE, UI_BUTTON_CLOSE, UI_BUTTON_OK, UI_BUTTON_CANCEL,
+    UI_BUTTON_YES, UI_BUTTON_NO, UI_BUTTON_RESET, UI_BUTTON_HARDRESET,
+    UI_BUTTON_MON, UI_BUTTON_DEBUG, UI_BUTTON_CONTENTS, UI_BUTTON_AUTOSTART
+} ui_button_t;
+
+typedef enum {
+    UI_JAM_RESET, UI_JAM_HARD_RESET, UI_JAM_MONITOR, UI_JAM_DEBUG
+} ui_jam_action_t;
+
+typedef XtCallbackProc ui_callback_t;
+typedef XtPointer ui_callback_data_t;
+
+#define UI_CALLBACK(name)                               \
+    void name(Widget w, ui_callback_data_t client_data, \
+              ui_callback_data_t call_data)
+
+/* These resources are used only by the UI modules.  */
+typedef struct {
+    char *html_browser_command;
+    int use_private_colormap;
+    int save_resources_on_exit;
+    int depth;
+} _ui_resources_t;
+extern _ui_resources_t _ui_resources;
+
+extern Widget _ui_top_level;
+
+/* ------------------------------------------------------------------------- */
 
 extern int ui_init_resources(void);
 extern int ui_init_cmdline_options(void);
 
-extern int UiInit(int *argc, char **argv);
-extern int UiInitFinish(void);
-extern UiWindow UiOpenCanvasWindow(const char *title, int width, int height, int no_autorepeat, UiExposureHandler exposure_proc, const palette_t *p, PIXEL pixel_return[]);
-extern void UiResizeCanvasWindow(UiWindow w, int height, int width);
-extern void UiMapCanvasWindow(UiWindow w);
-extern void UiUnmapCanvasWindow(UiWindow w);
-extern Window UiCanvasDrawable(UiWindow w);
-extern void UiDisplaySpeed(float percent, float framerate, int warp_flag);
-extern void UiToggleDriveStatus(int state);
-extern void UiDisplayDriveTrack(double track_number);
-extern void UiDisplayDriveLed(int status);
-extern void UiDisplayPaused(int flag);
-extern void UiDispatchEvents(void);
-extern void UiError(const char *format,...);
-extern UiJamAction UiJamDialog(const char *format,...);
-extern void UiMessage(const char *format,...);
-extern void UiShowText(const char *title, const char *text, int width, int height);
-extern char *UiFileSelect(const char *title, char *(*read_contents_func)(const char *), int allow_autostart, UiButton *button_return);
-extern UiButton UiInputString(const char *title, const char *prompt, char *buf, unsigned int buflen);
-extern UiButton UiAskConfirmation(const char *title, const char *text);
-extern void UiAutoRepeatOn(void);
-extern void UiAutoRepeatOff(void);
-extern void UiUpdateMenus(void);
-extern int UiExtendImageDialog(void);
+extern int ui_init(int *argc, char **argv);
+extern int ui_init_finish(void);
+extern void ui_set_left_menu(Widget w);
+extern void ui_set_right_menu(Widget w);
+extern ui_window_t ui_open_canvas_window(const char *title, int width, int height, int no_autorepeat, ui_exposure_handler_t exposure_proc, const palette_t *p, PIXEL pixel_return[]);
+extern void ui_resize_canvas_window(ui_window_t w, int height, int width);
+extern void ui_map_canvas_window(ui_window_t w);
+extern void ui_unmap_canvas_window(ui_window_t w);
+extern Window ui_canvas_get_drawable(ui_window_t w);
+extern void ui_display_speed(float percent, float framerate, int warp_flag);
+extern void ui_toggle_drive_status(int state);
+extern void ui_display_drive_track(double track_number);
+extern void ui_display_drive_led(int status);
+extern void ui_display_paused(int flag);
+extern void ui_dispatch_next_event(void);
+extern void ui_dispatch_events(void);
+extern void ui_error(const char *format,...);
+extern ui_jam_action_t ui_jam_dialog(const char *format,...);
+extern void ui_message(const char *format,...);
+extern void ui_show_text(const char *title, const char *text, int width, int height);
+extern char *ui_select_file(const char *title, char *(*read_contents_func)(const char *), int allow_autostart, ui_button_t *button_return);
+extern ui_button_t ui_input_string(const char *title, const char *prompt, char *buf, unsigned int buflen);
+extern ui_button_t ui_ask_confirmation(const char *title, const char *text);
+extern void ui_autorepeat_on(void);
+extern void ui_autorepeat_off(void);
+extern void ui_update_menus(void);
+extern int ui_extend_image_dialog(void);
+extern Widget ui_create_transient_shell(Widget parent, const char *name);
+extern void ui_popdown(Widget w);
+extern void ui_popup(Widget w, const char *title, Boolean wait_popdown);
+
 
 #endif /* !defined (_UI_XAW_H) */
