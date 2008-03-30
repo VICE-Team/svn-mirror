e292f435c62faf33c85ff9da6a768f45a57ef654
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:02:52 +0000

    This commit was generated by cvs2svn to compensate for changes in r2535, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@2536 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/msdos/mouse.c
@@ -0,0 +1,117 @@
+/*
+ * mouse.h - Mouse handling for MS-DOS.
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+/* This is a first rough implementation of mouse emulation for MS-DOS.
+   A smarter and less buggy emulation is of course possible. */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "mouse.h"
+
+#include "kbd.h"  /* for joy[]... (this is ugly and should be fixed) */
+#include "resources.h"
+#include "cmdline.h"
+
+int _mouse_enabled;
+int _mouse_available;
+int _mouse_x, _mouse_y;
+int _mouse_coords_dirty;
+
+static void my_mouse_callback(int flags);
+
+/* ------------------------------------------------------------------------- */
+
+static int set_mouse_enabled(resource_value_t v)
+{
+    _mouse_enabled = (int) v;
+    if (_mouse_enabled && _mouse_available)
+        mouse_callback = my_mouse_callback;
+    else
+        mouse_callback = NULL;
+    return 0;
+}
+
+static resource_t resources[] = {
+    { "Mouse", RES_INTEGER, (resource_value_t) 0,
+      (resource_value_t *) &_mouse_enabled, set_mouse_enabled },
+    { NULL }
+};
+
+int mouse_init_resources(void)
+{
+    return resources_register(resources);
+}
+
+/* ------------------------------------------------------------------------- */
+
+static cmdline_option_t cmdline_options[] = {
+    { "-mouse", SET_RESOURCE, 1, NULL, NULL,
+      "Mouse", NULL, NULL, "Enable emulation of the 1351 proportional mouse" },
+    { "+mouse", SET_RESOURCE, 0, NULL, NULL,
+      "Mouse", NULL, NULL, "Disable emulation of the 1351 proportional mouse" },
+    { NULL }
+};
+
+int mouse_init_cmdline_options(void)
+{
+    return cmdline_register_options(cmdline_options);
+}
+
+/* ------------------------------------------------------------------------- */
+
+static void my_mouse_callback(int flags)
+{
+    if (flags & MOUSE_FLAG_MOVE)
+        _mouse_coords_dirty = 1;
+    if (flags & MOUSE_FLAG_LEFT_UP)
+        joy[1] &= ~16;
+    if (flags & MOUSE_FLAG_LEFT_DOWN)
+        joy[1] |= 16;
+    if (flags & MOUSE_FLAG_RIGHT_UP)
+        joy[1] &= ~1;
+    if (flags & MOUSE_FLAG_RIGHT_DOWN)
+        joy[1] |= 1;
+}
+
+int mouse_init(void)
+{
+    printf("Attempting to initialize the mouse driver... ");
+    if (install_mouse() == -1) {
+        printf("Failed!\r\n");
+	_mouse_available = 0;
+        return -1;
+    }
+    printf("OK.  Mouse is available.\r\n");
+    _mouse_available = 1;
+
+    set_mouse_range(0, 0, ~0, ~0); /* This should not really be important... */
+    set_mouse_speed(1, 1);
+
+    mouse_callback = my_mouse_callback;
+
+    return 0;
+}
---
+++ vice/src/arch/msdos/mouse.h
@@ -0,0 +1,86 @@
+/*
+ * mouse.h - Mouse handling for MS-DOS.
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef _MOUSE_H
+#define _MOUSE_H
+
+#include <allegro.h>
+
+#include "types.h"
+
+extern int mouse_init_resources(void);
+extern int mouse_init_cmdline_options(void);
+extern int mouse_init(void);
+
+extern int _mouse_available;
+extern int _mouse_enabled;
+extern int _mouse_x, _mouse_y;
+extern int _mouse_coords_dirty;
+
+/* ------------------------------------------------------------------------- */
+
+inline static void _update_mouse(void)
+{
+    if (_mouse_coords_dirty) {
+        int x, y;
+
+        get_mouse_mickeys(&x, &y);
+        _mouse_x = (_mouse_x + (x >> 2)) & 0xff;
+        _mouse_y = (_mouse_y + (y >> 2)) & 0xff;
+        _mouse_coords_dirty = 0;
+    }
+}
+
+inline static BYTE mouse_get_x(void)
+{
+    if (!_mouse_available || !_mouse_enabled)
+        return 0xff;
+    _update_mouse();
+    return (BYTE) _mouse_x;
+}
+
+inline static BYTE mouse_get_y(void)
+{
+    if (!_mouse_available || !_mouse_enabled)
+        return 0xff;
+    _update_mouse();
+    return (BYTE) _mouse_y;
+}
+
+#if 0
+
+inline static int mouse_get_left_button(void)
+{
+    return mouse_b & 1;
+}
+
+inline static int mouse_get_right_button(void)
+{
+    return mouse_b & 2;
+}
+
+#endif
+
+#endif
