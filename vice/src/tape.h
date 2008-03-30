636b2f71559b5a3c9b016710e13bf25af494377d
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:48:00 +0000

    This commit was generated by cvs2svn to compensate for changes in r7951, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@7952 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/tape.h
+++ vice/src/tape.h
@@ -38,7 +38,7 @@ extern int tape_init(int buffer_pointer_addr, int st_addr,
 extern int tape_deinstall(void);
 extern void tape_find_header_trap(void);
 extern void tape_receive_trap(void);
-extern char *tape_get_file_name(void);
+extern const char *tape_get_file_name(void);
 
 #endif
 
