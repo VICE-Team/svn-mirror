a8c9063b700f3307e24d20f742f024bcd912281c
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 17:02:44 +0000

    This commit was generated by cvs2svn to compensate for changes in r16919, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@16920 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/version.h
+++ vice/src/version.h
@@ -28,11 +28,11 @@
 #define __VERSION_H__
 
 #ifndef VERSION
-#define VERSION "1.20.23"
+#define VERSION "1.20.24"
 #endif
 
 #ifndef VERSION_RC_NUMBER
-#define VERSION_RC_NUMBER 1,20,23,0
+#define VERSION_RC_NUMBER 1,20,24,0
 #endif
 
 #ifndef PACKAGE
--- vice/vice.spec
+++ vice/vice.spec
@@ -1,4 +1,4 @@
-%define version 1.20
+%define version 1.21
 %define rel     1
 %define prefix /usr/
 Summary: VICE, the Versatile Commodore Emulator
