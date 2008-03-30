6dc9ea39da2042536ca0f9ebc710865496e7477e
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:16:06 +0000

    This commit was generated by cvs2svn to compensate for changes in r4123, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@4124 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c128/c128cpu.c
+++ vice/src/c128/c128cpu.c
@@ -48,31 +48,17 @@
 
 /* ------------------------------------------------------------------------- */
 
-#define	NO_INSTRUCTION_FETCH_HACK
-
 /* C128 needs external reg_pc */
 #  define	NEED_REG_PC
-
-/* Do not include include this function when `_mem_read_base_tab_ptr' is 
-   not used.  Otherwise the native IRIX 6.2 compiler barfs.  */
-#define DO_NOT_INCLUDE_MEM_READ_BASE 1
 
 /* ------------------------------------------------------------------------- */
 
 extern read_func_ptr_t _mem_read_tab[];
 extern store_func_ptr_t _mem_write_tab[];
 
-#  define	JUMP(addr)	(reg_pc = (addr))
-
 #  define	PAGE_ZERO	page_zero
 
 #  define	PAGE_ONE	page_one
-
-#define STORE(addr, value) \
-    (_mem_write_tab[(addr) >> 8])((addr), (value))
-
-#define LOAD(addr) \
-    (_mem_read_tab[(addr) >> 8])((addr))
 
 #define STORE_ZERO(addr, value) \
     page_zero[(addr) & 0xff] = (value);
