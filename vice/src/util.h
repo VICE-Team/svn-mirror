311f76bc4ba3f868b2bda2656bc8e2ab9bada7fd
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 17:00:32 +0000

    This commit was generated by cvs2svn to compensate for changes in r16655, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@16656 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/util.h
+++ vice/src/util.h
@@ -69,9 +69,16 @@ extern int util_check_null_string(const char *string);
 
 extern int util_dword_read(FILE *fd, DWORD *buf, size_t num);
 extern int util_dword_write(FILE *fd, DWORD *buf, size_t num);
+
 extern void util_dword_to_be_buf(BYTE *buf, DWORD data);
 extern void util_dword_to_le_buf(BYTE *buf, DWORD data);
 extern DWORD util_le_buf_to_dword(BYTE *buf);
+extern DWORD util_be_buf_to_dword(BYTE *buf);
+
+extern void util_int_to_be_buf4(BYTE *buf, int data);
+extern void util_int_to_le_buf4(BYTE *buf, int data);
+extern int util_le_buf4_to_int(BYTE *buf);
+extern int util_be_buf4_to_int(BYTE *buf);
 
 extern void util_word_to_be_buf(BYTE *buf, WORD data);
 extern void util_word_to_le_buf(BYTE *buf, WORD data);
