1444b1584fb31cb11804ea4212ee230da4343580
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:05:29 +0000

    This commit was generated by cvs2svn to compensate for changes in r10049, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@10050 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/tape.h
+++ vice/src/tape.h
@@ -28,6 +28,8 @@
 #ifndef _TAPE_H
 #define _TAPE_H
 
+#include "types.h"
+
 #define TAPE_TYPE_T64 0
 #define TAPE_TYPE_TAP 1
 
@@ -38,6 +40,17 @@ typedef struct tape_image_s {
     void *data;
 } tape_image_t;
 
+struct tape_file_record_s {
+    BYTE name[17];
+    BYTE type;
+    ADDRESS start_addr;
+    ADDRESS end_addr;
+};
+typedef struct tape_file_record_s tape_file_record_t;
+
+
+extern tape_image_t *tape_image_dev1;
+
 struct trap_s;
 
 extern int tape_init(int buffer_pointer_addr, int st_addr,
@@ -45,6 +58,7 @@ extern int tape_init(int buffer_pointer_addr, int st_addr,
                      int stal_addr, int eal_addr, int kbd_buf_addr,
                      int kbd_buf_pending_addr, const struct trap_s *trap_list);
 extern int tape_deinstall(void);
+extern void tape_get_header(tape_image_t *tape_image, BYTE *name);
 extern void tape_find_header_trap(void);
 extern void tape_receive_trap(void);
 extern void tape_find_header_trap_plus4(void);
@@ -55,8 +69,19 @@ extern int tape_tap_attched(void);
 extern void tape_traps_install(void);
 extern void tape_traps_deinstall(void);
 
+extern tape_file_record_t *tape_get_current_file_record(
+                          tape_image_t *tape_image);
+extern int tape_seek_to_next_file(tape_image_t *tape_image,
+                                  unsigned int allow_rewind);
+
+extern int tape_internal_close_tape_image(tape_image_t *tape_image);
+extern tape_image_t *tape_internal_open_tape_image(const char *name,
+                                                   unsigned int read_only);
+/* External tape image interface.  */
 extern int tape_image_detach(unsigned int unit);
 extern int tape_image_attach(unsigned int unit, const char *name);
+extern int tape_image_open(tape_image_t *tape_image);
+extern int tape_image_close(tape_image_t *tape_image);
 
 #endif
 
