457b78dbc3b57550f93ca1991e4c4eedb8f10e2d
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:04:25 +0000

    This commit was generated by cvs2svn to compensate for changes in r2721, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@2722 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/unix/xaw/c128ui.c
+++ vice/src/arch/unix/xaw/c128ui.c
@@ -81,7 +81,8 @@ static ui_menu_entry_t sid_model_submenu[] = {
     { "*8580 (new)",
       (ui_callback_t) radio_SidModel, (ui_callback_data_t) 1, NULL },
 #ifdef HAVE_RESID
-    { "*6581 (old) reSID emulation",
+    { "--" },
+    { "*6581 (old) with reSID",
       (ui_callback_t) radio_SidModel, (ui_callback_data_t) 2, NULL },
 #endif
     { NULL }
