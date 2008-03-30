5fbb5d514adc3db1afd5586e2968c2ef69ba15ed
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:20:07 +0000

    This commit was generated by cvs2svn to compensate for changes in r11805, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@11806 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/unix/uidriveplus4.c
@@ -0,0 +1,105 @@
+/*
+ * uidriveplus4.c
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "drive.h"
+#include "resources.h"
+#include "uidrive.h"
+#include "uidriveiec.h"
+#include "uidriveplus4.h"
+#include "uimenu.h"
+
+
+UI_MENU_DEFINE_TOGGLE(DriveTrueEmulation)
+
+
+static ui_menu_entry_t set_drive0_type_submenu[] = {
+    { N_("*None"), (ui_callback_t)radio_Drive8Type,
+      (ui_callback_data_t)DRIVE_TYPE_NONE, NULL },
+    { "*1541", (ui_callback_t)radio_Drive8Type,
+      (ui_callback_data_t)DRIVE_TYPE_1541, NULL },
+    { "*1541-II", (ui_callback_t)radio_Drive8Type,
+      (ui_callback_data_t)DRIVE_TYPE_1541II, NULL },
+    { "*1551", (ui_callback_t)radio_Drive8Type,
+      (ui_callback_data_t)DRIVE_TYPE_1551, NULL },
+    { "*1571", (ui_callback_t)radio_Drive8Type,
+      (ui_callback_data_t)DRIVE_TYPE_1571, NULL },
+    { "*1581", (ui_callback_t)radio_Drive8Type,
+      (ui_callback_data_t)DRIVE_TYPE_1581, NULL },
+    { NULL }
+};
+
+static ui_menu_entry_t set_drive1_type_submenu[] = {
+    { N_("*None"), (ui_callback_t)radio_Drive9Type,
+      (ui_callback_data_t)DRIVE_TYPE_NONE, NULL },
+    { "*1541", (ui_callback_t)radio_Drive9Type,
+      (ui_callback_data_t)DRIVE_TYPE_1541, NULL },
+    { "*1541-II", (ui_callback_t)radio_Drive9Type,
+      (ui_callback_data_t)DRIVE_TYPE_1541II, NULL },
+    { "*1571", (ui_callback_t)radio_Drive9Type,
+      (ui_callback_data_t)DRIVE_TYPE_1571, NULL },
+    { "*1551", (ui_callback_t)radio_Drive9Type,
+      (ui_callback_data_t)DRIVE_TYPE_1551, NULL },
+    { "*1581", (ui_callback_t)radio_Drive9Type,
+      (ui_callback_data_t)DRIVE_TYPE_1581, NULL },
+    { NULL }
+};
+
+/* ------------------------------------------------------------------------- */
+
+static ui_menu_entry_t driveplus4_settings_submenu[] = {
+    { N_("*Enable true drive emulation"),
+      (ui_callback_t)toggle_DriveTrueEmulation, NULL, NULL },
+    { "--" },
+    { N_("Drive #8 model"),
+      NULL, NULL, set_drive0_type_submenu },
+    { N_("Drive #8 RAM expansion"),
+      NULL, NULL, set_drive0_expansion_submenu },
+    { N_("Drive #8 40-track image support"),
+      NULL, NULL, set_drive0_extend_image_policy_submenu },
+    { N_("Drive #8 idle method"),
+      NULL, NULL, set_drive0_idle_method_submenu },
+    { "--" },
+    { N_("Drive #9 model"),
+      NULL, NULL, set_drive1_type_submenu },
+    { N_("Drive #9 RAM expansion"),
+      NULL, NULL, set_drive1_expansion_submenu },
+    { N_("Drive #9 40-track image support"),
+      NULL, NULL, set_drive1_extend_image_policy_submenu },
+    { N_("Drive #9 idle method"),
+      NULL, NULL, set_drive1_idle_method_submenu },
+    { NULL }
+};
+
+ui_menu_entry_t ui_driveplus4_settings_menu[] = {
+    { N_("Drive settings"),
+      NULL, NULL, driveplus4_settings_submenu },
+    { NULL }
+};
+
