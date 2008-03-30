6822bfd430c70d9b94cd6208400ccad1ea347d2e
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:55:29 +0000

    This commit was generated by cvs2svn to compensate for changes in r16049, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@16050 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/version.h
+++ vice/src/version.h
@@ -28,11 +28,11 @@
 #define __VERSION_H__
 
 #ifndef VERSION
-#define VERSION "1.17.1"
+#define VERSION "1.17.2"
 #endif
 
 #ifndef VERSION_RC_NUMBER
-#define VERSION_RC_NUMBER 1,17,1,0
+#define VERSION_RC_NUMBER 1,17,2,0
 #endif
 
 #ifndef PACKAGE
