713932168f4fcb2b4e262c328c52bb5637d4a59f
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:56:47 +0000

    This commit was generated by cvs2svn to compensate for changes in r9005, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@9006 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/msdos/videoarch.h
+++ vice/src/arch/msdos/videoarch.h
@@ -54,7 +54,7 @@ typedef struct video_frame_buffer_s /* a bitmap structure */
 
 typedef void (*canvas_redraw_t)();
 
-struct canvas_s {
+struct video_canvas_s {
     unsigned int width, height;
     RGB colors[NUM_AVAILABLE_COLORS];
     canvas_redraw_t exposure_handler;
@@ -70,7 +70,7 @@ struct canvas_s {
     /* Currently invisible page.  */
     int back_page;
 };
-typedef struct canvas_s canvas_t;
+typedef struct video_canvas_s video_canvas_t;
 
 #define CANVAS_USES_TRIPLE_BUFFERING(c) (c->use_triple_buffering)
 
@@ -121,10 +121,11 @@ extern int video_in_gfx_mode(void);
 
 /* ------------------------------------------------------------------------- */
 
-inline static void canvas_refresh(canvas_t *c, video_frame_buffer_t *f,
-				  unsigned int xs, unsigned int ys,
-                                  unsigned int xi, unsigned int yi,
-                                  unsigned int w, unsigned int h)
+inline static void video_canvas_refresh(video_canvas_t *c,
+                                        video_frame_buffer_t *f,
+                                        unsigned int xs, unsigned int ys,
+                                        unsigned int xi, unsigned int yi,
+                                        unsigned int w, unsigned int h)
 {
     int y_diff;
 
@@ -166,7 +167,7 @@ inline static void canvas_refresh(canvas_t *c, video_frame_buffer_t *f,
     }
 }
 
-inline static void canvas_set_border_color(canvas_t *canvas, BYTE color)
+inline static void canvas_set_border_color(video_canvas_t *canvas, BYTE color)
 {
     inportb(0x3da);
     outportb(0x3c0, 0x31);
