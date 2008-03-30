ef8bb9546bf674d5920e2ae2d4319ce778eb1b4c
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:50:48 +0000

    This commit was generated by cvs2svn to compensate for changes in r15487, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@15488 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/plus4ui.c
+++ vice/src/arch/win32/plus4ui.c
@@ -44,6 +44,12 @@
 #include "winmain.h"
 
 
+static const unsigned int romset_dialog_resources[UIROM_TYPE_MAX] = {
+    IDD_PLUS4ROM_RESOURCE_DIALOG,
+    IDD_PLUS4ROMDRIVE_RESOURCE_DIALOG,
+    0 };
+
+
 static const ui_menu_toggle_t plus4_ui_menu_toggles[] = {
     { "TEDDoubleSize", IDM_TOGGLE_DOUBLESIZE },
     { "TEDDoubleScan", IDM_TOGGLE_DOUBLESCAN },
@@ -55,26 +61,36 @@ static const ui_menu_toggle_t plus4_ui_menu_toggles[] = {
 
 static const uirom_settings_t uirom_settings[] = {
     { UIROM_TYPE_MAIN, TEXT("Kernal"), "KernalName",
-      IDC_PLUS4ROM_KERNAL_FILE, IDC_PLUS4ROM_KERNAL_BROWSE },
+      IDC_PLUS4ROM_KERNAL_FILE, IDC_PLUS4ROM_KERNAL_BROWSE,
+      IDC_PLUS4ROM_KERNAL_RESOURCE },
     { UIROM_TYPE_MAIN, TEXT("Basic"), "BasicName",
-      IDC_PLUS4ROM_BASIC_FILE, IDC_PLUS4ROM_BASIC_BROWSE },
+      IDC_PLUS4ROM_BASIC_FILE, IDC_PLUS4ROM_BASIC_BROWSE,
+      IDC_PLUS4ROM_BASIC_RESOURCE },
     { UIROM_TYPE_MAIN, TEXT("3 plus 1 LO"), "3plus1loName",
-      IDC_PLUS4ROM_3P1LO_FILE, IDC_PLUS4ROM_3P1LO_BROWSE },
+      IDC_PLUS4ROM_3P1LO_FILE, IDC_PLUS4ROM_3P1LO_BROWSE,
+      IDC_PLUS4ROM_3P1LO_RESOURCE },
     { UIROM_TYPE_MAIN, TEXT("3 plus 1 HI"), "3plus1hiName",
-      IDC_PLUS4ROM_3P1HI_FILE, IDC_PLUS4ROM_3P1HI_BROWSE },
+      IDC_PLUS4ROM_3P1HI_FILE, IDC_PLUS4ROM_3P1HI_BROWSE,
+      IDC_PLUS4ROM_3P1HI_RESOURCE },
     { UIROM_TYPE_DRIVE, TEXT("1541"), "DosName1541",
-      IDC_DRIVEROM_1541_FILE, IDC_DRIVEROM_1541_BROWSE },
+      IDC_DRIVEROM_1541_FILE, IDC_DRIVEROM_1541_BROWSE,
+      IDC_DRIVEROM_1541_RESOURCE },
     { UIROM_TYPE_DRIVE, TEXT("1541-II"), "DosName1541ii",
-      IDC_DRIVEROM_1541II_FILE, IDC_DRIVEROM_1541II_BROWSE },
+      IDC_DRIVEROM_1541II_FILE, IDC_DRIVEROM_1541II_BROWSE,
+      IDC_DRIVEROM_1541II_RESOURCE },
     { UIROM_TYPE_DRIVE, TEXT("1551"), "DosName1551",
-      IDC_DRIVEROM_1551_FILE, IDC_DRIVEROM_1551_BROWSE },
+      IDC_DRIVEROM_1551_FILE, IDC_DRIVEROM_1551_BROWSE,
+      IDC_DRIVEROM_1551_RESOURCE },
     { UIROM_TYPE_DRIVE, TEXT("1570"), "DosName1570",
-      IDC_DRIVEROM_1570_FILE, IDC_DRIVEROM_1570_BROWSE },
+      IDC_DRIVEROM_1570_FILE, IDC_DRIVEROM_1570_BROWSE,
+      IDC_DRIVEROM_1570_RESOURCE },
     { UIROM_TYPE_DRIVE, TEXT("1571"), "DosName1571",
-      IDC_DRIVEROM_1571_FILE, IDC_DRIVEROM_1571_BROWSE },
+      IDC_DRIVEROM_1571_FILE, IDC_DRIVEROM_1571_BROWSE,
+      IDC_DRIVEROM_1571_RESOURCE },
     { UIROM_TYPE_DRIVE, TEXT("1581"), "DosName1581",
-      IDC_DRIVEROM_1581_FILE, IDC_DRIVEROM_1581_BROWSE },
-    { 0, NULL, NULL, 0, 0 }
+      IDC_DRIVEROM_1581_FILE, IDC_DRIVEROM_1581_BROWSE,
+      IDC_DRIVEROM_1581_RESOURCE },
+    { 0, NULL, NULL, 0, 0, 0 }
 };
 
 static const ui_res_value_list_t plus4_ui_res_values[] = {
@@ -92,7 +108,7 @@ static void plus4_ui_specific(WPARAM wparam, HWND hwnd)
       case IDM_ROM_SETTINGS:
         uirom_settings_dialog(hwnd, IDD_PLUS4ROM_SETTINGS_DIALOG,
                               IDD_PLUS4DRIVEROM_SETTINGS_DIALOG,
-                              uirom_settings);
+                              romset_dialog_resources, uirom_settings);
         break;
       case IDM_VIDEO_SETTINGS:
         ui_video_settings_dialog(hwnd, UI_VIDEO_CHIP_TED, UI_VIDEO_CHIP_NONE);
