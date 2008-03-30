35999f59f0f7ecfed44023c6d303b85ce99ed1c6
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:52:25 +0000

    This commit was generated by cvs2svn to compensate for changes in r8481, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@8482 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/printerdrv/driver-select.h
+++ vice/src/printerdrv/driver-select.h
@@ -29,6 +29,22 @@
 
 #include "types.h"
 
+struct driver_select_s {
+    const char *drv_name;
+    int (*drv_open)(int device);
+    void (*drv_close)(int fi);
+    int (*drv_putc)(int fi, BYTE b);
+    int (*drv_getc)(int fi, BYTE *b);
+    int (*drv_flush)(int fi);
+};
+typedef struct driver_select_s driver_select_t;
+
+extern void driver_select_init(void);
+extern int driver_select_init_resources(void);
+extern int driver_select_init_cmdline_options(void);
+
+extern void driver_select_register(driver_select_t *driver_select);
+
 extern int driver_select_open(int device);
 extern void driver_select_close(int fi);
 extern int driver_select_putc(int fi, BYTE b);
