8fa06bbf2a4f92b5e80537f0ef2bc70936db6cee
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:01:53 +0000

    This commit was generated by cvs2svn to compensate for changes in r2417, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@2418 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/unix/joystick.c
+++ vice/src/arch/unix/joystick.c
@@ -48,36 +48,40 @@ int joystick_port_map[2];
 
 /* Resources */
 
-static int joyport1select(resource_value_t v) {
+static int joyport1select(resource_value_t v)
+{
 
-  kbd_flag_joykeys(joystick_port_map[0], 0);	/* disable old mapping */
-  joystick_port_map[0] = (int) v;
-  kbd_flag_joykeys(joystick_port_map[0], 1);	/* enable new mapping */
+    kbd_flag_joykeys(joystick_port_map[0], 0);	/* disable old mapping */
+    joystick_port_map[0] = (int) v;
+    kbd_flag_joykeys(joystick_port_map[0], 1);	/* enable new mapping */
 
-  return 0;
+    return 0;
 };
 
-static int joyport2select(resource_value_t v) {
+static int joyport2select(resource_value_t v)
+{
 
-  kbd_flag_joykeys(joystick_port_map[1], 0);	/* disable old mapping */
-  joystick_port_map[1] = (int) v;
-  kbd_flag_joykeys(joystick_port_map[1], 1);	/* enable new mapping */
+    kbd_flag_joykeys(joystick_port_map[1], 0);	/* disable old mapping */
+    joystick_port_map[1] = (int) v;
+    kbd_flag_joykeys(joystick_port_map[1], 1);	/* enable new mapping */
 
-  return 0;
+    return 0;
 };
 
 
-static resource_t resources[] = {
-    { "JoyDevice1", RES_INTEGER, (resource_value_t) 0,
-      (resource_value_t *) &joystick_port_map[0], joyport1select },
-    { "JoyDevice2", RES_INTEGER, (resource_value_t) 0,
-      (resource_value_t *) &joystick_port_map[1], joyport2select },
-    { NULL },
+static resource_t resources[] =
+{
+    {"JoyDevice1", RES_INTEGER, (resource_value_t) 0,
+     (resource_value_t *) & joystick_port_map[0], joyport1select},
+    {"JoyDevice2", RES_INTEGER, (resource_value_t) 0,
+     (resource_value_t *) & joystick_port_map[1], joyport2select},
+    {NULL},
 };
 
 /* Command-line options.  */
 
-static cmdline_option_t cmdline_options[] = {
+static cmdline_option_t cmdline_options[] =
+{
     { "-joydev1", SET_RESOURCE, 1, NULL, NULL, "JoyDevice1", NULL,
       "<0-5>", "Set device for joystick port 1" },
     { "-joydev2", SET_RESOURCE, 1, NULL, NULL, "JoyDevice2", NULL,
@@ -122,149 +126,163 @@ int joyymax[2];
 
 void joystick_init(void)
 {
-  int i;
+    int i;
 
-  /* close all device files */
-  for(i=0;i<2;i++) {
-    if(ajoyfd[i] != -1)
-      close(ajoyfd[i]);
-    if(djoyfd[i] != -1)
-      close(djoyfd[i]);
-  }
-	 
-  /* open analog device files */
-  for(i=0;i<2;i++) {
-    
-    char* dev;
+    /* close all device files */
+    for (i = 0; i < 2; i++) {
+	if (ajoyfd[i] != -1)
+	    close(ajoyfd[i]);
+	if (djoyfd[i] != -1)
+	    close(djoyfd[i]);
+    }
+
+    /* open analog device files */
+    for (i = 0; i < 2; i++) {
+
+	char *dev;
 #ifdef LINUX_JOYSTICK
-    dev=(i==0)?"/dev/js0":"/dev/js1";
+	dev = (i == 0) ? "/dev/js0" : "/dev/js1";
 #elif defined(BSD_JOYSTICK)
-    dev=(i==0)?"/dev/joy0":"/dev/joy1";
+	dev = (i == 0) ? "/dev/joy0" : "/dev/joy1";
 #endif
-    
-    ajoyfd[i]=open(dev,O_RDONLY);
-    if (ajoyfd[i] < 0) {
-      fprintf(stderr, "Warning: couldn't open the joystick device %s!\n",dev);
-    }
-    else {
-      int j;
-      
-      /* calibration loop */
-      for(j=0;j<JOYCALLOOPS;j++) {
-	struct JS_DATA_TYPE js;
-	int status=read(ajoyfd[i],&js,JS_RETURN);
-	
-	if (status != JS_RETURN) {
-	  fprintf(stderr, "Warning: error reading the joystick device%s!\n",
-		  dev);
-	}
-	else {
-	  /* determine average */
-	  joyxcal[i]+=js.x;
-	  joyycal[i]+=js.y;
-	}
-      }
 
-      /* correct average */
-      joyxcal[i]/=JOYCALLOOPS;
-      joyycal[i]/=JOYCALLOOPS;
-      
-      /* determine treshoulds */
-      joyxmin[i]=joyxcal[i]-joyxcal[i]/JOYSENSITIVITY;
-      joyxmax[i]=joyxcal[i]+joyxcal[i]/JOYSENSITIVITY;
-      joyymin[i]=joyycal[i]-joyycal[i]/JOYSENSITIVITY;
-      joyymax[i]=joyycal[i]+joyycal[i]/JOYSENSITIVITY;
+	ajoyfd[i] = open(dev, O_RDONLY);
+	if (ajoyfd[i] < 0) {
+	    fprintf(stderr, "Warning: couldn't open the joystick device %s!\n", dev);
+	} else {
+	    int j;
 
-      printf("hardware joystick calibration %s:\n",dev);
-      printf("X: min: %i , mid: %i , max: %i\n",joyxmin[i],joyxcal[i],
-	     joyxmax[i]);
-      printf("Y: min: %i , mid: %i , max: %i\n",joyymin[i],joyycal[i],
-	     joyymax[i]);
+	    /* calibration loop */
+	    for (j = 0; j < JOYCALLOOPS; j++) {
+		struct JS_DATA_TYPE js;
+		int status = read(ajoyfd[i], &js, JS_RETURN);
+
+		if (status != JS_RETURN) {
+		    fprintf(stderr, "Warning: error reading the joystick device%s!\n",
+			    dev);
+		} else {
+		    /* determine average */
+		    joyxcal[i] += js.x;
+		    joyycal[i] += js.y;
+		}
+	    }
+
+	    /* correct average */
+	    joyxcal[i] /= JOYCALLOOPS;
+	    joyycal[i] /= JOYCALLOOPS;
+
+	    /* determine treshoulds */
+	    joyxmin[i] = joyxcal[i] - joyxcal[i] / JOYSENSITIVITY;
+	    joyxmax[i] = joyxcal[i] + joyxcal[i] / JOYSENSITIVITY;
+	    joyymin[i] = joyycal[i] - joyycal[i] / JOYSENSITIVITY;
+	    joyymax[i] = joyycal[i] + joyycal[i] / JOYSENSITIVITY;
+
+	    printf("hardware joystick calibration %s:\n", dev);
+	    printf("X: min: %i , mid: %i , max: %i\n", joyxmin[i], joyxcal[i],
+		   joyxmax[i]);
+	    printf("Y: min: %i , mid: %i , max: %i\n", joyymin[i], joyycal[i],
+		   joyymax[i]);
+	}
     }
-  }
-    
+
 #ifdef HAS_DIGITAL_JOYSTICK
-  /* open device files for digital joystick */
-  for(i=0;i<2;i++) {
-    char* dev;
-    dev=(i==0)?"/dev/djs0":"/dev/djs1";
+    /* open device files for digital joystick */
+    for (i = 0; i < 2; i++) {
+	char *dev;
+	dev = (i == 0) ? "/dev/djs0" : "/dev/djs1";
 
-    djoyfd[i]=open(dev,O_RDONLY);
-    if (djoyfd[i] < 0) {
-      fprintf(stderr, "Warning: couldn't open the joystick device %s!\n",dev);
+	djoyfd[i] = open(dev, O_RDONLY);
+	if (djoyfd[i] < 0) {
+	    fprintf(stderr, "Warning: couldn't open the joystick device %s!\n", dev);
+	}
     }
-  }
 #endif
 }
 
 void joystick_close(void)
 {
-  if(ajoyfd[0]>0) close (ajoyfd[0]);
-  if(ajoyfd[1]>0) close (ajoyfd[1]);
-  if(djoyfd[0]>0) close (djoyfd[0]);
-  if(djoyfd[1]>0) close (djoyfd[1]);
+    if (ajoyfd[0] > 0)
+	close(ajoyfd[0]);
+    if (ajoyfd[1] > 0)
+	close(ajoyfd[1]);
+    if (djoyfd[0] > 0)
+	close(djoyfd[0]);
+    if (djoyfd[1] > 0)
+	close(djoyfd[1]);
 }
 
 void joystick(void)
 {
-  int i;
-  for(i=1;i<=2;i++) {
-    int joyport=joystick_port_map[i-1];
+    int i;
+    for (i = 1; i <= 2; i++) {
+	int joyport = joystick_port_map[i - 1];
 
 #ifdef HAS_DIGITAL_JOYSTICK
-    if(joyport==JOYDEV_DIGITAL_0 || joyport==JOYDEV_DIGITAL_1) {
-      int status;
-      struct DJS_DATA_TYPE djs;
-      int djoyport=joyport-JOYDEV_DIGITAL_0;
-      
-      if(djoyfd[djoyport]>0) {
-	status=read(djoyfd[djoyport],&djs,DJS_RETURN);
-	if (status != DJS_RETURN) {
-	  fprintf(stderr,
-		  "Warning: error reading the digital joystick device!\n");
-	}
-	else {
-	  joy[i]=(joy[i]&0xe0)|((~(djs.switches>>3))&0x1f);
-	}
-      }
-    } else
-#endif
-    if (joyport==JOYDEV_ANALOG_0 || joyport==JOYDEV_ANALOG_1) {
-      int status;
-      struct JS_DATA_TYPE js;
-      int ajoyport=joyport-JOYDEV_ANALOG_0;
-      
-      if(ajoyfd[ajoyport]>0) {
-	status=read(ajoyfd[ajoyport],&js,JS_RETURN);
-	if (status != JS_RETURN) {
-	  fprintf(stderr, "Warning: error reading the joystick device!\n");
-	}
-	else {
-	  if (js.y<joyymin[ajoyport]) joy[i] |= 1;
-	  else joy[i] &= ~1;
-	  if (js.y>joyymax[ajoyport]) joy[i] |= 2;
-	  else joy[i] &= ~2;
-	  if (js.x<joyxmin[ajoyport]) joy[i] |= 4;
-	  else joy[i] &= ~4;
-	  if (js.x>joyxmax[ajoyport]) joy[i] |= 8;
-	  else joy[i] &= ~8;
-#ifdef LINUX_JOYSTICK
-	  if(js.buttons) joy[i] |= 16;
-#elif defined(BSD_JOYSTICK)
-	  if(js.b1 || js.b2) joy[i] |= 16;
-#endif
-	  else joy[i] &= ~16;
+	if (joyport == JOYDEV_DIGITAL_0 || joyport == JOYDEV_DIGITAL_1) {
+	    int status;
+	    struct DJS_DATA_TYPE djs;
+	    int djoyport = joyport - JOYDEV_DIGITAL_0;
 
+	    if (djoyfd[djoyport] > 0) {
+		status = read(djoyfd[djoyport], &djs, DJS_RETURN);
+		if (status != DJS_RETURN) {
+		    fprintf(stderr,
+			    "Warning: error reading the digital joystick device!\n");
+		} else {
+		    joy[i] = (joy[i] & 0xe0) | ((~(djs.switches >> 3)) & 0x1f);
+		}
+	    }
+	} else
+#endif
+	if (joyport == JOYDEV_ANALOG_0 || joyport == JOYDEV_ANALOG_1) {
+	    int status;
+	    struct JS_DATA_TYPE js;
+	    int ajoyport = joyport - JOYDEV_ANALOG_0;
+
+	    if (ajoyfd[ajoyport] > 0) {
+		status = read(ajoyfd[ajoyport], &js, JS_RETURN);
+		if (status != JS_RETURN) {
+		    fprintf(stderr, "Warning: error reading the joystick device!\n");
+		} else {
+		    if (js.y < joyymin[ajoyport])
+			joy[i] |= 1;
+		    else
+			joy[i] &= ~1;
+		    if (js.y > joyymax[ajoyport])
+			joy[i] |= 2;
+		    else
+			joy[i] &= ~2;
+		    if (js.x < joyxmin[ajoyport])
+			joy[i] |= 4;
+		    else
+			joy[i] &= ~4;
+		    if (js.x > joyxmax[ajoyport])
+			joy[i] |= 8;
+		    else
+			joy[i] &= ~8;
+#ifdef LINUX_JOYSTICK
+		    if (js.buttons)
+			joy[i] |= 16;
+#elif defined(BSD_JOYSTICK)
+		    if (js.b1 || js.b2)
+			joy[i] |= 16;
+#endif
+		    else
+			joy[i] &= ~16;
+
+		}
+	    }
 	}
-      }
     }
-  }      
-}                      
+}
 
 #else
 
-void joystick_init(void) {}
-void joystick_close(void) {}
+void joystick_init(void)
+{
+}
+void joystick_close(void)
+{
+}
 
-#endif /* HAS_JOYSTICK */
-
+#endif				/* HAS_JOYSTICK */
