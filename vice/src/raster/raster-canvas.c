84b7b189df8a84a472b4eacb5a792f469e1e5cd7
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:05:03 +0000

    This commit was generated by cvs2svn to compensate for changes in r9997, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@9998 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/raster/raster-canvas.c
@@ -0,0 +1,154 @@
+/*
+ * raster-canvas.c - Raster-based video chip emulation helper.
+ *
+ * Written by
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "machine.h"
+#include "raster-canvas.h"
+#include "raster.h"
+#include "videoarch.h"
+
+void raster_canvas_update_all(raster_t *raster)
+{
+    raster_viewport_t *viewport;
+
+    if (console_mode || vsid_mode || !raster->draw_buffer)
+        return;
+
+    viewport = &raster->viewport;
+
+    if (!viewport->update_canvas)
+        return;
+
+    video_canvas_refresh(viewport->canvas,
+                         raster->draw_buffer,
+                         raster->draw_buffer_width,
+#ifdef __OS2__
+                         raster->draw_buffer_height,
+#endif
+                         viewport->pixel_size.width * (viewport->first_x
+                                + raster->geometry.extra_offscreen_border_left),                         viewport->first_line * viewport->pixel_size.height,
+                         viewport->x_offset,
+                         viewport->y_offset,
+                         MIN(viewport->width,
+                             (raster->geometry.screen_size.width
+                             * viewport->pixel_size.width)),
+                         MIN(viewport->height,
+                             (raster->geometry.screen_size.height
+                              * viewport->pixel_size.height)));
+}
+
+inline static void update_canvas(raster_t *raster)
+{
+    raster_area_t *update_area;
+    raster_viewport_t *viewport;
+    int x, y, xx, yy;
+    int w, h;
+
+    if (console_mode || vsid_mode)
+        return;
+
+    update_area = &raster->update_area;
+    viewport = &raster->viewport;
+
+    if (update_area->is_null || !(viewport->update_canvas))
+        return;
+
+    x = update_area->xs;
+    y = update_area->ys;
+    xx = update_area->xs - viewport->first_x;
+    yy = update_area->ys - viewport->first_line;
+    w = update_area->xe - update_area->xs + 1;
+    h = update_area->ye - update_area->ys + 1;
+
+    if (video_render_get_fake_pal_state()) {
+        /* if pal emu is activated, more pixels have to be updated */
+        x -= 4;
+        xx -= 4;
+        w += 8;
+        h ++;
+    }
+
+    if (xx < 0) {
+        x -= xx;
+        w += xx;
+        xx = 0;
+    }
+
+    if (yy < 0) {
+        y -= yy;
+        h += yy;
+        yy = 0;
+    }
+    x += raster->geometry.extra_offscreen_border_left;
+
+    x *= viewport->pixel_size.width;
+    w *= viewport->pixel_size.width;
+
+    y *= viewport->pixel_size.height;
+    h *= viewport->pixel_size.height;
+
+    xx *= viewport->pixel_size.width;
+    xx += viewport->x_offset;
+
+    yy *= viewport->pixel_size.height;
+    yy += viewport->y_offset;
+
+    video_canvas_refresh(viewport->canvas,
+                         raster->draw_buffer,
+                         raster->draw_buffer_width,
+#ifdef __OS2__
+                         raster->draw_buffer_height,
+#endif
+                         x, y, xx, yy,
+                         MIN(w, viewport->width - xx),
+                         MIN(h, viewport->height - yy));
+
+    update_area->is_null = 1;
+}
+
+void raster_canvas_handle_end_of_frame(raster_t *raster)
+{
+    if (!console_mode && !vsid_mode) {
+      /* FIXME for SCREEN_MAX_SPRITE_WIDTH */
+      raster->draw_buffer_ptr = raster->draw_buffer
+                                + raster->geometry.extra_offscreen_border_left;
+    }
+
+    raster->current_line = 0;
+
+    if (raster->skip_frame)
+        return;
+
+    if (raster->dont_cache)
+        raster_canvas_update_all(raster);
+    else
+        update_canvas(raster);
+}
+
--- vice/src/video/video-render.c
+++ vice/src/video/video-render.c
@@ -26,6 +26,7 @@
 
 #include "vice.h"
 
+#include "raster-canvas.h"
 #include "raster.h"
 #include "render1x1.h"
 #include "render1x1pal.h"
@@ -45,254 +46,285 @@
 
 int video_render_get_fake_pal_state(void)
 {
-	return video_resources.delayloop_emulation;
+    return video_resources.delayloop_emulation;
 }
 
 void video_render_initconfig(video_render_config_t *config)
 {
-	int i;
-	config->rendermode=VIDEO_RENDER_NULL;
-	config->doublescan=0;
-	for (i=0;i<256;i++) config->physical_colors[i]=0;
+    int i;
+
+    config->rendermode = VIDEO_RENDER_NULL;
+    config->doublescan = 0;
+
+    for (i = 0; i < 256; i++)
+        config->physical_colors[i] = 0;
 }
 
-void video_render_setphysicalcolor(video_render_config_t *config, int index, DWORD color, int depth)
+void video_render_setphysicalcolor(video_render_config_t *config, int index,
+                                   DWORD color, int depth)
 {
-	switch (depth)
-	{
-	case 8:
-		color &= 0x000000FF;
-		color = color | (color << 8);
-		break;
-	case 16:
-		color &= 0x0000FFFF;
-		color = color | (color << 16);
-		break;
-	case 24:
-		color &= 0x00FFFFFF;
-		break;
-	case 32:
-		break;
-	}
-	config->physical_colors[index]=color;
+    switch (depth) {
+      case 8:
+        color &= 0x000000FF;
+        color = color | (color << 8);
+        break;
+      case 16:
+        color &= 0x0000FFFF;
+        color = color | (color << 16);
+        break;
+      case 24:
+        color &= 0x00FFFFFF;
+        break;
+      case 32:
+      break;
+    }
+    config->physical_colors[index] = color;
 }
 
-void video_render_main(video_render_config_t *config, BYTE *src, BYTE *trg, int width,
-                       int height, int xs, int ys, int xt, int yt, int pitchs,
-                       int pitcht, int depth)
+void video_render_main(video_render_config_t *config, BYTE *src, BYTE *trg,
+                       int width, int height, int xs, int ys, int xt, int yt,
+                       int pitchs, int pitcht, int depth)
 {
-	DWORD *colortab;
-	int doublescan,delayloop,rendermode,palmode;
+    DWORD *colortab;
+    int doublescan, delayloop, rendermode, palmode;
 
 #if 0
 printf("w:%i h:%i xs:%i ys:%i xt:%i yt:%i ps:%i pt:%i d%i\n",
         width, height, xs, ys, xt, yt, pitchs, pitcht, depth);
 #endif
 
-	if (width <= 0) return; /* some render routines don't like invalid width */
+    if (width <= 0)
+        return; /* some render routines don't like invalid width */
 
-	rendermode=config->rendermode;
-	doublescan=config->doublescan;
-	colortab=config->physical_colors;
+    rendermode = config->rendermode;
+    doublescan = config->doublescan;
+    colortab = config->physical_colors;
 
-	delayloop=video_resources.delayloop_emulation;
-	if (video_resources.ext_palette) delayloop = 0;
-	palmode=video_resources.pal_mode;
-	if (video_resources.pal_scanlineshade <= 0) doublescan = 0;
+    delayloop = video_resources.delayloop_emulation;
+    if (video_resources.ext_palette)
+        delayloop = 0;
+    palmode = video_resources.pal_mode;
+    if (video_resources.pal_scanlineshade <= 0)
+        doublescan = 0;
 
-	switch (rendermode)
-	{
-	case VIDEO_RENDER_NULL:
-		break;
+    switch (rendermode) {
+      case VIDEO_RENDER_NULL:
+        break;
 
-	case VIDEO_RENDER_PAL_1X1:
-		if (delayloop)
-		{
-			switch (depth)
-			{
-			case 8:
-				render_08_1x1_08(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-				return;
-			case 16:
-				switch (palmode)
-				{
-				default:
-				case VIDEO_RESOURCE_PAL_MODE_FAST:
-					render_16_1x1_08(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-					return;
-				case VIDEO_RESOURCE_PAL_MODE_SHARP:
-					render_16_1x1_palyc(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-					return;
-				case VIDEO_RESOURCE_PAL_MODE_BLUR:
-					render_16_1x1_pal(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-					return;
-				}
-				return;
-			case 24:
-				render_24_1x1_08(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-				return;
-			case 32:
-				switch (palmode)
-				{
-				default:
-				case VIDEO_RESOURCE_PAL_MODE_FAST:
-					render_32_1x1_08(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-					return;
-				case VIDEO_RESOURCE_PAL_MODE_SHARP:
-					render_32_1x1_palyc(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-					return;
-				case VIDEO_RESOURCE_PAL_MODE_BLUR:
-					render_32_1x1_pal(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-					return;
-				}
-				return;
-			}
-		}
-		else
-		{
-			switch (depth)
-			{
-			case 8:
-				render_08_1x1_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-				return;
-			case 16:
-				render_16_1x1_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-				return;
-			case 24:
-				render_24_1x1_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-				return;
-			case 32:
-				render_32_1x1_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-				return;
-			}
-		}
-		return;
+      case VIDEO_RENDER_PAL_1X1:
+        if (delayloop) {
+            switch (depth) {
+              case 8:
+                render_08_1x1_08(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht);
+                return;
+              case 16:
+                switch (palmode) {
+                  default:
+                  case VIDEO_RESOURCE_PAL_MODE_FAST:
+                    render_16_1x1_08(colortab, src, trg, width, height,
+                                     xs, ys, xt, yt, pitchs, pitcht);
+                    return;
+                  case VIDEO_RESOURCE_PAL_MODE_SHARP:
+                    render_16_1x1_palyc(colortab, src, trg, width, height,
+                                        xs, ys, xt, yt, pitchs, pitcht);
+                    return;
+                  case VIDEO_RESOURCE_PAL_MODE_BLUR:
+                    render_16_1x1_pal(colortab, src, trg, width, height,
+                                      xs, ys, xt, yt, pitchs, pitcht);
+                    return;
+                }
+                return;
+              case 24:
+                render_24_1x1_08(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht);
+                return;
+              case 32:
+                switch (palmode) {
+                  default:
+                  case VIDEO_RESOURCE_PAL_MODE_FAST:
+                    render_32_1x1_08(colortab, src, trg, width, height,
+                                     xs, ys, xt, yt, pitchs, pitcht);
+                    return;
+                  case VIDEO_RESOURCE_PAL_MODE_SHARP:
+                    render_32_1x1_palyc(colortab, src, trg, width, height,
+                                        xs, ys, xt, yt, pitchs, pitcht);
+                    return;
+                  case VIDEO_RESOURCE_PAL_MODE_BLUR:
+                    render_32_1x1_pal(colortab, src, trg, width, height,
+                                      xs, ys, xt, yt, pitchs, pitcht);
+                    return;
+                }
+                return;
+            }
+        } else {
+            switch (depth) {
+              case 8:
+                render_08_1x1_04(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht);
+                return;
+              case 16:
+                render_16_1x1_04(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht);
+                return;
+              case 24:
+                render_24_1x1_04(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht);
+                return;
+              case 32:
+                render_32_1x1_04(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht);
+                return;
+            }
+        }
+        return;
 
-	case VIDEO_RENDER_PAL_2X2:
-		if (delayloop)
-		{
-			switch (depth)
-			{
-			case 8:
-				render_08_2x2_08(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-				return;
-			case 16:
-				switch (palmode)
-				{
-				default:
-				case VIDEO_RESOURCE_PAL_MODE_FAST:
-					render_16_2x2_08(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-					return;
-				case VIDEO_RESOURCE_PAL_MODE_SHARP:
-					render_16_2x2_palyc(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-					return;
-				case VIDEO_RESOURCE_PAL_MODE_BLUR:
-					render_16_2x2_pal(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-					return;
-				}
-				return;
-			case 24:
-				render_24_2x2_08(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-				return;
-			case 32:
-				switch (palmode)
-				{
-				default:
-				case VIDEO_RESOURCE_PAL_MODE_FAST:
-					render_32_2x2_08(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-					return;
-				case VIDEO_RESOURCE_PAL_MODE_SHARP:
-					render_32_2x2_palyc(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-					return;
-				case VIDEO_RESOURCE_PAL_MODE_BLUR:
-					render_32_2x2_pal(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-					return;
-				}
-				return;
-			}
-		}
-		else
-		{
-			switch (depth)
-			{
-			case 8:
-				render_08_2x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-				return;
-			case 16:
-				render_16_2x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-				return;
-			case 24:
-				render_24_2x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-				return;
-			case 32:
-				render_32_2x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-				return;
-			}
-		}
-		return;
+      case VIDEO_RENDER_PAL_2X2:
+        if (delayloop) {
+            switch (depth) {
+              case 8:
+                render_08_2x2_08(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht, doublescan);
+                return;
+              case 16:
+                switch (palmode) {
+                  default:
+                  case VIDEO_RESOURCE_PAL_MODE_FAST:
+                    render_16_2x2_08(colortab, src, trg, width, height,
+                                     xs, ys, xt, yt, pitchs, pitcht,
+                                     doublescan);
+                    return;
+                  case VIDEO_RESOURCE_PAL_MODE_SHARP:
+                    render_16_2x2_palyc(colortab, src, trg, width, height,
+                                        xs, ys, xt, yt, pitchs, pitcht,
+                                        doublescan);
+                    return;
+                  case VIDEO_RESOURCE_PAL_MODE_BLUR:
+                    render_16_2x2_pal(colortab, src, trg, width, height,
+                                      xs, ys, xt, yt, pitchs, pitcht,
+                                      doublescan);
+                    return;
+                }
+                return;
+              case 24:
+                render_24_2x2_08(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht, doublescan);
+                return;
+              case 32:
+                switch (palmode) {
+                  default:
+                  case VIDEO_RESOURCE_PAL_MODE_FAST:
+                    render_32_2x2_08(colortab, src, trg, width, height,
+                                     xs, ys, xt, yt, pitchs, pitcht,
+                                     doublescan);
+                    return;
+                  case VIDEO_RESOURCE_PAL_MODE_SHARP:
+                    render_32_2x2_palyc(colortab, src, trg, width, height,
+                                        xs, ys, xt, yt, pitchs, pitcht,
+                                        doublescan);
+                    return;
+                  case VIDEO_RESOURCE_PAL_MODE_BLUR:
+                    render_32_2x2_pal(colortab, src, trg, width, height,
+                                      xs, ys, xt, yt, pitchs, pitcht,
+                                      doublescan);
+                    return;
+                }
+                return;
+            }
+        } else {
+            switch (depth) {
+              case 8:
+                render_08_2x2_04(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht, doublescan);
+                return;
+              case 16:
+                render_16_2x2_04(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht, doublescan);
+                return;
+              case 24:
+                render_24_2x2_04(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht, doublescan);
+                return;
+              case 32:
+                render_32_2x2_04(colortab, src, trg, width, height,
+                                 xs, ys, xt, yt, pitchs, pitcht, doublescan);
+                return;
+            }
+        }
+        return;
 
-	case VIDEO_RENDER_RGB_1X1:
-		switch (depth)
-		{
-		case 8:
-			render_08_1x1_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-			return;
-		case 16:
-			render_16_1x1_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-			return;
-		case 24:
-			render_24_1x1_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-			return;
-		case 32:
-			render_32_1x1_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht);
-			return;
-		}
-		return;
+      case VIDEO_RENDER_RGB_1X1:
+        switch (depth) {
+          case 8:
+            render_08_1x1_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht);
+            return;
+          case 16:
+            render_16_1x1_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht);
+            return;
+          case 24:
+            render_24_1x1_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht);
+            return;
+          case 32:
+            render_32_1x1_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht);
+            return;
+        }
+        return;
 
-	case VIDEO_RENDER_RGB_1X2:
-		switch (depth)
-		{
-		case 8:
-			render_08_1x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-			return;
-		case 16:
-			render_16_1x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-			return;
-		case 24:
-			render_24_1x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-			return;
-		case 32:
-			render_32_1x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-			return;
-		}
-		return;
+      case VIDEO_RENDER_RGB_1X2:
+        switch (depth) {
+          case 8:
+            render_08_1x2_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht, doublescan);
+            return;
+          case 16:
+            render_16_1x2_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht, doublescan);
+            return;
+          case 24:
+            render_24_1x2_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht, doublescan);
+            return;
+          case 32:
+            render_32_1x2_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht, doublescan);
+            return;
+        }
+        return;
 
-	case VIDEO_RENDER_RGB_2X2:
-		switch (depth)
-		{
-		case 8:
-			render_08_2x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-			return;
-		case 16:
-			render_16_2x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-			return;
-		case 24:
-			render_24_2x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-			return;
-		case 32:
-			render_32_2x2_04(colortab,src,trg,width,height,xs,ys,xt,yt,pitchs,pitcht,doublescan);
-			return;
-		}
-		return;
-	}
+      case VIDEO_RENDER_RGB_2X2:
+        switch (depth) {
+          case 8:
+            render_08_2x2_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht, doublescan);
+            return;
+          case 16:
+            render_16_2x2_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht, doublescan);
+            return;
+          case 24:
+            render_24_2x2_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht, doublescan);
+            return;
+          case 32:
+            render_32_2x2_04(colortab, src, trg, width, height,
+                             xs, ys, xt, yt, pitchs, pitcht, doublescan);
+            return;
+        }
+        return;
+    }
 }
 
 void video_refresh_all(struct video_canvas_s *c)
 {
-	raster_t *raster;
+    struct raster_s *raster;
 	
-	raster = raster_get_raster_from_canvas(c);
+    raster = raster_get_raster_from_canvas(c);
 	
-	if (raster != NULL)
-		raster_update_canvas_all(raster);
+    if (raster != NULL)
+        raster_canvas_update_all(raster);
 }
+
