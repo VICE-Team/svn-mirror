b65ffe2e1e87cc4c863c872c88e574322af03b22
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:54:36 +0000

    This commit was generated by cvs2svn to compensate for changes in r15943, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@15944 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/intl.c
+++ vice/src/arch/win32/intl.c
@@ -48,6 +48,7 @@
 typedef struct windows_iso_s {
     int windows_code;
     char *iso_language_code;
+    int code_page;
 } windows_iso_t;
 
 
@@ -57,13 +58,13 @@ typedef struct windows_iso_s {
    for the corresponding windows and iso codes */
 
 static windows_iso_t windows_to_iso[]={
-  {LANG_GERMAN, "de"},
-  {LANG_ENGLISH, "en"},
-  {LANG_FRENCH, "fr"},
-  {LANG_ITALIAN, "it"},
-  {LANG_DUTCH, "nl"},
-  {LANG_POLISH, "pl"},
-  {LANG_SWEDISH, "sv"},
+  {LANG_ENGLISH, "en", 28591},
+  {LANG_GERMAN, "de", 28591},
+  {LANG_FRENCH, "fr", 28591},
+  {LANG_ITALIAN, "it", 28591},
+  {LANG_DUTCH, "nl", 28591},
+  {LANG_POLISH, "pl", 28592},
+  {LANG_SWEDISH, "sv", 28591},
   {0, NULL}
 };
 
@@ -260,33 +261,6 @@ static int intl_table[][countof(language_table)] = {
 /* nl */  IDD_VICII_DIALOG_NL,
 /* pl */  IDD_VICII_DIALOG_PL,    /* fuzzy, size */
 /* sv */  IDD_VICII_DIALOG_SV},   /* fuzzy, size */
-
-/* ressid.rc */
-/* en */ {IDD_SID_GENERAL_SETTINGS_DIALOG,
-/* de */  IDD_SID_GENERAL_SETTINGS_DIALOG_DE,    /* fuzzy, size */
-/* fr */  IDD_SID_GENERAL_SETTINGS_DIALOG_FR,    /* fuzzy, size */
-/* it */  IDD_SID_GENERAL_SETTINGS_DIALOG_IT,    /* fuzzy, size */
-/* nl */  IDD_SID_GENERAL_SETTINGS_DIALOG_NL,
-/* pl */  IDD_SID_GENERAL_SETTINGS_DIALOG_PL,    /* fuzzy, size */
-/* sv */  IDD_SID_GENERAL_SETTINGS_DIALOG_SV},   /* fuzzy, size */
-
-/* ressid.rc */
-/* en */ {IDD_SID_RESID_SETTINGS_DIALOG,
-/* de */  IDD_SID_RESID_SETTINGS_DIALOG_DE,    /* fuzzy, size */
-/* fr */  IDD_SID_RESID_SETTINGS_DIALOG_FR,    /* fuzzy, size */
-/* it */  IDD_SID_RESID_SETTINGS_DIALOG_IT,    /* fuzzy, size */
-/* nl */  IDD_SID_RESID_SETTINGS_DIALOG_NL,
-/* pl */  IDD_SID_RESID_SETTINGS_DIALOG_PL,    /* fuzzy, size */
-/* sv */  IDD_SID_RESID_SETTINGS_DIALOG_SV},   /* fuzzy, size */
-
-/* ressid.rc */
-/* en */ {IDD_SID_HARDSID_SETTINGS_DIALOG,
-/* de */  IDD_SID_HARDSID_SETTINGS_DIALOG_DE,    /* fuzzy, size */
-/* fr */  IDD_SID_HARDSID_SETTINGS_DIALOG_FR,    /* fuzzy, size */
-/* it */  IDD_SID_HARDSID_SETTINGS_DIALOG_IT,    /* fuzzy, size */
-/* nl */  IDD_SID_HARDSID_SETTINGS_DIALOG_NL,
-/* pl */  IDD_SID_HARDSID_SETTINGS_DIALOG_PL,    /* fuzzy, size */
-/* sv */  IDD_SID_HARDSID_SETTINGS_DIALOG_SV},   /* fuzzy, size */
 
 /* resc64.rc */
 /* en */ {IDD_C64ROM_SETTINGS_DIALOG,
@@ -1548,33 +1522,6 @@ static int intl_translate_text_table[][countof(language_table)] = {
 /* pl */  IDS_ROM_SETTINGS_PL,    /* fuzzy */
 /* sv */  IDS_ROM_SETTINGS_SV},   /* fuzzy */
 
-/* ressid.rc */
-/* en */ {IDS_THIS_MACHINE_NO_SID,
-/* de */  IDS_THIS_MACHINE_NO_SID_DE,    /* fuzzy */
-/* fr */  IDS_THIS_MACHINE_NO_SID_FR,    /* fuzzy */
-/* it */  IDS_THIS_MACHINE_NO_SID_IT,    /* fuzzy */
-/* nl */  IDS_THIS_MACHINE_NO_SID_NL,
-/* pl */  IDS_THIS_MACHINE_NO_SID_PL,    /* fuzzy */
-/* sv */  IDS_THIS_MACHINE_NO_SID_SV},   /* fuzzy */
-
-/* ressid.rc */
-/* en */ {IDS_GENERAL,
-/* de */  IDS_GENERAL_DE,    /* fuzzy */
-/* fr */  IDS_GENERAL_FR,    /* fuzzy */
-/* it */  IDS_GENERAL_IT,    /* fuzzy */
-/* nl */  IDS_GENERAL_NL,
-/* pl */  IDS_GENERAL_PL,    /* fuzzy */
-/* sv */  IDS_GENERAL_SV},   /* fuzzy */
-
-/* ressid.rc */
-/* en */ {IDS_SID_SETTINGS,
-/* de */  IDS_SID_SETTINGS_DE,    /* fuzzy */
-/* fr */  IDS_SID_SETTINGS_FR,    /* fuzzy */
-/* it */  IDS_SID_SETTINGS_IT,    /* fuzzy */
-/* nl */  IDS_SID_SETTINGS_NL,
-/* pl */  IDS_SID_SETTINGS_PL,    /* fuzzy */
-/* sv */  IDS_SID_SETTINGS_SV},   /* fuzzy */
-
 /* res.rc */
 /* en */ {IDS_SAVE_SNAPSHOT_IMAGE,
 /* de */  IDS_SAVE_SNAPSHOT_IMAGE_DE,    /* fuzzy */
@@ -2335,42 +2282,6 @@ static int intl_translate_text_table[][countof(language_table)] = {
 /* pl */  IDS_SELECT_FILE_REU_PL,    /* fuzzy */
 /* sv */  IDS_SELECT_FILE_REU_SV},   /* fuzzy */
 
-/* ressid.rc */
-/* en */ {IDS_FAST,
-/* de */  IDS_FAST_DE,    /* fuzzy */
-/* fr */  IDS_FAST_FR,    /* fuzzy */
-/* it */  IDS_FAST_IT,    /* fuzzy */
-/* nl */  IDS_FAST_NL,
-/* pl */  IDS_FAST_PL,    /* fuzzy */
-/* sv */  IDS_FAST_SV},   /* fuzzy */
-
-/* ressid.rc */
-/* en */ {IDS_INTERPOLATING,
-/* de */  IDS_INTERPOLATING_DE,    /* fuzzy */
-/* fr */  IDS_INTERPOLATING_FR,    /* fuzzy */
-/* it */  IDS_INTERPOLATING_IT,    /* fuzzy */
-/* nl */  IDS_INTERPOLATING_NL,
-/* pl */  IDS_INTERPOLATING_PL,    /* fuzzy */
-/* sv */  IDS_INTERPOLATING_SV},   /* fuzzy */
-
-/* ressid.rc */
-/* en */ {IDS_RESAMPLING,
-/* de */  IDS_RESAMPLING_DE,    /* fuzzy */
-/* fr */  IDS_RESAMPLING_FR,    /* fuzzy */
-/* it */  IDS_RESAMPLING_IT,    /* fuzzy */
-/* nl */  IDS_RESAMPLING_NL,
-/* pl */  IDS_RESAMPLING_PL,    /* fuzzy */
-/* sv */  IDS_RESAMPLING_SV},   /* fuzzy */
-
-/* ressid.rc */
-/* en */ {IDS_FAST_RESAMPLING,
-/* de */  IDS_FAST_RESAMPLING_DE,    /* fuzzy */
-/* fr */  IDS_FAST_RESAMPLING_FR,    /* fuzzy */
-/* it */  IDS_FAST_RESAMPLING_IT,    /* fuzzy */
-/* nl */  IDS_FAST_RESAMPLING_NL,
-/* pl */  IDS_FAST_RESAMPLING_PL,    /* fuzzy */
-/* sv */  IDS_FAST_RESAMPLING_SV},   /* fuzzy */
-
 /* res.rc */
 /* en */ {IDS_FLEXIBLE,
 /* de */  IDS_FLEXIBLE_DE,    /* fuzzy */
@@ -2600,47 +2511,6 @@ static const WCHAR wcp28592[256] =
     0x0159, 0x016f, 0x00fa, 0x0171, 0x00fc, 0x00fd, 0x0163, 0x02d9
 };
 
-
-/* codepage 28605 (ISO 8859-15) to current codepage conversion tables */
-
-static char cp28605[256];
-
-static const WCHAR wcp28605[256] =
-{
-    0x0000, 0x0001, 0x0002, 0x0003, 0x0004, 0x0005, 0x0006, 0x0007,
-    0x0008, 0x0009, 0x000a, 0x000b, 0x000c, 0x000d, 0x000e, 0x000f,
-    0x0010, 0x0011, 0x0012, 0x0013, 0x0014, 0x0015, 0x0016, 0x0017,
-    0x0018, 0x0019, 0x001a, 0x001b, 0x001c, 0x001d, 0x001e, 0x001f,
-    0x0020, 0x0021, 0x0022, 0x0023, 0x0024, 0x0025, 0x0026, 0x0027,
-    0x0028, 0x0029, 0x002a, 0x002b, 0x002c, 0x002d, 0x002e, 0x002f,
-    0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037,
-    0x0038, 0x0039, 0x003a, 0x003b, 0x003c, 0x003d, 0x003e, 0x003f,
-    0x0040, 0x0041, 0x0042, 0x0043, 0x0044, 0x0045, 0x0046, 0x0047,
-    0x0048, 0x0049, 0x004a, 0x004b, 0x004c, 0x004d, 0x004e, 0x004f,
-    0x0050, 0x0051, 0x0052, 0x0053, 0x0054, 0x0055, 0x0056, 0x0057,
-    0x0058, 0x0059, 0x005a, 0x005b, 0x005c, 0x005d, 0x005e, 0x005f,
-    0x0060, 0x0061, 0x0062, 0x0063, 0x0064, 0x0065, 0x0066, 0x0067,
-    0x0068, 0x0069, 0x006a, 0x006b, 0x006c, 0x006d, 0x006e, 0x006f,
-    0x0070, 0x0071, 0x0072, 0x0073, 0x0074, 0x0075, 0x0076, 0x0077,
-    0x0078, 0x0079, 0x007a, 0x007b, 0x007c, 0x007d, 0x007e, 0x007f,
-    0x0080, 0x0081, 0x0082, 0x0083, 0x0084, 0x0085, 0x0086, 0x0087,
-    0x0088, 0x0089, 0x008a, 0x008b, 0x008c, 0x008d, 0x008e, 0x008f,
-    0x0090, 0x0091, 0x0092, 0x0093, 0x0094, 0x0095, 0x0096, 0x0097,
-    0x0098, 0x0099, 0x009a, 0x009b, 0x009c, 0x009d, 0x009e, 0x009f,
-    0x00a0, 0x00a1, 0x00a2, 0x00a3, 0x20ac, 0x00a5, 0x0160, 0x00a7,
-    0x0161, 0x00a9, 0x00aa, 0x00ab, 0x00ac, 0x00ad, 0x00ae, 0x00af,
-    0x00b0, 0x00b1, 0x00b2, 0x00b3, 0x017d, 0x00b5, 0x00b6, 0x00b7,
-    0x017e, 0x00b9, 0x00ba, 0x00bb, 0x0152, 0x0153, 0x0178, 0x00bf,
-    0x00c0, 0x00c1, 0x00c2, 0x00c3, 0x00c4, 0x00c5, 0x00c6, 0x00c7,
-    0x00c8, 0x00c9, 0x00ca, 0x00cb, 0x00cc, 0x00cd, 0x00ce, 0x00cf,
-    0x00d0, 0x00d1, 0x00d2, 0x00d3, 0x00d4, 0x00d5, 0x00d6, 0x00d7,
-    0x00d8, 0x00d9, 0x00da, 0x00db, 0x00dc, 0x00dd, 0x00de, 0x00df,
-    0x00e0, 0x00e1, 0x00e2, 0x00e3, 0x00e4, 0x00e5, 0x00e6, 0x00e7,
-    0x00e8, 0x00e9, 0x00ea, 0x00eb, 0x00ec, 0x00ed, 0x00ee, 0x00ef,
-    0x00f0, 0x00f1, 0x00f2, 0x00f3, 0x00f4, 0x00f5, 0x00f6, 0x00f7,
-    0x00f8, 0x00f9, 0x00fa, 0x00fb, 0x00fc, 0x00fd, 0x00fe, 0x00ff
-};
-
 char *intl_convert_cp(char *text, int cp)
 {
   int len,i;
@@ -2660,9 +2530,6 @@ char *intl_convert_cp(char *text, int cp)
     case 28592:
       cp_table=cp28592;
       break;
-    case 28605:
-      cp_table=cp28605;
-      break;
     default:
       cp_table=cp28591;
   }
@@ -2677,6 +2544,9 @@ char *intl_convert_cp(char *text, int cp)
 /* --------------------------------------------------------------------- */
 
 static char *intl_text_table[countof(intl_translate_text_table)][countof(language_table)];
+static char *intl_text_table2[16384][countof(language_table)];
+// the 16384 will be removed
+
 
 /* this routine fills in the table of resources and pointers to the
    text that belongs to them with the right data, this is because of
@@ -2687,9 +2557,34 @@ static char *intl_text_table[countof(intl_translate_text_table)][countof(languag
 
 static void intl_text_init(void)
 {
-  int i,j;
-  char temp_buffer[4098*sizeof(TCHAR)];
+int i, j, k;
+char temp_buffer[4098 * sizeof(TCHAR)];
+HRSRC hRes;
+HGLOBAL hGlob;
+WORD *p;
+int length;
 
+    for (i = 0; i < countof(language_table); i++) {
+        for (j = (13008 >> 4) + 1; j < (16384 >> 4); j++) {
+            hRes = FindResourceEx(NULL, RT_STRING, MAKEINTRESOURCE(j), MAKELANGID(windows_to_iso[i].windows_code, SUBLANG_NEUTRAL));
+            if (hRes) {
+                hGlob = LoadResource(NULL, hRes);
+                p = LockResource(hGlob);
+                for (k = 0; k < 16; k++) {
+                    ZeroMemory(temp_buffer, sizeof(temp_buffer));
+                    length = *p++;
+                    WideCharToMultiByte(CP_ACP, 0, p, length, temp_buffer, 4096, NULL, NULL);
+                    p = p + length;
+                    intl_text_table2[((j - 1) << 4) + k][i] = strdup(temp_buffer);
+                }
+                FreeResource(hGlob);
+            } else {
+                for (k = 0; k < 16; k++) {
+                    intl_text_table2[((j - 1) << 4) + k][i] = NULL;
+                }
+            }
+        }
+    }
   for (i = 0; i < countof(language_table); i++)
   {
     for (j = 0; j < countof(intl_translate_text_table); j++)
@@ -2710,9 +2605,6 @@ static void intl_text_init(void)
 
   /* prepare the codepage 28592 (ISO 8859-2) to current codepage conversion */
   WideCharToMultiByte(CP_ACP, 0, wcp28592, 256, cp28592, 256, NULL, NULL);  
-
-  /* prepare the codepage 28605 (ISO 8859-15) to current codepage conversion */
-  WideCharToMultiByte(CP_ACP, 0, wcp28605, 256, cp28605, 256, NULL, NULL);  
 }
 
 static void intl_text_free(void)
@@ -2727,6 +2619,14 @@ static void intl_text_free(void)
         lib_free(intl_text_table[j][i]);
     }
   }
+
+    for (i = 0; i < countof(language_table); i++) {
+        for (j = 0; j < countof(intl_text_table2); j++) {
+            if (intl_text_table2[j][i] != NULL) {
+                lib_free(intl_text_table2[j][i]);
+            }
+        }
+    }
 }
 
 char *intl_translate_text(int en_resource)
@@ -2746,6 +2646,40 @@ char *intl_translate_text(int en_resource)
     }
   }
   return "";
+}
+
+char *intl_translate_text_new(int en_resource)
+{
+char *text;
+
+    text = intl_text_table2[en_resource][current_language_index];
+    if (text == NULL) {
+        text = intl_text_table2[en_resource][0];
+    }
+    return text;
+/*
+HRSRC hRes;
+HGLOBAL hGlob;
+WORD *p;
+int n;
+int i;
+int length;
+char buffer[1024];
+
+    ZeroMemory(buffer, 1024);
+    hRes = FindResourceEx(NULL, RT_STRING, MAKEINTRESOURCE((en_resource >> 4) + 1), MAKELANGID(windows_to_iso[current_language_index].windows_code, SUBLANG_NEUTRAL));
+    hGlob = LoadResource(NULL, hRes);
+    p = LockResource(hGlob);
+    n = en_resource & 0xf;
+    for (i = 0; i < n; i++) {
+        length = *p++;
+        p = p + length;
+    }
+    length = *p++;
+    WideCharToMultiByte(CP_ACP, 0, p, length, buffer, 1024, NULL, NULL);
+
+    return strdup(buffer);
+    */
 }
 
 /* pre-translated main window caption text so the emulation won't
