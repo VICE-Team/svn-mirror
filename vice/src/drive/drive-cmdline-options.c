7cb384d86830673f932eb23cde723cfbc7966d41
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:39:31 +0000

    This commit was generated by cvs2svn to compensate for changes in r14133, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@14134 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/drive/drive-cmdline-options.c
+++ vice/src/drive/drive-cmdline-options.c
@@ -32,6 +32,7 @@
 #include "cmdline.h"
 #include "drive-cmdline-options.h"
 #include "drive.h"
+#include "lib.h"
 #include "machine-drive.h"
 
 
@@ -40,21 +41,38 @@ static const cmdline_option_t cmdline_options[] = {
       (void *)1, NULL, "Enable hardware-level emulation of disk drives" },
     { "+truedrive", SET_RESOURCE, 0, NULL, NULL, "DriveTrueEmulation",
       (void *)0, NULL, "Disable hardware-level emulation of disk drives" },
-    { "-drive8type", SET_RESOURCE, 1, NULL, NULL, "Drive8Type",
-      (void *)DRIVE_TYPE_1541, "<type>", "Set drive type (0: no drive)" },
-    { "-drive9type", SET_RESOURCE, 1, NULL, NULL, "Drive9Type",
-      (void *)DRIVE_TYPE_NONE, "<type>", "Set drive type (0: no drive)" },
-    { "-drive8extend", SET_RESOURCE, 1, NULL, NULL, "Drive8ExtendImagePolicy",
-      (void *)DRIVE_EXTEND_NEVER, "<method>",
+    { NULL }
+};
+
+static cmdline_option_t cmd_drive[] = {
+    { NULL, SET_RESOURCE, 1, NULL, NULL, NULL, NULL, "<type>",
+      "Set drive type (0: no drive)" },
+    { NULL, SET_RESOURCE, 1, NULL, NULL, NULL, NULL, "<method>",
       "Set drive 8 40 track extension policy (0: never, 1: ask, 2: on access)"},
-    { "-drive9extend", SET_RESOURCE, 1, NULL, NULL, "Drive9ExtendImagePolicy",
-      (void *)DRIVE_EXTEND_NEVER, "<method>",
-      "Set drive 9 40 track extension policy (0: never, 1: ask, 2: on access)"},
     { NULL }
 };
 
 int drive_cmdline_options_init(void)
 {
+    unsigned int dnr, i;
+
+    for (dnr = 0; dnr < DRIVE_NUM; dnr++) {
+        cmd_drive[0].name = lib_msprintf("-drive%itype", dnr + 8);
+        cmd_drive[0].resource_name
+            = lib_msprintf("Drive%iType", dnr + 8);
+        cmd_drive[1].name = lib_msprintf("-drive%iextend", dnr + 8);
+        cmd_drive[1].resource_name
+            = lib_msprintf("Drive%iExtendImagePolicy", dnr + 8);
+
+        if (cmdline_register_options(cmd_drive) < 0)
+            return -1;
+
+        for (i = 0; i < 2; i++) {
+            lib_free((char *)cmd_drive[i].name);
+            lib_free((char *)cmd_drive[i].resource_name);
+        }
+    }
+
     return machine_drive_cmdline_options_init()
         | cmdline_register_options(cmdline_options);
 }
