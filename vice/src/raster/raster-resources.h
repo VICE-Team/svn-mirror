571694ec86813c14bf3163a3d60c6c282a2fd3f9
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:09:58 +0000

    This commit was generated by cvs2svn to compensate for changes in r10587, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@10588 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/raster/raster-resources.h
+++ vice/src/raster/raster-resources.h
@@ -28,9 +28,10 @@
 #define _RASTER_RESOURCES_H
 
 struct raster_s;
+struct video_chip_cap_s;
 
 extern int raster_resources_chip_init(const char *chipname,
                                       struct raster_s *raster,
-                                      int double_size, int doulbe_scan);
+                                      struct video_chip_cap_s *video_chip_cap);
 #endif
 
