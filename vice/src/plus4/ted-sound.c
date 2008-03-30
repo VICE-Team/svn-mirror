6717952d7b44aa0f8968f54acc3b2a96deb8afa5
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:00:09 +0000

    This commit was generated by cvs2svn to compensate for changes in r9409, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@9410 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/plus4/ted-sound.c
+++ vice/src/plus4/ted-sound.c
@@ -30,26 +30,78 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "maincpu.h"
 #include "sound.h"
 #include "ted-sound.h"
 #include "utils.h"
 
+static BYTE siddata[5];
+
+/* Needed data for one voice.  */
+typedef struct voice_s
+{
+    /* Collect number of cycles elapsed  */
+    DWORD accu;
+    /* Toggle sign if accu has reached the compare value  */
+    DWORD compare;
+    /* Sign of the square wave  */
+    SWORD sign;
+} voice_t;
+
 struct sound_s
 {
-    int on;
-    CLOCK t;
-    BYTE sample;
-
-    double b;
-    double bs;
-
-    int speed;
-    int cycles_per_sec;
+    /* Number of voices  */
+    voice_t voice[2];
+    /* Volume multiplier  */
+    SWORD volume;
+    /* 8 cycles units per sample  */
+    DWORD speed;
+    /* Digital output?  */
+    BYTE digital;
+    /* Noise generator active?  */
+    BYTE noise;
 };
+
+/* FIXME: Find proper volume multiplier.  */
+static SWORD volume_tab[16] = {
+    0x0000, 0x0010, 0x0020, 0x0040, 0x0080, 0x0100, 0x0200, 0x0400,
+    0x0800, 0x0800, 0x0800, 0x0800, 0x0800, 0x0800, 0x0800, 0x0800 };
 
 int sound_machine_calculate_samples(sound_t *psid, SWORD *pbuf, int nr,
                                     int interleave, int *delta_t)
 {
+    int i;
+
+    if (psid->digital) {
+        for (i = 0; i < nr; i++) {
+            /* FIXME: Implement digis.  */
+            pbuf[i * interleave] = (SDWORD)(psid->volume * 0);
+        }
+    } else {
+        for (i = 0; i < nr; i++) {
+            if (psid->voice[0].sign) {
+                psid->voice[0].accu += psid->speed;
+                while (psid->voice[0].accu > psid->voice[0].compare) {
+                    psid->voice[0].sign *= -1;
+                    psid->voice[0].accu -= psid->voice[0].compare;
+                }
+            }
+
+            if (psid->voice[1].sign){
+                if (psid->noise) {
+                    /* FIXME: Implement noise.  */
+                } else {
+                    psid->voice[1].accu += psid->speed;
+                    while (psid->voice[1].accu > psid->voice[1].compare) {
+                        psid->voice[1].sign *= -1;
+                        psid->voice[1].accu -= psid->voice[1].compare;
+                    }
+                }
+            }
+            pbuf[i * interleave] = (SDWORD)(psid->volume *
+                                   (psid->voice[0].sign + psid->voice[1].sign));
+        }
+    }
     return 0;
 }
 
@@ -57,14 +109,18 @@ sound_t *sound_machine_open(int chipno)
 {
     sound_t *psid;
 
-    psid = xmalloc(sizeof(*psid));
-    memset(psid, 0, sizeof(psid));
+    psid = xmalloc(sizeof(sound_t));
+    memset(psid, 0, sizeof(sound_t));
 
     return psid;
 }
 
 int sound_machine_init(sound_t *psid, int speed, int cycles_per_sec)
 {
+    psid->speed = cycles_per_sec / speed / 8;
+
+    ted_sound_reset();
+
     return 1;
 }
 
@@ -73,28 +129,60 @@ void sound_machine_close(sound_t *psid)
     free(psid);
 }
 
-void store_petsnd_onoff(int value)
-{
-}
-
-void store_petsnd_rate(CLOCK t)
-{
-}
-
-void store_petsnd_sample(BYTE sample)
-{
-}
-
 void sound_machine_store(sound_t *psid, ADDRESS addr, BYTE val)
 {
-}
-
-void petsnd_reset(void)
-{
+    switch(addr) {
+      case 0x0e:
+        siddata[0] = val;
+        psid->voice[0].compare = (siddata[0] | (siddata[4] << 8)) + 1;
+        break;
+      case 0x0f:
+        siddata[1] = val;
+        psid->voice[1].compare = (siddata[1] | (siddata[2] << 8)) + 1;
+        break;
+      case 0x10:
+        siddata[2] = val & 3;
+        psid->voice[1].compare = (siddata[1] | (siddata[2] << 8)) + 1;
+        break;
+      case 0x11:
+        psid->volume = volume_tab[val & 0x0f];
+        if ((val & 0x10) ^ (siddata[3] & 0x10)) {
+            psid->voice[0].sign = (val & 0x10) ? 1 : 0;
+            psid->voice[0].accu = 0;
+        }
+        if ((val & 0x20) ^ (siddata[3] & 0x20)) {
+            psid->voice[1].sign = (val & 0x20) ? 1 : 0;
+            psid->voice[1].accu = 0;
+        }
+        if ((val & 0x60) == 0x60)
+            psid->noise = 1;
+        else
+            psid->noise = 0;
+        psid->digital = val & 0x80;
+        siddata[3] = val;
+        break;
+      case 0x12:
+        siddata[4] = val & 3;
+        psid->voice[0].compare = (siddata[0] | (siddata[4] << 8)) + 1;
+        break;
+    }
 }
 
 BYTE sound_machine_read(sound_t *psid, ADDRESS addr)
 {
+    switch(addr) {
+      case 0x0e:
+        return siddata[0];
+      case 0x0f:
+        return siddata[1];
+      case 0x10:
+        return siddata[2] | 0xc0;
+      case 0x11:
+        return siddata[3];
+      case 0x12:
+        return siddata[4];
+    }
+
     return 0;
 }
 
@@ -104,14 +192,15 @@ void sound_machine_prevent_clk_overflow(sound_t *psid, CLOCK sub)
 
 void sound_machine_reset(sound_t *psid, CLOCK cpu_clk)
 {
+    ADDRESS i;
+
+    for (i = 0x0e; i <= 0x12; i++)
+        sound_machine_store(psid, i, 0);
 }
 
 char *sound_machine_dump_state(sound_t *psid)
 {
-    char buf[256];
-
-    sprintf(buf, "on=%d sample=%d rate=%d\n", psid->on, psid->sample, psid->t);
-    return stralloc(buf);
+    return xmsprintf("#SID: clk=%d v=%d\n", clk, psid->volume);
 }
 
 int sound_machine_cycle_based(void)
@@ -122,5 +211,22 @@ int sound_machine_cycle_based(void)
 int sound_machine_channels(void)
 {
     return 1;
+}
+
+/* ---------------------------------------------------------------------*/
+
+void REGPARM2 ted_sound_store(ADDRESS addr, BYTE value)
+{
+    sound_store(addr, value, 0);
+}
+
+void ted_sound_reset(void)
+{
+    sound_reset();
+}
+
+BYTE REGPARM1 ted_sound_read(ADDRESS addr)
+{
+    return sound_read(addr, 0);
 }
 
