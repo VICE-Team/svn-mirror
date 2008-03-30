8ecfcc3fe6749574487e0b98927c975fb8129261
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:13:43 +0000

    This commit was generated by cvs2svn to compensate for changes in r11037, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@11038 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/video/render1x1.c
+++ vice/src/video/render1x1.c
@@ -42,14 +42,14 @@ void render_08_1x1_04(const DWORD *colortab, const BYTE *src, BYTE *trg,
     BYTE *tmptrg;
     unsigned int x, y, wstart, wfast, wend;
 
-    src += pitchs*ys + xs;
-    trg += pitcht*yt + xt;
+    src += pitchs * ys + xs;
+    trg += pitcht * yt + xt;
     if (width < 8) {
-        wstart=width;
-        wfast=0;
-        wend=0;
+        wstart = width;
+        wfast = 0;
+        wend = 0;
     } else {
-        wstart= 8 - ((unsigned int)trg & 7); /* alignment: 8 pixels*/
+        wstart = 8 - ((unsigned int)trg & 7); /* alignment: 8 pixels*/
         wfast = (width - wstart) >> 3;  /* fast loop for 8 pixel segments*/
         wend = (width - wstart) & 0x07; /* do not forget the rest*/
     }
@@ -57,7 +57,7 @@ void render_08_1x1_04(const DWORD *colortab, const BYTE *src, BYTE *trg,
         tmpsrc = src;
         tmptrg = trg;
         for (x = 0; x < wstart; x++) {
-            *tmptrg++ =(BYTE)colortab[*tmpsrc++];
+            *tmptrg++ = (BYTE)colortab[*tmpsrc++];
         }
         for (x = 0; x < wfast; x++) {
             tmptrg[0] = (BYTE)colortab[tmpsrc[0]];
@@ -103,12 +103,10 @@ void render_16_1x1_04(const DWORD *colortab, const BYTE *src, BYTE *trg,
     for (y = 0; y < height; y++) {
         tmpsrc = src;
         tmptrg = (WORD *)trg;
-        for (x = 0; x < wstart; x++)
-        {
+        for (x = 0; x < wstart; x++) {
                 *tmptrg++ = (WORD)colortab[*tmpsrc++];
         }
-        for (x = 0; x< wfast; x++)
-        {
+        for (x = 0; x< wfast; x++) {
             tmptrg[0] = (WORD)colortab[tmpsrc[0]];
             tmptrg[1] = (WORD)colortab[tmpsrc[1]];
             tmptrg[2] = (WORD)colortab[tmpsrc[2]];
@@ -154,7 +152,7 @@ void render_24_1x1_04(const DWORD *colortab, const BYTE *src, BYTE *trg,
     for (y = 0; y < height; y++) {
         tmpsrc = src;
         tmptrg = trg;
-        for (x = 0;x < wstart; x++) {
+        for (x = 0; x < wstart; x++) {
             color = colortab[*tmpsrc++];
             tmptrg[0] = (BYTE)color;
             color >>= 8;
@@ -226,13 +224,13 @@ void render_32_1x1_04(const DWORD *colortab, const BYTE *src, BYTE *trg,
         wfast = (width - wstart) >> 3; /* fast loop for 8 pixel segments*/
         wend = (width - wstart) & 0x07; /* do not forget the rest*/
     }
-    for (y = 0;y < height; y++) {
+    for (y = 0; y < height; y++) {
         tmpsrc = src;
         tmptrg = (DWORD *)trg;
         for (x = 0; x < wstart; x++) {
             *tmptrg++ = colortab[*tmpsrc++];
         }
-        for (x=0;x<wfast;x++) {
+        for (x = 0; x < wfast; x++) {
             tmptrg[0] = colortab[tmpsrc[0]];
             tmptrg[1] = colortab[tmpsrc[1]];
             tmptrg[2] = colortab[tmpsrc[2]];
@@ -275,7 +273,7 @@ void render_08_1x1_08(const DWORD *colortab, const BYTE *src, BYTE *trg,
         wfast = 0;
         wend = 0;
     } else {
-        wstart= 8 - ((unsigned int)trg & 7); /* alignment: 8 pixels*/
+        wstart = 8 - ((unsigned int)trg & 7); /* alignment: 8 pixels*/
         wfast = (width - wstart) >> 3; /* fast loop for 8 pixel segments*/
         wend = (width - wstart) & 0x07; /* do not forget the rest*/
     }
@@ -284,7 +282,7 @@ void render_08_1x1_08(const DWORD *colortab, const BYTE *src, BYTE *trg,
         tmpsrc = src;
         tmptrg = trg;
         for (x = 0; x < wstart; x++) {
-            *tmptrg++=(BYTE)colortab[*tmpsrc++ | (*tmppre++ << 4)];
+            *tmptrg++ = (BYTE)colortab[*tmpsrc++ | (*tmppre++ << 4)];
         }
         for (x = 0; x < wfast; x++) {
             tmptrg[0] = (BYTE)colortab[tmpsrc[0] | (tmppre[0] << 4)];
@@ -337,7 +335,7 @@ void render_16_1x1_08(const DWORD *colortab, const BYTE *src, BYTE *trg,
         for (x = 0; x < wstart; x++) {
             *tmptrg++ = (WORD)colortab[*tmpsrc++ | (*tmppre++ << 4)];
         }
-        for (x=0;x<wfast;x++) {
+        for (x = 0; x < wfast; x++) {
             tmptrg[0] = (WORD)colortab[tmpsrc[0] | (tmppre[0] << 4)];
             tmptrg[1] = (WORD)colortab[tmpsrc[1] | (tmppre[1] << 4)];
             tmptrg[2] = (WORD)colortab[tmpsrc[2] | (tmppre[2] << 4)];
@@ -386,7 +384,7 @@ void render_24_1x1_08(const DWORD *colortab, const BYTE *src, BYTE *trg,
     for (y = 0; y < height; y++) {
         tmpsrc = src;
         tmptrg = trg;
-        for (x = 0;x < wstart; x++) {
+        for (x = 0; x < wstart; x++) {
             color = colortab[*tmpsrc++ | (*tmppre++ << 4)];
             *tmptrg++ = (BYTE)color;
             color >>= 8;
@@ -455,7 +453,7 @@ void render_32_1x1_08(const DWORD *colortab, const BYTE *src, BYTE *trg,
         wfast = 0;
         wend = 0;
     } else {
-        wstart= 8 - ((unsigned int)trg & 7); /* alignment: 8 pixels*/
+        wstart = 8 - ((unsigned int)trg & 7); /* alignment: 8 pixels*/
         wfast = (width - wstart) >> 3; /* fast loop for 8 pixel segments*/
         wend = (width - wstart) & 0x07; /* do not forget the rest*/
     }
@@ -488,5 +486,4 @@ void render_32_1x1_08(const DWORD *colortab, const BYTE *src, BYTE *trg,
         trg += pitcht;
     }
 }
-
 
