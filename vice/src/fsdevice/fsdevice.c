672d0c5b8d79e86598cf9d905509081182614338
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:43:22 +0000

    This commit was generated by cvs2svn to compensate for changes in r14595, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@14596 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/fsdevice/fsdevice.c
+++ vice/src/fsdevice/fsdevice.c
@@ -35,37 +35,11 @@
 
 #include "vice.h"
 
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef __riscos
-#ifdef __IBMC__
-#include <direct.h>
-#include "snippets/dirport.h"
-#else
-#include <dirent.h>
-#endif
-#endif
-
-#ifdef HAVE_ERRNO_H
-#include <errno.h>
-#endif
-#ifdef HAVE_SYS_STAT_H
-#include <sys/stat.h>
-#endif
-#ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h>
-#endif
-#ifdef HAVE_UNISTD_H
-#include <unistd.h>
-#endif
-
-#include "archdep.h"
 #include "attach.h"
-#include "charset.h"
-#include "cmdline.h"
 #include "fileio.h"
 #include "fsdevice-close.h"
 #include "fsdevice-flush.h"
@@ -77,9 +51,7 @@
 #include "fsdevicetypes.h"
 #include "log.h"
 #include "machine-bus.h"
-#include "p00.h"
 #include "resources.h"
-#include "util.h"
 #include "vdrive-command.h"
 #include "vdrive.h"
 
@@ -93,10 +65,6 @@ static unsigned int fs_eptr[4];
 static size_t fs_elen[4];
 
 char fs_dirmask[MAXPATHLEN];
-
-static int fsdevice_compare_wildcards(char *name, char *p00name);
-static int fsdevice_reduce_filename_p00(char *filename, int len);
-static size_t fsdevice_eliminate_char_p00(char *filename, int pos);
 
 /* ------------------------------------------------------------------------- */
 
@@ -194,191 +162,6 @@ int fsdevice_error_get_byte(vdrive_t *vdrive, BYTE *data)
     }
 
     return rc;
-}
-
-FILE *fsdevice_find_pc64_name(vdrive_t *vdrive, char *name, int length,
-                              char *pname)
-{
-    struct dirent *dirp;
-    char *p;
-    DIR *dp;
-    char p00id[8], p00name[17], p00dummy[2];
-    FILE *fd;
-
-    name[length] = '\0';
-
-    dp = opendir(fsdevice_get_path(vdrive->unit));
-    if (dp == NULL)
-        return NULL;
-
-    do {
-        dirp = readdir(dp);
-        if (dirp != NULL) {
-            strcpy(pname, fsdevice_get_path(vdrive->unit));
-            strcat(pname, FSDEV_DIR_SEP_STR);
-            strcat(pname, dirp->d_name);
-            p = pname;
-            if (p00_check_name(p) >= 0) {
-                fd = fopen(p, MODE_READ);
-                if (!fd)
-                    continue;
-                fread((char *) p00id, 8, 1, fd);
-                if (ferror(fd)) {
-                    fclose(fd);
-                    continue;
-                }
-                p00id[7] = '\0';
-                if (!strncmp(p00id, "C64File", 7)) {
-                    fread((char *) p00name, 16, 1, fd);
-                    if (ferror(fd)) {
-                        fclose(fd);
-                        continue;
-                    }
-                    p00name[16] = '\0';
-                    if (fsdevice_compare_wildcards(name, p00name) > 0) {
-                        fread((char *) p00dummy, 2, 1, fd);
-                        if (ferror(fd)) {
-                            fclose(fd);
-                            continue;
-                        }
-                        closedir(dp);
-                        return fd;
-                    }
-                }
-                fclose(fd);
-            }
-        }
-    }
-    while (dirp != NULL);
-    closedir(dp);
-    return NULL;
-}
-
-static int fsdevice_compare_wildcards(char *name, char *p00name)
-{
-    size_t i, len;
-
-    len = strlen(name);
-    if (len == 0)
-        return 0;
-
-    for (i = 0; i < len; i++) {
-        if (name[i] == '*')
-            return 1;
-        if (name[i] != '?' && name[i] != p00name[i])
-            return 0;
-    }
-    if (strlen(p00name) > len)
-        return 0;
-    return 1;
-}
-
-void fsdevice_compare_file_name(vdrive_t *vdrive, char *fsname2,
-                                char *fsname, int secondary)
-{
-    struct dirent *dirp;
-    DIR *dp;
-
-    dp = opendir(fsdevice_get_path(vdrive->unit));
-    do {
-        fileio_info_t *finfo = NULL;
-
-        dirp = readdir(dp);
-        if (dirp != NULL) {
-            if (fsdevice_compare_wildcards(fsname2, dirp->d_name) > 0) {
-                finfo = fileio_open(dirp->d_name,
-                                    fsdevice_get_path(vdrive->unit),
-                                    FILEIO_FORMAT_P00 | FILEIO_FORMAT_RAW,
-                                    FILEIO_COMMAND_READ
-                                    | FILEIO_COMMAND_FSNAME,
-                                    FILEIO_TYPE_PRG);
-                if (finfo == NULL)
-                    continue;
-
-                if (finfo->format == FILEIO_FORMAT_RAW) {
-                    strcpy(fsname, fsdevice_get_path(vdrive->unit));
-                    strcat(fsname, FSDEV_DIR_SEP_STR);
-                    strcat(fsname, dirp->d_name);
-                    closedir(dp);
-                    return;
-                }
-            }
-        }
-        if (finfo != NULL)
-            fileio_close(finfo);
-    } while (dirp != NULL);
-    closedir(dp);
-    return;
-}
-
-static int fsdevice_reduce_filename_p00(char *filename, int len)
-{
-    int i, j;
-
-    for (i = len - 1; i >= 0; i--) {
-        if (filename[i] == '_')
-            if (fsdevice_eliminate_char_p00(filename, i) <= 8)
-                return 8;
-        }
-
-    for (i = 0; i < len; i++) {
-        if (strchr("AEIOU", filename[i]) != NULL)
-            break;
-    }
-
-    for (j = len - 1; j >= i; j--) {
-        if (strchr("AEIOU", filename[j]) != NULL)
-            if (fsdevice_eliminate_char_p00(filename, j) <= 8)
-                return 8;
-    }
-
-    for (i = len - 1; i >= 0; i--) {
-        if (isalpha((int) filename[i]))
-            if (fsdevice_eliminate_char_p00(filename, i) <= 8)
-                return 8;
-    }
-
-    for (i = len - 1; i >= 0; i--)
-        if (fsdevice_eliminate_char_p00(filename, i) <= 8)
-            return 8;
-
-    return 1;
-}
-
-static size_t fsdevice_eliminate_char_p00(char *filename, int pos)
-{
-    memcpy(&filename[pos], &filename[pos+1], 16 - pos);
-    return strlen(filename);
-}
-
-int fsdevice_evaluate_name_p00(char *name, int length, char *filename)
-{
-    int i, j;
-
-    memset(filename, 0, 17);
-
-    for (i = 0, j = 0; i < length; i++) {
-        switch (name[i]) {
-          case ' ':
-          case '-':
-            filename[j++] = '_';
-            break;
-          default:
-            if (islower((int) name[i])) {
-                filename[j++] = toupper(name[i]);
-                break;
-            }
-            if (isalnum((int) name[i])) {
-                filename[j++] = name[i];
-                break;
-            }
-        }
-    }
-    if (j == 0) {
-        strcpy(filename, "_");
-        j++;
-    }
-    return ((j > 8) ? fsdevice_reduce_filename_p00(filename, j) : j);
 }
 
 int fsdevice_attach(unsigned int device, const char *name)
