5a04dd78a8147bf6014ee64a65aedfe85a1c8691
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:34:33 +0000

    This commit was generated by cvs2svn to compensate for changes in r6337, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@6338 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/vdrive/vdrive.h
+++ vice/src/vdrive/vdrive.h
@@ -254,5 +254,12 @@ extern int vdrive_parse_name(const char *name, unsigned int length,
 extern void vdrive_close_all_channels(vdrive_t *vdrive);
 extern int vdrive_calculate_disk_half(unsigned int type);
 extern int vdrive_get_max_sectors(unsigned int type, unsigned int track);
+
+extern vdrive_t *vdrive_internal_open_disk_image(const char *name);
+extern void vdrive_internal_close_disk_image(vdrive_t *vdrive);
+extern int vdrive_internal_create_format_disk_image(const char *filename,
+                                                    const char *diskname,
+                                                    int type);
+
 #endif
 
