977f4147e86eaf54b418dae728f9b7b7587e0db8
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 14:55:44 +0000

    This commit was generated by cvs2svn to compensate for changes in r1699, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@1700 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/c64/c64iec.c
@@ -0,0 +1,137 @@
+/*
+ * c64iec.c - IEC bus handling for the C64.
+ *
+ * Written by
+ *  Daniel Sladic (sladic@eecg.toronto.edu)
+ *  Andreas Boose (boose@unixserv.rz.fh-hannover.de)
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *  André Fachat (fachat@physik.tu-chemnitz.de)
+ *  Teemu Rantanen (tvr@cs.hut.fi)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+
+#include "vice.h"
+#include "resources.h"
+#include "viad.h"
+#include "types.h"
+#include "iecdrive.h"
+
+static BYTE drive_bus, drive_data, cpu_bus; /* FIXME: ugly name `drive_data'. */
+
+/* This is the IEC line status as seen by the CIA and VIA ports.  */
+static BYTE drive_port, cpu_port;
+
+static BYTE parallel_cable_cpu_value = 0xff;
+static BYTE parallel_cable_drive_value = 0xff;
+
+inline static void update_ports(void)
+{
+    cpu_port = cpu_bus & drive_bus;
+    drive_port = (((cpu_port >> 4) & 0x4)
+		  | (cpu_port >> 7)
+		  | ((cpu_bus << 3) & 0x80));
+}
+
+void iec_drive_write(BYTE data)
+{
+    if (!app_resources.true1541)
+	return;
+
+    drive_bus = (((data << 3) & 0x40)
+		 | ((data << 6) & ((~data ^ cpu_bus) << 3) & 0x80));
+    drive_data = data;
+    update_ports();
+}
+
+BYTE iec_drive_read(void)
+{
+    if (!app_resources.true1541)
+	return 0;
+
+    return drive_port;
+}
+
+/* The C64 has all bus lines in one I/O byte in a CIA.  If this byte is read or
+   modified, these routines are called. */
+
+void iec_cpu_write(BYTE data)
+{
+    if (!app_resources.true1541)
+	return;
+
+    true1541_cpu_execute();
+
+    cpu_bus = (((data << 2) & 0x80)
+	       | ((data << 2) & 0x40)
+	       | ((data << 1) & 0x10));
+
+    /* FIXME: this is slow, we should avoid doing it when not necessary.  */
+
+    set_atn(!(cpu_bus & 0x10));
+
+    drive_bus = (((drive_data << 3) & 0x40)
+		 | ((drive_data << 6)
+		    & ((~drive_data ^ cpu_bus) << 3)
+		    & 0x80));
+
+    update_ports();
+}
+
+BYTE iec_cpu_read(void)
+{
+    if (!app_resources.true1541)
+	return 0;
+
+    true1541_cpu_execute();
+    return cpu_port;
+}
+
+
+void parallel_cable_cpu_write(BYTE data, int handshake)
+{
+    true1541_cpu_execute();
+    if (handshake)
+	viaD1_signal(VIA_SIG_CB1, VIA_SIG_FALL);
+    parallel_cable_cpu_value = data;
+}
+
+BYTE parallel_cable_cpu_read(void)
+{
+    true1541_cpu_execute();
+    viaD1_signal(VIA_SIG_CB1, VIA_SIG_FALL);
+    return parallel_cable_cpu_value & parallel_cable_drive_value;
+}
+
+void parallel_cable_drive_write(BYTE data, int handshake)
+{
+    if (handshake)
+	cia2_set_flag();
+    parallel_cable_drive_value = data;
+}
+
+BYTE parallel_cable_drive_read(int handshake)
+{
+    if (handshake)
+	cia2_set_flag();
+    return parallel_cable_cpu_value & parallel_cable_drive_value;
+}
+
