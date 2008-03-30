43a710f9316881b84cebf8f8cb9043ee896699ae
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:04:10 +0000

    This commit was generated by cvs2svn to compensate for changes in r9891, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@9892 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/vsync.h
+++ vice/src/vsync.h
@@ -27,13 +27,15 @@
 #ifndef _VSYNC_H
 #define _VSYNC_H
 
+struct video_canvas_s;
+
 extern void vsync_suspend_speed_eval(void);
 extern void vsync_sync_reset(void);
 extern int vsync_resources_init(void);
 extern int vsync_cmdline_options_init(void);
 extern void vsync_init(void (*hook)(void));
 extern void vsync_set_machine_parameter(double refresh_rate, long cycles);
-extern int vsync_do_vsync(int been_skipped);
+extern int vsync_do_vsync(struct video_canvas_s *c, int been_skipped);
 extern int vsync_disable_timer(void);
 
 #endif
