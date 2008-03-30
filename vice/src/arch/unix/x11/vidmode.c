5141fab081e1dd00290ae94b2d995f0a2479c9ab
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:11:19 +0000

    This commit was generated by cvs2svn to compensate for changes in r10749, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@10750 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/unix/x11/vidmode.c
+++ vice/src/arch/unix/x11/vidmode.c
@@ -24,7 +24,7 @@
  *
  */
 
-/* 
+/*
 */
 #define FS_VIDMODE_DEBUG
 
@@ -35,15 +35,15 @@
 #include <X11/extensions/xf86vmode.h>
 #include <string.h>
 
-#include "types.h"
-#include "vidmode.h"
 #include "log.h"
-#include "resources.h"
-#include "uisettings.h"
-#include "uimenu.h"
-#include "utils.h"
 #include "fullscreen-common.h"
+#include "resources.h"
+#include "types.h"
+#include "uimenu.h"
+#include "uisettings.h"
+#include "utils.h"
 #include "videoarch.h"
+#include "vidmode.h"
 #include "x11ui.h"
 #include "raster/raster.h"
 
@@ -54,8 +54,9 @@ int vm_selected_videomode = -1;
 int vm_is_enabled = 0;
 
 /* ---------------------------------------------------------------*/
-static log_t vm_log = LOG_ERR;
-static int vm_mode_count, vm_index;
+static log_t vidmode_log = LOG_ERR;
+static int vm_mode_count;
+static unsigned int vm_index;
 static XF86VidModeModeInfo **vm_modes;
 static int vm_available = 0;
 static Display *vm_display;
@@ -63,71 +64,63 @@ static int vm_screen;
 static int saved_h, saved_w;
 
 typedef struct {
-  int modeindex;
-  char name[30];
+    int modeindex;
+    char *name;
 } vm_bestvideomode_t;
 
-static vm_bestvideomode_t vm_bestmodes[10];
+static vm_bestvideomode_t *vm_bestmodes = NULL;
 
 
-static int vidmode_available_modes(void)
+static unsigned int vidmode_available_modes(void)
 {
 #ifdef FS_VIDMODE_DEBUG
-    int i;
+    unsigned int i;
     for (i = 0; i < vm_index; i++)
-	log_message(vm_log, "found mode: %s", vm_bestmodes[i].name);
-#endif    
+        log_message(vidmode_log, "found mode: %s", vm_bestmodes[i].name);
+#endif
     return vm_index;
 }
 
-static char *
-vidmode_mode_name(int i)
+static char *vidmode_mode_name(unsigned int i)
 {
     return vm_bestmodes[i].name;
 }
 
 /* ---------------------------------------------------------------*/
-int
-vidmode_init(Display *display, int screen)
+int vidmode_init(Display *display, int screen)
 {
-    int hz, i;
-    
-    if (vm_log == LOG_ERR)
-    {
-	vm_log = log_open("VidMode");
-    }
-    
-    if (!XF86VidModeGetAllModeLines(display, screen, &vm_mode_count, 
-				    &vm_modes)) 
-    {
-	log_error(vm_log, _("Error getting video mode information - disabling vidmode extension."));
-	vm_available = 0;
-	return 1;
+    unsigned int hz, i;
+
+    vidmode_log = log_open("VidMode");
+
+    if (!XF86VidModeGetAllModeLines(display, screen, &vm_mode_count,
+        &vm_modes)) {
+        log_error(vidmode_log, _("Error getting video mode information - disabling vidmode extension."));
+        vm_available = 0;
+        return 1;
     }
 
-    for (i = 0; i < vm_mode_count; i++) 
-    {
-	if (vm_modes[i]->hdisplay <= 800 &&
-	    vm_modes[i]->hdisplay >= 320 &&
-	    vm_modes[i]->vdisplay <= 600 &&
-	    vm_modes[i]->vdisplay >= 200) 
-	{
-	    vm_bestmodes[vm_index].modeindex=i;
-	    hz = vm_modes[i]->dotclock * 1000 /
-		(vm_modes[i]->vtotal * vm_modes[i]->htotal);
-	    snprintf(vm_bestmodes[vm_index].name, 17,
-		     " %ix%i-%iHz",
-		     vm_modes[i]->hdisplay,
-		     vm_modes[i]->vdisplay,
-		     hz);
-	    if (++vm_index > 9)
-		break;
-	}
+    for (i = 0; i < vm_mode_count; i++) {
+        if (vm_modes[i]->hdisplay <= 800 &&
+            vm_modes[i]->hdisplay >= 320 &&
+            vm_modes[i]->vdisplay <= 600 &&
+            vm_modes[i]->vdisplay >= 200) {
+            vm_bestmodes = (vm_bestvideomode_t *)xrealloc(vm_bestmodes,
+                           (vm_index + 1) * sizeof(vm_bestvideomode_t));
+            vm_bestmodes[vm_index].modeindex = i;
+            hz = vm_modes[i]->dotclock * 1000 /
+                 (vm_modes[i]->vtotal * vm_modes[i]->htotal);
+            vm_bestmodes[vm_index].name = xmsprintf(" %ix%i-%iHz",
+                                                    vm_modes[i]->hdisplay,
+                                                    vm_modes[i]->vdisplay, hz);
+            if (++vm_index > 29)
+                break;
+        }
     }
 
     if (vm_mode_count == 0)
-	return 0;
-    resources_set_value("SelectedFullscreenMode", (resource_value_t) 0);
+        return 0;
+    resources_set_value("SelectedFullscreenMode", (resource_value_t)0);
     vm_selected_videomode = 0;
     vm_display = display;
     vm_screen = screen;
@@ -136,127 +129,105 @@ vidmode_init(Display *display, int screen)
     return 0;
 }
 
-int
-vidmode_set_bestmode(resource_value_t v, void *param)
+int vidmode_set_bestmode(resource_value_t v, void *param)
 {
-    int i = (int) v;
+    int i = (int)v;
 
     if (i < 0 || vm_available == 0)
-	return 0;
-    
-    log_message(vm_log, "selected mode: %s", vm_bestmodes[i].name);
+        return 0;
+
+    log_message(vidmode_log, "selected mode: %s", vm_bestmodes[i].name);
     vm_selected_videomode = i;
-    
+
     return 0;
 }
 
-int
-vidmode_set_mode(resource_value_t v, void *param)
+int vidmode_set_mode(resource_value_t v, void *param)
 {
-    if (v)
-    {
-	int x = 0, y = 0;
-	int status_w, status_h;
-	extern ui_window_t status_bar;
-	
-	XF86VidModeModeInfo *vm;
-	
-	log_message(vm_log, "Enabling Vidmode with%s", 
-		    vm_bestmodes[vm_selected_videomode].name);
-	vm = vm_modes[vm_bestmodes[vm_selected_videomode].modeindex];
+    if (v) {
+        int x = 0, y = 0;
+        int status_w, status_h;
+        extern ui_window_t status_bar;
 
-	vm_is_enabled = 1;
-	saved_w = fs_cached_raster->viewport.width;
-	saved_h = fs_cached_raster->viewport.height;
+        XF86VidModeModeInfo *vm;
 
-	x11ui_get_widget_size(status_bar, &status_w, &status_h);
-	log_message(vm_log, "Status bar: %dx%d", status_w, status_h);
-	raster_resize_viewport(fs_cached_raster, 
-			       vm->hdisplay + 10, vm->vdisplay-status_h + 10);
-	x11ui_move_canvas_window(((video_canvas_t *)fs_cached_raster->canvas)->emuwindow,
-			      0, 0);
-	ui_dispatch_events();
-	x11ui_canvas_position(((video_canvas_t *)fs_cached_raster->canvas)->emuwindow,
-			   &x, &y);
-	
-	XF86VidModeSwitchToMode(vm_display, vm_screen, vm);
-	XF86VidModeSetViewPort(vm_display, vm_screen, x + 5, y + 5);
-	XWarpPointer(vm_display,
-		     None, DefaultRootWindow(vm_display), 
-		     0, 0, vm->hdisplay, vm->vdisplay,
-		     x + vm->hdisplay / 2,
-		     y + vm->vdisplay / 2);
-		     
-    }
-    else
-    {
-	if (!vm_is_enabled)
-	    return 0;
-	vm_is_enabled = 0;
-	log_message(vm_log, "Disabling Vidmode");
-	XF86VidModeSwitchToMode(vm_display, vm_screen,
-				vm_modes[0]);
-	raster_resize_viewport(fs_cached_raster, saved_w, saved_h);
-	
+        log_message(vidmode_log, "Enabling Vidmode with%s",
+                    vm_bestmodes[vm_selected_videomode].name);
+        vm = vm_modes[vm_bestmodes[vm_selected_videomode].modeindex];
+
+        vm_is_enabled = 1;
+        saved_w = fs_cached_raster->canvas->draw_buffer->canvas_width;
+        saved_h = fs_cached_raster->canvas->draw_buffer->canvas_height;
+
+        x11ui_get_widget_size(status_bar, &status_w, &status_h);
+        log_message(vidmode_log, "Status bar: %dx%d", status_w, status_h);
+
+        fs_cached_raster->canvas->draw_buffer->canvas_width
+            = vm->hdisplay + 10;
+        fs_cached_raster->canvas->draw_buffer->canvas_height
+            = vm->vdisplay-status_h + 10;
+        video_viewport_resize(fs_cached_raster->canvas);
+
+        x11ui_move_canvas_window(fs_cached_raster->canvas->emuwindow, 0, 0);
+        ui_dispatch_events();
+        x11ui_canvas_position(fs_cached_raster->canvas->emuwindow, &x, &y);
+
+        XF86VidModeSwitchToMode(vm_display, vm_screen, vm);
+        XF86VidModeSetViewPort(vm_display, vm_screen, x + 5, y + 5);
+        XWarpPointer(vm_display,
+                     None, DefaultRootWindow(vm_display),
+                     0, 0, vm->hdisplay, vm->vdisplay,
+                     x + vm->hdisplay / 2,
+                     y + vm->vdisplay / 2);
+
+    } else {
+        if (!vm_is_enabled)
+            return 0;
+        vm_is_enabled = 0;
+        log_message(vidmode_log, "Disabling Vidmode");
+        XF86VidModeSwitchToMode(vm_display, vm_screen, vm_modes[0]);
+
+        fs_cached_raster->canvas->draw_buffer->canvas_width = saved_w;
+        fs_cached_raster->canvas->draw_buffer->canvas_height = saved_h;
+        video_viewport_resize(fs_cached_raster->canvas);
+
     }
     return 0;
 }
 
-int
-vidmode_available(void)
+int vidmode_available(void)
 {
     return vm_available;
 }
 
-void
-vidmode_create_menus(void)
+void vidmode_create_menus(void)
 {
-    int i, amodes, index = -1;
-    char buf[50];
-    char *modename;
+    unsigned int i, amodes;
     ui_menu_entry_t *resolutions_submenu;
-
-    buf[0] = '*';
-    buf[50] = '\0';
 
     amodes = vidmode_available_modes();
 
-    resolutions_submenu = (ui_menu_entry_t*)xmalloc(sizeof(ui_menu_entry_t) *
-                          (size_t)(amodes + 1));
+    resolutions_submenu = (ui_menu_entry_t *)xcalloc((size_t)(amodes + 1),
+                          sizeof(ui_menu_entry_t));
 
-    for(i = 0; i < amodes ; i++) {
-        modename = vidmode_mode_name(i);
-
-        buf[1] = '\0';
-        strncat(buf + 1, modename, 48);
+    for (i = 0; i < amodes ; i++) {
         resolutions_submenu[i].string =
-            (ui_callback_data_t) stralloc(buf);
+            (ui_callback_data_t)xmsprintf("*%s", vidmode_mode_name(i));
         resolutions_submenu[i].callback =
-            (ui_callback_t) radio_SelectedFullscreenMode;
-        resolutions_submenu[i].callback_data =
-            (ui_callback_data_t) i;
-        resolutions_submenu[i].sub_menu = NULL;
-        resolutions_submenu[i].hotkey_keysym = 0;
-        resolutions_submenu[i].hotkey_modifier =
-            (ui_hotkey_modifier_t) 0;
+            (ui_callback_t)radio_SelectedFullscreenMode;
+        resolutions_submenu[i].callback_data = (ui_callback_data_t)i;
     }
 
-    resolutions_submenu[i].string = (ui_callback_data_t) NULL;
-    i = 0;
-    while (ui_fullscreen_settings_submenu[i].string)
-    {
-	if (strncmp(ui_fullscreen_settings_submenu[i].string, 
-		    "VidMode", 7) == 0)
-	{
-	    index = i;
-	    break;
-	}
-	i++;
+    for (i = 0; ui_fullscreen_settings_submenu[i].string; i++) {
+        if (strncmp(ui_fullscreen_settings_submenu[i].string,
+            "VidMode", 7) == 0) {
+            if (amodes > 0)
+                ui_fullscreen_settings_submenu[i].sub_menu
+                    = resolutions_submenu;
+            break;
+        }
     }
-    
-    if (amodes > 0 && index >= 0)
-        ui_fullscreen_settings_submenu[index].sub_menu = resolutions_submenu;
-
 }
 
-#endif /* USE_XF86_VIDMODE_EXT */
+#endif
+
