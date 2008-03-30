3cc53710ac2f3b8eb6e279a6e3c02cd87f46535a
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:46:47 +0000

    This commit was generated by cvs2svn to compensate for changes in r7805, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@7806 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/autostart.c
+++ vice/src/autostart.c
@@ -85,9 +85,9 @@ static enum {
 /* Log descriptor.  */
 static log_t autostart_log = LOG_ERR;
 
-/* Flag: was true 1541 emulation turned on when we started booting the disk
+/* Flag: was true drive emulation turned on when we started booting the disk
    image?  */
-static int orig_true1541_state = 0;
+static int orig_drive_true_emulation_state = 0;
 
 /* PETSCII name of the program to load. NULL if default */
 static BYTE *autostart_program_name = NULL;
@@ -95,8 +95,8 @@ static BYTE *autostart_program_name = NULL;
 /* Minimum number of cycles before we feed BASIC with commands.  */
 static CLOCK min_cycles;
 
-/* Flag: Do we want to switch true 1541 emulation on/off during autostart?  */
-static int handle_true1541;
+/* Flag: Do we want to switch true drive emulation on/off during autostart?  */
+static int handle_drive_true_emulation;
 
 /* Flag: autostart is initialized.  */
 static int autostart_enabled = 0;
@@ -135,13 +135,13 @@ static enum { YES, NO, NOT_YET } check(const char *s)
     return YES;
 }
 
-static void set_true1541_mode(int on)
+static void set_true_drive_emulation_mode(int on)
 {
     resources_set_value("DriveTrueEmulation", (resource_value_t) on);
     ui_update_menus();
 }
 
-static int get_true1541_state(void)
+static int get_true_drive_emulation_state(void)
 {
     int value;
 
@@ -165,7 +165,7 @@ static void load_snapshot_trap(ADDRESS unused_addr, void *unused_data)
 /* ------------------------------------------------------------------------- */
 
 /* Initialize autostart.  */
-int autostart_init(CLOCK _min_cycles, int _handle_true1541,
+int autostart_init(CLOCK _min_cycles, int _handle_drive_true_emulation,
                    int _blnsw, int _pnt, int _pntr, int _lnmx)
 {
     blnsw = _blnsw;
@@ -180,7 +180,7 @@ int autostart_init(CLOCK _min_cycles, int _handle_true1541,
     }
 
     min_cycles = _min_cycles;
-    handle_true1541 = _handle_true1541;
+    handle_drive_true_emulation = _handle_drive_true_emulation;
 
     if (_min_cycles)
 	autostart_enabled = 1;
@@ -206,15 +206,15 @@ void autostart_disable(void)
    is reached.  */
 static void disk_eof_callback(void)
 {
-    if (handle_true1541) {
+    if (handle_drive_true_emulation) {
         BYTE id[2];
 
-        if (orig_true1541_state) {
+        if (orig_drive_true_emulation_state) {
             log_message(autostart_log, "Turning true drive emulation on.");
             vdrive_bam_get_disk_id(8, id);
         }
-        set_true1541_mode(orig_true1541_state);
-        if (orig_true1541_state) {
+        set_true_drive_emulation_mode(orig_drive_true_emulation_state);
+        if (orig_drive_true_emulation_state) {
             drive_set_disk_id_memory(0, id);
         }
     }
@@ -234,7 +234,7 @@ static void disk_attention_callback(void)
 
     serial_set_attention_callback(NULL);
 
-    /* Next step is waiting for end of loading, to turn true 1541 emulation
+    /* Next step is waiting for end of loading, to turn true drive emulation
        on.  */
     serial_set_eof_callback(disk_eof_callback);
 }
@@ -255,12 +255,12 @@ void autostart_advance(void)
           case YES:
             log_message(autostart_log, "Loading file.");
             if (autostart_program_name) {
-                tmp = concat("LOAD\"", autostart_program_name,
-                             "\"\r", NULL);
+                tmp = concat("LOAD\"", autostart_program_name, "\"\r", NULL);
                 kbd_buf_feed(tmp);
                 free(tmp);
-            } else
+            } else {
                 kbd_buf_feed("LOAD\r");
+            }
             autostartmode = AUTOSTART_LOADINGTAPE;
             deallocate_program_name();
             datasette_control(DATASETTE_CONTROL_START);
@@ -297,24 +297,24 @@ void autostart_advance(void)
                                 autostart_program_name);
                 else
                     log_message(autostart_log, "Loading program '*'");
-                orig_true1541_state = get_true1541_state();
-                if (handle_true1541) {
+                orig_drive_true_emulation_state = get_true_drive_emulation_state();
+                if (handle_drive_true_emulation) {
                     resources_get_value("VirtualDevices",
                                         (resource_value_t *) &traps);
                     if (traps) {
-                        if (orig_true1541_state)
+                        if (orig_drive_true_emulation_state)
                             log_message(autostart_log,
                                         "Switching true drive emulation off.");
-                        set_true1541_mode(0);
+                        set_true_drive_emulation_mode(0);
                     } else {
-                        if (!orig_true1541_state)
+                        if (!orig_drive_true_emulation_state)
                             log_message(autostart_log,
                                         "Switching true drive emulation on.");
-                        set_true1541_mode(1);
+                        set_true_drive_emulation_mode(1);
                     }
-                } else
+                } else {
                     traps = 1;
-
+                }
                 if (autostart_program_name)
                     tmp = xmsprintf("LOAD\"%s\",8,1\r", autostart_program_name);
                 else
@@ -343,9 +343,9 @@ void autostart_advance(void)
       case AUTOSTART_HASSNAPSHOT:
         switch (check("READY.")) {
           case YES:
-              log_message(autostart_log, "Restoring snapshot.");
-              maincpu_trigger_trap(load_snapshot_trap,(void*)0);
-              autostartmode = AUTOSTART_DONE;
+            log_message(autostart_log, "Restoring snapshot.");
+            maincpu_trigger_trap(load_snapshot_trap,(void*)0);
+            autostartmode = AUTOSTART_DONE;
             break;
           case NO:
             autostart_disable();
@@ -359,10 +359,10 @@ void autostart_advance(void)
         return;
     }
 
-    if (autostartmode == AUTOSTART_ERROR && handle_true1541) {
+    if (autostartmode == AUTOSTART_ERROR && handle_drive_true_emulation) {
         log_message(autostart_log, "Now turning true drive emulation %s.",
-                    orig_true1541_state ? "on" : "off");
-	set_true1541_mode(orig_true1541_state);
+                    orig_drive_true_emulation_state ? "on" : "off");
+	set_true_drive_emulation_mode(orig_drive_true_emulation_state);
     }
 }
 
@@ -374,7 +374,8 @@ static void reboot_for_autostart(const char *program_name, int mode)
     if (!autostart_enabled)
 	return;
 
-    log_message(autostart_log, "Resetting the machine to autostart '%s'", program_name?program_name:"*");
+    log_message(autostart_log, "Resetting the machine to autostart '%s'",
+                program_name ? program_name : "*");
     mem_powerup();
     autostart_ignore_reset = 1;
     deallocate_program_name();
@@ -382,8 +383,8 @@ static void reboot_for_autostart(const char *program_name, int mode)
 	autostart_program_name = (BYTE *)stralloc(program_name);
     maincpu_trigger_reset();
     /* The autostartmode must be set AFTER the shutdown to make the autostart
-     threadsafe for OS/2 */
-    autostartmode=mode;
+       threadsafe for OS/2 */
+    autostartmode = mode;
 }
 
 /* ------------------------------------------------------------------------- */
@@ -408,8 +409,9 @@ int autostart_snapshot(const char *file_name, const char *program_name)
     snapshot_close(snap);
 
     /*autostart_program_name = (BYTE *)stralloc(file_name);
-    maincpu_trigger_trap(load_snapshot_trap,(void*)0);*/
-    reboot_for_autostart(file_name, AUTOSTART_HASSNAPSHOT);	/* use for snapshot */
+    maincpu_trigger_trap(load_snapshot_trap, (void*)0);*/
+    /* use for snapshot */
+    reboot_for_autostart(file_name, AUTOSTART_HASSNAPSHOT);
 
     return 0;
 }
@@ -442,10 +444,33 @@ int autostart_tape(const char *file_name, const char *program_name,
 
     autostartmode = AUTOSTART_ERROR;
     deallocate_program_name();
+
     if (name)
         free(name);
 
     return -1;
+}
+
+/* Cope with 0xa0 padded file names.  */
+static void autostart_disk_cook_name(char **name)
+{
+    unsigned int pos;
+
+    pos = 0;
+
+    while((*name)[pos] != '\0') {
+        if (((unsigned char)((*name)[pos])) == 0xa0) {
+            char *ptr;
+
+            ptr = xmalloc(pos + 1);
+            memcpy(ptr, *name, pos);
+            ptr[pos] = '\0';
+            free(*name);
+            *name = ptr;
+            break;
+        }
+        pos++;
+    }
 }
 
 /* Autostart disk image `file_name'.  */
@@ -457,23 +482,25 @@ int autostart_disk(const char *file_name, const char *program_name,
     if (!file_name || !autostart_enabled)
 	return -1;
 
-    /* get program name first to avoid more than one file handle open on image */
+    /* Get program name first to avoid more than one file handle open on
+       image.  */
     if (!program_name && program_number > 0)
         name = image_contents_disk_filename_by_number(file_name,
                                                       program_number);
     else
-        name = stralloc(program_name?program_name:"*");
+        name = stralloc(program_name ? program_name : "*");
 
-    if (name)
-        if (!(file_system_attach_disk(8, file_name) < 0))
-        {
+    if (name) {
+        autostart_disk_cook_name(&name);
+        if (!(file_system_attach_disk(8, file_name) < 0)) {
             log_message(autostart_log,
                         "Attached file `%s' as a tape image.", file_name);
             reboot_for_autostart(name, AUTOSTART_HASDISK);
-            free (name);
+            free(name);
 
             return 0;
         }
+    }
 
     autostartmode = AUTOSTART_ERROR;
     deallocate_program_name();
@@ -508,17 +535,16 @@ int autostart_prg(const char *file_name)
     }
 
     p00_type = p00_check_name(file_name);
-    if (p00_type >= 0)
-    {
+    if (p00_type >= 0) {
         if (p00_type == FT_PRG &&
-            p00_read_header(f, (BYTE *)p00_header_file_name, NULL))
+            p00_read_header(f, (BYTE *)p00_header_file_name, NULL)) {
             p00_type = -1;
-        else
-            if (p00_type != FT_PRG)
-            {
+        } else {
+            if (p00_type != FT_PRG) {
                 fclose(f);
                 return -1;
             }
+        }
     }
 
     /* Extract the directory path to allow FS-based drive emulation to
@@ -552,7 +578,7 @@ int autostart_prg(const char *file_name)
 
     /* Setup FS-based drive emulation.  */
     fsdevice_set_directory(directory ? directory : ".", 8);
-    set_true1541_mode(0);
+    set_true_drive_emulation_mode(0);
     resources_set_value("VirtualDevices", (resource_value_t) 1);
     resources_set_value("FSDevice8ConvertP00", (resource_value_t) 1);
     file_system_detach_disk(8);
@@ -620,10 +646,10 @@ int autostart_device(int num)
 	return -1;
 
     switch (num) {
-    case 8:
+      case 8:
         reboot_for_autostart(NULL, AUTOSTART_HASDISK);
         return 0;
-    case 1:
+      case 1:
         reboot_for_autostart(NULL, AUTOSTART_HASTAPE);
         return 0;
     }
@@ -645,3 +671,4 @@ void autostart_reset(void)
     }
     autostart_ignore_reset = 0;
 }
+
