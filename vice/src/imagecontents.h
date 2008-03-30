ba2eebda06dd1a55199b7cc0556b9d3d06db03aa
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:10:29 +0000

    This commit was generated by cvs2svn to compensate for changes in r3449, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@3450 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/imagecontents.h
@@ -0,0 +1,59 @@
+/*
+ * imagecontents.h - Extract the directory from disk/tape images.
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _IMAGE_CONTENTS_H
+#define _IMAGE_CONTENTS_H
+
+#define IMAGE_CONTENTS_NAME_LEN       16
+#define IMAGE_CONTENTS_ID_LEN         5
+#define IMAGE_CONTENTS_FILE_NAME_LEN  16
+#define IMAGE_CONTENTS_TYPE_LEN       5
+
+struct _image_contents_file_list {
+    char name[IMAGE_CONTENTS_FILE_NAME_LEN + 1];
+    char type[IMAGE_CONTENTS_TYPE_LEN + 1];
+
+    unsigned int size;
+
+    struct _image_contents_file_list *prev, *next;
+};
+typedef struct _image_contents_file_list image_contents_file_list_t;
+
+struct _contents {
+    char name[IMAGE_CONTENTS_NAME_LEN + 1];
+    char id[IMAGE_CONTENTS_ID_LEN + 1];
+    int blocks_free;   /* -1: no free space.  */
+    image_contents_file_list_t *file_list;
+};
+typedef struct _contents image_contents_t;
+
+/* ------------------------------------------------------------------------- */
+
+image_contents_t *image_contents_read_disk(const char *file_name);
+image_contents_t *image_contents_read_tape(const char *file_name);
+void image_contents_free(image_contents_t *contents);
+
+#endif
