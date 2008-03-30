384cec47787e58d7e1357970aa2db61121cabc9d
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:30:33 +0000

    This commit was generated by cvs2svn to compensate for changes in r5857, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@5858 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/win32/uidatasette.c
@@ -0,0 +1,127 @@
+/*
+ * uidatasette.c - Implementation of the datasette settings dialog box.
+ *
+ * Written by
+ *  Andreas Matthies <andreas.matthies@arcormail.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <windows.h>
+
+#include "datasette.h"
+#include "res.h"
+#include "resources.h"
+#include "ui.h"
+#include "uidatasette.h"
+#include "winmain.h"
+
+
+static int ui_datasette_zero_gap_delay[]={
+    1000,
+    2000,
+    5000,
+    10000,
+    20000,
+    50000,
+    100000
+};
+
+
+
+static void init_datasette_dialog(HWND hwnd)
+{
+HWND    snd_hwnd;
+int     res_value;
+int     res_value_loop;
+int     active_value;
+
+    resources_get_value("DatasetteResetWithCPU",
+        (resource_value_t *)&res_value);
+    CheckDlgButton(hwnd, IDC_DATASETTE_RESET_WITH_CPU, res_value
+                   ? BST_CHECKED : BST_UNCHECKED);
+    
+    snd_hwnd=GetDlgItem(hwnd,IDC_DATASETTE_ZERO_GAP_DELAY);
+    for (res_value_loop = 0; res_value_loop < 7; res_value_loop++) {
+        char st[10];
+        itoa(ui_datasette_zero_gap_delay[res_value_loop],st,10);
+        SendMessage(snd_hwnd,CB_ADDSTRING,0,(LPARAM)st);
+    }
+    resources_get_value("DatasetteZeroGapDelay",
+        (resource_value_t *)&res_value);
+    active_value = 4; /* default */
+    for (res_value_loop = 0; res_value_loop < 7; res_value_loop++) {
+        if (ui_datasette_zero_gap_delay[res_value_loop] == res_value)
+            active_value = res_value_loop;
+    }
+    SendMessage(snd_hwnd,CB_SETCURSEL,(WPARAM)active_value,0);
+
+    snd_hwnd=GetDlgItem(hwnd,IDC_DATASETTE_SPEED_TUNING);
+    for (res_value_loop = 0; res_value_loop < 8; res_value_loop++) {
+        char st[10];
+        itoa (res_value_loop,st,10);
+        SendMessage(snd_hwnd,CB_ADDSTRING,0,(LPARAM)st);
+    }
+    resources_get_value("DatasetteSpeedTuning",(resource_value_t *)&res_value);
+    SendMessage(snd_hwnd,CB_SETCURSEL,(WPARAM)res_value,0);
+}
+
+
+
+static BOOL CALLBACK dialog_proc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+    int command;
+
+    switch (msg) {
+        case WM_COMMAND:
+            command=LOWORD(wparam);
+            switch (command) {
+                case IDOK:
+                    resources_set_value("DatasetteResetWithCPU", (resource_value_t)
+                        (IsDlgButtonChecked
+                            (hwnd,IDC_DATASETTE_RESET_WITH_CPU)==BST_CHECKED ?
+                            1 : 0 ));
+                    resources_set_value("DatasetteSpeedTuning",(resource_value_t)
+                        SendMessage(GetDlgItem(
+                        hwnd,IDC_DATASETTE_SPEED_TUNING),CB_GETCURSEL,0,0));
+                    resources_set_value("DatasetteZeroGapDelay",(resource_value_t)
+                        ui_datasette_zero_gap_delay[SendMessage(GetDlgItem(hwnd,IDC_DATASETTE_ZERO_GAP_DELAY),CB_GETCURSEL,0,0)]);
+                    EndDialog(hwnd,0);
+                    return TRUE;
+            }
+            return FALSE;
+        case WM_CLOSE:
+            EndDialog(hwnd,0);
+            return TRUE;
+        case WM_INITDIALOG:
+            init_datasette_dialog(hwnd);
+            return TRUE;
+    }
+    return FALSE;
+}
+
+
+void ui_datasette_settings_dialog(HWND hwnd)
+{
+    DialogBox(winmain_instance,(LPCTSTR)IDD_DATASETTE_SETTINGS_DIALOG,hwnd,dialog_proc);
+}
+
