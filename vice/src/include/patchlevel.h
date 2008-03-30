523f069ec6c3fbd3f2484cbf38e910ca4fb4b885
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 14:56:58 +0000

    This commit was generated by cvs2svn to compensate for changes in r1841, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@1842 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/include/patchlevel.h
+++ vice/src/include/patchlevel.h
@@ -4,7 +4,7 @@
  */
 
 /* Global VICE release version.  */
-#define VERSION		"0.14.2.14"
+#define VERSION		"0.14.2.15"
 
 /* Define this for unstable, untested versions for developers only.  */
 #define UNSTABLE
