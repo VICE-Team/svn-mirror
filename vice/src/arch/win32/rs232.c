d347a22e8405e7c997acabc9c049cd462339adb2
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:51:45 +0000

    This commit was generated by cvs2svn to compensate for changes in r15601, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@15602 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/rs232.c
+++ vice/src/arch/win32/rs232.c
@@ -73,11 +73,11 @@ static const resource_t resources[] = {
     { "RsDevice1", RES_STRING, (resource_value_t)"10.0.0.1:25232",
       (void *)&devfile[0], set_devfile, (void *)0 },
     { "RsDevice2", RES_STRING, (resource_value_t)"10.0.0.1:25232",
-      (void *)&devfile[1], set_devfile, (void *)0 },
+      (void *)&devfile[1], set_devfile, (void *)1 },
     { "RsDevice3", RES_STRING, (resource_value_t)"10.0.0.1:25232",
-      (void *)&devfile[2], set_devfile, (void *)0 },
+      (void *)&devfile[2], set_devfile, (void *)2 },
     { "RsDevice4", RES_STRING, (resource_value_t)"10.0.0.1:25232",
-      (void *)&devfile[3], set_devfile, (void *)0 },
+      (void *)&devfile[3], set_devfile, (void *)3 },
     { NULL }
 };
 
