20bad704492dd6eb97fe0729b994defa06bce529
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:50:12 +0000

    This commit was generated by cvs2svn to compensate for changes in r15415, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@15416 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/dma.c
+++ vice/src/dma.c
@@ -40,22 +40,29 @@ void dma_maincpu_steal_cycles(CLOCK start_clk, int num, CLOCK sub)
     CLOCK irq_sub = 0;
     CLOCK nmi_sub = 0;
 #endif
-
     interrupt_cpu_status_t *cs = maincpu_int_status;
+    CLOCK dma_start;
 
     if (num == 0)
         return;
+
+    dma_start = start_clk + sub;
 
     if (start_clk == cs->last_stolen_cycles_clk)
         cs->num_last_stolen_cycles += num;
     else
         cs->num_last_stolen_cycles = num;
 
-    /*log_debug("START %i NUM %i SUB %i MAIN %i",
-              start_clk, num, sub, maincpu_clk);*/
+    /*log_debug("START %i NUM %i SUB %i MAIN %i DMAST %i",
+              start_clk, num, sub, maincpu_clk, dma_start);*/
 
+#ifdef NEW_INTERUPT
+    cs->num_cycles_left[cs->num_dma_per_opcode] = maincpu_clk - dma_start;
+    cs->dma_start_clk[cs->num_dma_per_opcode] = dma_start;
+#else
     cs->num_cycles_left[cs->num_dma_per_opcode] = maincpu_clk - start_clk;
     cs->dma_start_clk[cs->num_dma_per_opcode] = start_clk;
+#endif
     (cs->num_dma_per_opcode)++;
 
 #ifdef DEBUG
@@ -64,12 +71,12 @@ void dma_maincpu_steal_cycles(CLOCK start_clk, int num, CLOCK sub)
 #endif
 
 #ifdef NEW_INTERUPT
-    if (cs->irq_clk >= start_clk && start_clk == (maincpu_clk - 1)
+    if (cs->irq_clk >= start_clk && dma_start == (maincpu_clk - 1)
         && cs->num_dma_per_opcode == 1) {
         /*log_debug("DECR");*/
         irq_sub = 1;
     }
-    if (cs->nmi_clk >= start_clk && start_clk == (maincpu_clk - 1)
+    if (cs->nmi_clk >= start_clk && dma_start == (maincpu_clk - 1)
         && cs->num_dma_per_opcode == 1) {
         /*log_debug("DECR");*/
         nmi_sub = 1;
@@ -77,19 +84,23 @@ void dma_maincpu_steal_cycles(CLOCK start_clk, int num, CLOCK sub)
 #endif
 
     maincpu_clk += num;
-    cs->last_stolen_cycles_clk = start_clk + num + sub;
 
+#ifdef NEW_INTERUPT
+    cs->last_stolen_cycles_clk = dma_start + num;
+#else
+    cs->last_stolen_cycles_clk = start_clk + num + sub;
+#endif
     /*log_debug("IRQCLK %i LASTSTOLEN %i",
               cs->irq_clk, cs->last_stolen_cycles_clk);*/
 
 #ifdef NEW_INTERUPT
-    if (cs->irq_clk > start_clk)
-        cs->irq_clk = cs->irq_clk + num - (cs->irq_clk - start_clk);
+    if (cs->irq_clk > dma_start)
+        cs->irq_clk = cs->last_stolen_cycles_clk;
     else
         cs->irq_clk += num;
 
-    if (cs->nmi_clk > start_clk)
-        cs->nmi_clk = cs->nmi_clk + num - (cs->nmi_clk - start_clk);
+    if (cs->nmi_clk > dma_start)
+        cs->nmi_clk = cs->last_stolen_cycles_clk;
     else
         cs->nmi_clk += num;
 
