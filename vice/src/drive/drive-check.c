33dff1f7c3ebb435c3d9cb73e1ec74068b0107a2
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:42:27 +0000

    This commit was generated by cvs2svn to compensate for changes in r14485, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@14486 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/drive/drive-check.c
+++ vice/src/drive/drive-check.c
@@ -32,24 +32,112 @@
 #include "machine-drive.h"
 
 
+static unsigned int drive_check_ieee(unsigned int type)
+{
+    switch (type) {
+      case DRIVE_TYPE_2031:
+      case DRIVE_TYPE_2040:
+      case DRIVE_TYPE_3040:
+      case DRIVE_TYPE_4040:
+      case DRIVE_TYPE_1001:
+      case DRIVE_TYPE_8050:
+      case DRIVE_TYPE_8250:
+        return 1;
+    }
+
+    return 0;
+}
+
+static unsigned int drive_check_iec(unsigned int type)
+{
+    switch (type) {
+      case DRIVE_TYPE_1541:
+      case DRIVE_TYPE_1541II:
+      case DRIVE_TYPE_1570:
+      case DRIVE_TYPE_1571:
+      case DRIVE_TYPE_1571CR:
+      case DRIVE_TYPE_1581:
+        return 1;
+    }
+
+    return 0;
+}
+
+unsigned int drive_check_old(unsigned int type)
+{
+    switch (type) {
+      case DRIVE_TYPE_2040:
+      case DRIVE_TYPE_3040:
+      case DRIVE_TYPE_4040:
+      case DRIVE_TYPE_1001:
+      case DRIVE_TYPE_8050:
+      case DRIVE_TYPE_8250:
+        return 1;
+    }
+
+    return 0;
+}
+
+unsigned int drive_check_dual(unsigned int type)
+{
+    switch (type) {
+      case DRIVE_TYPE_2040:
+      case DRIVE_TYPE_3040:
+      case DRIVE_TYPE_4040:
+      case DRIVE_TYPE_8050:
+      case DRIVE_TYPE_8250:
+        return 1;
+    }
+
+    return 0;
+}
+
+static unsigned int drive_check_tcbm(unsigned int type)
+{
+    switch (type) {
+      case DRIVE_TYPE_1551:
+        return 1;
+    }
+
+    return 0;
+}
+
+unsigned int drive_check_bus(unsigned int drive_type, unsigned int dnr,
+                             unsigned int bus_map)
+{
+    if (drive_type == DRIVE_TYPE_NONE)
+        return 1;
+
+    if (drive_check_ieee(drive_type) && (bus_map & IEC_BUS_IEEE))
+        return 1;
+
+    if (drive_check_iec(drive_type) && (bus_map & IEC_BUS_IEC))
+        return 1;
+
+    if (drive_check_tcbm(drive_type) && (bus_map & IEC_BUS_TCBM))
+        return 1;
+
+    return 0;
+}
+
 int drive_check_type(unsigned int drive_type, unsigned int dnr)
 {
-    if (!drive_match_bus(drive_type, dnr, iec_available_busses()))
+    if (!drive_check_bus(drive_type, dnr, iec_available_busses()))
         return 0;
 
-    if (DRIVE_IS_DUAL(drive_type)) {
+    if (drive_check_dual(drive_type)) {
         if (dnr > 0) {
             /* A second dual drive is not supported.  */
             return 0;
         } else {
-            if (drive_context[dnr]->drive->type != DRIVE_TYPE_NONE)
+            if (drive_context[1]->drive->type != DRIVE_TYPE_NONE)
                 /* Disable dual drive if second drive is enabled.  */
                 return 0;
         }
     }
 
     /* If the first drive is dual no second drive is supported at all.  */
-    if (DRIVE_IS_DUAL(drive_context[0]->drive->type) && dnr > 0)
+    if (drive_check_dual(drive_context[0]->drive->type) && dnr > 0)
         return 0;
 
     if (machine_drive_rom_check_loaded(drive_type) < 0)
