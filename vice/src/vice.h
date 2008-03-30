488ac6034f20f94df0960157b976722c06d41436
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:06:32 +0000

    This commit was generated by cvs2svn to compensate for changes in r10175, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@10176 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/vice.h
+++ vice/src/vice.h
@@ -81,7 +81,7 @@
 
 /* ------------------------------------------------------------------------- */
 /* Which OS is using the common keyboard routines?  */
-#if !defined(__MSDOS__) && !defined(WIN32) && !defined(__riscos) && !defined(__OS2__) && !defined(__BEOS__)
+#if !defined(WIN32) && !defined(__riscos) && !defined(__OS2__) && !defined(__BEOS__)
 #define COMMON_KBD
 #endif
 
