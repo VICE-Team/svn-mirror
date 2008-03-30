c4d09040e6cf6376a77bfde7e604f96c28321dfe
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:54:41 +0000

    This commit was generated by cvs2svn to compensate for changes in r8753, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@8754 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/vic20/vic-mem.c
+++ vice/src/vic20/vic-mem.c
@@ -34,7 +34,6 @@
 #include "vic20mem.h"
 #include "vic20sound.h"
 
-
 
 /* VIC access functions. */
 
@@ -51,7 +50,7 @@ vic_store(ADDRESS addr, BYTE value)
 
     switch (addr)
     {
-    case 0:                     /* $9000  Screen X Location. */
+      case 0:                     /* $9000  Screen X Location. */
         /*
             VIC checks in cycle n for peek($9000)=n
             and in this case opens the horizontal flipflop
@@ -65,16 +64,17 @@ vic_store(ADDRESS addr, BYTE value)
 
             xstop = xstart + vic.text_cols * 8;
             if (xstop >= vic.screen_width)
-                xstop = vic.screen_width - 1; /* FIXME: SCREEN-MIXUP not handled */
+                xstop = vic.screen_width - 1;
+                /* FIXME: SCREEN-MIXUP not handled */
 
-            if (vic.raster.display_xstart < (signed int)VIC_RASTER_CYCLE(clk) * 4
-                && !vic.raster.blank_this_line)
-            {
+            if (vic.raster.display_xstart
+                < (signed int)VIC_RASTER_CYCLE(clk) * 4
+                && !vic.raster.blank_this_line) {
                 /* the line has already started */
-                raster_add_int_change_next_line (&vic.raster,
+                raster_add_int_change_next_line(&vic.raster,
                     &vic.raster.display_xstart, xstart);
 
-                raster_add_int_change_next_line (&vic.raster,
+                raster_add_int_change_next_line(&vic.raster,
                     (int *)(&vic.raster.geometry.gfx_position.x), xstart);
             } else {
                 vic.raster.display_xstart = xstart;
@@ -89,10 +89,11 @@ vic_store(ADDRESS addr, BYTE value)
             return;
         }
 
-    case 1:                     /* $9001  Screen Y Location. */
+      case 1:                     /* $9001  Screen Y Location. */
         /*
             VIC checks from cycle 1 of line r*2 to cycle 0 of line r*2+2
-            if peek($9001)=r is true and in this case it opens the vertical flipflop
+            if peek($9001)=r is true and in this case it opens the vertical
+            flipflop
         */
         {
             unsigned int ystart;
@@ -102,26 +103,23 @@ vic_store(ADDRESS addr, BYTE value)
             vic.pending_ystart = ystart;
 
             if ((vic.raster.display_ystart > VIC_RASTER_Y(clk - 2))
-                || vic.raster.display_ystart < 0)
-            {
+                || vic.raster.display_ystart < 0) {
                 /* the vertical flipflop isn't open yet */
-                if (ystart / 2 < VIC_RASTER_Y(clk - 1) / 2)
-                {
-                    /* new ystart is already passed, vertical flipflop may not open this frame */
+                if (ystart / 2 < VIC_RASTER_Y(clk - 1) / 2) {
+                    /* new ystart is already passed, vertical flipflop may
+                       not open this frame */
                     vic.raster.display_ystart = vic.raster.display_ystop = -1;
                 }
-                else if (ystart / 2 > VIC_RASTER_Y(clk - 1) / 2)
-                {
+                else if (ystart / 2 > VIC_RASTER_Y(clk - 1) / 2) {
                     /* the frame starts later */
                     vic.raster.display_ystart = ystart;
 
                     vic.raster.display_ystop = ystart
                         + vic.text_lines * vic.char_height;
 
-                    vic.raster.geometry.gfx_position.y = ystart - vic.first_displayed_line;
-                }
-                else
-                {
+                    vic.raster.geometry.gfx_position.y
+                        = ystart - vic.first_displayed_line;
+                } else {
                     /* the frame starts somewhere here */
                     vic.raster.display_ystart = VIC_RASTER_Y(clk);
 
@@ -131,8 +129,8 @@ vic_store(ADDRESS addr, BYTE value)
                     vic.raster.geometry.gfx_position.y = VIC_RASTER_Y(clk)
                         - vic.first_displayed_line;
 
-                    if (vic.raster.display_xstart < (signed int)VIC_RASTER_CYCLE(clk))
-                    {
+                    if (vic.raster.display_xstart
+                        < (signed int)VIC_RASTER_CYCLE(clk)) {
                         /* too late to start in current line */
                         vic.raster.blank_this_line = 1;
                     }
@@ -141,7 +139,7 @@ vic_store(ADDRESS addr, BYTE value)
             return;
         }
 
-    case 2:                     /* $9002  Columns Displayed. */
+      case 2:                     /* $9002  Columns Displayed. */
         {
             int new_text_cols = MIN(value & 0x7f, VIC_SCREEN_MAX_TEXT_COLS);
 
@@ -160,37 +158,39 @@ vic_store(ADDRESS addr, BYTE value)
                 vic.raster.geometry.text_size.width = new_text_cols;
             } else {
                 /* later changes are visible in the next line */
-                raster_add_int_change_next_line (&vic.raster,
+                raster_add_int_change_next_line(&vic.raster,
                     (int *)(&vic.text_cols), new_text_cols);
 
-                raster_add_int_change_next_line (&vic.raster,
+                raster_add_int_change_next_line(&vic.raster,
                     &vic.raster.display_xstop, new_xstop);
 
-                raster_add_int_change_next_line (&vic.raster,
+                raster_add_int_change_next_line(&vic.raster,
                     (int *)(&vic.raster.geometry.gfx_size.width),
                     new_text_cols * 8);
 
-                raster_add_int_change_next_line (&vic.raster,
+                raster_add_int_change_next_line(&vic.raster,
                     (int *)(&vic.raster.geometry.text_size.width),
                     new_text_cols);
             }
         }
 
-        vic_update_memory_ptrs ();
+        vic_update_memory_ptrs();
 
         return;
 
-    case 3:                     /* $9003  Rows Displayed, Character size . */
+      case 3:                     /* $9003  Rows Displayed, Character size . */
         {
             static int old_char_height = -1;
 
-            int new_text_lines = MIN((value & 0x7e) >> 1, VIC_SCREEN_MAX_TEXT_LINES);
+            int new_text_lines = MIN((value & 0x7e) >> 1,
+                                 VIC_SCREEN_MAX_TEXT_LINES);
 
             int new_char_height = (value & 0x1) ? 16 : 8;
 
             if (VIC_RASTER_Y(clk) == 0 && VIC_RASTER_CYCLE(clk) <= 2)
             {
-                /* changes up to cycle 2 of rasterline 0 are visible in current frame */
+                /* changes up to cycle 2 of rasterline 0 are visible in
+                   current frame */
                 vic.text_lines = new_text_lines;
 
                 vic.raster.geometry.gfx_size.height = new_text_lines * 8;
@@ -205,26 +205,24 @@ vic_store(ADDRESS addr, BYTE value)
 
             if (old_char_height != new_char_height)
             {
-                if (VIC_RASTER_CYCLE(clk) >= 1)
-                {
-                    raster_add_int_change_next_line (&vic.raster,
+                if (VIC_RASTER_CYCLE(clk) >= 1) {
+                    raster_add_int_change_next_line(&vic.raster,
                         (int *)(&vic.row_increase_line), new_char_height);
                 } else {
                     vic.row_increase_line = new_char_height;
                 }
 
                 /* this is quite strange */
-                if (vic.raster.ycounter == 7 && !vic.raster.blank_this_line)
-                {
-                    if (old_char_height == 8 && new_char_height == 16)
-                    {
-                        vic.row_offset =
-                            (VIC_RASTER_CYCLE(clk) - vic.raster.display_xstart / 4 - 3) / 2;
+                if (vic.raster.ycounter == 7 && !vic.raster.blank_this_line) {
+                    if (old_char_height == 8 && new_char_height == 16) {
+                        vic.row_offset = (VIC_RASTER_CYCLE(clk)
+                                         - vic.raster.display_xstart / 4 - 3)
+                                         / 2;
                     } else {
                         vic.row_offset = -1; /* use vic.text_cols for offset */
                     }
                 }
-                raster_add_int_change_foreground (&vic.raster,
+                raster_add_int_change_foreground(&vic.raster,
                     VIC_RASTER_CHAR(VIC_RASTER_CYCLE(clk) + 2),
                     (int*)&vic.char_height,
                     new_char_height);
@@ -235,31 +233,31 @@ vic_store(ADDRESS addr, BYTE value)
 
       return;
 
-    case 4:                     /* $9004  Raster line count -- read only. */
-      return;
-
-    case 5:                     /* $9005  Video and char matrix base
-                                   address. */
-      vic_update_memory_ptrs ();
-      return;
-
-    case 6:                     /* $9006. */
-    case 7:                     /* $9007  Light Pen X,Y. */
-      VIC_DEBUG_REGISTER (("(light pen register, read-only)."));
-      return;
-
-    case 8:                     /* $9008. */
-    case 9:                     /* $9009  Paddle X,Y. */
-      return;
-
-    case 10:                    /* $900A  Bass Enable and Frequency. */
-    case 11:                    /* $900B  Alto Enable and Frequency. */
-    case 12:                    /* $900C  Soprano Enable and Frequency. */
-    case 13:                    /* $900D  Noise Enable and Frequency. */
-        store_vic_sound (addr, value);
+      case 4:                     /* $9004  Raster line count -- read only. */
         return;
 
-    case 14:                    /* $900E  Auxiliary Colour, Master Volume. */
+      case 5:                     /* $9005  Video and char matrix base
+                                   address. */
+        vic_update_memory_ptrs();
+        return;
+
+      case 6:                     /* $9006. */
+      case 7:                     /* $9007  Light Pen X,Y. */
+        VIC_DEBUG_REGISTER(("(light pen register, read-only)."));
+        return;
+
+      case 8:                     /* $9008. */
+      case 9:                     /* $9009  Paddle X,Y. */
+        return;
+
+      case 10:                    /* $900A  Bass Enable and Frequency. */
+      case 11:                    /* $900B  Alto Enable and Frequency. */
+      case 12:                    /* $900C  Soprano Enable and Frequency. */
+      case 13:                    /* $900D  Noise Enable and Frequency. */
+        store_vic_sound(addr, value);
+        return;
+
+      case 14:                    /* $900E  Auxiliary Colour, Master Volume. */
         /*
             changes of auxiliary color in cycle n is visible at pixel 4*(n-7)
         */
@@ -267,9 +265,8 @@ vic_store(ADDRESS addr, BYTE value)
             static int old_aux_color = -1;
             int new_aux_color = value>>4;
 
-            if (new_aux_color != old_aux_color)
-            {
-                raster_add_int_change_foreground (&vic.raster,
+            if (new_aux_color != old_aux_color) {
+                raster_add_int_change_foreground(&vic.raster,
                     VIC_RASTER_CHAR(VIC_RASTER_CYCLE(clk)),
                     &vic.auxiliary_color,
                     new_aux_color);
@@ -279,15 +276,16 @@ vic_store(ADDRESS addr, BYTE value)
             }
         }
 
-        store_vic_sound (addr, value);
+        store_vic_sound(addr, value);
         return;
 
-    case 15:                    /* $900F  Screen and Border Colors,
-                                   Reverse Video. */
+      case 15:                    /* $900F  Screen and Border Colors,
+                                     Reverse Video. */
         /*
-            changes of border/background in cycle n are visible at pixel 4*(n-7),
-            changes of reverse mode at pixel 4*(n-7)+3, which is quite ugly 'cause
-            we're using a character-based emulation :(FIXME)
+            changes of border/background in cycle n are visible at pixel
+            4*(n-7),
+            changes of reverse mode at pixel 4*(n-7)+3, which is quite ugly
+            'cause we're using a character-based emulation :(FIXME)
         */
         {
             static int old_video_mode = -1;
@@ -297,11 +295,11 @@ vic_store(ADDRESS addr, BYTE value)
 
             new_background_color = value>>4;
             new_border_color = value & 0x7;
-            new_video_mode = ((value & 0x8) ? VIC_STANDARD_MODE : VIC_REVERSE_MODE);
+            new_video_mode = ((value & 0x8)
+                             ? VIC_STANDARD_MODE : VIC_REVERSE_MODE);
 
-            if (new_background_color != old_background_color)
-            {
-                raster_add_int_change_background (&vic.raster,
+            if (new_background_color != old_background_color) {
+                raster_add_int_change_background(&vic.raster,
                     VIC_RASTER_X(VIC_RASTER_CYCLE(clk)),
                     &vic.raster.background_color,
                     new_background_color);
@@ -309,15 +307,15 @@ vic_store(ADDRESS addr, BYTE value)
                 old_background_color = new_background_color;
             }
 
-            if (new_border_color != old_border_color)
-            {
-                raster_add_int_change_border (&vic.raster,
+            if (new_border_color != old_border_color) {
+                raster_add_int_change_border(&vic.raster,
                     VIC_RASTER_X(VIC_RASTER_CYCLE(clk)),
                     &vic.raster.border_color,
                     new_border_color);
 
-                /* we also need the border color in multicolor mode, so we duplicate it */
-                raster_add_int_change_foreground (&vic.raster,
+                /* we also need the border color in multicolor mode,
+                   so we duplicate it */
+                raster_add_int_change_foreground(&vic.raster,
                     VIC_RASTER_CHAR(VIC_RASTER_CYCLE(clk)),
                     &vic.mc_border_color,
                     new_border_color);
@@ -326,9 +324,8 @@ vic_store(ADDRESS addr, BYTE value)
                 old_border_color = new_border_color;
             }
 
-            if (new_video_mode != old_video_mode)
-            {
-                raster_add_int_change_foreground (&vic.raster,
+            if (new_video_mode != old_video_mode) {
+                raster_add_int_change_foreground(&vic.raster,
                     VIC_RASTER_CHAR(VIC_RASTER_CYCLE(clk)),
                     &vic.raster.video_mode,
                     new_video_mode);
@@ -346,16 +343,16 @@ vic_store(ADDRESS addr, BYTE value)
 BYTE REGPARM1
 vic_read(ADDRESS addr)
 {
-  addr &= 0xf;
+    addr &= 0xf;
 
-  switch (addr)
-    {
-    case 3:
-      return ((VIC_RASTER_Y (clk + vic.cycle_offset) & 1) << 7) | (vic.regs[3] & ~0x80);
-    case 4:
-      return VIC_RASTER_Y (clk + vic.cycle_offset) >> 1;
-    default:
-      return vic.regs[addr];
+    switch (addr) {
+      case 3:
+        return ((VIC_RASTER_Y(clk + vic.cycle_offset) & 1) << 7)
+               | (vic.regs[3] & ~0x80);
+      case 4:
+        return VIC_RASTER_Y(clk + vic.cycle_offset) >> 1;
+      default:
+        return vic.regs[addr];
     }
 }
 
