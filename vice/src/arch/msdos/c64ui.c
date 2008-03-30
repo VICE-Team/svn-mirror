979a3d908e26f9e49254d9a18f404006548f8e78
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:02:48 +0000

    This commit was generated by cvs2svn to compensate for changes in r2527, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@2528 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/msdos/c64ui.c
+++ vice/src/arch/msdos/c64ui.c
@@ -149,12 +149,17 @@ static tui_menu_item_def_t vic_ii_menu_items[] = {
 
 /* ------------------------------------------------------------------------- */
 
+TUI_MENU_DEFINE_TOGGLE(Mouse)
 TUI_MENU_DEFINE_TOGGLE(REU)
 TUI_MENU_DEFINE_TOGGLE(EmuID)
 TUI_MENU_DEFINE_TOGGLE(IEEE488)
 
 static tui_menu_item_def_t special_menu_items[] = {
     { "--" },
+    { "1531 _Mouse Emulation:",
+      "Emulate a Commodore 1351 proportional mouse connected to joystick port #1",
+      toggle_Mouse_callback, NULL, 3,
+      TUI_MENU_BEH_CONTINUE, NULL, NULL },
     { "512K _RAM Expansion Unit (C1750):",
       "Emulate auxiliary 512K RAM Expansion Unit",
       toggle_REU_callback, NULL, 3,
