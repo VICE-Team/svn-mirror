7b1d7774132bc51433d969eaa3755f1753467a40
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:16:08 +0000

    This commit was generated by cvs2svn to compensate for changes in r11327, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@11328 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/drive/drive.c
+++ vice/src/drive/drive.c
@@ -983,7 +983,7 @@ static void drive_set_half_track(int num, drive_t *dptr)
                                 * NUM_MAX_BYTES_TRACK));
 
     if (dptr->GCR_current_track_size != 0)
-#if 1
+#if 0
         dptr->GCR_head_offset
             *= (dptr->gcr->track_size[dptr->current_half_track
             / 2 - 1]) / dptr->GCR_current_track_size;
