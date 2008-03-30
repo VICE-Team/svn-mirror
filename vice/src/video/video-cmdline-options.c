11abc6223f207eee14bd154ce28c27c0843323a9
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:12:47 +0000

    This commit was generated by cvs2svn to compensate for changes in r10925, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@10926 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/video/video-cmdline-options.c
+++ vice/src/video/video-cmdline-options.c
@@ -26,9 +26,150 @@
 
 #include "vice.h"
 
+#include <stdio.h>
 
-int video_cmdline_options_chip_init(const char *chipname)
+#include "cmdline.h"
+#include "utils.h"
+#include "video.h"
+
+
+static const char *cname_chip_size[] =
 {
+    "-", "dsize", "DoubleSize",
+    "+", "dsize", "DoubleSize",
+    NULL
+};
+
+static cmdline_option_t cmdline_options_chip_size[] =
+{
+    { NULL, SET_RESOURCE, 0, NULL, NULL, NULL,
+      (void *)1, NULL, "Enable double size" },
+    { NULL, SET_RESOURCE, 0, NULL, NULL, NULL,
+      (void *)0, NULL, "Disable double size" },
+    { NULL }
+};
+
+static const char *cname_chip_scan[] =
+{
+    "-", "dscan", "DoubleScan",
+    "+", "dscan", "DoubleScan",
+    NULL
+};
+
+static cmdline_option_t cmdline_options_chip_scan[] =
+{
+    { NULL, SET_RESOURCE, 0, NULL, NULL, NULL,
+      (void *)1, NULL, "Enable double scan" },
+    { NULL, SET_RESOURCE, 0, NULL, NULL, NULL,
+      (void *)0, NULL, "Disable double scan" },
+    { NULL }
+};
+
+static const char *cname_chip_fullscreen[] =
+{
+   "-", "full", "Fullscreen",
+   "+", "full", "Fullscreen",
+   "-", "fulldevice", "FullscreenDevice",
+   "-", "fulldsize", "FullscreenDoubleSize",
+   "+", "fulldsize", "FullscreenDoubleSize",
+   "-", "fulldscan", "FullscreenDoubleScan",
+   "+", "fulldscan", "FullscreenDoubleScan",
+   NULL
+};
+
+static cmdline_option_t cmdline_options_chip_fullscreen[] =
+{
+    { NULL, SET_RESOURCE, 0, NULL, NULL, NULL,
+      (void *)1, NULL, "Enable fullscreen mode" },
+    { NULL, SET_RESOURCE, 0, NULL, NULL, NULL,
+      (void *)0, NULL, "Disable fullscreen mode" },
+    { NULL, SET_RESOURCE, 1, NULL, NULL, NULL,
+      NULL, "<device>", "Select fullscreen device" },
+    { NULL, SET_RESOURCE, 0, NULL, NULL, NULL,
+      (void *)1, NULL, "Enable double size in fullscreen mode" },
+    { NULL, SET_RESOURCE, 0, NULL, NULL, NULL,
+      (void *)0, NULL, "Disable double size in fullscreen mode" },
+    { NULL, SET_RESOURCE, 0, NULL, NULL, NULL,
+      (void *)1, NULL, "Enable double scan in fullscreen mode" },
+    { NULL, SET_RESOURCE, 0, NULL, NULL, NULL,
+      (void *)0, NULL, "Disable double scan in fullscreen mode" },
+    { NULL }
+};
+
+static const char *cname_chip_fullscreen_mode[] =
+{
+    "-", "fullmode", "FullscreenMode",
+    NULL
+};
+
+static cmdline_option_t cmdline_options_chip_fullscreen_mode[] =
+{
+    { NULL, SET_RESOURCE, 1, NULL, NULL, NULL,
+      NULL, "<mode>", "Select fullscreen mode" },
+    { NULL }
+};
+
+int video_cmdline_options_chip_init(const char *chipname,
+                                    video_chip_cap_t *video_chip_cap)
+{
+    unsigned int i, j;
+
+    if (video_chip_cap->dsize_allowed) {
+        for (i = 0; cname_chip_size[i * 3] != NULL; i++) {
+            cmdline_options_chip_size[i].name
+                = concat(cname_chip_size[i * 3], chipname,
+                cname_chip_size[i * 3 + 1], NULL);
+            cmdline_options_chip_size[i].resource_name
+                = concat(chipname, cname_chip_size[i * 3 + 2], NULL);
+        }
+
+        if (cmdline_register_options(cmdline_options_chip_size) < 0)
+            return -1;
+    }
+
+    if (video_chip_cap->dscan_allowed) {
+        for (i = 0; cname_chip_scan[i * 3] != NULL; i++) {
+            cmdline_options_chip_scan[i].name
+                = concat(cname_chip_scan[i * 3], chipname,
+                cname_chip_scan[i * 3 + 1], NULL);
+            cmdline_options_chip_scan[i].resource_name
+                = concat(chipname, cname_chip_scan[i * 3 + 2], NULL);
+        }
+
+        if (cmdline_register_options(cmdline_options_chip_scan) < 0)
+            return -1;
+    }
+
+    if (video_chip_cap->fullscreen.device_num > 0) {
+        for (i = 0; cname_chip_fullscreen[i * 3] != NULL; i++) {
+            cmdline_options_chip_fullscreen[i].name
+                = concat(cname_chip_fullscreen[i * 3], chipname,
+                cname_chip_fullscreen[i * 3 + 1], NULL);
+            cmdline_options_chip_fullscreen[i].resource_name
+                = concat(chipname, cname_chip_fullscreen[i * 3 + 2], NULL);
+        }
+
+        if (cmdline_register_options(cmdline_options_chip_fullscreen) < 0)
+            return -1;
+
+        for (j = 0; j < video_chip_cap->fullscreen.device_num; j++) {
+            for (i = 0; cname_chip_fullscreen_mode[i * 3] != NULL; i++) {
+                cmdline_options_chip_fullscreen_mode[i].name
+                    = concat(cname_chip_fullscreen_mode[i * 3], chipname,
+                    video_chip_cap->fullscreen.device_name[j],
+                    cname_chip_fullscreen_mode[i * 3 + 1], NULL);
+                cmdline_options_chip_fullscreen_mode[i].resource_name
+                    = concat(chipname,
+                    video_chip_cap->fullscreen.device_name[j],
+                    cname_chip_fullscreen_mode[i * 3 + 2], NULL);
+            }
+
+            if (cmdline_register_options(cmdline_options_chip_fullscreen_mode)
+                < 0)
+                return -1;
+        }
+    }
+
     return 0;
 }
 
