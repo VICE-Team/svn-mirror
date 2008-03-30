cea5be5b21397eddea8da244a293d4c53bb427ae
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:28:21 +0000

    This commit was generated by cvs2svn to compensate for changes in r5593, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@5594 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/c64.h
+++ vice/src/c64/c64.h
@@ -50,7 +50,7 @@
 #define C64_NTSCOLD_SCREEN_LINES    263
 #define C64_NTSCOLD_CYCLES_PER_RFSH (C64_NTSCOLD_SCREEN_LINES \
                                     * C64_NTSCOLD_CYCLES_PER_LINE)
-#define C64_NTSC_OLDRFSH_PER_SEC  (1.0 / ((double)C64_NTSCOLD_CYCLES_PER_RFSH \
+#define C64_NTSCOLD_RFSH_PER_SEC  (1.0 / ((double)C64_NTSCOLD_CYCLES_PER_RFSH \
                                         / (double)C64_NTSCOLD_CYCLES_PER_SEC))
 
 #endif
