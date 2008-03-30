46e7afb4efacf8f08b6191a641244fb3e7059481
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:12:32 +0000

    This commit was generated by cvs2svn to compensate for changes in r3695, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@3696 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/tape.c
+++ vice/src/tape.c
@@ -1,11 +1,11 @@
 /*
- * tape.h - Cassette drive and P00.
+ * tape.c - (Guess what?) Tape unit emulation.
  *
- * Written by:
- *  Dan Fandrich	 (dan@fch.wimsey.bc.ca)
- *  Jouko Valta		 (jopi@stekt.oulu.fi)
- *  Martin Pottendorfer  (Martin.Pottendorfer@autalcatel.at)
- *  Ettore Perazzoli	 (ettore@comm2000.it)
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * Based on older code by
+ *  Jouko Valta (jopi@stekt.oulu.fi)
  *
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
@@ -27,432 +27,296 @@
  *
  */
 
-/* FIXME: P00 should be moved from here. */
-/* FIXME: Logging should be separate from `LOG_DEFAULT'.  */
+/* tapeunit.c -- Cassette drive interface.  The cassette interface consists
+   of traps in tape access routines Find, Write Header, Send Data, and Get
+   Data, so that actual operation can be controlled by C routines.  To
+   support turboloaders, it would be necessary to emulate the tape encoding
+   used. That is much slower though. :( */
+
+/* FIXME: This should be splitted into a T64 and a tape device part.  */
 
 #include "vice.h"
 
 #ifdef STDC_HEADERS
-#ifdef __riscos
-#include "ROlib.h"
-#else
-#include <sys/types.h>
-#include <unistd.h>
-#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#endif
-
-#include "vdrive.h"
-#include "tape.h"
-#include "zfile.h"
-#include "utils.h"
-#include "charsets.h"
-
-#ifndef SEEK_SET
-#define SEEK_SET 0
-typedef long       fpos_t;
-#endif
-
-#ifdef __GNUC__
-#define PACK __attribute__ ((packed))	/* pack structures on byte boundaries */
+#include <assert.h>
+#ifdef __riscos
+#include "ROlib.h"
 #else
-#define PACK		/* pack using a compiler switch instead */
+#include <unistd.h>
+#include <errno.h>
+#endif
 #endif
 
-typedef struct {
-	BYTE Magic[8] PACK;
-	char CbmName[17] PACK;
-	BYTE RecordSize PACK;		/* REL file record size */
-} X00HDR;
+#include "tape.h"
 
-/* Global */
+#include "log.h"
+#include "mem.h"
+#include "maincpu.h"
+#include "serial.h"
+#include "t64.h"
+#include "traps.h"
+#include "types.h"
+#include "utils.h"
+#include "zfile.h"
 
-extern char *slot_type[];
+/* #define DEBUG_TAPE */
 
-static const BYTE MagicHeaderP00[8] = "C64File\0";
+/* Cassette Format Constants */
+#define CAS_TYPE_OFFSET	0
+#define CAS_STAD_OFFSET	1	/* start address */
+#define CAS_ENAD_OFFSET	3	/* end address */
+#define CAS_NAME_OFFSET	5	/* filename */
 
-/*---------------------------------------------------------------------------*/
+#define CAS_TYPE_PRG	1	/* Binary Program */
+#define CAS_TYPE_BAS	3	/* Relocatable Program */
+#define CAS_TYPE_DATA	4	/* Data Record */
+#define CAS_TYPE_EOF	5	/* End of Tape marker */
 
-/*
- * Check if given filename has valid P00 extension.
- * Return the filetype or -1 if no match.
- */
+static int fn(void);
 
-int  is_pc64name (const char *name)
-{
-    int   t = -1;
-    char *p;
+/* CPU addresses for tape routine variables.  */
+static int buffer_pointer_addr;
+static int st_addr;
+static int verify_flag_addr;
+static int stal_addr;
+static int eal_addr;
+static int kbd_buf_addr;
+static int kbd_buf_pending_addr;
+static int irqval;
+static int irqtmp;
 
+/* Flag: has tape been initialized?  */
+static int tape_is_initialized = 0;
 
-    if (name == NULL || (p = strrchr(name, '.')) == NULL || strlen(++p) != 3)
-	return (-1);
+/* Tape traps to be installed.  */
+static const trap_t *tape_traps;
 
-    if (!isdigit((int)p[1]) || !isdigit((int)p[2]))
-	return (-1);
+/* Tape currently attached.  */
+static t64_t *attached_tape = NULL;
 
-    switch (toupper(*p)) {
-      case 'D': t = FT_DEL; break;
-      case 'S': t = FT_SEQ; break;
-      case 'P': t = FT_PRG; break;
-      case 'U': t = FT_USR; break;
-      case 'R': t = FT_REL; break;
-    }
-
-    return (t);
-}
-
-
-/*
- * The read pointer must be positioned at the beginning of the
- * file before calling this routine. (stdin cannot be fseek(2)'ed)
- */
-
-int  read_pc64header (FILE *fd, char *name, int *reclen)
-{
-    X00HDR  hdr;
-
-    if (fread (&hdr, sizeof(X00HDR), 1, fd) != 1) {
-	log_error(LOG_DEFAULT, "P00 header read failed.");
-	return (FD_RDERR);
-    }
-
-    if (memcmp (hdr.Magic, MagicHeaderP00, 8) != 0)
-	return (FD_BADIMAGE);
-
-    strncpy (name, hdr.CbmName, 16);
-    *reclen = hdr.RecordSize;		/* REL file record size */
-
-    return (FD_OK);
-}
-
-
-int  write_pc64header (FILE *fd, const char *name, int reclen)
-{
-    X00HDR  hdr;
-
-
-    memset (&hdr, 0, sizeof(X00HDR));
-    strncpy ((char *)hdr.Magic, (char *)MagicHeaderP00, 8);
-    strncpy (hdr.CbmName, name, 16);
-
-    hdr.RecordSize = reclen;		/* REL file record size */
-
-    if (fseek(fd, 0, SEEK_SET) != 0)
-	return (FD_WRTERR);
-
-    return
-	(fwrite(&hdr, sizeof(X00HDR), 1, fd));
-}
-
-
-/*
- * If the file is a PC64 file, read the actual filename from the header.
- * Otherwise, return the filename converted to appropriate PETSCII
- * representation.
- *
- * This function cannot be used on stdin.
- */
-
-char  *pc_get_cbmname (FILE *fd, char *fsname)
-{
-    static char cbmname[24];
-    char  *p;
-    int    type;
-    int    reclen;
-
-
-    type = is_pc64name (fsname);
-
-    fseek(fd, 0, SEEK_SET);
-
-    if (type >= 0 && read_pc64header (fd, cbmname, &reclen) == FD_OK) {
-	for (p = cbmname; *p && (BYTE)*p != 0xA0; ++p);
-	sprintf (p, ",%s", slot_type[type]);
-    }
-    else {
-	rewind (fd);		/* There is no header */
-
-
-	/* name */
-
-	if ((p = strrchr(fsname, '/')) == NULL || !p++)
-	    p = fsname;
-
-	if (!p || !*p)
-	    return (NULL);
-
-
-	/* Get Basename and extension from FSname */
-
-	strncpy (cbmname, p, 16);
-
-	if ((p = strrchr(fsname, '.')) != NULL && strlen(++p) == 3) {
-	    ;
-	}
-
-	if (type >= 0) {
-	    p = cbmname + strlen(cbmname);
-	    sprintf (p, ",%s", slot_type[type]);
-	}
-	else
-	    strcat (cbmname, ",prg");		/* Best default */
-    } /* !p00 */
-
-
-    return (cbmname);
-}
-
-
+/* Logging goes here.  */
+static log_t tape_log = LOG_ERR;
 
 /* ------------------------------------------------------------------------- */
 
-/*
- * Read directory
- *
- * P00 is just a regular file with a simple header prepended, so just read the
- * header and return the name and addresses evaluated.
- *
- * This function cannot be used on stdin.
- */
-
-
-int  DirP00(FILE *fd, char *CbmName, int *start, int *end)
+static inline void set_st(BYTE b)
 {
-    X00HDR  hdr;
-    unsigned int  FileLength;
-    int     realtype = -1;
-    char   *FileExt;
-    BYTE    buf[2];
-
-
-    if (fseek(fd, 0, SEEK_SET) != 0)
-	return (FD_RDERR);
-
-    if (fread(&hdr, sizeof(hdr), 1, fd) != 1) {
-	return (FD_RDERR);
-    }
-
-
-    /* If archive type is unknown, see if file is REL */
-
-    if (/*(realtype == X00) && */ (hdr.RecordSize > 0))
-	realtype = FT_REL;
-
-
-    switch (realtype) {
-	case FT_DEL: FileExt = "DEL"; break;
-	case FT_SEQ: FileExt = "SEQ"; break;
-	case FT_PRG: FileExt = "PRG"; break;
-	case FT_USR: FileExt = "USR"; break;
-	case FT_REL: FileExt = "REL"; break;
-	default:  FileExt = "???";
-    }
-
-    strncpy(CbmName, (char *) hdr.CbmName, 16);
-    CbmName[16] = 0;		/* never need this on a good P00 file */
-
-    strcat (CbmName, ",");
-    strcat (CbmName, FileExt);
-
-
-    /* Start and End Addresses */
-
-    FileLength = file_length(fileno(fd)) - sizeof(hdr) -2; /* skip start addr */
-
-    if (fread(buf, 2, 1, fd) != 1) {
-	return (FD_RDERR);
-    }
-
-    *start= *buf | (buf[1] << 8);
-    *end = *start + FileLength;
-
-    return (FD_OK);
-}
-
-
-/* ------------------------------------------------------------------------- */
-
-/*
- * T64 Tape Image handling functions
- */
-
-#define TAPE_HDR_SIZE	64
-#define TAPE_DIR_SIZE	32
-
-#define TAPE64_HEADERS	2
-
-static const char *MagicHeaderT64[] = {
-  "C64 tape image file",
-  "C64S tape file",
-  "C64S tape image file" /* EP */
-};
-
-
-/* ------------------------------------------------------------------------- */
-
-/*
- * Check if given filename has valid .T64 extension.
- * Return the filetype or -1 if no match.
- */
-
-int  is_t64name (const char *name)
-{
-    char *p;
-
-
-    if (name == NULL || (p = strrchr(name, '.')) == NULL || strlen(++p) != 3)
-	return (-1);
-
-    if (toupper(*p) == 'T' && p[1] == '6' && p[2] == '4')
-	return (1);
-
-    return (-1);
-}
-
-
-/*
- * Check the image for a valid T64 header and return the
- * number of files it carries.
- */
-
-int  check_t64_header (FILE *fd)
-{
-    BYTE  buf[TAPE_HDR_SIZE];
-    int   maxentries = 0;
-
-
-    if (fseek(fd, 0, SEEK_SET) != 0)
-	return (FD_RDERR);
-
-    fread(buf, 1, TAPE_HDR_SIZE, fd);
-
-    if (memcmp(buf, (BYTE *)MagicHeaderT64[0], strlen(MagicHeaderT64[0])) &&
-	memcmp(buf, (BYTE *)MagicHeaderT64[1], strlen(MagicHeaderT64[1])) &&
-        memcmp(buf, (BYTE *)MagicHeaderT64[2], strlen(MagicHeaderT64[2])))
-	return (FD_BADIMAGE);
-
-
-    if (buf[32] != 0 || buf[33] != 1)
-	log_error (LOG_DEFAULT, "Unknown tape version.");
-
-    maxentries = buf[34];
-    maxentries |= (buf[35] << 8);
-
-    if (!maxentries) {
-	log_error(LOG_DEFAULT, "Cannot locate any files on the tape.");
-	return (FD_BADIMAGE);		/* tapeimage, that is */
-    }
-
-    log_message(LOG_DEFAULT, "Tape version %d.%.2d with %2d out of %d files.",
-                buf[33], buf[32], (buf[37] << 8) | buf[36], maxentries);
-
-    return (maxentries);
+    mem_store(st_addr, (mem_read(st_addr) | b));
 }
 
 /* ------------------------------------------------------------------------- */
 
-/* Read the list of files in the T64 image and return it as a long malloc'ed
-   ASCII string. */
-char *read_tape_image_contents(const char *fname)
+/* Initialize the tape emulation, using the traps in `trap_list'.  */
+/* FIXME: This should be passed through a struct.  */
+int tape_init(int _buffer_pointer_addr,
+              int _st_addr,
+              int _verify_flag_addr,
+              int _irqtmp,
+              int _irqval,
+              int _stal_addr,
+              int _eal_addr,
+              int _kbd_buf_addr,
+              int _kbd_buf_pending_addr,
+              const trap_t *trap_list)
 {
-    BYTE inbuf[TAPE_HDR_SIZE + 1];
-    char linebuf[512], *outbuf;
-    int outbuf_size, max_outbuf_size;
-    int no_entries, max_entries, res, len, i;
-    file_desc_t fd;
+    const trap_t *p;
 
-    fd = zopen(fname, O_RDONLY, 0);
-    if (fd == ILLEGAL_FILE_DESC)
-	return NULL;
+    if (tape_log == LOG_ERR)
+        tape_log = log_open("Tape");
 
-    /* Check whether this really looks like a tape image. */
-    if (read (fd, inbuf, TAPE_HDR_SIZE) != TAPE_HDR_SIZE
-	|| ((memcmp(inbuf, (BYTE *)MagicHeaderT64[0],
-		    strlen(MagicHeaderT64[0]))
-	     && memcmp(inbuf, (BYTE *)MagicHeaderT64[1],
-		       strlen(MagicHeaderT64[1]))
-	     && memcmp(inbuf, (BYTE *)MagicHeaderT64[2],
-		       strlen(MagicHeaderT64[2]))))) {
-	zclose(fd);
-	return NULL;
+    /* Set addresses of tape routine variables.  */
+    st_addr = _st_addr;
+    buffer_pointer_addr = _buffer_pointer_addr;
+    verify_flag_addr = _verify_flag_addr;
+    irqtmp = _irqtmp;
+    irqval = _irqval;
+    stal_addr = _stal_addr;
+    eal_addr = _eal_addr;
+    kbd_buf_addr = _kbd_buf_addr;
+    kbd_buf_pending_addr = _kbd_buf_pending_addr;
+
+    tape_traps = trap_list;
+    if (tape_traps != 0) {
+	for (p = tape_traps; p->func != NULL; p++)
+	    traps_add(p);
     }
 
-    max_entries = (unsigned int)inbuf[34] + (unsigned int)inbuf[35] * 256;
-    no_entries = (unsigned int)inbuf[36] + (unsigned int)inbuf[37] * 256;
+    if (tape_is_initialized)
+	return 0;
+    tape_is_initialized = 1;
 
-    /* Check a little bit... */
-    if (no_entries > max_entries) {
-	log_error(LOG_DEFAULT, "Tape inconsistency, giving up!");
-	zclose(fd);
-	return NULL;
-    }
-
-    /* Many T64 images are broken... */
-    if (!no_entries)
-	no_entries = 1;
-
-    /* Seek to start of file records. */
-    if (lseek (fd, 64, SEEK_SET) < 0) {
-	zclose(fd);
-	return NULL;
-    }
-
-    /* Allocate the buffer. */
-    outbuf_size = 0;
-    max_outbuf_size = 4096;
-    outbuf = xmalloc(max_outbuf_size);
-
-    inbuf[40 + 24] = '\0';
-    len = sprintf(linebuf, "0 .\"%s\"\n", &inbuf[40]);
-    bufcat(outbuf, &outbuf_size, &max_outbuf_size, linebuf, len);
-
-    for (i = 0; i < no_entries; i++) {
-	char *ext;
-	int start_addr, end_addr;
-	int blocks;
-
-	res = read(fd, inbuf, 32);
-	if (res != 32) {
-	    zclose(fd);
-	    free(outbuf);
-	    return NULL;
-	}
-	if (inbuf[0] == 0)
-	    /* Don't print free slots. */
-	    continue;
-	else if (inbuf[0] == 1)
-	    ext = "PRG";
-	else
-	    ext = "???";
-
-	/* Cut name. */
-	inbuf[32] = '\0';
-
-        start_addr = (unsigned int)inbuf[2] + (unsigned int)inbuf[3] * 256;
-        end_addr = (unsigned int)inbuf[4] + (unsigned int)inbuf[5] * 256;
-        blocks = (end_addr - start_addr) / 256 + 1;
-
-	if (blocks <= 0)
-	    len = sprintf(linebuf, "???   \"%s\" %s\n", &inbuf[16], ext);
-	else
-	    len = sprintf(linebuf, "%-6d\"%s\" %s\n", blocks, &inbuf[16], ext);
-
-	bufcat(outbuf, &outbuf_size, &max_outbuf_size, linebuf, len);
-    }
-
-    len = sprintf(linebuf, "%d/%d ENTRIES USED.\n", no_entries, max_entries);
-    bufcat(outbuf, &outbuf_size, &max_outbuf_size, linebuf, len);
-
-    /* Add trailing zero. */
-    *(outbuf + outbuf_size) = '\0';
-
-    /* Convert from PETSCII to ASCII. */
-    petconvstring(outbuf, 1);
-
-    zclose(fd);
-    return outbuf;
+    return 0;
 }
 
+/* ------------------------------------------------------------------------- */
+
+/* Functions to attach and detach tape image files.  */
+
+/* Detach.  */
+int tape_detach_image(void)
+{
+    int retval;
+
+    if (attached_tape != NULL) {
+        log_message(tape_log,
+                    "Detaching tape image `%s'.", attached_tape->file_name);
+        
+        /* Gone.  */
+        retval = t64_close(attached_tape);
+        attached_tape = NULL;
+
+        /* Tape detached: release play button.  */
+        mem_set_tape_sense(0);
+    } else
+        retval = 0;
+
+    return retval;
+}
+
+/* Attach.  */
+int tape_attach_image(const char *name)
+{
+    t64_t *new_tape;
+
+    if (!name || !*name)
+	return -1;
+
+    new_tape = t64_open(name);
+    if (new_tape == NULL)
+        return -1;
+
+    tape_detach_image();
+    attached_tape = new_tape;
+
+    log_message(tape_log, "T64 image '%s' attached.", name);
+
+    /* Tape attached: press play button.  */
+    mem_set_tape_sense(1);
+
+    return 0;
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* Tape traps.  These functions implement the standard kernal replacements
+   for the tape functions.  Every emulator can either use these traps, or
+   install its own ones, by passing an appropriate `trap_list' to
+   `tape_init()'.  */
+
+/* Find the next Tape Header and load it onto the Tape Buffer.  */
+void tape_find_header_trap(void)
+{
+    int err;
+    BYTE *cassette_buffer;
+
+    cassette_buffer = ram + (mem_read(buffer_pointer_addr)
+                             | (mem_read(buffer_pointer_addr + 1) << 8));
+
+    if (attached_tape == NULL) {
+        err = 1;
+    } else {
+        t64_file_record_t *rec = NULL;
+
+        err = 0;
+        do {
+            if (t64_seek_to_next_file(attached_tape, 1) < 0) {
+                err = 1;
+                break;
+            }
+
+            rec = t64_get_current_file_record(attached_tape);
+        } while (rec->entry_type != T64_FILE_RECORD_NORMAL);
+
+        if (!err) {
+            cassette_buffer[CAS_TYPE_OFFSET] = CAS_TYPE_PRG;
+            cassette_buffer[CAS_STAD_OFFSET] = rec->start_addr & 0xff;
+            cassette_buffer[CAS_STAD_OFFSET + 1] = rec->start_addr >> 8;
+            memcpy(cassette_buffer + CAS_NAME_OFFSET,
+                   rec->cbm_name, T64_REC_CBMNAME_LEN);
+        }
+    }
+
+    if (err)
+	cassette_buffer[CAS_TYPE_OFFSET] = CAS_TYPE_EOF;
+
+    mem_store(st_addr, 0);	/* Clear the STATUS word.  */
+    mem_store(verify_flag_addr, 0);
+
+    if (irqtmp) {
+	mem_store(irqtmp, irqval & 0xff);
+	mem_store(irqtmp + 1, (irqval >> 8) & 0xff);
+    }
+
+    /* Check if STOP has been pressed.  */
+    {
+	int i, n = mem_read(kbd_buf_pending_addr);
+
+        MOS6510_REGS_SET_CARRY(&maincpu_regs, 0);
+	for (i = 0; i < n; i++) {
+	    if (mem_read(kbd_buf_addr + i) == 0x3) {
+                MOS6510_REGS_SET_CARRY(&maincpu_regs, 1);
+		break;
+	    }
+        }
+    }
+
+    MOS6510_REGS_SET_ZERO(&maincpu_regs, 1);
+}
+
+/* Cassette Data transfer trap.
+
+   XR flags the function to be performed on IRQ:
+
+   08   Write tape
+   0a   Write tape leader
+   0c   Normal keyscan
+   0e   Read tape
+
+   Luckily enough, these values are valid for all the machines.  */
+void tape_receive_trap(void)
+{
+    WORD start, end, len;
+    BYTE st;
+
+    start = (mem_read(stal_addr) | (mem_read(stal_addr + 1) << 8));
+    end = (mem_read(eal_addr) | (mem_read(eal_addr + 1) << 8));
+
+    switch (MOS6510_REGS_GET_X(&maincpu_regs)) {
+      case 0x0e:
+        {
+            /* Read block.  */
+            len = end - start;
+
+            if (t64_read(attached_tape,
+                         ram + (int) start, (int) len) == (int) len) {
+                st = 0x40;	/* EOF */
+            } else {
+                st = 0x10;
+
+                log_warning(tape_log,
+                            "Unexpected end of tape: file may be truncated.");
+            }
+        }
+        break;
+      default:
+        log_error(tape_log, "Kernal command $02X not supported.",
+                  MOS6510_REGS_GET_X(&maincpu_regs));
+        st = 0x40;
+        break;
+    }
+
+    /* Set registers and flags like the Kernal routine does.  */
+
+    if (irqtmp) {
+	mem_store(irqtmp, irqval & 0xff);
+	mem_store(irqtmp + 1, (irqval >> 8) & 0xff);
+    }
+
+    set_st(st);			/* EOF and possible errors */
+
+    MOS6510_REGS_SET_CARRY(&maincpu_regs, 0);
+    MOS6510_REGS_SET_INTERRUPT(&maincpu_regs, 0);
+}
--- vice/src/tape.h
+++ vice/src/tape.h
@@ -1,8 +1,8 @@
 /*
- * tape.h - Cassette drive and P00.
+ * tape.h - (Guess what?) Tape unit emulation.
  *
  * Written by
- *   Jouko Valta (jopi@stekt.oulu.fi)
+ *  Jouko Valta (jopi@stekt.oulu.fi)
  *
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
@@ -24,102 +24,21 @@
  *
  */
 
-/* FIXME: P00 should be moved from here. */
+#ifndef _TAPE_H
+#define _TAPE_H
 
-#ifndef _CBM_TAPE_H_
-#define _CBM_TAPE_H_
-
-#include <stdio.h>              /* FILE */
+#include <stdio.h>
 
 #include "types.h"
+#include "traps.h"
 
-/* Image File Formats */
+extern int tape_detach_image(void);
+extern int tape_attach_image(const char *name);
+extern int tape_init(int buffer_pointer_addr, int st_addr,
+                     int verify_flag_addr, int irqtmp, int irqval,
+                     int stal_addr, int eal_addr, int kbd_buf_addr,
+                     int kbd_buf_pending_addr, const trap_t *trap_list);
+extern void tape_find_header_trap(void);
+extern void tape_receive_trap(void);
 
-#define TFF_INDEX	0	/* Special name index file */
-#define TFF_CBM		1
-#define TFF_SFX		2
-#define TFF_LYNX	3
-#define TFF_P00		4
-#define TFF_TAPE	5
-#define TFF_DISK	6	/* Also adjust the format list on tapeunit.c */
-
-
-/* Emulation Mode */
-
-#define TEM_BYTE	0
-#define TEM_ASCII	1	/* convert data */
-#define TEM_TONE	2
-#define TEM_NORMAL	4
-#define TEM_TURBO	8
-
-
-/* T64 Image File Layout */
-
-#define TAPE_HDR_SIZE 64
-#define TAPE_DIR_SIZE 32
-
-
-/* Cassette Format Constants */
-
-#define CAS_TYPE_OFFSET	0
-#define CAS_STAD_OFFSET	1	/* start address */
-#define CAS_ENAD_OFFSET	3	/* end address */
-#define CAS_NAME_OFFSET	5	/* filename */
-
-#define CAS_TYPE_PRG	1	/* Binary Program */
-#define CAS_TYPE_BAS	3	/* Relocatable Program */
-#define CAS_TYPE_DATA	4	/* Data Record */
-#define CAS_TYPE_EOF	5	/* End of Tape marker */
-
-
-#ifdef __GNUC__
-#define PACK __attribute__ ((packed))	/* pack structures on byte boundaries */
-#else
-#define PACK		/* pack using a compiler switch instead */
-#endif
-
-
-typedef struct {
-    BYTE   type PACK;
-    BYTE   start[2] PACK;
-    BYTE   end[2] PACK;
-    BYTE   name[16] PACK;
-
-} CBM_CASBUF;
-
-
-typedef struct {
-    int    type;		/* emulation type */
-
-    int    ImageFormat;		/* Image File format: X00, FDX, FS, ... */
-
-    FILE  *FileDs;
-    char   ActiveName[256];
-
-
-    /* Emulation */
-
-    int    entries;		/* (maximum) Number of files on image */
-    int    FileType;		/* PRG, SEQ, ... */
-
-    BYTE  *directory;		/* Tape directory buffer */
-    short  entry;		/* Number of current entry slot */
-
-    int    sense;		/* tape sense */
-    int    state;		/* stream state */
-
-} TAPE;
-
-/* ------------------------------------------------------------------------- */
-
-extern int  is_pc64name(const char *name);
-extern int  write_pc64header(FILE *fd, const char *name, int reclen);
-extern int  read_pc64header(FILE *fd, char *name, int *reclen);
-extern char *pc_get_cbmname(FILE *fd, char *fsname);
-extern int  DirP00(FILE *fd, char *CbmName, int *start, int *end);
-
-extern int  check_t64_header(FILE *f);
-extern int  is_t64name(const char *name);
-extern char *read_tape_image_contents(const char *fname);
-
-#endif  /* _CBM_TAPE_H_ */
+#endif /* ndef _TAPE_H */
