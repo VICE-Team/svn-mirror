275499658674ea6ec646b861799b7f0600271eba
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:31:57 +0000

    This commit was generated by cvs2svn to compensate for changes in r13225, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@13226 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/drive/viad.h
+++ vice/src/drive/viad.h
@@ -28,45 +28,46 @@
 #ifndef _VIAD_H
 #define _VIAD_H
 
+#include "drivetypes.h"
 #include "types.h"
 
 struct drive_context_s;
 struct drive_s;
-struct drivevia_context_s;
+struct via_context_s;
 struct snapshot_s;
 
 extern void via1d_setup_context(struct drive_context_s *ctxptr);
 extern void via2d_setup_context(struct drive_context_s *ctxptr);
 
-extern void drive_via_set_atn(struct drive_context_s *ctxptr, int state);
+extern void drive_via_set_atn(struct via_context_s *via_context, int state);
 extern void via1_set_atn(struct drive_context_s *ctxptr, BYTE state);
 
 extern void via1d_init(struct drive_context_s *ctxptr);
-extern void via1d_reset(struct drive_context_s *ctxptr);
-extern void via1d_signal(struct drive_context_s *ctxptr, int line, int edge);
+extern void via1d_reset(struct via_context_s *via_context);
+extern void via1d_signal(struct via_context_s *via_context, int line, int edge);
 extern void REGPARM3 via1d_store(struct drive_context_s *ctxptr, WORD addr,
                                  BYTE byte);
 extern BYTE REGPARM2 via1d_read(struct drive_context_s *ctxptr, WORD addr);
 extern BYTE REGPARM2 via1d_peek(struct drive_context_s *ctxptr, WORD addr);
-extern int via1d_snapshot_write_module(struct drive_context_s *ctxptr,
+extern int via1d_snapshot_write_module(struct via_context_s *via_context,
                                        struct snapshot_s *p);
-extern int via1d_snapshot_read_module(struct drive_context_s *ctxptr,
+extern int via1d_snapshot_read_module(struct via_context_s *via_context,
                                       struct snapshot_s *p);
 
 extern void via2d_init(struct drive_context_s *ctxptr);
-extern void via2d_reset(struct drive_context_s *ctxptr);
-extern void via2d_signal(struct drive_context_s *ctxptr, int line, int edge);
+extern void via2d_reset(struct via_context_s *via_context);
+extern void via2d_signal(struct via_context_s *via_context, int line, int edge);
 extern void REGPARM3 via2d_store(struct drive_context_s *ctxptr, WORD addr,
                                  BYTE byte);
 extern BYTE REGPARM2 via2d_read(struct drive_context_s *ctxptr, WORD addr);
 extern BYTE REGPARM2 via2d_peek(struct drive_context_s *ctxptr, WORD addr);
-extern int via2d_snapshot_write_module(struct drive_context_s *ctxptr,
+extern int via2d_snapshot_write_module(struct via_context_s *via_context,
                                        struct snapshot_s *p);
-extern int via2d_snapshot_read_module(struct drive_context_s *ctxptr,
+extern int via2d_snapshot_read_module(struct via_context_s *via_context,
                                       struct snapshot_s *p);
 
 typedef struct via_initdesc_s {
-    struct drivevia_context_s *via_ptr;
+    struct via_context_s *via_ptr;
     void (*clk)(CLOCK, void*);
     void (*int_t1)(CLOCK);
     void (*int_t2)(CLOCK);
@@ -76,10 +77,12 @@ typedef struct via_initdesc_s {
 extern void via_drive_init(struct drive_context_s *ctxptr,
                            const via_initdesc_t *via_desc);
 
-#define drive0_via_set_atn(state) drive_via_set_atn(&drive0_context, state)
-#define drive1_via_set_atn(state) drive_via_set_atn(&drive1_context, state)
-#define via1d0_signal(l, e)       via1d_signal(&drive0_context, l, e)
-#define via1d1_signal(l, e)       via1d_signal(&drive1_context, l, e)
+#define drive0_via_set_atn(state) drive_via_set_atn(&(drive0_context.via1), \
+                                  state)
+#define drive1_via_set_atn(state) drive_via_set_atn(&(drive1_context.via1), \
+                                  state)
+#define via1d0_signal(l, e)       via1d_signal(&(drive0_context.via1), l, e)
+#define via1d1_signal(l, e)       via1d_signal(&(drive1_context.via1), l, e)
 
 extern void viad2_update_pcr(int pcrval, struct drive_s *dptr);
 
