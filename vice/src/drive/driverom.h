2555f64f886c6b1352090f48a0642de7a454f724
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:19:21 +0000

    This commit was generated by cvs2svn to compensate for changes in r11713, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@11714 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/drive/driverom.h
+++ vice/src/drive/driverom.h
@@ -42,47 +42,11 @@
 #define DRIVE_ROM3040_SIZE            0x3000
 #define DRIVE_ROM4040_SIZE            0x3000
 
-#define DRIVE_ROM1541_CHECKSUM      1988651
-
-/* RAM/ROM.  */
-extern BYTE drive_rom1541[];
-extern BYTE drive_rom1541ii[];
-extern BYTE drive_rom1551[];
-extern BYTE drive_rom1571[];
-extern BYTE drive_rom1581[];
-extern BYTE drive_rom2031[];
-extern BYTE drive_rom1001[];
-extern BYTE drive_rom2040[];
-extern BYTE drive_rom3040[];
-extern BYTE drive_rom4040[];
-
-/* If nonzero, the ROM image has been loaded.  */
-extern unsigned int rom1541_loaded;
-extern unsigned int rom1541ii_loaded;
-extern unsigned int rom1551_loaded;
-extern unsigned int rom1571_loaded;
-extern unsigned int rom1581_loaded;
-extern unsigned int rom2031_loaded;
-extern unsigned int rom1001_loaded;
-extern unsigned int rom2040_loaded;
-extern unsigned int rom3040_loaded;
-extern unsigned int rom4040_loaded;
-
 extern void drive_rom_init(void);
-extern void drive_rom_setup_image(unsigned int dnr);
 extern void drive_rom_initialize_traps(unsigned int dnr);
 extern int drive_rom_load_images(void);
-extern int drive_rom_do_1541_checksum(void);
-extern int drive_rom_load_1541(void);
-extern int drive_rom_load_1541ii(void);
-extern int drive_rom_load_1551(void);
-extern int drive_rom_load_1571(void);
-extern int drive_rom_load_1581(void);
-extern int drive_rom_load_2031(void);
-extern int drive_rom_load_1001(void);
-extern int drive_rom_load_2040(void);
-extern int drive_rom_load_3040(void);
-extern int drive_rom_load_4040(void);
+
+extern int drive_rom_load_ok;
 
 #endif
 
