773130f3d0eb600feac77afd0135a1df8bf8f38d
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:21:50 +0000

    This commit was generated by cvs2svn to compensate for changes in r12011, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@12012 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/sid/resid.cc
+++ vice/src/sid/resid.cc
@@ -4,6 +4,7 @@
  * Written by
  *  Teemu Rantanen <tvr@cs.hut.fi>
  *  Dag Lem <resid@nimrod.no>
+ *  Andreas Boose <viceteam@t-online.de>
  *
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
@@ -35,6 +36,7 @@ extern "C" {
 #include "log.h"
 #include "utils.h"
 #include "resources.h"
+#include "sid-snapshot.h"
 #include "sound.h"
 #include "types.h"
 
@@ -157,6 +159,51 @@ char *resid_dump_state(sound_t *psid)
     return stralloc("");
 }
 
+void resid_state_read(sound_t *psid, sid_snapshot_state_t *sid_state)
+{
+    SID::State state;
+    unsigned int i;
+
+    state = psid->sid.read_state();
+
+    for (i = 0; i < 0x20; i++) {
+        sid_state->sid_register[i] = (BYTE)state.sid_register[i];
+    }
+
+    sid_state->bus_value = (BYTE)state.bus_value;
+    sid_state->bus_value_ttl = (DWORD)state.bus_value_ttl;
+    for (i = 0; i < 3; i++) {
+        sid_state->accumulator[i] = (DWORD)state.accumulator[i];
+        sid_state->shift_register[i] = (DWORD)state.shift_register[i];
+        sid_state->rate_counter[i] = (WORD)state.rate_counter[i];
+        sid_state->exponential_counter[i] = (WORD)state.exponential_counter[i];
+        sid_state->envelope_counter[i] = (BYTE)state.envelope_counter[i];
+        sid_state->hold_zero[i] = (BYTE)state.hold_zero[i];
+    }
+}
+
+void resid_state_write(sound_t *psid, sid_snapshot_state_t *sid_state)
+{
+    SID::State state;
+    unsigned int i;
+
+    for (i = 0; i < 0x20; i++) {
+        state.sid_register[i] = (char)sid_state->sid_register[i];
+    }
+
+    state.bus_value = (reg8)sid_state->bus_value;
+    state.bus_value_ttl = (cycle_count)sid_state->bus_value_ttl;
+    for (i = 0; i < 3; i++) {
+        state.accumulator[i] = (reg24)sid_state->accumulator[i];
+        state.shift_register[i] = (reg24)sid_state->shift_register[i];
+        state.rate_counter[i] = (reg16)sid_state->rate_counter[i];
+        state.exponential_counter[i] = (reg16)sid_state->exponential_counter[i];
+        state.envelope_counter[i] = (reg8)sid_state->envelope_counter[i];
+        state.hold_zero[i] = (bool)sid_state->hold_zero[i];
+    }
+
+    psid->sid.write_state((const SID::State)state);
+}
 
 sid_engine_t resid_hooks =
 {
@@ -168,7 +215,9 @@ sid_engine_t resid_hooks =
     resid_reset,
     resid_calculate_samples,
     resid_prevent_clk_overflow,
-    resid_dump_state
+    resid_dump_state,
+    resid_state_read,
+    resid_state_write
 };
 
 } // extern "C"
