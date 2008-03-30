9a59b13847e2981e1f45977273b7b561ca6ddd50
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 17:01:18 +0000

    This commit was generated by cvs2svn to compensate for changes in r16747, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@16748 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/drive/iec/iec.c
+++ vice/src/drive/iec/iec.c
@@ -39,6 +39,7 @@
 #include "iecdrive.h"
 #include "iecrom.h"
 #include "lib.h"
+#include "mc6821.h"
 #include "memiec.h"
 #include "resources.h"
 #include "types.h"
@@ -73,6 +74,7 @@ void iec_drive_init(struct drive_context_s *drv)
     cia1571_init(drv);
     cia1581_init(drv);
     wd1770d_init(drv);
+    mc6821_init(drv);
 }
 
 void iec_drive_reset(struct drive_context_s *drv)
@@ -105,6 +107,7 @@ void iec_drive_reset(struct drive_context_s *drv)
     }
     /* FIXME:  which drive type needs this chip?? */
     wd1770d_reset(drv);
+    mc6821_reset(drv);
 }
 
 void iec_drive_mem_init(struct drive_context_s *drv, unsigned int type)
