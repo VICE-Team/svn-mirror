b6aa5ae21ad355656377c5c06451ca4fcb606b88
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:05:26 +0000

    This commit was generated by cvs2svn to compensate for changes in r10043, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@10044 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/tape/tapeimage.c
+++ vice/src/tape/tapeimage.c
@@ -27,6 +27,8 @@
 #include "vice.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "datasette.h"
 #include "log.h"
@@ -34,101 +36,134 @@
 #include "tap.h"
 #include "tape.h"
 #include "ui.h"
+#include "utils.h"
 
-
-/* T64 tape currently attached.  */
-t64_t *attached_t64_tape = NULL;
-
-/* TAP tape currently attached.  */
-tap_t *attached_tap_tape = NULL;
 
 static log_t tape_image_log = LOG_ERR;
 
-/* Detach.  */
-int tape_image_detach(unsigned int unit)
+int tape_image_close(tape_image_t *tape_image)
 {
-    int retval;
+    int retval = 0;
 
-    if (attached_t64_tape != NULL) {
-        log_message(tape_image_log,
-                    "Detaching T64 image `%s'.", attached_t64_tape->file_name);
-
-        /* Gone.  */
-        retval = t64_close(attached_t64_tape);
-        attached_t64_tape = NULL;
-        ui_display_tape_current_image("");
-
-        /* Tape detached: release play button.  */
-        datasette_set_tape_sense(0);
-
-        return retval;
+    switch (tape_image->type) {
+      case TAPE_TYPE_T64:
+        retval = t64_close((t64_t *)tape_image->data);
+        break;
+      case TAPE_TYPE_TAP:
+        retval = tap_close((tap_t *)tape_image->data);
+        break;
     }
 
-    if (attached_tap_tape != NULL) {
-        log_message(tape_image_log,
-                    "Detaching TAP image `%s'.", attached_tap_tape->file_name);
+    free(tape_image->name);
+    tape_image->name = NULL;
 
-        /* Gone.  */
-        retval = tap_close(attached_tap_tape);
-        attached_tap_tape = NULL;
-        ui_display_tape_current_image("");
-        datasette_set_tape_image(NULL);
-
-        tape_traps_install();
-
-        return retval;
-    }
-
-    return 0;
+    return retval;
 }
 
-/* Attach.  */
-int tape_image_attach(unsigned int unit, const char *name)
+int tape_image_open(tape_image_t *tape_image)
 {
     t64_t *new_t64_tape;
     tap_t *new_tap_tape;
 
-    if (!name || !*name)
-        return -1;
-
-    new_t64_tape = t64_open(name);
+    new_t64_tape = t64_open(tape_image->name, &tape_image->read_only);
     if (new_t64_tape != NULL) {
-        tape_image_detach(1);
-        attached_t64_tape = new_t64_tape;
-        ui_display_tape_current_image(name);
-
-        log_message(tape_image_log, "T64 image '%s' attached.", name);
-
-        /* Tape attached: press play button.  */
-        datasette_set_tape_sense(1);
-
+        tape_image->data = (void *)new_t64_tape;
+        tape_image->type = TAPE_TYPE_T64;
         return 0;
     }
 
-    new_tap_tape = tap_open(name);
+    new_tap_tape = tap_open(tape_image->name, &tape_image->read_only);
     if (new_tap_tape != NULL) {
-        tape_image_detach(1);
-        attached_tap_tape = new_tap_tape;
-        ui_display_tape_current_image(name);
-
-        datasette_set_tape_image(new_tap_tape);
-
-        log_message(tape_image_log, "TAP image '%s' attached.", name);
-        log_message(tape_image_log, "TAP image version: %i, system: %i.",
-                    new_tap_tape->version, new_tap_tape->system);
-
-        tape_traps_deinstall();
-
+        tape_image->data = (void *)new_tap_tape;
+        tape_image->type = TAPE_TYPE_TAP;
         return 0;
     }
 
     return -1;
 }
 
-int tape_tap_attched(void)
+/* Detach.  */
+int tape_image_detach(unsigned int unit)
 {
-    if (attached_tap_tape != NULL)
-        return 1;
+    int retval = 0;
+
+    if (unit != 1)
+        return -1;
+
+    if (tape_image_dev1->name == NULL)
+        return 0;
+
+    switch (tape_image_dev1->type) {
+      case TAPE_TYPE_T64:
+        log_message(tape_image_log,
+                    "Detaching T64 image `%s'.", tape_image_dev1->name);
+        /* Tape detached: release play button.  */
+        datasette_set_tape_sense(0);
+        break;
+      case TAPE_TYPE_TAP:
+        log_message(tape_image_log,
+                    "Detaching TAP image `%s'.", tape_image_dev1->name);
+        datasette_set_tape_image(NULL);
+
+        tape_traps_install();
+        break;
+      default:
+        log_error(tape_image_log, "Unknown tape type %i.",
+                  tape_image_dev1->type);
+    }
+
+    retval = tape_image_close(tape_image_dev1);
+
+    ui_display_tape_current_image("");
+
+    return retval;
+}
+
+/* Attach.  */
+int tape_image_attach(unsigned int unit, const char *name)
+{
+    tape_image_t tape_image;
+
+    if (unit != 1)
+        return -1;
+
+    if (!name || !*name)
+        return -1;
+
+    tape_image.name = stralloc(name);
+    tape_image.read_only = 0;
+
+    if (tape_image_open(&tape_image) < 0) {
+        free(tape_image.name);
+        log_error(tape_image_log, "Cannot open file `%s'", name);
+        return -1;
+    }
+
+    tape_image_detach(unit);
+
+    memcpy(tape_image_dev1, &tape_image, sizeof(tape_image_t));
+
+    ui_display_tape_current_image(tape_image_dev1->name);
+
+    switch (tape_image_dev1->type) {
+      case TAPE_TYPE_T64:
+        log_message(tape_image_log, "T64 image '%s' attached.", name);
+        /* Tape attached: press play button.  */
+        datasette_set_tape_sense(1);
+        break;
+      case TAPE_TYPE_TAP:
+        datasette_set_tape_image((tap_t *)tape_image_dev1->data);
+        log_message(tape_image_log, "TAP image '%s' attached.", name);
+        log_message(tape_image_log, "TAP image version: %i, system: %i.",
+                    ((tap_t *)tape_image_dev1->data)->version,
+                    ((tap_t *)tape_image_dev1->data)->system);
+        tape_traps_deinstall();
+        break;
+      default:
+        log_error(tape_image_log, "Unknown tape type %i.",
+                  tape_image_dev1->type);
+        return -1;
+    }
 
     return 0;
 }
