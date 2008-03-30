d2f5f2254b7ec5fa3334cbe4f8ab778015dfd021
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:30:42 +0000

    This commit was generated by cvs2svn to compensate for changes in r5875, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@5876 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/msdos/vsidui.c
@@ -0,0 +1,42 @@
+/*
+ * vsidui.c - Implementation of the C64-specific part of the UI.
+ *
+ * Written by
+ *  Dag Lem <resid@nimrod.no>
+ * based on c64ui.c written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  André Fachat <fachat@physik.tu-chemnitz.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+int vsid_ui_init(void)
+{
+    return 0;
+}
+
+
+int vsid_ui_exit(void)
+{
+    return 0;
+}
+
