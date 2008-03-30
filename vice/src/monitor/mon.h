047994b0b8c932b3290b07ac6b4a264b4034e53a
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 15:50:26 +0000

    This commit was generated by cvs2svn to compensate for changes in r8243, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@8244 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/monitor/mon.h
+++ vice/src/monitor/mon.h
@@ -1,10 +1,10 @@
 /*
- * mon.h - The VICE built-in monitor.
+ * mon.h - The VICE built-in monitor, external interface.
  *
  * Written by
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
  *  Daniel Sladic <sladic@eecg.toronto.edu>
  *  Ettore Perazzoli <ettore@comm2000.it>
- *  Andreas Boose <boose@linux.rz.fh-hannover.de>
  *
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
@@ -31,9 +31,11 @@
 
 #include "types.h"
 
-/* Types */
+/** Generic interface.  **/
+#define NUM_MEMSPACES 4
 
-typedef int bool;
+#define any_watchpoints(mem) \
+    (watchpoints_load[(mem)] || watchpoints_store[(mem)])
 
 enum mon_int {
     MI_NONE = 0,
@@ -41,35 +43,6 @@ enum mon_int {
     MI_WATCH = 1 << 1,
     MI_STEP = 1 << 2
 };
-
-enum t_reg_id {
-   e_A,
-   e_X,
-   e_Y,
-   e_PC,
-   e_SP,
-   e_FLAGS,
-   e_AF,
-   e_BC,
-   e_DE,
-   e_HL,
-   e_IX,
-   e_IY,
-   e_I,
-   e_R,
-   e_AF2,
-   e_BC2,
-   e_DE2,
-   e_HL2
-};
-typedef enum t_reg_id REG_ID;
-
-enum t_memory_op {
-   e_load,
-   e_store,
-   e_load_store
-};
-typedef enum t_memory_op MEMORY_OP;
 
 enum t_memspace {
    e_default_space = 0,
@@ -79,96 +52,6 @@ enum t_memspace {
    e_invalid_space
 };
 typedef enum t_memspace MEMSPACE;
-
-typedef unsigned int MON_ADDR;
-typedef unsigned int MON_REG;
-
-enum t_conditional {
-   e_INV,
-   e_EQU,
-   e_NEQ,
-   e_GT,
-   e_LT,
-   e_GTE,
-   e_LTE,
-   e_AND,
-   e_OR
-};
-typedef enum t_conditional CONDITIONAL;
-
-enum t_radixtype {
-   e_default_radix,
-   e_hexadecimal,
-   e_decimal,
-   e_octal,
-   e_binary
-};
-typedef enum t_radixtype RADIXTYPE;
-
-enum t_action {
-   e_OFF = 0,
-   e_ON = 1,
-   e_TOGGLE = 2
-};
-typedef enum t_action ACTION;
-
-struct cond_node_s {
-   int operation;
-   int value;
-   MON_REG reg_num;
-   bool is_reg;
-   bool is_parenthized;
-   struct cond_node_s *child1;
-   struct cond_node_s *child2;
-};
-typedef struct cond_node_s cond_node_t;
-
-typedef void monitor_toggle_func_t(int value);
-
-struct cpu_int_status_s;
-struct mos6510_regs_s;
-struct z80_regs_s;
-
-/* This is the standard interface through which the monitor accesses a
-   certain CPU.  */
-struct monitor_interface_s {
-
-    /* Pointer to the registers of the 6502 CPU.  */
-    struct mos6510_regs_s *cpu_regs;
-
-    /* Pointer to the registers of the Z80 CPU.  */
-    struct z80_regs_s *z80_cpu_regs;
-
-    /* Pointer to the alarm/interrupt status.  */
-    struct cpu_int_status_s *int_status;
-
-    /* Pointer to the machine's clock counter.  */
-    CLOCK *clk;
-
-#if 0
-    /* Pointer to a function that writes to memory.  */
-    read_func_t *read_func;
-
-    /* Pointer to a function that reads from memory.  */
-    store_func_t *store_func;
-#endif
-
-    int current_bank;
-    const char **(*mem_bank_list)(void);
-    int (*mem_bank_from_name)(const char *name);
-    BYTE (*mem_bank_read)(int bank, ADDRESS addr);
-    BYTE (*mem_bank_peek)(int bank, ADDRESS addr);
-    void (*mem_bank_write)(int bank, ADDRESS addr, BYTE byte);
-
-    struct mem_ioreg_list_s *(*mem_ioreg_list_get)(void);
-
-    /* Pointer to a function to disable/enable watchpoint checking.  */
-    monitor_toggle_func_t *toggle_watchpoints_func;
-
-    /* Update bank base (used for drives).  */
-    void (*set_bank_base)(void);
-};
-typedef struct monitor_interface_s monitor_interface_t;
 
 enum CPU_TYPE_s {
     CPU_6502,
@@ -185,151 +68,82 @@ struct monitor_cpu_type_s {
     void (*mon_register_set_val)(int mem, int reg_id, WORD val);
     void (*mon_register_print)(int mem);
     struct mon_reg_list_s *(*mon_register_list_get)(int mem);
-    void (*mon_register_list_set)(struct mon_reg_list_s *mon_reg_list, int mem);
-};
+    void (*mon_register_list_set)(struct mon_reg_list_s *mon_reg_list, int mem);};
 typedef struct monitor_cpu_type_s monitor_cpu_type_t;
 
-/* Defines */
+/* This is the standard interface through which the monitor accesses a
+   certain CPU.  */
+struct monitor_interface_s {
+    /* Pointer to the registers of the 6502 CPU.  */
+    struct mos6510_regs_s *cpu_regs;
 
-#ifndef TRUE
-#define TRUE 1
-#endif
+    /* Pointer to the registers of the Z80 CPU.  */
+    struct z80_regs_s *z80_cpu_regs;
 
-#ifndef FALSE
-#define FALSE 0
-#endif
+    /* Pointer to the alarm/interrupt status.  */
+    struct cpu_int_status_s *int_status;
 
-#define HI16(x) ((x)&0xffff0000)
-#define LO16(x) ((x)&0xffff)
-#define LO16_TO_HI16(x) (((x)&0xffff)<<16)
-#define HI16_TO_LO16(x) (((x)>>16)&0xffff)
+    /* Pointer to the machine's clock counter.  */
+    CLOCK *clk;
 
-#define STATE_INITIAL  0
-#define STATE_FNAME    1
-#define STATE_REG_ASGN 2
-#define STATE_ROL      3
-#define STATE_BNAME    4
-#define STATE_CTYPE    5
+    int current_bank;
+    const char **(*mem_bank_list)(void);
+    int (*mem_bank_from_name)(const char *name);
+    BYTE (*mem_bank_read)(int bank, ADDRESS addr);
+    BYTE (*mem_bank_peek)(int bank, ADDRESS addr);
+    void (*mem_bank_write)(int bank, ADDRESS addr, BYTE byte);
 
-#define FIRST_SPACE e_comp_space
-#define LAST_SPACE e_disk9_space
+    struct mem_ioreg_list_s *(*mem_ioreg_list_get)(void);
 
-#define NUM_MEMSPACES 4
-#define DEFAULT_DISASSEMBLY_SIZE 40
+    /* Pointer to a function to disable/enable watchpoint checking.  */
+    /*monitor_toggle_func_t *toggle_watchpoints_func;*/
+    void (*toggle_watchpoints_func)(int value);
 
-#define any_watchpoints_load(mem) (watchpoints_load[(mem)] != NULL)
-#define any_watchpoints_store(mem) (watchpoints_store[(mem)] != NULL)
-#define any_watchpoints(mem) (watchpoints_load[(mem)] || watchpoints_store[(mem)])
+    /* Update bank base (used for drives).  */
+    void (*set_bank_base)(void);
+};
+typedef struct monitor_interface_s monitor_interface_t;
 
-#define new_cond ((cond_node_t *)(xmalloc(sizeof(cond_node_t))))
-#define addr_memspace(ma) (HI16_TO_LO16(ma))
-#define addr_location(ma) (LO16(ma))
-#define new_addr(m, l) (LO16_TO_HI16(m) | (l))
-#define new_reg(m, r) (LO16_TO_HI16(m) | (r))
-#define reg_memspace(mr) (HI16_TO_LO16(mr))
-#define reg_regid(mr) (LO16(mr))
 
-/* Global variables */
-
-extern const char *_mon_space_strings[];
-
-struct console_s;
-struct monitor_cpu_type_s;
-
-extern struct console_s *console_log;
-extern int sidefx;
-extern int exit_mon;
-extern int mon_console_close_on_leaving;
-extern RADIXTYPE default_radix;
-extern MEMSPACE default_memspace;
-extern bool asm_mode;
-extern MEMSPACE caller_space;
-extern unsigned mon_mask[NUM_MEMSPACES];
-extern char *playback_name;
-extern bool playback;
-extern MON_ADDR asm_mode_addr;
-extern struct monitor_cpu_type_s monitor_cpu_type;
-extern MON_ADDR dot_addr[NUM_MEMSPACES];
-extern const char *mon_memspace_string[];
-extern int mon_stop_output;
-extern monitor_interface_t *mon_interfaces[NUM_MEMSPACES];
-extern bool force_array[NUM_MEMSPACES];
-
+/* Externals */
 struct break_list_s;
 extern struct break_list_s *watchpoints_load[NUM_MEMSPACES];
 extern struct break_list_s *watchpoints_store[NUM_MEMSPACES];
+extern MEMSPACE caller_space;
+extern unsigned mon_mask[NUM_MEMSPACES];
 
-/* Function declarations */
+
+
+/* Prototypes */
 extern void monitor_init(monitor_interface_t *maincpu_interface,
                          monitor_interface_t *drive8_interface_init,
                          monitor_interface_t *drive9_interface_init,
                          struct monitor_cpu_type_s **asmarray);
-extern bool mon_force_import(MEMSPACE mem);
+extern void mon(ADDRESS a);
+
+extern int mon_force_import(MEMSPACE mem);
 extern void mon_check_icount(ADDRESS a);
 extern void mon_check_icount_interrupt(void);
 extern void mon_check_watchpoints(ADDRESS a);
-extern void mon(ADDRESS a);
-extern void mon_open(ADDRESS a);
-extern int  mon_process(char *cmd);
-extern void mon_close(int check);
-
-extern void mon_add_number_to_buffer(int number);
-extern void mon_add_string_to_buffer(char *str);
-extern void mon_display_memory(int radix_type, MON_ADDR start_addr,
-                               MON_ADDR end_addr);
-extern void mon_display_data(MON_ADDR start_addr, MON_ADDR end_addr, int x,
-                             int y);
-extern void mon_display_screen(void);
-extern void mon_move_memory(MON_ADDR start_addr, MON_ADDR end_addr,
-                            MON_ADDR dest);
-extern void mon_compare_memory(MON_ADDR start_addr, MON_ADDR end_addr,
-                               MON_ADDR dest);
-extern void mon_fill_memory(MON_ADDR start_addr, MON_ADDR end_addr,
-                            unsigned char *data);
-extern void mon_hunt_memory(MON_ADDR start_addr, MON_ADDR end_addr,
-                            unsigned char *data);
-extern void mon_instructions_step(int count);
-extern void mon_instructions_next(int count);
-extern void mon_instruction_return(void);
-extern void mon_stack_up(int count);
-extern void mon_stack_down(int count);
-extern void mon_print_convert(int val);
-extern void mon_change_dir(const char *path);
-extern void mon_cpu_type(const char *cpu_type);
-extern void mon_bank(MEMSPACE mem, const char *bank);
-extern void mon_display_io_regs(void);
-extern void mon_evaluate_default_addr(MON_ADDR *a);
-extern void mon_set_mem_val(MEMSPACE mem, ADDRESS mem_addr, BYTE val);
-extern bool mon_inc_addr_location(MON_ADDR *a, unsigned inc);
-extern void mon_start_assemble_mode(MON_ADDR addr, char *asm_line);
-extern long mon_evaluate_address_range(MON_ADDR *start_addr, MON_ADDR *end_addr,
-                                       bool must_be_range, WORD default_len);
-
-extern bool check_drive_emu_level_ok(int drive_num);
-extern void mon_print_conditional(cond_node_t *cnode);
-extern void mon_delete_conditional(cond_node_t *cnode);
-extern int mon_evaluate_conditional(cond_node_t *cnode);
-extern bool mon_is_valid_addr(MON_ADDR a);
-extern bool mon_is_in_range(MON_ADDR start_addr, MON_ADDR end_addr,
-                            unsigned loc);
-
-extern BYTE mon_get_mem_val(MEMSPACE mem, ADDRESS mem_addr);
-extern BYTE mon_get_mem_val_ex(MEMSPACE mem, int bank, ADDRESS mem_addr);
-extern void mon_jump(MON_ADDR addr);
-
-extern char *mon_symbol_table_lookup_name(MEMSPACE mem, ADDRESS addr);
-extern int mon_symbol_table_lookup_addr(MEMSPACE mem, char *name);
-extern void mon_add_name_to_symbol_table(MON_ADDR addr, char *name);
-extern void mon_remove_name_from_symbol_table(MEMSPACE mem, char *name);
-extern void mon_print_symbol_table(MEMSPACE mem);
-extern void mon_load_symbols(MEMSPACE mem, const char *filename);
-extern void mon_save_symbols(MEMSPACE mem, const char *filename);
-
-extern void mon_record_commands(char *filename);
-extern void mon_end_recording(void);
 
 extern void mon_watch_push_load_addr(ADDRESS addr, MEMSPACE mem);
 extern void mon_watch_push_store_addr(ADDRESS addr, MEMSPACE mem);
+
+
+/** Breakpoint interface.  */
+#define check_breakpoints(mem, addr) \
+    mon_breakpoint_check_checkpoint(mem, addr, breakpoints[mem])
+
+/* Externals */
+extern struct break_list_s *breakpoints[NUM_MEMSPACES];
+
+/* Prototypes */
+extern int mon_breakpoint_check_checkpoint(MEMSPACE mem, ADDRESS addr,
+                                           struct break_list_s *list);
+
+
+/** Register interface.  */
+extern struct mon_reg_list_s *mon_register_list_get(int mem);
 
 #endif
 
---
+++ vice/src/monitor/montypes.h
@@ -0,0 +1,238 @@
+/*
+ * montypes.h - The VICE built-in monitor, internal interface.
+ *
+ * Written by
+ *  Daniel Sladic <sladic@eecg.toronto.edu>
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _MONTYPES_H
+#define _MONTYPES_H
+
+#include "mon.h"
+#include "types.h"
+
+/* Types */
+
+typedef int bool;
+
+enum t_reg_id {
+   e_A,
+   e_X,
+   e_Y,
+   e_PC,
+   e_SP,
+   e_FLAGS,
+   e_AF,
+   e_BC,
+   e_DE,
+   e_HL,
+   e_IX,
+   e_IY,
+   e_I,
+   e_R,
+   e_AF2,
+   e_BC2,
+   e_DE2,
+   e_HL2
+};
+typedef enum t_reg_id REG_ID;
+
+enum t_memory_op {
+   e_load,
+   e_store,
+   e_load_store
+};
+typedef enum t_memory_op MEMORY_OP;
+
+typedef unsigned int MON_ADDR;
+typedef unsigned int MON_REG;
+
+enum t_conditional {
+   e_INV,
+   e_EQU,
+   e_NEQ,
+   e_GT,
+   e_LT,
+   e_GTE,
+   e_LTE,
+   e_AND,
+   e_OR
+};
+typedef enum t_conditional CONDITIONAL;
+
+enum t_radixtype {
+   e_default_radix,
+   e_hexadecimal,
+   e_decimal,
+   e_octal,
+   e_binary
+};
+typedef enum t_radixtype RADIXTYPE;
+
+enum t_action {
+   e_OFF = 0,
+   e_ON = 1,
+   e_TOGGLE = 2
+};
+typedef enum t_action ACTION;
+
+struct cond_node_s {
+   int operation;
+   int value;
+   MON_REG reg_num;
+   bool is_reg;
+   bool is_parenthized;
+   struct cond_node_s *child1;
+   struct cond_node_s *child2;
+};
+typedef struct cond_node_s cond_node_t;
+
+typedef void monitor_toggle_func_t(int value);
+
+/* Defines */
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#define HI16(x) ((x)&0xffff0000)
+#define LO16(x) ((x)&0xffff)
+#define LO16_TO_HI16(x) (((x)&0xffff)<<16)
+#define HI16_TO_LO16(x) (((x)>>16)&0xffff)
+
+#define STATE_INITIAL  0
+#define STATE_FNAME    1
+#define STATE_REG_ASGN 2
+#define STATE_ROL      3
+#define STATE_BNAME    4
+#define STATE_CTYPE    5
+
+#define FIRST_SPACE e_comp_space
+#define LAST_SPACE e_disk9_space
+
+#define DEFAULT_DISASSEMBLY_SIZE 40
+
+#define any_watchpoints_load(mem) (watchpoints_load[(mem)] != NULL)
+#define any_watchpoints_store(mem) (watchpoints_store[(mem)] != NULL)
+
+#define new_cond ((cond_node_t *)(xmalloc(sizeof(cond_node_t))))
+#define addr_memspace(ma) (HI16_TO_LO16(ma))
+#define addr_location(ma) (LO16(ma))
+#define new_addr(m, l) (LO16_TO_HI16(m) | (l))
+#define new_reg(m, r) (LO16_TO_HI16(m) | (r))
+#define reg_memspace(mr) (HI16_TO_LO16(mr))
+#define reg_regid(mr) (LO16(mr))
+
+/* Global variables */
+
+extern const char *_mon_space_strings[];
+
+struct console_s;
+struct monitor_cpu_type_s;
+
+extern struct console_s *console_log;
+extern int sidefx;
+extern int exit_mon;
+extern int mon_console_close_on_leaving;
+extern RADIXTYPE default_radix;
+extern MEMSPACE default_memspace;
+extern bool asm_mode;
+extern char *playback_name;
+extern bool playback;
+extern MON_ADDR asm_mode_addr;
+extern struct monitor_cpu_type_s monitor_cpu_type;
+extern MON_ADDR dot_addr[NUM_MEMSPACES];
+extern const char *mon_memspace_string[];
+extern int mon_stop_output;
+extern monitor_interface_t *mon_interfaces[NUM_MEMSPACES];
+extern bool force_array[NUM_MEMSPACES];
+
+/* Function declarations */
+extern void mon_open(ADDRESS a);
+extern int  mon_process(char *cmd);
+extern void mon_close(int check);
+
+extern void mon_add_number_to_buffer(int number);
+extern void mon_add_string_to_buffer(char *str);
+extern void mon_display_memory(int radix_type, MON_ADDR start_addr,
+                               MON_ADDR end_addr);
+extern void mon_display_data(MON_ADDR start_addr, MON_ADDR end_addr, int x,
+                             int y);
+extern void mon_display_screen(void);
+extern void mon_move_memory(MON_ADDR start_addr, MON_ADDR end_addr,
+                            MON_ADDR dest);
+extern void mon_compare_memory(MON_ADDR start_addr, MON_ADDR end_addr,
+                               MON_ADDR dest);
+extern void mon_fill_memory(MON_ADDR start_addr, MON_ADDR end_addr,
+                            unsigned char *data);
+extern void mon_hunt_memory(MON_ADDR start_addr, MON_ADDR end_addr,
+                            unsigned char *data);
+extern void mon_instructions_step(int count);
+extern void mon_instructions_next(int count);
+extern void mon_instruction_return(void);
+extern void mon_stack_up(int count);
+extern void mon_stack_down(int count);
+extern void mon_print_convert(int val);
+extern void mon_change_dir(const char *path);
+extern void mon_cpu_type(const char *cpu_type);
+extern void mon_bank(MEMSPACE mem, const char *bank);
+extern void mon_display_io_regs(void);
+extern void mon_evaluate_default_addr(MON_ADDR *a);
+extern void mon_set_mem_val(MEMSPACE mem, ADDRESS mem_addr, BYTE val);
+extern bool mon_inc_addr_location(MON_ADDR *a, unsigned inc);
+extern void mon_start_assemble_mode(MON_ADDR addr, char *asm_line);
+extern long mon_evaluate_address_range(MON_ADDR *start_addr, MON_ADDR *end_addr,
+                                       bool must_be_range, WORD default_len);
+
+extern bool check_drive_emu_level_ok(int drive_num);
+extern void mon_print_conditional(cond_node_t *cnode);
+extern void mon_delete_conditional(cond_node_t *cnode);
+extern int mon_evaluate_conditional(cond_node_t *cnode);
+extern bool mon_is_valid_addr(MON_ADDR a);
+extern bool mon_is_in_range(MON_ADDR start_addr, MON_ADDR end_addr,
+                            unsigned loc);
+
+extern BYTE mon_get_mem_val(MEMSPACE mem, ADDRESS mem_addr);
+extern BYTE mon_get_mem_val_ex(MEMSPACE mem, int bank, ADDRESS mem_addr);
+extern void mon_jump(MON_ADDR addr);
+
+extern char *mon_symbol_table_lookup_name(MEMSPACE mem, ADDRESS addr);
+extern int mon_symbol_table_lookup_addr(MEMSPACE mem, char *name);
+extern void mon_add_name_to_symbol_table(MON_ADDR addr, char *name);
+extern void mon_remove_name_from_symbol_table(MEMSPACE mem, char *name);
+extern void mon_print_symbol_table(MEMSPACE mem);
+extern void mon_load_symbols(MEMSPACE mem, const char *filename);
+extern void mon_save_symbols(MEMSPACE mem, const char *filename);
+
+extern void mon_record_commands(char *filename);
+extern void mon_end_recording(void);
+
+extern void mon_watch_push_load_addr(ADDRESS addr, MEMSPACE mem);
+extern void mon_watch_push_store_addr(ADDRESS addr, MEMSPACE mem);
+
+#endif
+
