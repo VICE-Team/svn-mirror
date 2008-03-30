679d933def210d3dd59da1775f9a131543abb3d9
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:32:20 +0000

    This commit was generated by cvs2svn to compensate for changes in r13271, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@13272 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/vic20/vic20via1.c
+++ vice/src/vic20/vic20via1.c
@@ -39,105 +39,40 @@
 #include "log.h"
 #include "maincpu.h"
 #include "types.h"
+#include "via.h"
 #include "vic20.h"
 #include "vic20iec.h"
 #include "vic20via.h"
 
 
-#define myclk           (*(via_context->clk_ptr))
-#define myvia           (via_context->via)
-#define myviaifr        (via_context->ifr)
-#define myviaier        (via_context->ier)
-#define myviatal        (via_context->tal)
-#define myviatbl        (via_context->tbl)
-#define myviatau        (via_context->tau)
-#define myviatbu        (via_context->tbu)
-#define myviatai        (via_context->tai)
-#define myviatbi        (via_context->tbi)
-#define myviapb7        (via_context->pb7)
-#define myviapb7x       (via_context->pb7x)
-#define myviapb7o       (via_context->pb7o)
-#define myviapb7xx      (via_context->pb7xx)
-#define myviapb7sx      (via_context->pb7sx)
-#define oldpa           (via_context->oldpa)
-#define oldpb           (via_context->oldpb)
-#define myvia_ila       (via_context->ila)
-#define myvia_ilb       (via_context->ilb)
-#define ca2_state       (via_context->ca2_state)
-#define cb2_state       (via_context->cb2_state)
-#define myvia_t1_alarm  (via_context->t1_alarm)
-#define myvia_t2_alarm  (via_context->t2_alarm)
-
-#define via_read_clk    (via_context->read_clk)
-#define via_read_offset (via_context->read_offset)
-#define via_last_read   (via_context->last_read)
-#define snap_module_name (via_context->my_module_name)
-
-#define myvia_int_num   (via_context->int_num)
-#define MYVIA_INT       (via_context->irq_line)
-
-#define mycpu_rmw_flag  (*(via_context->rmw_flag))
-
-#define myvia_reset     via1_reset
-
-#define myvia_store     via1x_store
-#define myvia_read      via1x_read
-#define myvia_peek      via1x_peek
-
-void REGPARM2 myvia_store(via_context_t *via_context, WORD addr, BYTE data);
-BYTE REGPARM1 myvia_read(via_context_t *via_context, WORD addr);
-BYTE REGPARM1 myvia_peek(via_context_t *via_context, WORD addr);
-
 void REGPARM2 via1_store(WORD addr, BYTE data)
 {
-    myvia_store(&(machine_context.via1), addr, data);
+    viacore_store(&(machine_context.via1), addr, data);
 }
 
 BYTE REGPARM1 via1_read(WORD addr)
 {
-    return myvia_read(&(machine_context.via1), addr);
+    return viacore_read(&(machine_context.via1), addr);
 }
 
 BYTE REGPARM1 via1_peek(WORD addr)
 {
-    return myvia_peek(&(machine_context.via1), addr);
+    return viacore_peek(&(machine_context.via1), addr);
 }
 
-#define myvia_log       (via_context->log)
-#define myvia_signal    via1_signal
-#define myvia_snapshot_read_module via1_snapshot_read_module
-#define myvia_snapshot_write_module via1_snapshot_write_module
-
-
-static void via_set_ca2(int state)
+static void set_ca2(int state)
 {
 }
 
-static void via_set_cb2(int state)
+static void set_cb2(int state)
 {
 }
 
-static void via_set_int(via_context_t *via_context, unsigned int int_num,
-                        int value)
+static void set_int(via_context_t *via_context, unsigned int int_num,
+                    int value)
 {
     interrupt_set_irq(maincpu_int_status, int_num, value,
                       *(via_context->clk_ptr));
-}
-
-void vic20via1_setup_context(machine_context_t *machine_context)
-{
-    machine_context->via1.context = NULL;
-
-    machine_context->via1.rmw_flag = &maincpu_rmw_flag;
-    machine_context->via1.clk_ptr = &maincpu_clk;
-
-    sprintf(machine_context->via1.myname, "Via1");
-    sprintf(machine_context->via1.my_module_name, "VIA1");
-    machine_context->via1.read_clk = 0;
-    machine_context->via1.read_offset = 0;
-    machine_context->via1.last_read = 0;
-    machine_context->via1.irq_line = IK_IRQ;
-    machine_context->via1.log = LOG_ERR;
 }
 
 static void undump_acr(via_context_t *via_context, BYTE byte)
@@ -173,14 +108,15 @@ inline static void store_prb(via_context_t *via_context, BYTE byte,
                              BYTE myoldpb, WORD addr)
 {
     if ((byte ^ myoldpb) & 8)
-        datasette_toggle_write_bit((~myvia[VIA_DDRB] | byte) & 0x8);
+        datasette_toggle_write_bit((~(via_context->via[VIA_DDRB]) | byte)
+                                   & 0x8);
 }
 
 static void undump_pcr(via_context_t *via_context, BYTE byte)
 {
 }
 
-static void res_via(via_context_t *via_context)
+static void reset(via_context_t *via_context)
 {
 /*iec_pcr_write(0x22);*/
 }
@@ -188,7 +124,7 @@ static void res_via(via_context_t *via_context)
 inline static BYTE store_pcr(via_context_t *via_context, BYTE byte, WORD addr)
 {
     /* FIXME: this should use via_set_ca2() and via_set_cb2() */
-    if (byte != myvia[VIA_PCR]) {
+    if (byte != via_context->via[VIA_PCR]) {
         register BYTE tmp = byte;
         /* first set bit 1 and 5 to the real output values */
         if ((tmp & 0x0c) != 0x0c)
@@ -203,8 +139,8 @@ inline static BYTE read_pra(via_context_t *via_context, WORD addr)
 {
     BYTE byte;
     /* FIXME: not 100% sure about this... */
-    BYTE val = ~myvia[VIA_DDRA];
-    BYTE msk = oldpb;
+    BYTE val = ~(via_context->via[VIA_DDRA]);
+    BYTE msk = via_context->oldpb;
     BYTE m;
     int i;
 
@@ -212,7 +148,7 @@ inline static BYTE read_pra(via_context_t *via_context, WORD addr)
         if (!(msk & m))
             val &= ~rev_keyarr[i];
 
-    byte = val | (myvia[VIA_PRA] & myvia[VIA_DDRA]);
+    byte = val | (via_context->via[VIA_PRA] & via_context->via[VIA_DDRA]);
     return byte;
 }
 
@@ -220,8 +156,8 @@ inline static BYTE read_prb(via_context_t *via_context)
 {
     BYTE byte;
     /* FIXME: not 100% sure about this... */
-    BYTE val = ~myvia[VIA_DDRB];
-    BYTE msk = oldpa;
+    BYTE val = ~(via_context->via[VIA_DDRB]);
+    BYTE msk = via_context->oldpa;
     int m, i;
 
     for (m = 0x1, i = 0; i < 8; m <<= 1, i++)
@@ -233,27 +169,23 @@ inline static BYTE read_prb(via_context_t *via_context)
     if ((joystick_value[1] | joystick_value[2]) & 0x8)
         val &= 0x7f;
 
-    byte = val | (myvia[VIA_PRB] & myvia[VIA_DDRB]);
+    byte = val | (via_context->via[VIA_PRB] & via_context->via[VIA_DDRB]);
     return byte;
 }
 
-static void clk_overflow_callback(via_context_t *, CLOCK, void *);
-static void int_myviat1(via_context_t *, CLOCK);
-static void int_myviat2(via_context_t *, CLOCK);
-
 static void clk_overflow_callback_via1(CLOCK sub, void *data)
 {
-    clk_overflow_callback(&(machine_context.via1), sub, data);
+    viacore_clk_overflow_callback(&(machine_context.via1), sub, data);
 }
 
 static void int_via1t1(CLOCK c)
 {
-    int_myviat1(&(machine_context.via1), c);
+    viacore_intt1(&(machine_context.via1), c);
 }
 
 static void int_via1t2(CLOCK c)
 {
-    int_myviat2(&(machine_context.via1), c);
+    viacore_intt2(&(machine_context.via1), c);
 }
 
 void via1_init(via_context_t *via_context)
@@ -275,7 +207,40 @@ void via1_init(via_context_t *via_context)
     clk_guard_add_callback(maincpu_clk_guard, clk_overflow_callback_via1, NULL);
 }
 
-#define VIA_SHARED_CODE
+void vic20via1_setup_context(machine_context_t *machine_context)
+{
+    via_context_t *via;
 
-#include "viacore.c"
+    via = &(machine_context->via1);
+
+    via->context = NULL;
+
+    via->rmw_flag = &maincpu_rmw_flag;
+    via->clk_ptr = &maincpu_clk;
+
+    sprintf(via->myname, "Via1");
+    sprintf(via->my_module_name, "VIA1");
+    via->read_clk = 0;
+    via->read_offset = 0;
+    via->last_read = 0;
+    via->irq_line = IK_IRQ;
+    via->log = LOG_ERR;
+
+    via->undump_pra = undump_pra;
+    via->undump_prb = undump_prb;
+    via->undump_pcr = undump_pcr;
+    via->undump_acr = undump_acr;
+    via->store_pra = store_pra;
+    via->store_prb = store_prb;
+    via->store_pcr = store_pcr;
+    via->store_acr = store_acr;
+    via->store_sr = store_sr;
+    via->store_t2l = store_t2l;
+    via->read_pra = read_pra;
+    via->read_prb = read_prb;
+    via->set_int = set_int;
+    via->set_ca2 = set_ca2;
+    via->set_cb2 = set_cb2;
+    via->reset = reset;
+}
 
