879c69afdadab916206bad3f5a2083816e4cdceb
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 16:44:29 +0000

    This commit was generated by cvs2svn to compensate for changes in r14729, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@14730 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/main.c
+++ vice/src/main.c
@@ -44,7 +44,6 @@
 #include "console.h"
 #include "debug.h"
 #include "drive.h"
-#include "fliplist.h"
 #include "fullscreen.h"
 #include "init.h"
 #include "initcmdline.h"
@@ -148,7 +147,6 @@ int main_program(int argc, char **argv)
         resources_set_value("SoundSuspendTime", (resource_value_t)0);
     } else {
         int retval = resources_load(NULL);
-        char *filename;
 
         if (retval < 0) {
             /* The resource file might contain errors, and thus certain
@@ -158,9 +156,6 @@ int main_program(int argc, char **argv)
                 return -1;
             }
         }
-        filename = archdep_default_fliplist_file_name();
-        flip_load_list((unsigned int)-1, filename, 0);
-        lib_free(filename);
     }
 
     if (initcmdline_check_args(argc, argv) < 0)
