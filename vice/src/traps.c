83425adae12bbbc8bf94df6a6bd9fbfb3f80e006
trikalio <Spiro.Trikaliotis@gmx.de>
2008-03-30 14:57:46 +0000

    This commit was generated by cvs2svn to compensate for changes in r1933, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@1934 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/traps.c
+++ vice/src/traps.c
@@ -4,6 +4,7 @@
  * Written by
  *   Teemu Rantanen (tvr@cs.hut.fi)
  *   Jarkko Sonninen (sonninen@lut.fi)
+ *   Ettore Perazzoli (ettore@comm2000.it)
  *
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
@@ -35,6 +36,9 @@
 #include "maincpu.h"
 #include "mem.h"
 #include "interrupt.h"
+#include "resources.h"
+#include "cmdline.h"
+#include "utils.h"
 
 /* #define DEBUG_TRAPS */
 
@@ -45,89 +49,168 @@ typedef struct _traplist_t {
 
 static traplist_t *traplist;
 
-void    initialize_traps()
+static int install_trap(const trap_t *t);
+static int remove_trap(const trap_t *t);
+
+/* ------------------------------------------------------------------------- */
+
+/* Trap-related resources.  */
+
+/* Flag: Should we avoid installing traps at all?  */
+static int no_traps_enabled;
+
+static int set_no_traps_enabled(resource_value_t v)
+{
+    int new_value = (int) v;
+
+    if ((!no_traps_enabled && new_value) || (no_traps_enabled && !new_value)) {
+        if (new_value) {
+            /* Traps have been disabled.  */
+            traplist_t *p;
+
+            for (p = traplist; p != NULL; p = p->next)
+                remove_trap(p->trap);
+        } else {
+            /* Traps have been enabled.  */
+            traplist_t *p;
+
+            for (p = traplist; p != NULL; p = p->next)
+                install_trap(p->trap);
+        }
+    }
+
+    no_traps_enabled = new_value;
+    return 0;
+}
+
+static resource_t resources[] = {
+    { "NoTraps", RES_INTEGER, (resource_value_t) 0,
+      (resource_value_t) &no_traps_enabled, set_no_traps_enabled },
+    { NULL }
+};
+
+int traps_init_resources(void)
+{
+    return resources_register(resources);
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* Trap-related command-line options.  */
+
+static cmdline_option_t cmdline_options[] = {
+    { "-traps", SET_RESOURCE, 0, NULL, NULL, "NoTraps", (resource_value_t) 0,
+      NULL, "Enable Kernal serial traps for fast serial/tape emulation" },
+    { "+traps", SET_RESOURCE, 0, NULL, NULL, "NoTraps", (resource_value_t) 1,
+      NULL, "Do not install any Kernal traps" },
+    { NULL }
+};
+
+int traps_init_cmdline_options(void)
+{
+    return cmdline_register_options(cmdline_options);
+}
+
+/* ------------------------------------------------------------------------- */
+
+void traps_init(void)
 {
     traplist = NULL;
 }
 
-int     set_trap(const trap_t *t)
+static int install_trap(const trap_t *t)
 {
-    int     i;
-    traplist_t *p;
+    int i;
 
-#ifdef DEBUG_TRAPS
-    printf("setting up trap %s at 0x%x\n", t -> name, t -> address);
-#endif
+    printf("TRAPS: Patching ROM for trap `%s' at $%04X\n",
+           t->name, t->address);
 
     for (i = 0; i < 3; i++) {
-	if (read_rom(t -> address + i) != t -> check[i]) {
-	    printf("incorrect checkbyte for trap %s. not installed\n",
-		t -> name);
-	    return 1;
+	if (read_rom(t->address + i) != t->check[i]) {
+	    printf("TRAPS: Incorrect checkbyte for trap `%s'.  Not installed.\n",
+                   t->name);
+	    return -1;
 	}
     }
 
-    /*
-     * BRK (0x00) is trap-opcode
-     */
-    store_rom(t -> address, 0x00);
+    /* BRK (0x00) is trap-opcode.  */
+    store_rom(t->address, 0x00);
 
-    p = (traplist_t *) malloc (sizeof (traplist_t));
+    return 0;
+}
+
+int traps_add(const trap_t *t)
+{
+    int i;
+    traplist_t *p;
+
+    printf("TRAPS: Adding trap `%s' at $%04X.\n", t->name, t->address);
+
+    p = (traplist_t *) xmalloc (sizeof (traplist_t));
     p->next = traplist;
     p->trap = t;
     traplist = p;
 
+    if (!no_traps_enabled)
+        install_trap(t);
+
     return 0;
 }
 
+static int remove_trap(const trap_t *t)
+{
+    printf("TRAPS: Removing trap `%s' at $%04X.\n", t->name, t->address);
 
-int     remove_trap(const trap_t *t)
+    if (read_rom(t->address) != 0x00) {
+	printf("TRAPS: No trap `%s' installed?\n", t->name);
+        return -1;
+    }
+
+    store_rom(t->address, t->check[0]);
+    return 0;
+}
+
+int traps_remove(const trap_t *t)
 {
     traplist_t *p = traplist, *prev = NULL;
 
-#ifdef DEBUG_TRAPS
-    printf("removing trap %s at 0x%x\n", t -> name, t -> address);
-#endif
-
-    if (read_rom(t -> address) != 0x00)
-	printf("incorrect checkbyte for trap %s.\n", t -> name);
-
-    store_rom(t -> address, t->check[0]);
-
-
     while (p) {
-	if (p -> trap -> address == t -> address)
+	if (p->trap->address == t->address)
 	    break;
 	prev = p;
-	p = p -> next;
+	p = p->next;
     }
 
     if (!p) {
-	printf("cannot remove trap %s.\n", t -> name);
+	printf("TRAPS: Trap `%s' not found.\n", t->name);
 	return -1;
     }
 
     if (prev)
-	prev -> next = p -> next;
+	prev->next = p->next;
     else
-	traplist = p -> next;
+	traplist = p ->next;
 
-    free ((char *)p);
+    free(p);
+
+    if (!no_traps_enabled)
+        remove_trap(t);
+
     return 0;
 }
 
-int     trap_handler(void)
+int traps_handler(void)
 {
     traplist_t *p = traplist;
 
     while (p) {
-	if (p -> trap -> address == maincpu_regs.pc) {
-	    (*p -> trap -> func) ();
+	if (p->trap->address == maincpu_regs.pc) {
+	    (*p->trap->func)();
             maincpu_regs.pc = p->trap->resume_address;
-	    return 0;
+            return 0;
 	}
-	p = p -> next;
+	p = p->next;
     }
 
-    return 1;
+    return -1;
 }
