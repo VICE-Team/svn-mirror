48769b58c14d6e6ee957a0c9336e3abae38faea2
trikalio <Spiro.Trikaliotis@gmx.de>
2008-04-06 09:36:54 +0000

    Imported 1.6.6
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@17676 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/os2/fonts/vice-cbm.asm
+++
@@ -1,4 +0,0 @@
-code segment word      ;Makes dummy code segment aligned on word boundary
-db "empty_segment"     ;Initializes a string in dummy segment
-code ends              ;Dummy segment ends here
-end                    ;Terminates source file
