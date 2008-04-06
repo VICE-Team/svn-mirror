7c3c68b1fa69cb8bd83e23742c620c37112bf91f
trikalio <Spiro.Trikaliotis@gmx.de>
2008-04-06 14:58:21 +0000

    Imported 1.13.24
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@18322 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/msdos/ui.c
+++ vice/src/arch/msdos/ui.c
@@ -358,9 +358,11 @@ void ui_enable_drive_status(ui_drive_enable_t state, int *drive_led_color)
 
 /* drive_base is either 8 or 0 depending on unit or drive display.
    Dual drives display drive 0: and 1: instead of unit 8: and 9: */
-void ui_display_drive_track(int drive_number, int drive_base, 
-						double track_number)
+void ui_display_drive_track(unsigned int drive_number, unsigned int drive_base, 
+                            unsigned int half_track_number)
 {
+    double track_number = (double)half_track_number;
+
     ui_status_track[drive_number] = track_number;
     ui_draw_drive_status(drive_number);
 
