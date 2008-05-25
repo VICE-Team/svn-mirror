71162dd5024a548cd0fcbad8d5a56bb53f5b9b8b
strik <strik@users.sourceforge.net>
2008-05-25 19:05:02 +0000

    2008-05-25  Spiro Trikaliotis  <spiro.trikaliotis@gmx.de>
    	* Version 1.22.16.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@18809 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/version.h
+++ vice/src/version.h
@@ -28,11 +28,11 @@
 #define __VERSION_H__
 
 #ifndef VERSION
-#define VERSION "1.22.15"
+#define VERSION "1.22.16"
 #endif
 
 #ifndef VERSION_RC_NUMBER
-#define VERSION_RC_NUMBER 1,22,15,0
+#define VERSION_RC_NUMBER 1,22,16,0
 #endif
 
 #ifndef PACKAGE
