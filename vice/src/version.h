1b7f373b2c2fccd63cf18c1f3dadf4b52be837c7
strik <strik@users.sourceforge.net>
2008-07-04 19:16:34 +0000

    2008-07-04  Spiro Trikaliotis  <spiro.trikaliotis@gmx.de>
    	* Version 1.22.23
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@18959 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/version.h
+++ vice/src/version.h
@@ -28,11 +28,11 @@
 #define __VERSION_H__
 
 #ifndef VERSION
-#define VERSION "1.22.22"
+#define VERSION "1.22.23"
 #endif
 
 #ifndef VERSION_RC_NUMBER
-#define VERSION_RC_NUMBER 1,22,22,0
+#define VERSION_RC_NUMBER 1,22,23,0
 #endif
 
 #ifndef PACKAGE
