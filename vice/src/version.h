85f58246e4060cc79b80a6a27146ed1d24fef319
strik <strik@users.sourceforge.net>
2008-11-10 16:05:19 +0000

    Version 2.0.8
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@19587 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/version.h
+++ vice/src/version.h
@@ -28,11 +28,11 @@
 #define __VERSION_H__
 
 #ifndef VERSION
-#define VERSION "2.0.7"
+#define VERSION "2.0.8"
 #endif
 
 #ifndef VERSION_RC_NUMBER
-#define VERSION_RC_NUMBER 2,0,7,0
+#define VERSION_RC_NUMBER 2,0,8,0
 #endif
 
 #ifndef PACKAGE
