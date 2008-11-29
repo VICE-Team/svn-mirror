e37dc7e5d80d090a304a4fd1c7021036af836a21
strik <strik@users.sourceforge.net>
2008-11-29 17:58:19 +0000

    Reworked code which was using Unix style calls sometimes (SOCKET is unsigned on Windows, but tested for < 0 to find out about invalid socket; used close() for closing a socket, not closesocket(), ...); use gethostbyname() to accept names for remote targets, too.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@19754 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/rs232net.c
+++ vice/src/arch/win32/rs232net.c
@@ -2,6 +2,8 @@
  * rs232net.c - RS232 emulation.
  *
  * Written by
+ *  Tim Newsham
+ *  Spiro Trikaliotis <spiro.trikaliotis@gmx.de>
  *
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
@@ -45,6 +47,7 @@
 #include <io.h>
 #endif
 
+#include "lib.h"
 #include "log.h"
 #include "rs232.h"
 #include "types.h"
@@ -75,14 +78,14 @@ int rs232net_cmdline_options_init(void)
 /* ------------------------------------------------------------------------- */
 
 typedef struct rs232net {
-    int inuse;
-    SOCKET fd;
-    char *file;
-    int rts;
-    int dtr;
+    int inuse; /*!< 0 if the connection has not been opened, 1 otherwise. */
+    SOCKET fd; /*!< the SOCKET for the connection. If fd is INVALID_SOCKET
+                    although inuse == 1, then the socket has been closed
+                    because of a previous error. This prevents the error
+                    log from being flooded with error messages. */
 } rs232net_t;
 
-static rs232net_t fds[RS232_NUM_DEVICES];
+static rs232net_t fds[RS232_NUM_DEVICES] = { 0 };
 
 static log_t rs232net_log = LOG_ERR;
 
@@ -95,14 +98,12 @@ void rs232net_init(void)
 {
     WORD wVersionRequested = MAKEWORD(1, 1);
     WSADATA wsaData;
-    int i;
 
-    WSAStartup(wVersionRequested, &wsaData);
+    rs232net_log = log_open("RS232NET");
 
-    for (i = 0; i < RS232_NUM_DEVICES; i++)
-        fds[i].inuse = 0;
-
-    rs232net_log = log_open("RS232");
+    if ( WSAStartup(wVersionRequested, &wsaData) != 0 ) {
+        log_message(rs232net_log, "WSAStartup() failed!");
+    }
 }
 
 /* reset RS232 stuff */
@@ -118,82 +119,143 @@ void rs232net_reset(void)
 }
 
 static int
-getaddr(char *dev, struct sockaddr_in *ad)
+getaddr(const char * const dev_in, struct sockaddr_in *ad)
 {
-    char *p;
+    char *port;
+    char *dev;
+    int ret = -1;
 
-    memset(ad, 0, sizeof ad);
-    ad->sin_family = AF_INET;
+    /*! \todo: Also accept HOSTNAME:port */
 
-    dev = strdup(dev);
-    p = strchr(dev, ':');
-    if(!p) {
-        free(dev);
-        return -1;
-    }
+    do {
+        struct hostent * hostentry;
 
-    *p = 0;
-    ad->sin_addr.s_addr = inet_addr(dev);
-    ad->sin_port = htons((unsigned short)atoi(p+1));
-    free(dev);
-    if(ad->sin_addr.s_addr == -1 || ad->sin_port == 0)
-        return -1;
+        ad->sin_family = AF_INET;
 
-    return 0;
+        dev = lib_stralloc(dev_in);
+        port = strchr(dev, ':');
+
+        if ( ! port ) {
+            break;
+        }
+
+        *port++ = 0;
+
+        /* first, check if we have a host name */
+
+        hostentry = gethostbyname(dev);
+
+        if (hostentry != NULL && hostentry->h_addrtype == AF_INET) {
+            /* yes, we have a host name: process it */
+            if ( hostentry->h_length != sizeof ad->sin_addr.s_addr ) {
+                /* something weird happened... SHOULD NOT HAPPEN! */
+                log_error(rs232net_log,
+                          "gethostbyname() returned an IPv4 address, "
+                          "but the length is wrong: %u", hostentry->h_length );
+                break;
+            }
+
+            memcpy(&ad->sin_addr.s_addr,
+                   hostentry->h_addr_list[0],
+                   sizeof ad->sin_addr.s_addr);
+        }
+        else {
+            /* no host name: Assume it is an IP address */
+            ad->sin_addr.s_addr = inet_addr(dev);
+        }
+
+        ad->sin_port = htons( (unsigned short) atoi(port) );
+
+        if ( ntohl(ad->sin_addr.s_addr) != -1 && ntohs(ad->sin_port) != 0 ) {
+            ret = 0;
+        }
+
+        log_message(rs232net_log, "Connecting to %s:%u", 
+                                  inet_ntoa(ad->sin_addr), ntohs(ad->sin_port));
+
+    } while (0);
+
+    lib_free(dev);
+
+    return ret;
 }
 
 /* opens a rs232 window, returns handle to give to functions below. */
 int rs232net_open(int device)
 {
-    struct sockaddr_in ad;
-    int i;
+    struct sockaddr_in ad = { 0 };
+    int index = -1;
 
-    // parse the address
-    if(getaddr(rs232_devfile[device], &ad) == -1) {
-        log_error(rs232net_log, "Bad device name.  Should be ipaddr:port.");
-        return -1;
-    }
+    do {
+        int i;
 
-    for (i = 0; i < RS232_NUM_DEVICES; i++) {
-        if (!fds[i].inuse)
+        /* parse the address */
+        if ( getaddr(rs232_devfile[device], &ad) == -1 ) {
+            log_error(rs232net_log, "Bad device name.  Should be ipaddr:port, but is '%s'.",
+                                    rs232_devfile[device]);
             break;
-    }
-    if (i >= RS232_NUM_DEVICES) {
-        log_error(rs232net_log, "No more devices available.");
-        return -1;
-    }
+        }
 
-    DEBUG_LOG_MESSAGE((rs232net_log, "rs232net_open(device=%d).", device));
+        for (i = 0; i < RS232_NUM_DEVICES; i++) {
+            if (!fds[i].inuse) {
+                break;
+            }
+        }
 
-    // connect socket
-    fds[i].fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(fds[i].fd == INVALID_SOCKET ||
-       connect(fds[i].fd, (struct sockaddr*)&ad, sizeof ad) == -1) {
-        log_error(rs232net_log, "Cant open connection.");
-        return -1;
-    }
+        if (i >= RS232_NUM_DEVICES) {
+            log_error(rs232net_log, "No more devices available.");
+            break;
+        }
 
-    fds[i].inuse = 1;
-    fds[i].file = rs232_devfile[device];
-    return i;
+        DEBUG_LOG_MESSAGE((rs232net_log, "rs232net_open(device=%d).", device));
+
+        /* connect socket */
+        fds[i].fd = socket(AF_INET, SOCK_STREAM, 0);
+        if ( fds[i].fd == INVALID_SOCKET ) {
+            log_error(rs232net_log, "Can't open socket.");
+            break;
+        }
+
+        if ( connect(fds[i].fd, (struct sockaddr*) &ad, sizeof ad) == SOCKET_ERROR ) {
+            log_error(rs232net_log, "Cant open connection.");
+            break;
+        }
+
+        fds[i].inuse = 1;
+
+        index = i;
+
+    } while (0);
+
+    return index;
+}
+
+static void rs232net_closesocket(int index)
+{
+    closesocket(fds[index].fd);
+    fds[index].fd = INVALID_SOCKET;
 }
 
 /* closes the rs232 window again */
 void rs232net_close(int fd)
 {
-    DEBUG_LOG_MESSAGE((rs232net_log, "close(fd=%d).", fd));
+    do {
 
-    if (fd < 0 || fd >= RS232_NUM_DEVICES) {
-        log_error(rs232net_log, "Attempt to close invalid fd %d.", fd);
-        return;
-    }
-    if (!fds[fd].inuse) {
-        log_error(rs232net_log, "Attempt to close non-open fd %d.", fd);
-        return;
-    }
+        DEBUG_LOG_MESSAGE((rs232net_log, "close(fd=%d).", fd));
 
-    close(fds[fd].fd);
-    fds[fd].inuse = 0;
+        if (fd < 0 || fd >= RS232_NUM_DEVICES) {
+            log_error(rs232net_log, "Attempt to close invalid fd %d.", fd);
+            break;
+        }
+        if (!fds[fd].inuse) {
+            log_error(rs232net_log, "Attempt to close non-open fd %d.", fd);
+            break;
+        }
+
+        rs232net_closesocket(fd);
+        fds[fd].inuse = 0;
+
+    } while (0);
 }
 
 /* sends a byte to the RS232 line */
@@ -210,18 +272,17 @@ int rs232net_putc(int fd, BYTE b)
         return -1;
     }
 
-    /* silently drop if socket is shut */
-    if (fds[fd].fd < 0)
+    /* silently drop if socket is shut because of a previous error */
+    if (fds[fd].fd == INVALID_SOCKET)
         return 0;
 
     /* for the beginning... */
     DEBUG_LOG_MESSAGE((rs232net_log, "Output `%c'.", b));
 
     n = send(fds[fd].fd, &b, 1, 0);
-    if (n != 1) {
-        log_error(rs232net_log, "Error writing: %s.", strerror(errno));
-        close(fds[fd].fd);
-        fds[fd].fd = -1;
+    if (n == SOCKET_ERROR) {
+        log_error(rs232net_log, "Error writing: %u.", WSAGetLastError());
+        rs232net_closesocket(fd);
         return -1;
     }
 
@@ -232,51 +293,60 @@ int rs232net_putc(int fd, BYTE b)
 int rs232net_getc(int fd, BYTE * b)
 {
     int ret;
-    size_t n;
-    fd_set rdset;
-    struct timeval ti;
+    size_t no_of_read_byte = -1;
 
-    if (fd < 0 || fd >= RS232_NUM_DEVICES) {
-        log_error(rs232net_log, "Attempt to read from invalid fd %d.", fd);
-        return -1;
-    }
-    if (!fds[fd].inuse) {
-        log_error(rs232net_log, "Attempt to read from non-open fd %d.", fd);
-        return -1;
-    }
+    do {
+        fd_set rdset;
+        struct timeval ti;
 
-    /* silently drop if socket is shut */
-    if (fds[fd].fd < 0)
-        return 0;
-
-    FD_ZERO(&rdset);
-    FD_SET(fds[fd].fd, &rdset);
-    ti.tv_sec = ti.tv_usec = 0;
-    ret = select(fds[fd].fd + 1, &rdset, NULL, NULL, &ti);
-
-    if (ret > 0 && (FD_ISSET(fds[fd].fd, &rdset))) {
-        n = recv(fds[fd].fd, b, 1, 0);
-        if (n != 1) {
-            if(n < 0)
-                log_error(rs232net_log, "Error reading: %s.", strerror(errno));
-            else
-                log_error(rs232net_log, "EOF");
-            close(fds[fd].fd);
-            fds[fd].fd = -1;
-            return -1;
+        if (fd < 0 || fd >= RS232_NUM_DEVICES) {
+            log_error(rs232net_log, "Attempt to read from invalid fd %d.", fd);
+            break;
         }
-        return 1;
-    }
-    return 0;
+
+        if (!fds[fd].inuse) {
+            log_error(rs232net_log, "Attempt to read from non-open fd %d.", fd);
+            break;
+        }
+
+        /* from now on, assume everything is ok, 
+           but we have not received any bytes */
+        no_of_read_byte = 0;
+
+        /* silently drop if socket is shut because of a previous error  */
+        if (fds[fd].fd == INVALID_SOCKET) {
+            break;
+        }
+
+        FD_ZERO(&rdset);
+        FD_SET(fds[fd].fd, &rdset);
+        ti.tv_sec = ti.tv_usec = 0;
+        ret = select(fds[fd].fd + 1, &rdset, NULL, NULL, &ti);
+
+        if (ret > 0 && (FD_ISSET(fds[fd].fd, &rdset))) {
+
+            no_of_read_byte = recv(fds[fd].fd, b, 1, 0);
+
+            if ( no_of_read_byte != 1 ) {
+                if ( no_of_read_byte == SOCKET_ERROR ) {
+                    log_error(rs232net_log, "Error reading: %u.", WSAGetLastError());
+                }
+                else {
+                    log_error(rs232net_log, "EOF");
+                }
+                rs232net_closesocket(fd);
+                no_of_read_byte = -1;
+            }
+        }
+    } while (0);
+
+    return no_of_read_byte;
 }
 
 /* set the status lines of the RS232 device */
 int rs232net_set_status(int fd, enum rs232handshake_out status)
 {
-    fds[fd].rts = (status & RS232_HSO_RTS) ? 1 : 0;
-    fds[fd].dtr = (status & RS232_HSO_DTR) ? 1 : 0;
-
-    /*! \todo signal the RS232 device the current status, too */
+    /* unused */
 
     return 0;
 }
