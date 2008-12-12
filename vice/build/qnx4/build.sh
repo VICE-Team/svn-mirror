6b46df4ecc40b0fd10ccc7162134e17ade40136b
blackystardust <blackystardust68@yahoo.com>
2008-12-12 03:58:28 +0000

    Fixed some qnx 4.x issues.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@19809 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/build/qnx4/build.sh
+++ vice/build/qnx4/build.sh
@@ -15,6 +15,6 @@ sed s/"-rm -f libplus4.a"/"-rm -f -r libplus4.a;cp plus4mem.o ..\/;mv plus4mem.c
 mv src/monitor/Makefile.in src/monitor/Makefile.in.orig
 sed s/"-rm -f libmonitor.a"/"-rm -f -r libmonitor.a;cp monitor.o ..\/;mv monitor.c monitor2.c;echo >monitor.c;$\(CC\) -c -o monitor.o monitor.c;mv monitor2.c monitor.c"/ <src/monitor/Makefile.in.orig >src/monitor/Makefile.in
 mv src/Makefile.in src/Makefile.in.orig
-sed -e s/"@X128_WINRES@"/"monitor.o"/ -e s/"@X64_WINRES@"/"monitor.o"/ -e s/"@XVIC_WINRES@"/"monitor.o"/ -e s/"@XPET_WINRES@"/"monitor.o"/ -e s/"@XPLUS4_WINRES@"/"plus4mem.o monitor.o"/ -e s/"@XCBM2_WINRES@"/"monitor.o"/ <src/Makefile.in.orig >src/Makefile.in
+sed -e s/"@X128_WINRES@"/"monitor.o"/ -e s/"@X64_WINRES@"/"monitor.o"/ -e s/"@X64DTV_WINRES@"/"monitor.o"/ -e s/"@XVIC_WINRES@"/"monitor.o"/ -e s/"@XPET_WINRES@"/"monitor.o"/ -e s/"@XPLUS4_WINRES@"/"plus4mem.o monitor.o"/ -e s/"@XCBM2_WINRES@"/"monitor.o"/ <src/Makefile.in.orig >src/Makefile.in
 ./configure -v --prefix=/usr/local cross_compiling=yes ac_cv_c_bigendian=no
 make
