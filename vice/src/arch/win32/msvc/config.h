0d4f0e6c9774bd8f2e176be9493b0322b039510d
amatthies <andreas.matthies@gmx.net>
2009-02-21 13:29:08 +0000

    Replaced buggy d3d9.h check for now with a static HAVE_D3D9_H.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@20240 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/msvc/config.h
+++ vice/src/arch/win32/msvc/config.h
@@ -52,14 +52,7 @@
 #define HAVE_HTONS              1
 #define HAVE_NETWORK            1
 #define HAVE_GETCWD             1
-
-/* Hack to check if d3d9.h is present */
-#define _WINSOCKAPI_
-#include <dsound.h>
-#undef _WINSOCKAPI_
-#if (DIRECTSOUND_VERSION >= 0x0900)
 #define HAVE_D3D9_H				1
-#endif
 
 #define __i386__                1
 
