ef5710b30c1183a7848d56a5c8cdd0240191a4c2
alankila <alankila@bel.fi>
2009-03-16 08:59:03 +0000

    Revert vsync syncing code for now. The earlier fix in sound.c still helps.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@20401 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/vsync.c
+++ vice/src/vsync.c
@@ -289,7 +289,7 @@ int vsync_do_vsync(struct video_canvas_s *c, int been_skipped)
     static unsigned long next_frame_start = 0;
     unsigned long network_hook_time = 0;
 
-   /*
+    /*
      * this are the counters to show how many frames are skipped
      * since the last vsync_display_speed
      */
@@ -300,6 +300,11 @@ int vsync_do_vsync(struct video_canvas_s *c, int been_skipped)
      * This are the frames which are skipped in a row
      */
     static int skipped_redraw = 0;
+
+    /* Adjustment of frame output frequency. */
+    static unsigned long adjust_start;
+    static int frames_adjust;
+    static signed long min_sdelay, prev_sdelay;
 
     double sound_delay;
     int skip_next_frame;
@@ -388,7 +393,13 @@ int vsync_do_vsync(struct video_canvas_s *c, int been_skipped)
     /* Start afresh after "out of sync" cases. */
     if (sync_reset) {
         sync_reset = 0;
-        frame_ticks = (frame_ticks + frame_ticks_orig) / 2;
+
+        adjust_start = now;
+        frames_adjust = 0;
+        min_sdelay = LONG_MAX;
+        prev_sdelay = 0;
+
+        frame_ticks = frame_ticks_orig;
     }
 
 
@@ -473,28 +484,54 @@ int vsync_do_vsync(struct video_canvas_s *c, int been_skipped)
         next_frame_start = now;
     }
 
-    /* Adjust audio-video sync every frame. This works kinda like just allowing
-     * audio driver to block when writing. */
-    if (!network_connected()) {
+    /* Adjust frame output frequency to match sound speed.
+       This only kicks in for cycle based sound and SOUND_ADJUST_EXACT. */
+    if (frames_adjust < INT_MAX) {
+        frames_adjust++;
+    }
+
+    /* Adjust audio-video sync 10 times per second. This is required for
+     * keeping sound buffers appropriately filled when the buffers are short,
+     * only some few dozen ms errors in sync are tolerated. */
+    if (!network_connected()
+        && (signed long)(now - adjust_start) >= vsyncarch_freq / 10) {
+        if (min_sdelay != LONG_MAX) {
+            /* Account for both relative and absolute delay. */
+            signed long adjust = (min_sdelay - prev_sdelay + min_sdelay / 2)
+                                 / frames_adjust;
+            /* Maximum adjustment step 1%. */
+            if (labs(adjust) > frame_ticks/100) {
+                adjust = adjust / labs(adjust) * frame_ticks / 100;
+            }
+            frame_ticks -= adjust;
+
+            frames_adjust = 0;
+            prev_sdelay = min_sdelay;
+            min_sdelay = LONG_MAX;
+        }
+
+        adjust_start = now;
+    } else {
         if (sound_delay) {
             /* Actual sound delay is sound delay minus vsync delay. */
             signed long sdelay =
                 (signed long)(sound_delay*vsyncarch_freq) - delay;
-            /* correct frame ticks gradually towards the right delay. This acts
-             * as a long-term average. I must confess that this technique has
-             * its limits... :-/ */
-            frame_ticks -= (sdelay + 64) >> 7;
+
+            /* Find smallest delay in this period. We don't compare
+               absolute values since we trust negative delays more
+               than positive delays. The reason for this is that a
+               higher delay has a greater chance of being caused by
+               e.g. OS scheduling. */
+            if (sdelay < min_sdelay) {
+                min_sdelay = sdelay;
+            }
         }
     }
+
     next_frame_start += frame_ticks;
 
     vsyncarch_postsync();
-#if 0
-    FILE *fd = fopen("/tmp/latencylog.txt", "a");
-    fprintf(fd, "%d %ld %ld %lf\n",
-        vsync_frame_counter, frame_ticks, delay, sound_delay * 1000000);
-    fclose(fd);
-#endif
+
     return skip_next_frame;
 }
 
