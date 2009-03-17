cbbe69292d945f8023fbdb22293cacbfe33d3c8a
strik <strik@users.sourceforge.net>
2009-03-17 17:58:31 +0000

    Define Windows constants which are not know for MingW cross-compilation.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@20427 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/ui.c
+++ vice/src/arch/win32/ui.c
@@ -94,6 +94,17 @@
 #include "winmain.h"
 #include "statusbar.h"
 
+#ifndef PBT_APMSUSPEND
+# define PBT_APMSUSPEND        4
+#endif
+
+#ifndef PBT_APMRESUMECRITICAL
+# define PBT_APMRESUMECRITICAL 6
+#endif
+
+#ifndef PBT_APMRESUMESUSPEND
+# define PBT_APMRESUMESUSPEND  7
+#endif
 
 extern char *intl_speed_at_text;
 extern int _mouse_x, _mouse_y, _mouse_enabled;
