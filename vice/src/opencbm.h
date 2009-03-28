2df19907ddc5e93d4a817ad24ec8f9687881b4b7
strik <strik@users.sourceforge.net>
2009-03-28 19:43:00 +0000

    Get rid of that __u_char type.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@20482 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/opencbm.h
+++ vice/src/opencbm.h
@@ -31,7 +31,6 @@ extern "C" {
 
 #define CBMAPIDECL __cdecl
   /* this is already defined here! #define WINAPI */
-# define __u_char unsigned char
 # define CBM_FILE HANDLE
 #else
   /* we have linux */
@@ -40,7 +39,6 @@ extern "C" {
 # define WINAPI
 # define CBM_FILE int
 #ifdef MACOSX_SUPPORT
-# define __u_char unsigned char
 #endif
 #endif
 
@@ -66,11 +64,11 @@ EXTERN void CBMAPIDECL cbm_driver_close(CBM_FILE f);
 /* FIXME: port isn't used yet */
 EXTERN const char * CBMAPIDECL cbm_get_driver_name(int port);
 
-EXTERN int CBMAPIDECL cbm_listen(CBM_FILE f, __u_char dev, __u_char secadr);
-EXTERN int CBMAPIDECL cbm_talk(CBM_FILE f, __u_char dev, __u_char secadr);
+EXTERN int CBMAPIDECL cbm_listen(CBM_FILE f, unsigned char dev, unsigned char secadr);
+EXTERN int CBMAPIDECL cbm_talk(CBM_FILE f, unsigned char dev, unsigned char secadr);
 
-EXTERN int CBMAPIDECL cbm_open(CBM_FILE f, __u_char dev, __u_char secadr, const void *fname, size_t len);
-EXTERN int CBMAPIDECL cbm_close(CBM_FILE f, __u_char dev, __u_char secadr);
+EXTERN int CBMAPIDECL cbm_open(CBM_FILE f, unsigned char dev, unsigned char secadr, const void *fname, size_t len);
+EXTERN int CBMAPIDECL cbm_close(CBM_FILE f, unsigned char dev, unsigned char secadr);
 
 EXTERN int CBMAPIDECL cbm_raw_read(CBM_FILE f, void *buf, size_t size);
 EXTERN int CBMAPIDECL cbm_raw_write(CBM_FILE f, const void *buf, size_t size);
@@ -82,8 +80,8 @@ EXTERN int CBMAPIDECL cbm_get_eoi(CBM_FILE f);
 
 EXTERN int CBMAPIDECL cbm_reset(CBM_FILE f);
 
-EXTERN __u_char CBMAPIDECL cbm_pp_read(CBM_FILE f);
-EXTERN void CBMAPIDECL cbm_pp_write(CBM_FILE f, __u_char c);
+EXTERN unsigned char CBMAPIDECL cbm_pp_read(CBM_FILE f);
+EXTERN void CBMAPIDECL cbm_pp_write(CBM_FILE f, unsigned char c);
 
 EXTERN int CBMAPIDECL cbm_iec_poll(CBM_FILE f);
 EXTERN int CBMAPIDECL cbm_iec_get(CBM_FILE f, int line);
@@ -91,10 +89,10 @@ EXTERN void CBMAPIDECL cbm_iec_set(CBM_FILE f, int line);
 EXTERN void CBMAPIDECL cbm_iec_release(CBM_FILE f, int line);
 EXTERN int CBMAPIDECL cbm_iec_wait(CBM_FILE f, int line, int state);
 
-EXTERN int CBMAPIDECL cbm_upload(CBM_FILE f, __u_char dev, int adr, const void *prog, size_t size);
+EXTERN int CBMAPIDECL cbm_upload(CBM_FILE f, unsigned char dev, int adr, const void *prog, size_t size);
 
-EXTERN int CBMAPIDECL cbm_device_status(CBM_FILE f, __u_char dev, void *buf, size_t bufsize);
-EXTERN int CBMAPIDECL cbm_exec_command(CBM_FILE f, __u_char dev, const void *cmd, size_t len);
+EXTERN int CBMAPIDECL cbm_device_status(CBM_FILE f, unsigned char dev, void *buf, size_t bufsize);
+EXTERN int CBMAPIDECL cbm_exec_command(CBM_FILE f, unsigned char dev, const void *cmd, size_t len);
 
 EXTERN int CBMAPIDECL cbm_identify(CBM_FILE f, int drv,
                               enum cbm_device_type_e *t,
