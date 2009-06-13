642b82449982ceeeac755ab74e40324f0b8daeee
blackystardust <blackystardust68@yahoo.com>
2009-06-13 01:41:24 +0000

    Added better midi driver selection support to the win32 sdl midi ui.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@20958 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/sdl/menu_midi.c
+++ vice/src/arch/sdl/menu_midi.c
@@ -103,20 +103,119 @@ UI_MENU_DEFINE_STRING(MIDIOutName)
 
 /* win32 MIDI settings */
 #if defined(WIN32_COMPILE) && !defined(__XBOX__)
-/* FIXME proper submenu with available devices */
-UI_MENU_DEFINE_INT(MIDIInDev)
-UI_MENU_DEFINE_INT(MIDIOutDev)
+
+#include <windows.h>
+#include <mmsystem.h>
+
+UI_MENU_DEFINE_RADIO(MIDIInDev)
+UI_MENU_DEFINE_RADIO(MIDIOutDev)
+
+static ui_menu_entry_t midi_in_dyn_menu[21];
+static ui_menu_entry_t midi_out_dyn_menu[21];
+
+static int midi_in_dyn_menu_init = 0;
+static int midi_out_dyn_menu_init = 0;
+
+UI_MENU_CALLBACK(MIDIInDev_dynmenu_callback)
+{
+    MMRESULT ret;
+    MIDIINCAPS mic;
+    int i;
+    int num_in = midiInGetNumDevs();
+
+    /* rebuild menu if it already exists. */
+    if (midi_in_dyn_menu_init != 0) {
+        for (i = 0; midi_in_dyn_menu[i].string != NULL; i++) {
+            lib_free(midi_in_dyn_menu[i].string);
+        }
+    } else {
+        midi_in_dyn_menu_init = 1;
+    }
+
+    if (num_in == 0) {
+        midi_in_dyn_menu[0].string = (char *)lib_stralloc("No Devices Present");
+        midi_in_dyn_menu[0].type = MENU_ENTRY_TEXT;
+        midi_in_dyn_menu[0].callback = seperator_callback;
+        midi_in_dyn_menu[0].data = NULL;
+        midi_in_dyn_menu[1].string = NULL;
+        midi_in_dyn_menu[i].type = 0;
+        midi_in_dyn_menu[i].callback = NULL;
+        midi_in_dyn_menu[i].data = NULL;
+    } else {
+        for (i = 0; (i < num_in) && (i < 20); i++) {
+            ret = midiInGetDevCaps(i, &mic, sizeof(MIDIINCAPS));
+            if (ret == MMSYSERR_NOERROR) {
+                midi_in_dyn_menu[i].string = (char *)lib_stralloc(mic.szPname);
+            } else {
+                midi_in_dyn_menu[i].string = (char *)lib_stralloc("Error getting name");
+            }
+            midi_in_dyn_menu[i].type = MENU_ENTRY_RESOURCE_RADIO;
+            midi_in_dyn_menu[i].callback = radio_MIDIInDev_callback;
+            midi_in_dyn_menu[i].data = (ui_callback_data_t)i;
+        }
+        midi_in_dyn_menu[i].string = NULL;
+        midi_in_dyn_menu[i].type = 0;
+        midi_in_dyn_menu[i].callback = NULL;
+        midi_in_dyn_menu[i].data = NULL;
+    }
+    return "->";
+}
+
+UI_MENU_CALLBACK(MIDIOutDev_dynmenu_callback)
+{
+    MMRESULT ret;
+    MIDIOUTCAPS moc;
+    int i;
+    int num_out = midiOutGetNumDevs();
+
+    /* rebuild menu if it already exists. */
+    if (midi_out_dyn_menu_init != 0) {
+        for (i = 0; midi_out_dyn_menu[i].string != NULL; i++) {
+            lib_free(midi_out_dyn_menu[i].string);
+        }
+    } else {
+        midi_out_dyn_menu_init = 1;
+    }
+
+    if (num_out == 0) {
+        midi_out_dyn_menu[0].string = (char *)lib_stralloc("No Devices Present");
+        midi_out_dyn_menu[0].type = MENU_ENTRY_TEXT;
+        midi_out_dyn_menu[0].callback = seperator_callback;
+        midi_out_dyn_menu[0].data = NULL;
+        midi_out_dyn_menu[1].string = NULL;
+        midi_out_dyn_menu[i].type = 0;
+        midi_out_dyn_menu[i].callback = NULL;
+        midi_out_dyn_menu[i].data = NULL;
+    } else {
+        for (i = 0; (i < num_out) && (i < 20); i++) {
+            ret = midiOutGetDevCaps(i, &moc, sizeof(MIDIOUTCAPS));
+            if (ret == MMSYSERR_NOERROR) {
+                midi_out_dyn_menu[i].string = (char *)lib_stralloc(moc.szPname);
+            } else {
+                midi_out_dyn_menu[i].string = (char *)lib_stralloc("Error getting name");
+            }
+            midi_out_dyn_menu[i].type = MENU_ENTRY_RESOURCE_RADIO;
+            midi_out_dyn_menu[i].callback = radio_MIDIOutDev_callback;
+            midi_out_dyn_menu[i].data = (ui_callback_data_t)i;
+        }
+        midi_out_dyn_menu[i].string = NULL;
+        midi_out_dyn_menu[i].type = 0;
+        midi_out_dyn_menu[i].callback = NULL;
+        midi_out_dyn_menu[i].data = NULL;
+    }
+    return "->";
+}
 
 #define VICE_SDL_MIDI_ARCHDEP_ITEMS \
     SDL_MENU_ITEM_SEPARATOR, \
     { "MIDI-In", \
-      MENU_ENTRY_RESOURCE_STRING, \
-      int_MIDIInDev_callback, \
-      (ui_callback_data_t)"MIDI-In device" }, \
+      MENU_ENTRY_SUBMENU, \
+      MIDIInDev_dynmenu_callback, \
+      (ui_callback_data_t)midi_in_dyn_menu }, \
     { "MIDI-Out", \
-      MENU_ENTRY_RESOURCE_STRING, \
-      int_MIDIOutDev_callback, \
-      (ui_callback_data_t)"MIDI-Out device" },
+      MENU_ENTRY_SUBMENU, \
+      MIDIOutDev_dynmenu_callback, \
+      (ui_callback_data_t)midi_out_dyn_menu },
 
 #endif /* defined(WIN32_COMPILE) && !defined(__XBOX__) */
 
