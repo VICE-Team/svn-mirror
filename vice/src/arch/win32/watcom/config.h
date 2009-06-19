defb5d4ed1d5f4c5b0eb57cef72a040403204521
blackystardust <blackystardust68@yahoo.com>
2009-06-19 19:29:26 +0000

    Updated the win32 watcom project files to make it compile again.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@20990 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/watcom/config.h
+++ vice/src/arch/win32/watcom/config.h
@@ -5,62 +5,63 @@
 
 #pragma warning(disable:4996)
 
-#define HAVE_ALLOCA             1
-#define RETSIGTYPE              void
-#define HAS_JOYSTICK            1
-#define HAVE_MOUSE              1
-#define HAVE_HARDSID            1
-#define HAVE_RS232              1
-#define HAVE_NETWORK            1
+#define HAVE_ALLOCA                1
+#define RETSIGTYPE                 void
+#define HAS_JOYSTICK               1
+#define HAVE_MOUSE                 1
+#define HAVE_HARDSID               1
+#define HAVE_RS232                 1
+#define HAVE_NETWORK               1
 
-#define HAS_LONGLONG_INTEGER    1
-#define HAS_UNLOCKRESOURCE      1
-#define SIZEOF_UNSIGNED_INT     4
-#define SIZEOF_UNSIGNED_LONG    4
-#define SIZEOF_UNSIGNED_SHORT   2
-#define HAVE_ATEXIT             1
-#define HAVE_MEMMOVE            1
-#define HAVE_STRERROR           1
-#define HAVE_FCNTL_H            1
-#define HAVE_LIMITS_H           1
-#define HAVE_COMMCTRL_H         1
-#define HAVE_SHLOBJ_H           1
-#define HAVE_DIRECT_H           1
-#define HAVE_ERRNO_H            1
-#define HAVE_IO_H               1
-#define HAVE_PROCESS_H          1
-#define HAVE_SYS_TYPES_H        1
-#define HAVE_SYS_STAT_H         1
-#define HAVE_SIGNAL_H           1
-#define HAVE_WINIOCTL_H         1
-#define DWORD_IS_LONG           1
-#define HAVE_OPENCBM            1
-#define HAVE_FFMPEG             1
-#define HAVE_FFMPEG_SWSCALE     1
-#define HAVE_MIDI               1
-#define HAVE_CRTDBG             1
-#define HAS_TRANSLATION         1
-#define HAVE_HTONL              1
-#define HAVE_HTONS              1
-#define HAVE_STRNCASECMP        1
-#define HAVE_PROCESS_H          1
-#define HAVE_GUIDLIB            1
-#define HAVE_GETCWD             1
-#define HAVE_DINPUT             1
-#define HAVE_DINPUT_LIB         1
+#define HAS_LONGLONG_INTEGER       1
+#define HAS_UNLOCKRESOURCE         1
+#define SIZEOF_UNSIGNED_INT        4
+#define SIZEOF_UNSIGNED_LONG       4
+#define SIZEOF_UNSIGNED_SHORT      2
+#define HAVE_ATEXIT                1
+#define HAVE_MEMMOVE               1
+#define HAVE_STRERROR              1
+#define HAVE_FCNTL_H               1
+#define HAVE_LIMITS_H              1
+#define HAVE_COMMCTRL_H            1
+#define HAVE_SHLOBJ_H              1
+#define HAVE_DIRECT_H              1
+#define HAVE_ERRNO_H               1
+#define HAVE_IO_H                  1
+#define HAVE_PROCESS_H             1
+#define HAVE_SYS_TYPES_H           1
+#define HAVE_SYS_STAT_H            1
+#define HAVE_SIGNAL_H              1
+#define HAVE_WINIOCTL_H            1
+#define DWORD_IS_LONG              1
+#define HAVE_OPENCBM               1
+#define HAVE_FFMPEG                1
+#define HAVE_FFMPEG_SWSCALE        1
+#define HAVE_FFMPEG_HEADER_SUBDIRS 1
+#define HAVE_MIDI                  1
+#define HAVE_CRTDBG                1
+#define HAS_TRANSLATION            1
+#define HAVE_HTONL                 1
+#define HAVE_HTONS                 1
+#define HAVE_STRNCASECMP           1
+#define HAVE_PROCESS_H             1
+#define HAVE_GUIDLIB               1
+#define HAVE_GETCWD                1
+#define HAVE_DINPUT                1
+#define HAVE_DINPUT_LIB            1
 
-#define USE_DXSOUND             1
+#define USE_DXSOUND                1
 
-#define __i386__                1
+#define __i386__                   1
 
 #define inline
 
 #define _ANONYMOUS_UNION
 
-#define MSVC_RC                 1
+#define MSVC_RC                    1
 
-#define strcasecmp(s1, s2)      _stricmp(s1, s2)
-#define HAVE_STRCASECMP         1
+#define strcasecmp(s1, s2)         _stricmp(s1, s2)
+#define HAVE_STRCASECMP            1
 
 #define snprintf _snprintf
 
