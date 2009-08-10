aff12896b1694d9340a2a5fa81660ebfba47c0d6
blackystardust <blackystardust68@yahoo.com>
2009-08-10 08:59:27 +0000

    Added some more code cleanups.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@21403 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/amigaos/mui/uirs232user.c
+++ vice/src/arch/amigaos/mui/uirs232user.c
@@ -35,76 +35,76 @@
 #include "translate.h"
 
 static int ui_rs232user_enable_translate[] = {
-  IDMS_DISABLED,
-  IDS_ENABLED,
-  0
+    IDMS_DISABLED,
+    IDS_ENABLED,
+    0
 };
 
 static char *ui_rs232user_enable[countof(ui_rs232user_enable_translate)];
 
 static const int ui_rs232user_enable_values[] = {
-  0,
-  1,
-  -1
+    0,
+    1,
+    -1
 };
 
 static int ui_rs232user_device_translate[] = {
-  IDS_RS232_DEVICE_1,
-  IDS_RS232_DEVICE_2,
-  IDS_RS232_DEVICE_3,
-  IDS_RS232_DEVICE_4,
-  0
+    IDS_RS232_DEVICE_1,
+    IDS_RS232_DEVICE_2,
+    IDS_RS232_DEVICE_3,
+    IDS_RS232_DEVICE_4,
+    0
 };
 
 static char *ui_rs232user_device[countof(ui_rs232user_device_translate)];
 
 static const int ui_rs232user_device_values[] = {
-  0,
-  1,
-  2,
-  3,
-  -1
+    0,
+    1,
+    2,
+    3,
+    -1
 };
 
 static char *ui_rs232user_baud[] = {
-  "300",
-  "600",
-  "1200",
-  "2400",
-  "4800",
-  "9600",
-  NULL
+    "300",
+    "600",
+    "1200",
+    "2400",
+    "4800",
+    "9600",
+    NULL
 };
 
 static const int ui_rs232user_baud_values[] = {
-  300,
-  600,
-  1200,
-  2400,
-  4800,
-  9600,
-  -1
+    300,
+    600,
+    1200,
+    2400,
+    4800,
+    9600,
+    -1
 };
 
 static ui_to_from_t ui_to_from[] = {
-  { NULL, MUI_TYPE_CYCLE, "RsUserEnable", ui_rs232user_enable, ui_rs232user_enable_values },
-  { NULL, MUI_TYPE_CYCLE, "RsUserDev", ui_rs232user_device, ui_rs232user_device_values },
-  { NULL, MUI_TYPE_CYCLE, "RsUserBaud", ui_rs232user_baud, ui_rs232user_baud_values },
-  UI_END /* mandatory */
+    {NULL, MUI_TYPE_CYCLE, "RsUserEnable", ui_rs232user_enable, ui_rs232user_enable_values},
+    {NULL, MUI_TYPE_CYCLE, "RsUserDev", ui_rs232user_device, ui_rs232user_device_values},
+    {NULL, MUI_TYPE_CYCLE, "RsUserBaud", ui_rs232user_baud, ui_rs232user_baud_values},
+    UI_END /* mandatory */
 };
 
 static APTR build_gui(void)
 {
-  return GroupObject,
-    CYCLE(ui_to_from[0].object, translate_text(IDS_USERPORT_RS232), ui_rs232user_enable)
-    CYCLE(ui_to_from[1].object, translate_text(IDS_USERPORT_DEVICE), ui_rs232user_device)
-    CYCLE(ui_to_from[2].object, translate_text(IDS_USERPORT_BAUD_RATE), ui_rs232user_baud)
-  End;
+    return GroupObject,
+             CYCLE(ui_to_from[0].object, translate_text(IDS_USERPORT_RS232), ui_rs232user_enable)
+             CYCLE(ui_to_from[1].object, translate_text(IDS_USERPORT_DEVICE), ui_rs232user_device)
+             CYCLE(ui_to_from[2].object, translate_text(IDS_USERPORT_BAUD_RATE), ui_rs232user_baud)
+           End;
 }
 
 void ui_rs232user_settings_dialog(void)
 {
-  intl_convert_mui_table(ui_rs232user_enable_translate, ui_rs232user_enable);
-  intl_convert_mui_table(ui_rs232user_device_translate, ui_rs232user_device);
-  mui_show_dialog(build_gui(), translate_text(IDS_RS232_USERPORT_SETTINGS), ui_to_from);
+    intl_convert_mui_table(ui_rs232user_enable_translate, ui_rs232user_enable);
+    intl_convert_mui_table(ui_rs232user_device_translate, ui_rs232user_device);
+    mui_show_dialog(build_gui(), translate_text(IDS_RS232_USERPORT_SETTINGS), ui_to_from);
 }
