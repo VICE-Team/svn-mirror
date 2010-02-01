77eb3881346412ed8db8c21c60c768971b3eec08
blackystardust <blackystardust68@yahoo.com>
2010-02-01 05:17:52 +0000

    Updated the gp2x make bindist script to also handle the creation of the wiz bindist.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@22193 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/unix/gp2x/make-bindist.sh
+++ vice/src/arch/unix/gp2x/make-bindist.sh
@@ -3,53 +3,62 @@
 #
 # written by Marco van den Heuvel <blackystardust68@yahoo.com>
 #
-# make-bindist.sh <strip> <vice-version> <zip|nozip> <top-srcdir>
-#                 $1      $2             $3          $4
+# make-bindist.sh <system> <strip> <vice-version> <zip|nozip> <top-srcdir>
+#                 $1       $2      $3             $4          $5
 
-STRIP=$1
-VICEVERSION=$2
-ZIPKIND=$3
-TOPSRCDIR=$4
+SYSTEM=$1
+STRIP=$2
+VICEVERSION=$3
+ZIPKIND=$4
+TOPSRCDIR=$5
 
-echo Generating GP2X port binary distribution.
-rm -f -r vice-gp2x-$VICEVERSION
-mkdir vice-gp2x-$VICEVERSION
+if test x"$SYSTEM" = "xGP2X"; then
+  smallsystem="gp2x"
+fi
+
+if test x"$SYSTEM" = "xWIZ"; then
+  smallsystem="wiz"
+fi
+
+echo Generating $SYSTEM port binary distribution.
+rm -f -r vice-$smallsystem-$VICEVERSION
+mkdir vice-$smallsystem-$VICEVERSION
 $STRIP src/x64
 $STRIP src/x128
 $STRIP src/xvic
 $STRIP src/xpet
 $STRIP src/xplus4
 $STRIP src/xcbm2
-cp src/x64 src/x128 src/xvic vice-gp2x-$VICEVERSION
-cp src/xpet src/xplus4 src/xcbm2 vice-gp2x-$VICEVERSION
-cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/x64.gpe vice-gp2x-$VICEVERSION
-cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/x128.gpe vice-gp2x-$VICEVERSION
-cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/xvic.gpe vice-gp2x-$VICEVERSION
-cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/xpet.gpe vice-gp2x-$VICEVERSION
-cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/xplus4.gpe vice-gp2x-$VICEVERSION
-cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/xcbm2.gpe vice-gp2x-$VICEVERSION
-mkdir vice-gp2x-$VICEVERSION/data
-cp -a $TOPSRCDIR/data/C128 $TOPSRCDIR/data/C64 $TOPSRCDIR/data/CBM-II $TOPSRCDIR/data/DRIVES vice-gp2x-$VICEVERSION/data
-cp -a $TOPSRCDIR/data/PET $TOPSRCDIR/data/PLUS4 $TOPSRCDIR/data/VIC20 vice-gp2x-$VICEVERSION/data
-mkdir vice-gp2x-$VICEVERSION/snapshots
-mkdir vice-gp2x-$VICEVERSION/tmp
-rm `find vice-gp2x-$VICEVERSION -name "Makefile*"`
-rm `find vice-gp2x-$VICEVERSION -name "amiga_*.vkm"`
-rm `find vice-gp2x-$VICEVERSION -name "beos_*.vkm"`
-rm `find vice-gp2x-$VICEVERSION -name "dos_*.vkm"`
-rm `find vice-gp2x-$VICEVERSION -name "os2*.vkm"`
-rm `find vice-gp2x-$VICEVERSION -name "win_*.vkm"`
-rm `find vice-gp2x-$VICEVERSION -name "RO*.vkm"`
-rm `find vice-gp2x-$VICEVERSION -name "sdl*.vkm"`
-rm `find vice-gp2x-$VICEVERSION -name "*.vsc"`
+cp src/x64 src/x128 src/xvic vice-$smallsystem-$VICEVERSION
+cp src/xpet src/xplus4 src/xcbm2 vice-$smallsystem-$VICEVERSION
+cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/x64.gpe vice-$smallsystem-$VICEVERSION
+cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/x128.gpe vice-$smallsystem-$VICEVERSION
+cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/xvic.gpe vice-$smallsystem-$VICEVERSION
+cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/xpet.gpe vice-$smallsystem-$VICEVERSION
+cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/xplus4.gpe vice-$smallsystem-$VICEVERSION
+cp $TOPSRCDIR/src/arch/unix/gp2x/gpe-files/xcbm2.gpe vice-$smallsystem-$VICEVERSION
+mkdir vice-$smallsystem-$VICEVERSION/data
+cp -a $TOPSRCDIR/data/C128 $TOPSRCDIR/data/C64 $TOPSRCDIR/data/CBM-II $TOPSRCDIR/data/DRIVES vice-$smallsystem-$VICEVERSION/data
+cp -a $TOPSRCDIR/data/PET $TOPSRCDIR/data/PLUS4 $TOPSRCDIR/data/VIC20 vice-$smallsystem-$VICEVERSION/data
+mkdir vice-$smallsystem-$VICEVERSION/snapshots
+mkdir vice-$smallsystem-$VICEVERSION/tmp
+rm `find vice-$smallsystem-$VICEVERSION -name "Makefile*"`
+rm `find vice-$smallsystem-$VICEVERSION -name "amiga_*.vkm"`
+rm `find vice-$smallsystem-$VICEVERSION -name "beos_*.vkm"`
+rm `find vice-$smallsystem-$VICEVERSION -name "dos_*.vkm"`
+rm `find vice-$smallsystem-$VICEVERSION -name "os2*.vkm"`
+rm `find vice-$smallsystem-$VICEVERSION -name "win_*.vkm"`
+rm `find vice-$smallsystem-$VICEVERSION -name "RO*.vkm"`
+rm `find vice-$smallsystem-$VICEVERSION -name "sdl*.vkm"`
+rm `find vice-$smallsystem-$VICEVERSION -name "*.vsc"`
 if test x"$ZIPKIND" = "xzip"; then
   if test x"$ZIP" = "x"; then
-    zip -r -9 -q vice-gp2x-$VICEVERSION.zip vice-gp2x-$VICEVERSION
+    zip -r -9 -q vice-$smallsystem-$VICEVERSION.zip vice-$smallsystem-$VICEVERSION
   else
-    $ZIP vice-gp2x-$VICEVERSION.zip vice-gp2x-$VICEVERSION
+    $ZIP vice-$smallsystem-$VICEVERSION.zip vice-$smallsystem-$VICEVERSION
   fi
-  rm -f -r vice-gp2x-$VICEVERSION
-  echo GP2X port binary distribution archive generated as vice-gp2x-$VICEVERSION.zip
+  rm -f -r vice-$smallsystem-$VICEVERSION
+  echo $SYSTEM port binary distribution archive generated as vice-$smallsystem-$VICEVERSION.zip
 else
-  echo GP2X port binary distribution directory generated as vice-gp2x-$VICEVERSION
+  echo $SYSTEM port binary distribution directory generated as vice-$smallsystem-$VICEVERSION
 fi
