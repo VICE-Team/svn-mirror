cad05e369fa0f38e206799e48ef522e6058decbe
blackystardust <blackystardust68@yahoo.com>
2010-02-24 07:16:58 +0000

    Added openstep makebindist script.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@22317 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/unix/openstep/make-bindist.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+# make-bindist.sh for the OPENSTEP port
+#
+# written by Marco van den Heuvel <blackystardust68@yahoo.com>
+#
+# make-bindist.sh <strip> <vice-version> <prefix> <zip|nozip> <topsrcdir> <make-command>
+#                 $1      $2             $3       $4          $5          $6
+
+STRIP=$1
+VICEVERSION=$2
+PREFIX=$3
+ZIPKIND=$4
+TOPSRCDIR=$5
+MAKECOMMAND=$6
+
+if test x"$PREFIX" != "x/usr/local"; then
+  echo Error: installation path is not /usr/local
+  exit 1
+fi
+
+if [ ! -e src/x64 -o ! -e src/x64dtv -o ! -e src/x128 -o ! -e src/xvic -o ! -e src/xpet -o ! -e src/xplus4 -o ! -e src/xcbm2 -o ! -e src/c1541 -o ! -e src/petcat -o ! -e src/cartconv ]
+then
+  echo Error: \"make\" needs to be done first
+  exit 1
+fi
+
+echo Generating OpenStep port binary distribution.
+rm -f -r VICE-$VICEVERSION
+curdir=`pwd`
+gnumake -e prefix=$curdir/VICE-$VICEVERSION/usr/local VICEDIR=$curdir/VICE-$VICEVERSION/usr/local/lib/vice install
+$STRIP VICE-$VICEVERSION/usr/local/bin/x64
+$STRIP VICE-$VICEVERSION/usr/local/bin/x64dtv
+$STRIP VICE-$VICEVERSION/usr/local/bin/x128
+$STRIP VICE-$VICEVERSION/usr/local/bin/xvic
+$STRIP VICE-$VICEVERSION/usr/local/bin/xpet
+$STRIP VICE-$VICEVERSION/usr/local/bin/xplus4
+$STRIP VICE-$VICEVERSION/usr/local/bin/xcbm2
+$STRIP VICE-$VICEVERSION/usr/local/bin/c1541
+$STRIP VICE-$VICEVERSION/usr/local/bin/petcat
+$STRIP VICE-$VICEVERSION/usr/local/bin/cartconv
+if test x"$ZIPKIND" = "xzip"; then
+  /NextAdmin/Installer.App/package $curdir/VICE-$VICEVERSION/usr/local $TOPSRCDIR/src/arch/unix/openstep/vice.info
+  file >/tmp/vice.tmp VICE-$VICEVERSION/usr/local/bin/x64
+  i386_found=`fgrep i386 /tmp/vice.tmp`
+  m68k_found='fgrep m68k /tmp/vice.tmp`
+  sparc_found=`fgrep sparc /tmp/vice.tmp`
+  hppa_found=`fgrep hppa /tmp/vice.tmp`
+
+  PLATFORMS=""
+
+  if test x"$m68k_found" != "x"; then
+    PLATFORMS="N"
+  fi
+
+  if test x"$i386_found" != "x"; then
+    PLATFORMS="$PLATFORMSI"
+  fi
+
+  if test x"$hppa_found" != "x"; then
+    PLATFORMS="$PLATFORMSH"
+  fi
+
+  if test x"$sparc_found" != "x"; then
+    PLATFORMS="$PLATFORMSS"
+  fi
+
+  tar -cvf - vice.pkg | gzip -9c > VICE-$VICEVERSION-OS-$PLATFORMS.tar.gz
+
+  rm -f -r VICE-$VICEVERSION vice.pkg
+
+  echo OpenStep port binary package generated as VICE-$VICEVERSION-OS-$PLATFORMS.tar.gz
+else
+  echo OpenStep port binary distribution directory generated as VICE-$VICEVERSION
+fi
