6a2f2bf1cf390f371c9b6b9fb259f75e15d018ed
strik <strik@users.sourceforge.net>
2010-10-05 19:39:06 +0000

    Added capability to debug the IEC bus.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@22958 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/c64iec.c
+++ vice/src/c64/c64iec.c
@@ -40,6 +40,82 @@
 #include "maincpu.h"
 #include "types.h"
 
+/* #define DEBUG_IECBUS_VCD */
+
+#ifdef DEBUG_IECBUS_VCD
+
+# include "machine.h"
+# include "log.h"
+# include <string.h>
+
+
+#define IEC_DEBUG_PORTS() iec_debug_ports()
+
+static void iec_debug_ports(void)
+{
+    unsigned int unit;
+
+    /*! memory for the old state of the iecbus */
+    static iecbus_t old_iecbus;
+
+    /*! remember if this is the first call of this function (= 1) or not.
+     *  We need this so that in case of the first call, all values are output.
+     */
+    static int firstcall = 1;
+
+    static unsigned long cycles_per_second = 0;
+
+    unsigned long time_usec;
+
+    if (firstcall) {
+        cycles_per_second = machine_get_cycles_per_second();
+
+        if (cycles_per_second == 0) {
+            /* make sure to output the cycle count instead. */
+            cycles_per_second = 1000000;
+        }
+    }
+
+    time_usec = (unsigned long) ( (1000000. * maincpu_clk) / cycles_per_second );
+
+    if ( (old_iecbus.cpu_bus != iecbus.cpu_bus) || firstcall) {
+        log_message(LOG_DEFAULT, "#%lu: cpu_bus changed from $%02x to $%02x.", time_usec, old_iecbus.cpu_bus, iecbus.cpu_bus);
+        old_iecbus.cpu_bus = iecbus.cpu_bus;
+    }
+
+    if ( (old_iecbus.cpu_port != iecbus.cpu_port) || firstcall) {
+        log_message(LOG_DEFAULT, "#%lu: cpu_port changed from $%02x to $%02x.", time_usec, old_iecbus.cpu_port, iecbus.cpu_port);
+        old_iecbus.cpu_port = iecbus.cpu_port;
+    }
+
+    if ( (old_iecbus.drv_port != iecbus.drv_port) || firstcall) {
+        log_message(LOG_DEFAULT, "#%lu: drv_port changed from $%02x to $%02x.", time_usec, old_iecbus.drv_port, iecbus.drv_port);
+        old_iecbus.drv_port = iecbus.drv_port;
+    }
+
+    for (unit = 0; unit < 8 + DRIVE_NUM; unit++) {
+        if ( (old_iecbus.drv_bus[unit] != iecbus.drv_bus[unit]) || firstcall) {
+            log_message(LOG_DEFAULT, "#%lu: drv_bus[ %2u] changed from $%02x to $%02x.", time_usec, unit, old_iecbus.drv_bus[unit], iecbus.drv_bus[unit]);
+            old_iecbus.drv_bus[unit] = iecbus.drv_bus[unit];
+        }
+
+        if ( (old_iecbus.drv_data[unit] != iecbus.drv_data[unit]) || firstcall) {
+            log_message(LOG_DEFAULT, "#%lu: drv_data[%2u] changed from $%02x to $%02x.", time_usec, unit, old_iecbus.drv_data[unit], iecbus.drv_data[unit]);
+            old_iecbus.drv_data[unit] = iecbus.drv_data[unit];
+        }
+    }
+
+    /* remember: we already have been called, no extra processing */
+
+    firstcall = 0;
+}
+
+#else
+
+#define IEC_DEBUG_PORTS()
+
+#endif
+
 void iec_update_cpu_bus(BYTE data)
 {
     iecbus.cpu_bus = (((data << 2) & 0x80) | ((data << 2) & 0x40) | ((data << 1) & 0x10));
@@ -55,6 +131,8 @@ void iec_update_ports(void)
     }
 
     iecbus.drv_port = (((iecbus.cpu_port >> 4) & 0x4) | (iecbus.cpu_port >> 7) | ((iecbus.cpu_bus << 3) & 0x80));
+
+    IEC_DEBUG_PORTS();
 }
 
 void iec_update_ports_embedded(void)
