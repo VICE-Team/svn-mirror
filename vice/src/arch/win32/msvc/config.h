30c0c857fcc8c2bbc274b5fdd608e3a04294009c
amatthies <andreas.matthies@gmx.net>
2010-12-19 14:39:32 +0000

    Moved HAVE_D3D9_H hack back to videoarch.h due to MSVC6 problems and include this in res<machine>.rc. This gives me some "WS_CAPTION redefinition" warnings in winuser.rh now. Please test if MSVC6 compiles.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23334 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/msvc/config.h
+++ vice/src/arch/win32/msvc/config.h
@@ -85,12 +85,6 @@
 
 #define MSVC_RC                 1
 
-#define _WINSOCKAPI_
-#include <dsound.h> 	 
-#undef _WINSOCKAPI_
-#if (DIRECTSOUND_VERSION >= 0x0900) 	 
-#define HAVE_D3D9_H 1
-#endif
 #define strcasecmp(s1, s2)      _stricmp(s1, s2)
 #define HAVE_STRCASECMP         1
 
