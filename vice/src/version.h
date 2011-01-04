ab77d5ac76af755b2f37344981c8181b8fa832c9
blackystardust <blackystardust68@yahoo.com>
2011-01-04 21:01:02 +0000

    Version 2.2.10.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23403 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/version.h
+++ vice/src/version.h
@@ -28,11 +28,11 @@
 #define VICE__VERSION_H__
 
 #ifndef VERSION
-#define VERSION "2.2.9"
+#define VERSION "2.2.10"
 #endif
 
 #ifndef VERSION_RC_NUMBER
-#define VERSION_RC_NUMBER 2,2,9,0
+#define VERSION_RC_NUMBER 2,2,10,0
 #endif
 
 #ifndef PACKAGE
