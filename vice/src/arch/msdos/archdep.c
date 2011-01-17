b7b9ecc05bd5d50ff84c46a0193b66723dd69c2e
blackystardust <blackystardust68@yahoo.com>
2011-01-17 18:08:22 +0000

    Added fix file permission support to the dos port.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23483 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/msdos/archdep.c
+++ vice/src/arch/msdos/archdep.c
@@ -408,7 +408,7 @@ int archdep_stat(const char *file_name, unsigned int *len, unsigned int *isdir)
 /* set permissions of given file to rw, respecting current umask */
 int archdep_fix_permissions(const char *file_name)
 {
-    return 0;
+    return _chmod(file_name, 1, 0);
 }
 
 int archdep_file_is_blockdev(const char *name)
