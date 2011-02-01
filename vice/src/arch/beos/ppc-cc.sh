67312750f3ea6dbacbac6a592649b89a903637c2
blackystardust <blackystardust68@yahoo.com>
2011-02-01 22:12:41 +0000

    Refined to use a.out as output file and not produce a.out if -c was given.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23598 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/beos/ppc-cc.sh
+++ vice/src/arch/beos/ppc-cc.sh
@@ -4,13 +4,13 @@ foundo=no
 
 for i in $*
 do
-  if test x"$i" = "x-o"; then
+  if test x"$i" = "x-o" -o x"$i" = "x-c"; then
     foundo=yes
   fi
 done
 
 if test x"$foundo" = "xno"; then
-  mwccppc $* -o aout
+  mwccppc $* -o a.out
 else
   mwccppc $*
 fi
