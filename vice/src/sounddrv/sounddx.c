6c949b79963850ae8758c16c00e0f7ffa24df2cd
fabbo <fabrizio.ge@tiscalinet.it>
2011-02-11 12:54:23 +0000

    Do not compile sounddx.c if not necessary
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23678 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/sounddrv/sounddx.c
+++ vice/src/sounddrv/sounddx.c
@@ -28,6 +28,8 @@
 
 #include "vice.h"
 
+#ifdef USE_DXSOUND
+
 #include <stdio.h>
 
 #if __GNUC__>2 || (__GNUC__==2 && __GNUC_MINOR__>=95)
@@ -546,3 +548,5 @@ int sound_init_dx_device(void)
 {
     return sound_register_device(&dx_device);
 }
+
+#endif /*USE_DXSOUND*/
