06350f3daaed4ba8a505b42b646acf9e75c8adc2
blackystardust <blackystardust68@yahoo.com>
2011-02-16 00:24:23 +0000

    Don't use builtin_expect for qnx4.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23712 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/build/qnx4/build.sh
+++ vice/build/qnx4/build.sh
@@ -10,11 +10,6 @@ if [ ! -f configure.in ]; then
   fi
 fi
 
-mv src/plus4/Makefile.in src/plus4/Makefile.in.orig
-sed s/"-rm -f libplus4.a"/"-rm -f -r libplus4.a;cp plus4mem.o ..\/;mv plus4mem.c plus4mem2.c;echo >plus4mem.c;$\(CC\) -c -o plus4mem.o plus4mem.c;mv plus4mem2.c plus4mem.c"/ <src/plus4/Makefile.in.orig >src/plus4/Makefile.in
-mv src/monitor/Makefile.in src/monitor/Makefile.in.orig
-sed s/"-rm -f libmonitor.a"/"-rm -f -r libmonitor.a;cp monitor.o ..\/;mv monitor.c monitor2.c;echo >monitor.c;$\(CC\) -c -o monitor.o monitor.c;mv monitor2.c monitor.c"/ <src/monitor/Makefile.in.orig >src/monitor/Makefile.in
-mv src/Makefile.in src/Makefile.in.orig
-sed -e s/"@X128_WINRES@"/"monitor.o"/ -e s/"@X64_WINRES@"/"monitor.o"/ -e s/"@X64DTV_WINRES@"/"monitor.o"/ -e s/"@XVIC_WINRES@"/"monitor.o"/ -e s/"@XPET_WINRES@"/"monitor.o"/ -e s/"@XPLUS4_WINRES@"/"plus4mem.o monitor.o"/ -e s/"@XCBM2_WINRES@"/"monitor.o"/ <src/Makefile.in.orig >src/Makefile.in
-./configure -v --prefix=/usr/local cross_compiling=yes ac_cv_c_bigendian=no
-make
+curdir=`pwd`
+./configure -v --prefix=/usr/local cross_compiling=yes ac_cv_c_bigendian=no resid_cv_builtin_expect=no
+make CC="/bin/sh $curdir/src/arch/unix/qnx4/qnx4-cc.sh"
