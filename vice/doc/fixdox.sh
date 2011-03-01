23bc2c0d33d81d6afa8821786487a22d59e460d7
blackystardust <blackystardust68@yahoo.com>
2011-03-01 00:58:56 +0000

    Added .txt format documentation file fixing.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23799 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/doc/fixdox.sh
+++ vice/doc/fixdox.sh
@@ -13,48 +13,76 @@ FORMAT=$1
 
 fixtxt()
 {
+# remove START-INFO-DIR-ENTRY and END-INFO-DIR-ENTRY lines
+  while read data
+  do
+    if test x"$data" != "xSTART-INFO-DIR-ENTRY" -a x"$data" != "xEND-INFO-DIR-ENTRY"; then
+      echo "$data"
+    else
+      if test x"$data" = "xSTART-INFO-DIR-ENTRY"; then
+# fix the title
+        read data
+        header=""
+        for i in $data
+        do
+          if test x"$header" != "x"; then
+             header="$header $i"
+          fi
+          if test x"$header" = "x" -a x"$i" = "x(vice)."; then
+             header=" "
+          fi
+        done
+        echo $header
+      fi
+    fi
+  done
 }
 
 fixchm()
 {
+  echo not implemented yet
 }
 
 fixhlp()
 {
+  echo not implemented yet
 }
 
 fixguide()
 {
+  echo not implemented yet
 }
 
 fixpdf()
 {
+  echo not implemented yet
 }
 
 fixipf()
 {
+  echo not implemented yet
 }
 
 if test x"$FORMAT" = "xtxt"; then
-  fixtxt()
+  fixtxt
 fi
 
 if test x"$FORMAT" = "xchm"; then
-  fixchm()
+  fixchm
 fi
 
 if test x"$FORMAT" = "xhlp"; then
-  fixhlp()
+  fixhlp
 fi
 
 if test x"$FORMAT" = "xguide"; then
-  fixguide()
+  fixguide
 fi
 
 if test x"$FORMAT" = "xpdf"; then
-  fixpdf()
+  fixpdf
 fi
 
 if test x"$FORMAT" = "xipf"; then
-  fixipf()
+  fixipf
 fi
