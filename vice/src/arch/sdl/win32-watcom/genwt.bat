af0269b955f8c0527ebe08e6ea28be1a4d977ef6
blackystardust <blackystardust68@yahoo.com>
2011-03-08 19:18:20 +0000

    Added generation of infocontrib.h to the sdl win32 ow project files.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23833 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/sdl/win32-watcom/genwt.bat
+++ vice/src/arch/sdl/win32-watcom/genwt.bat
@@ -1,15 +1,20 @@
 @echo off
 copy ..\..\..\translate.txt trans.txt
+copy ..\..\..\..\doc\vice.texi vice.tex
+copy ..\..\..\infocontrib.sec ic.sed
 if Windows_NT == %OS% goto winnt
 goto use16
 :winnt
 if defined ProgramFiles(x86) GOTO use32
 :use16
+genic vice.tex ic.h ic.sed
 gentr trans.txt trans.h transtab.h
 goto after
 :use32
+geninfocontrib vice.tex ic.h ic.sed
 gentranslate32 trans.txt trans.h transtab.h
 :after
+copy ic.h ..\..\..\infocontrib.h
 copy trans.h ..\..\..\translate.h
 copy transtab.h ..\..\..\translate_table.h
-del trans.txt trans.h transtab.h
+del ic.h ic.sed vice.tex trans.txt trans.h transtab.h
