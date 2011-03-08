6f8e7050691d8c5901266163ac0f0fabaa43703b
blackystardust <blackystardust68@yahoo.com>
2011-03-08 19:53:53 +0000

    Added generation of infocontrib.h to the os/2 ow project files.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23835 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/os2/watcom/genwt.bat
+++ vice/src/arch/os2/watcom/genwt.bat
@@ -1,15 +1,20 @@
 @echo off
+copy ..\..\..\..\doc\vice.texi vice.tex
+copy ..\..\..\infocontrib.sed ic.sed
 copy ..\..\..\translate.txt trans.txt
 if Windows_NT == %OS% goto winnt
 goto use16
 :winnt
 if defined ProgramFiles(x86) GOTO use32
 :use16
+genic vice.tex ic.h ic.sed
 gentr trans.txt trans.h transtab.h
 goto after
 :use32
+geninfocontrib32 vice.tex ic.h ic.sed
 gentranslate32 trans.txt trans.h transtab.h
 :after
+copy ic.h ..\..\..\infocontrib.h
 copy trans.h ..\..\..\translate.h
 copy transtab.h ..\..\..\translate_table.h
-del trans.txt trans.h transtab.h
+del vice.tex ic.h ic.sed trans.txt trans.h transtab.h
