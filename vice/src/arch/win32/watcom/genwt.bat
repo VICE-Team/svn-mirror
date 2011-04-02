3379c4e267526ffdee840ceed4d6e8d7574ec088
blackystardust <blackystardust68@yahoo.com>
2011-04-02 22:40:14 +0000

    Added support for stringid.h to the win32 ow project files.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23901 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/watcom/genwt.bat
+++ vice/src/arch/win32/watcom/genwt.bat
@@ -12,17 +12,18 @@ goto use16
 if defined ProgramFiles(x86) GOTO use32
 :use16
 genic vice.tex ic.h ic.sed
-genwr res.h menuid.h res.txt resd.txt resm.txt ress.txt
+genwr res.h menuid.h strid.h res.txt resd.txt resm.txt ress.txt
 gentr trans.txt trans.h transtab.h
 goto after
 :use32
 geninfocontrib32 vice.tex ic.h ic.sed
-genwinres32 res.h menuid.h res.txt resd.txt resm.txt ress.txt
+genwinres32 res.h menuid.h strid.h res.txt resd.txt resm.txt ress.txt
 gentranslate32 trans.txt trans.h transtab.h
 :after
 copy ic.h ..\..\..\infocontrib.h 
 copy res.h ..\res.h
 copy menuid.h ..\menuid.h
+copy strid.h ..\stringid.h
 copy trans.h ..\..\..\translate.h
 copy transtab.h ..\..\..\translate_table.h
-del ic.h vice.tex ic.sed res.txt resd.txt resm.txt ress.txt trans.txt res.h menuid.h trans.h transtab.h
+del ic.h vice.tex ic.sed res.txt resd.txt resm.txt ress.txt trans.txt res.h menuid.h strid.h trans.h transtab.h
