c4e453768bfae3b6c698f25b9fec7f9c386c270d
pottendo <pottendo@gmx.net>
2011-04-05 19:39:53 +0000

    Check whether `/usr/bin/uname' is valid. E.g. Ubuntu has /bin/uname.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@23912 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/sedinfocontrib_h.sh
+++ vice/src/sedinfocontrib_h.sh
@@ -5,7 +5,7 @@
 # written by Christian Vogelgsang <chris@vogelgsang.org>
 
 # on Macs we need to set the correct locale otherwise the 8 Bit chars in sed generate errors
-if [ `/usr/bin/uname` = "Darwin" ]; then
+test -f /usr/bin/uname && if [ `/usr/bin/uname` = "Darwin" ]; then
   export LC_CTYPE=en_US.ISO8859-15
 fi
 
