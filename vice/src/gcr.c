e41bd1f59e2c7354f13111b80cee4cd1b1b56465
gpz <groepaz@gmx.net>
2011-07-04 03:44:32 +0000

    handle non byte aligned gcr data when decoding buffer. fixes bug 3200309
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@24247 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/gcr.c
+++ vice/src/gcr.c
@@ -25,6 +25,14 @@
  *
  */
 
+/* #define DEBUG_GCR */
+
+#ifdef DEBUG_GCR
+#define DBG(_x_)  log_debug _x_
+#else
+#define DBG(_x_)
+#endif
+
 #include "vice.h"
 
 #include <ctype.h>
@@ -34,6 +42,7 @@
 
 #include "gcr.h"
 #include "lib.h"
+#include "log.h"
 #include "types.h"
 
 
@@ -141,14 +150,38 @@ void gcr_convert_GCR_to_sector(BYTE *buffer, BYTE *ptr,
 {
     BYTE *offset = ptr;
     BYTE *GCR_track_end = GCR_track_start_ptr + GCR_current_track_size;
-    BYTE GCR_header[5];
-    int i, j;
+    BYTE GCR_header[6];
+    int i, j, s, shift;
+
+    /* additional 1 bits are part of the previous sync and must
+       be shifted out. so check/count these here */
+    shift = 0;
+    i = *(offset);
+    while (i & 0x80) {
+        i <<= 1;
+        shift++;
+    }
 
     for (i = 0; i < 65; i++) {
+        /* get 5 bytes of gcr data */
         for (j = 0; j < 5; j++) {
             GCR_header[j] = *(offset++);
-            if (offset >= GCR_track_end)
+            if (offset >= GCR_track_end) {
                 offset = GCR_track_start_ptr;
+            }
+        }
+        /* if the gcr data is not aligned, shift accordingly */
+        if (shift) {
+            GCR_header[5] = *(offset);
+            for (s = 0; s < shift; s++) {
+                for (j = 0; j < 5; j++) {
+                    GCR_header[j] <<= 1;
+                    if (GCR_header[j + 1] & 0x80) {
+                        GCR_header[j] |= 1;
+                    }
+                }
+                GCR_header[5] <<= 1;
+            }
         }
         gcr_convert_GCR_to_4bytes(GCR_header, buffer);
         buffer += 4;
@@ -161,19 +194,20 @@ BYTE *gcr_find_sector_header(unsigned int track, unsigned int sector,
 {
     BYTE *offset = gcr_track_start_ptr;
     BYTE *GCR_track_end = gcr_track_start_ptr + gcr_current_track_size;
-    BYTE GCR_header[5], header_data[4];
-    int i, wrap_over = 0;
+    BYTE GCR_header[6], header_data[4];
+    int i, wrap_over = 0, shift;
     unsigned int sync_count;
 
     sync_count = 0;
 
     while ((offset < GCR_track_end) && !wrap_over) {
+        /* find next sync start */
         while (*offset != 0xff) {
             offset++;
             if (offset >= GCR_track_end)
                 return NULL;
         }
-
+        /* skip to sync end */
         while (*offset == 0xff) {
             offset++;
             if (offset == GCR_track_end) {
@@ -184,7 +218,7 @@ BYTE *gcr_find_sector_header(unsigned int track, unsigned int sector,
             if ((++sync_count) >= gcr_current_track_size)
                 return NULL;
         }
-
+        /* get next 5(+1) gcr bytes, which are the header */
         for (i = 0; i < 5; i++) {
             GCR_header[i] = *(offset++);
             if (offset >= GCR_track_end) {
@@ -192,13 +226,30 @@ BYTE *gcr_find_sector_header(unsigned int track, unsigned int sector,
                 wrap_over = 1;
             }
         }
+        GCR_header[5] = *(offset);
+        /* shift out additional 1 bits, which are part of the sync */
+        shift = 0;
+        while (GCR_header[0] & 0x80) {
+            for (i = 0; i < 5; i++) {
+                GCR_header[i] <<= 1;
+                if (GCR_header[i + 1] & 0x80) {
+                    GCR_header[i] |= 1;
+                }
+            }
+            GCR_header[5] <<= 1;
+            shift++;
+        }
 
         gcr_convert_GCR_to_4bytes(GCR_header, header_data);
-
         if (header_data[0] == 0x08) {
             /* FIXME: Add some sanity checks here.  */
-            if (header_data[2] == sector && header_data[3] == track)
+            if (header_data[2] == sector && header_data[3] == track) {
+                DBG(("GCR: shift: %d hdr: %02x %02x sec:%02d trk:%02d", shift, header_data[0], header_data[1], header_data[2], header_data[3]));
+                if (shift) {
+                    log_warning(LOG_DEFAULT,"GCR data is not byte aligned (trk %d sec %d)", header_data[3], header_data[2]);
+                }
                 return offset;
+            }
         }
     }
     return NULL;
