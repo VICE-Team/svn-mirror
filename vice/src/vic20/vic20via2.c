6e0bf5ce803b3221538c93afe1f6d308c5a725c0
soci <soci@c64.rulez.org>
2011-10-01 07:35:53 +0000

    I'm sure this was not tested with rs232...
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@24745 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/vic20/vic20via2.c
+++ vice/src/vic20/vic20via2.c
@@ -237,12 +237,8 @@ inline static BYTE read_prb(via_context_t *via_context)
     byte = via_context->via[VIA_PRB] | ~(via_context->via[VIA_DDRB]);
 
 #ifdef HAVE_RS232
-    if (rsuser_enabled)
+    if (rsuser_enabled) {
         byte = rsuser_read_ctrl();
-    } else
-#else
-    {
-        byte = 0xff;
     }
 #endif
 
