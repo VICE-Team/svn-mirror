9f6181ff708d51e98f68c0ac6bf83c3cb659881e
soci <soci@c64.rulez.org>
2011-10-08 09:28:03 +0000

    Fix SO timing for BVC/BVS. See: http://sourceforge.net/tracker/?func=detail&aid=3288706&group_id=223021&atid=1057617
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@24769 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/6510core.c
+++ vice/src/6510core.c
@@ -2344,8 +2344,10 @@ trap_skipped:
 
           case 0x50:            /* BVC $nnnn */
 #ifdef DRIVE_CPU
+            CLK_ADD(CLK, -2);
             if (drivecpu_byte_ready())
                 LOCAL_SET_OVERFLOW(1);
+            CLK_ADD(CLK, 2);
 #endif
             BRANCH(!LOCAL_OVERFLOW(), p1);
             break;
@@ -2452,8 +2454,10 @@ trap_skipped:
 
           case 0x70:            /* BVS $nnnn */
 #ifdef DRIVE_CPU
+            CLK_ADD(CLK, -2);
             if (drivecpu_byte_ready())
                 LOCAL_SET_OVERFLOW(1);
+            CLK_ADD(CLK, 2);
 #endif
             BRANCH(LOCAL_OVERFLOW(), p1);
             break;
