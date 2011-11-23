e59f4d3673070e60cccefdd1f8d3a3bf26e62ff8
blackystardust <blackystardust68@yahoo.com>
2011-11-23 16:51:41 +0000

    Added tape stubs to vsid and adapted the builds for vsid to no longer use the tape lib.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@24877 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/vsidstubs.c
+++ vice/src/c64/vsidstubs.c
@@ -38,6 +38,9 @@
 #include "gfxoutput.h"   /* FIXME: this include can be removed once the structs defined in this include are no longer used in stubs */
 #include "midi.h"
 #include "machine-printer.h"
+#include "snapshot.h"
+#include "tap.h"
+#include "tape.h"
 #include "vicii-phi1.h"
 
 /*******************************************************************************
@@ -635,6 +638,105 @@ BYTE ds1216e_read(rtc_ds1216e_t *context, WORD address, BYTE origbyte)
 /* FIXME: these can be removed once the GUI's have been adapted for vsid */
 int userport_joystick_enable = 0;
 int userport_joystick_type = 0;
+
+
+/*******************************************************************************
+    tape
+*******************************************************************************/
+
+tape_image_t *tape_image_dev1 = NULL;
+
+int tape_image_attach(unsigned int unit, const char *name)
+{
+    return 0;
+}
+
+void tape_shutdown(void)
+{
+}
+
+int tape_tap_attached(void)
+{
+    return 0;
+}
+
+int tape_seek_start(tape_image_t *tape_image)
+{
+    return 0;
+}
+
+int tape_seek_to_file(tape_image_t *tape_image, unsigned int file_number)
+{
+    return 0;
+}
+
+void tape_image_event_playback(unsigned int unit, const char *filename)
+{
+}
+
+int tape_image_detach(unsigned int unit)
+{
+    return 0;
+}
+
+int tap_seek_start(tap_t *tap)
+{
+    return 0;
+}
+
+int tape_image_create(const char *name, unsigned int type)
+{
+    return 0;
+}
+
+int tape_snapshot_write_module(snapshot_t *s, int save_image)
+{
+    return 0;
+}
+
+int tape_snapshot_read_module(snapshot_t *s)
+{
+    return 0;
+}
+
+int tape_read(tape_image_t *tape_image, BYTE *buf, size_t size)
+{
+    return 0;
+}
+
+tape_file_record_t *tape_get_current_file_record(tape_image_t *tape_image)
+{
+    return NULL;
+}
+
+int tape_image_open(tape_image_t *tape_image)
+{
+    return 0;
+}
+
+int tape_image_close(tape_image_t *tape_image)
+{
+    return 0;
+}
+
+int tape_internal_close_tape_image(tape_image_t *tape_image)
+{
+    return 0;
+}
+
+tape_image_t *tape_internal_open_tape_image(const char *name, unsigned int read_only)
+{
+    return NULL;
+}
+
+int tape_seek_to_next_file(tape_image_t *tape_image, unsigned int allow_rewind)
+{
+    return 0;
+}
+
+void tape_get_header(tape_image_t *tape_image, BYTE *name)
+{
+}
 
 /*******************************************************************************
     FIXME: this "function" is never called, the functions referenced here cause
