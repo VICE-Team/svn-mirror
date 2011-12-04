074314185bb27a93eb136ec94f68e6a66756dba4
soci <soci@c64.rulez.org>
2011-12-04 18:51:01 +0000

    Extensions are returned without a dot
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@24960 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/cart/ide64.c
+++ vice/src/c64/cart/ide64.c
@@ -318,13 +318,13 @@ static void detect_ide64_image(struct drive_s *drive)
     drive->type = ATA_DRIVE_CF;
     ext = util_get_extension(drive->filename);
     if (ext) {
-        if (!strcasecmp(ext, ".cfa")) {
+        if (!strcasecmp(ext, "cfa")) {
             drive->type = ATA_DRIVE_CF;
-        } else if (!strcasecmp(ext, ".hdd")) {
+        } else if (!strcasecmp(ext, "hdd")) {
             drive->type = ATA_DRIVE_HDD;
-        } else if (!strcasecmp(ext, ".fdd")) {
+        } else if (!strcasecmp(ext, "fdd")) {
             drive->type = ATA_DRIVE_FDD;
-        } else if (!strcasecmp(ext, ".iso")) {
+        } else if (!strcasecmp(ext, "iso")) {
             drive->type = ATA_DRIVE_CD;
         }
     }
