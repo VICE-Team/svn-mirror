4f2272e88f5c1dc243c16370c04d9ba7429a9891
fabbo <fabrizio.ge@tiscalinet.it>
2012-02-18 00:14:40 +0000

    Unify Xaw versions of uimon.c and console.c
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@25300 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/unix/x11/xaw/console.c
+++ vice/src/arch/unix/x11/xaw/console.c
@@ -35,11 +35,16 @@
 #include "console.h"
 #include "lib.h"
 #include "log.h"
+#include "ui.h"
+#include "uimon.h"
 
-static FILE *mon_input, *mon_output;
+static console_t *console_log_local = NULL;
 
 #if defined(HAVE_READLINE)
 #include <readline/readline.h>
+#include <readline/history.h>
+#else
+static FILE *mon_input, *mon_output;
 #endif
 
 int console_init(void)
@@ -51,52 +56,67 @@ int console_init(void)
     return 0;
 }
 
-console_t *console_open(const char *id)
+console_t *uimon_window_open(void)
 {
-    console_t *console;
-
     if (!isatty(fileno(stdin))) {
         log_error(LOG_DEFAULT, "console_open: stdin is not a tty.");
-        return NULL;
+        console_log_local = NULL;
     }
-    if (!isatty(fileno(stdout))) {
+    else if (!isatty(fileno(stdout))) {
         log_error(LOG_DEFAULT, "console_open: stdout is not a tty.");
-        return NULL;
+        console_log_local = NULL;
     }
+    else {
+        console_log_local = lib_malloc(sizeof(console_t));
 
-    console = lib_malloc(sizeof(console_t));
+#if !defined(HAVE_READLINE)
+        mon_input = stdin;
+        mon_output = stdout;
+#endif
 
-    mon_input = stdin;
-    mon_output = stdout;
-
-    console->console_xres = 80;
-    console->console_yres = 25;
-    console->console_can_stay_open = 1;
-    console->console_cannot_output = 0;
-
-    return console;
+        console_log_local->console_xres = 80;
+        console_log_local->console_yres = 25;
+        console_log_local->console_can_stay_open = 1;
+        console_log_local->console_cannot_output = 0;
+    }
+    ui_focus_monitor();
+    return console_log_local;
 }
 
-int console_close(console_t *log)
+void uimon_window_close(void)
 {
-    lib_free(log);
+    lib_free(console_log_local);
+    console_log_local = NULL;
 
-    return 0;
+    uimon_window_suspend();
 }
 
-int console_out(console_t *log, const char *format, ...)
+void uimon_window_suspend( void )
 {
-    va_list ap;
+    ui_restore_focus();
+#ifdef HAVE_MOUSE
+    ui_check_mouse_cursor();
+#endif
+}
 
-    va_start(ap, format);
-    vfprintf(stdout, format, ap);
-    va_end(ap);
+console_t *uimon_window_resume(void)
+{
+    if (console_log_local) {
+        ui_focus_monitor();
+        return console_log_local;
+    }
+    log_error(LOG_DEFAULT, "uimon_window_resume: log was not opened.");
+    return uimon_window_open();
+}
 
+int uimon_out(const char *buffer)
+{
+    fprintf(stdout, "%s", buffer);
     return 0;
 }
 
 #ifndef HAVE_READLINE
-char *readline(const char *prompt)
+static char *readline(const char *prompt)
 {
     char *p = lib_malloc(1024);
 
@@ -120,7 +140,7 @@ char *readline(const char *prompt)
 }
 #endif
 
-char *console_in(console_t *log, const char *prompt)
+char *uimon_get_in(char **ppchCommandLine, const char *prompt)
 {
     char *p, *ret_sting;
 
@@ -138,3 +158,12 @@ int console_close_all(void)
 {
     return 0;
 }
+
+void uimon_notify_change( void )
+{
+}
+
+void uimon_set_interface(struct monitor_interface_s **monitor_interface_init, int count)
+{
+}
+
