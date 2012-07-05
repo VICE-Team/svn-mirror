2cf26c6719314838708ed1f8767a9366b41d3d48
blackystardust <blackystardust68@yahoo.com>
2012-07-05 17:21:46 +0000

    VICE version 2.3.20
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@25954 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/version.h
+++ vice/src/version.h
@@ -28,11 +28,11 @@
 #define VICE__VERSION_H__
 
 #ifndef VERSION
-#define VERSION "2.3.19"
+#define VERSION "2.3.20"
 #endif
 
 #ifndef VERSION_RC_NUMBER
-#define VERSION_RC_NUMBER 2,3,19,0
+#define VERSION_RC_NUMBER 2,3,20,0
 #endif
 
 #ifndef PACKAGE
