4385d9101c3eb6c55e03b3987a6442a7ce43bced
blackystardust <blackystardust68@yahoo.com>
2012-09-16 23:02:52 +0000

    Added userport rtc support to the dos tui.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@26193 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/msdos/c64ui.c
+++ vice/src/arch/msdos/c64ui.c
@@ -211,6 +211,7 @@ static tui_menu_item_def_t rom_menu_items[] = {
 /* ------------------------------------------------------------------------- */
 
 TUI_MENU_DEFINE_TOGGLE(SFXSoundSampler)
+TUI_MENU_DEFINE_TOGGLE(UserportRTC)
 
 int c64ui_init(void)
 {
@@ -286,6 +287,12 @@ int c64ui_init(void)
     tui_menu_add_item(ui_ioextensions_submenu, "Enable SFX Sound Sampler",
                       "Enable SFX Sound Sampler",
                       toggle_SFXSoundSampler_callback,
+                      NULL, 3,
+                      TUI_MENU_BEH_CONTINUE);
+
+    tui_menu_add_item(ui_ioextensions_submenu, "Enable Userport RTC",
+                      "Enable Userport RTC",
+                      toggle_UserportRTC_callback,
                       NULL, 3,
                       TUI_MENU_BEH_CONTINUE);
 
