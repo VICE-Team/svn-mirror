5a946cc3219e28323c5dcc799258ea58b1f1cbf8
cnvogelg <chris@vogelgsang.org>
2012-12-03 16:53:38 +0000

    macosx: fixed mouse handling
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@26587 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/unix/macosx/cocoa/mousedrv.m
+++ vice/src/arch/unix/macosx/cocoa/mousedrv.m
@@ -34,17 +34,20 @@
 // mouse.c
 extern int _mouse_enabled;
 
-static float pointerX;
-static float pointerY;
+static BOOL firstMove;
+static float last_x;
+static float last_y;
+static float mouse_x;
+static float mouse_y;
 static unsigned long mouse_timestamp = 0;
 
-static int  scaleX;
-static int  scaleY;
+static int  scale_x;
+static int  scale_y;
 
 static int set_scale_x(int val, void *param)
 {
     if((val > 0)&&(val <= 64)) {
-        scaleX = val;
+        scale_x = val;
     }
     return 0;
 }
@@ -52,17 +55,17 @@ static int set_scale_x(int val, void *param)
 static int set_scale_y(int val, void *param)
 {
     if((val > 0)&&(val <= 64)) {
-        scaleY = val;
+        scale_y = val;
     }
     return 0;
 }
 
 static resource_int_t resources_int[] =
 {
-    { "MouseScaleX", 2, RES_EVENT_NO, NULL,
-       &scaleX, set_scale_x, NULL },
-    { "MouseScaleY", 2, RES_EVENT_NO, NULL,
-       &scaleY, set_scale_y, NULL },
+    { "MouseScaleX", 1, RES_EVENT_NO, NULL,
+       &scale_x, set_scale_x, NULL },
+    { "MouseScaleY", 1, RES_EVENT_NO, NULL,
+       &scale_y, set_scale_y, NULL },
     { NULL }
  };
 
@@ -97,27 +100,56 @@ void mousedrv_init(void)
 void mousedrv_mouse_changed(void)
 {
     [[theVICEMachine machineNotifier] postToggleMouseNotification:_mouse_enabled];
+
+    if(_mouse_enabled) {
+        firstMove = YES;
+        mouse_x = 0.0f;
+        mouse_y = 0.0f;
+    }
 }
 
 // the HW polls the position
 int mousedrv_get_x(void)
 {
-    return (int)(x * scaleX) & 0xffff;
+    return (int)(mouse_x) & 0xffff;
 }
 
 int mousedrv_get_y(void)
 {
-    return (int)(y * scaleY) & 0xffff;
+    return (int)(mouse_y) & 0xffff;
 }
 
-// FIXME: I think this should use relative movements,
-// and possibly reposition the pointer when too close
-// to the edge.
+// this call receives absolute mouse pos on canvas
 void mouse_move_f(float x, float y)
 {
-    pointerX = x;
-    pointerY = y;
     mouse_timestamp = vsyncarch_gettime();
+
+    if(firstMove) {
+        // ignore first move
+        firstMove = NO;
+    } else {
+        // calc delta movement
+        float dx = x - last_x;
+        float dy = y - last_y;
+    
+        // apply (optional) scale on movement
+        dx *= scale_x;
+        dy *= scale_y;
+    
+        // add onto current mouse pos
+        mouse_x += dx;
+        mouse_y += dy;
+        
+        // map to 0 .. 0xffff range
+        while (mouse_x < 0.0) mouse_x += 65536.0;
+        while (mouse_x >= 65536.0) mouse_x -= 65536.0;
+        while (mouse_y < 0.0) mouse_y += 65536.0;
+        while (mouse_y >= 65536.0) mouse_y -= 65536.0;
+    }
+    
+    // store last pos
+    last_x = x;
+    last_y = y;
 }
 
 unsigned long mousedrv_get_timestamp(void)
