f673c639eecb35c3b2e3813cbf7969ffcf52c142
blackystardust <blackystardust68@yahoo.com>
2013-01-29 02:50:31 +0000

    Fixed the vice_commonall android module.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@27160 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/android/AnVICE/jni/vice_commonall/Android.mk
+++ vice/src/arch/android/AnVICE/jni/vice_commonall/Android.mk
@@ -12,7 +12,8 @@ $(MY_PATH2)/src/raster \
 $(MY_PATH2)/src/video \
 $(MY_PATH2)/src/monitor \
 $(MY_PATH2)/src/core \
-$(MY_PATH2)/src/platform
+$(MY_PATH2)/src/platform \
+$(MY_PATH2)/src/arch/sdl
 
 # Add more subdirs here, like src/subdir1 src/subdir2
 
@@ -56,72 +57,6 @@ LOCAL_CPP_EXTENSION := .cpp
 # Uncomment to also add C sources
 #LOCAL_SRC_FILES += $(foreach F, $(CG_SUBDIRS), $(addprefix $(F)/,$(notdir $(wildcard $(LOCAL_PATH)/$(F)/*.c))))
 LOCAL_SRC_FILES += \
-	$(MY_PATH2)/src/arch/sdl/archdep.c \
-	$(MY_PATH2)/src/arch/sdl/blockdev.c \
-	$(MY_PATH2)/src/arch/sdl/catweaselmkiii.c \
-	$(MY_PATH2)/src/arch/sdl/console.c \
-	$(MY_PATH2)/src/arch/sdl/coproc.c \
-	$(MY_PATH2)/src/arch/sdl/dynlib.c \
-	$(MY_PATH2)/src/arch/sdl/fullscreen.c \
-	$(MY_PATH2)/src/arch/sdl/hardsid.c \
-	$(MY_PATH2)/src/arch/sdl/joy.c \
-	$(MY_PATH2)/src/arch/sdl/kbd.c \
-	$(MY_PATH2)/src/arch/sdl/lightpendrv.c \
-	$(MY_PATH2)/src/arch/sdl/menu_c64cart.c \
-	$(MY_PATH2)/src/arch/sdl/menu_c64_common_expansions.c \
-	$(MY_PATH2)/src/arch/sdl/menu_c64_expansions.c \
-	$(MY_PATH2)/src/arch/sdl/menu_c64hw.c \
-	$(MY_PATH2)/src/arch/sdl/menu_c64model.c \
-	$(MY_PATH2)/src/arch/sdl/menu_common.c \
-	$(MY_PATH2)/src/arch/sdl/menu_debug.c \
-	$(MY_PATH2)/src/arch/sdl/menu_drive.c \
-	$(MY_PATH2)/src/arch/sdl/menu_drive_rom.c \
-	$(MY_PATH2)/src/arch/sdl/menu_ffmpeg.c \
-	$(MY_PATH2)/src/arch/sdl/menu_help.c \
-	$(MY_PATH2)/src/arch/sdl/menu_joystick.c \
-	$(MY_PATH2)/src/arch/sdl/menu_lightpen.c \
-	$(MY_PATH2)/src/arch/sdl/menu_midi.c \
-	$(MY_PATH2)/src/arch/sdl/menu_mouse.c \
-	$(MY_PATH2)/src/arch/sdl/menu_network.c \
-	$(MY_PATH2)/src/arch/sdl/menu_printer.c \
-	$(MY_PATH2)/src/arch/sdl/menu_ram.c \
-	$(MY_PATH2)/src/arch/sdl/menu_reset.c \
-	$(MY_PATH2)/src/arch/sdl/menu_rom.c \
-	$(MY_PATH2)/src/arch/sdl/menu_rs232.c \
-	$(MY_PATH2)/src/arch/sdl/menu_screenshot.c \
-	$(MY_PATH2)/src/arch/sdl/menu_settings.c \
-	$(MY_PATH2)/src/arch/sdl/menu_sid.c \
-	$(MY_PATH2)/src/arch/sdl/menu_snapshot.c \
-	$(MY_PATH2)/src/arch/sdl/menu_sound.c \
-	$(MY_PATH2)/src/arch/sdl/menu_speed.c \
-	$(MY_PATH2)/src/arch/sdl/menu_tape.c \
-	$(MY_PATH2)/src/arch/sdl/menu_tfe.c \
-	$(MY_PATH2)/src/arch/sdl/menu_video.c \
-	$(MY_PATH2)/src/arch/sdl/mididrv.c \
-	$(MY_PATH2)/src/arch/sdl/mousedrv.c \
-	$(MY_PATH2)/src/arch/sdl/parsid.c \
-	$(MY_PATH2)/src/arch/sdl/rs232.c \
-	$(MY_PATH2)/src/arch/sdl/rs232dev.c \
-	$(MY_PATH2)/src/arch/sdl/rs232net.c \
-	$(MY_PATH2)/src/arch/sdl/sdlmain.c \
-	$(MY_PATH2)/src/arch/sdl/signals.c \
-	$(MY_PATH2)/src/arch/sdl/rawnetarch.c \
-	$(MY_PATH2)/src/arch/sdl/ui.c \
-	$(MY_PATH2)/src/arch/sdl/uicmdline.c \
-	$(MY_PATH2)/src/arch/sdl/uifilereq.c \
-	$(MY_PATH2)/src/arch/sdl/uihotkey.c \
-	$(MY_PATH2)/src/arch/sdl/uimenu.c \
-	$(MY_PATH2)/src/arch/sdl/uimon.c \
-	$(MY_PATH2)/src/arch/sdl/uimsgbox.c \
-	$(MY_PATH2)/src/arch/sdl/uipause.c \
-	$(MY_PATH2)/src/arch/sdl/uipoll.c \
-	$(MY_PATH2)/src/arch/sdl/uistatusbar.c \
-	$(MY_PATH2)/src/arch/sdl/video.c \
-	$(MY_PATH2)/src/arch/sdl/vkbd.c \
-	$(MY_PATH2)/src/arch/sdl/vsidui.c \
-	$(MY_PATH2)/src/arch/sdl/vsyncarch.c \
-	$(MY_PATH2)/src/arch/sdl/x64_ui.c \
-	$(MY_PATH2)/src/arch/sdl/x64sc_ui.c \
 	$(MY_PATH2)/src/serial/fsdrive.c \
 	$(MY_PATH2)/src/serial/serial.c \
 	$(MY_PATH2)/src/serial/serial-device.c \
