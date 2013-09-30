acc6eb8f1d9dca6ae71090af176b20f0423c1b44
blackystardust <blackystardust68@yahoo.com>
2013-09-30 15:23:53 +0000

    No hppa arch option in openstep.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@27770 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/build/openstep/build.sh
+++ vice/build/openstep/build.sh
@@ -13,7 +13,6 @@ curdir=`pwd`
 
 build_i386=no
 build_m68k=no
-build_hppa=no
 build_sparc=no
 
 if test x"$1" = "xi386" -o x"$2" = "xi386" -o x"$3" = "xi386" -o x"$4" = "xi386"; then
@@ -24,17 +23,13 @@ if test x"$1" = "xm68k" -o x"$2" = "xm68k" -o x"$3" = "xm68k" -o x"$4" = "xm68k"
   build_m68k=yes
 fi
 
-if test x"$1" = "xhppa" -o x"$2" = "xhppa" -o x"$3" = "xhppa" -o x"$4" = "xhppa"; then
-  build_hppa=yes
-fi
-
 if test x"$1" = "xsparc" -o x"$2" = "xsparc" -o x"$3" = "xsparc" -o x"$4" = "xsparc"; then
   build_sparc=yes
 fi
 
-if test x"$build_i386" = "xno" -a x"$build_m68k" = "xno" -a x"$build_hppa" = "xno" -a x"$build_sparc" = "xno"; then
+if test x"$build_i386" = "xno" -a x"$build_m68k" = "xno" -a x"$build_sparc" = "xno"; then
   echo "At least one platform needs to be selected for the build"
-  echo "Usage: build.sh [i386] [m68k] [hppa] [sparc]"
+  echo "Usage: build.sh [i386] [m68k] [sparc]"
   exit 1
 fi
 
@@ -84,28 +79,6 @@ if test x"$build_m68k" = "xyes"; then
   fi
 fi
 
-if test x"$build_hppa" = "xyes"; then
-  if test x"$makedone" = "xyes"; then
-    make clean
-  fi
-  CFLAGS="-s -arch hppa" ./configure -v --host=hppa-next-openstep --prefix=/usr/local -disable-nls --without-resid
-  make
-  makedone=yes
-  if [ ! -e src/x64 -o ! -e src/x64dtv -o ! -e src/x128 -o ! -e src/xvic -o ! -e src/xpet -o ! -e src/xplus4 -o ! -e src/xcbm2 -o ! -e src/c1541 -o ! -e src/petcat -o ! -e src/cartconv ]
-  then
-    echo Error: One (or more) hppa binaries missing
-    exit 1
-  fi
-  if test x"$build_i386" = "xyes" -o x"$build_m68k" = "xyes" -o x"$build_sparc" = "xyes"; then
-    for i in x128 x64 x64dtv xcbm2 xpet xplus4 xvic c1541 cartconv petcat
-    do
-      mv src/$i bins/$i.hppa
-    done
-  else
-    single_build=yes
-  fi
-fi
-
 if test x"$build_sparc" = "xyes"; then
   if test x"$makedone" = "xyes"; then
     make clean
@@ -139,10 +112,6 @@ if test x"$single_build" = "xno"; then
 
     if test x"$build_m68k" = "xyes"; then
         binaries="$binaries bins/$i.m68k"
-    fi
-
-    if test x"$build_hppa" = "xyes"; then
-        binaries="$binaries bins/$i.hppa"
     fi
 
     if test x"$build_sparc" = "xyes"; then
