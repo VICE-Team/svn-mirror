a9001a52e858446dc2e0edc39dfb1fd0a07435c7
blackystardust <blackystardust68@yahoo.com>
2013-10-24 00:15:32 +0000

    Added missing table.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@27797 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/util.c
+++ vice/src/util.c
@@ -918,6 +918,15 @@ size_t strlcpy(char *dst, const char *src, size_t maxlen)
 }
 #endif
 
+#if !defined(HAVE_LTOA) || !defined(HAVE_ULTOA)
+static const char ntoa_table[] = {
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z'
+};
+#endif
+
 /* Taken from SDL */
 #ifndef HAVE_LTOA
 char *ltoa(long value, char *string, int radix)
