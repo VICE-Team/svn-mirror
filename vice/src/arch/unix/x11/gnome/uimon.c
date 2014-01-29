7c3fc087dbaa9d7141e63379725fddf2694894ab
fabbo <fabrizio.ge@tiscalinet.it>
2014-01-29 23:26:42 +0000

    Prevent resizing of monitor window
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@27904 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/unix/x11/gnome/uimon.c
+++ vice/src/arch/unix/x11/gnome/uimon.c
@@ -311,6 +311,7 @@ console_t *uimon_window_open(void)
         gtk_window_set_position(GTK_WINDOW(fixed.window), GTK_WIN_POS_CENTER);
         gtk_widget_set_app_paintable(fixed.window, TRUE);
         gtk_window_set_deletable(GTK_WINDOW(fixed.window), TRUE);
+        gtk_window_set_resizable(GTK_WINDOW(fixed.window), FALSE);
         fixed.term = vte_terminal_new();
         vte_terminal_set_scrollback_lines (VTE_TERMINAL(fixed.term), 1000);
         vte_terminal_set_scroll_on_output (VTE_TERMINAL(fixed.term), TRUE);
