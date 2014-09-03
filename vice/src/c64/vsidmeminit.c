e76c66e1d1b1d72ea185b927f8bdb224b05ed7d8
blackystardust <blackystardust68@yahoo.com>
2014-09-03 20:45:08 +0000

    Simplified the vsid memory handling and removed some unused vsid stubs.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@28340 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/vsidmeminit.c
+++ vice/src/c64/vsidmeminit.c
@@ -46,8 +46,6 @@
 
  missing: BA,CAS(not needed),RW(through tables),AEC
 
- bit 4 - !game
- bit 3 - !exrom
  bit 2 - loram
  bit 1 - hiram
  bit 0 - charen
@@ -63,71 +61,14 @@
  6 0x06                      io        ker
  7 0x07            bas       io        ker
 
- 8 0x08
- 9 0x09                      chr
-10 0x0a            romh      chr       ker
-11 0x0b  roml      bas/romh  chr       ker      8k game
-12 0x0c
-13 0x0d                      io
-14 0x0e            romh      io        ker
-15 0x0f  roml      bas/romh  io        ker      8k game
-
-16 0x10  roml      -         io        romh     ultimax
-17 0x11  roml      -         io        romh     ultimax
-18 0x12  roml      -         io        romh     ultimax
-19 0x13  roml      -         io        romh     ultimax
-20 0x14  roml      -         io        romh     ultimax
-21 0x15  roml      -         io        romh     ultimax
-22 0x16  roml      -         io        romh     ultimax
-23 0x17  roml      -         io        romh     ultimax
-
-24 0x18
-25 0x19
-26 0x1a            romh      chr       ker
-27 0x1b  roml      romh      chr       ker      16k game
-28 0x1c
-29 0x1d                      io
-30 0x1e            romh      io        ker
-31 0x1f  roml      romh      io        ker      16k game
-
 */
 
-/* IO is enabled at memory configs 5, 6, 7 and Ultimax.  */
+/* IO is enabled at memory configs 5, 6, 7 and mirrors since game/exrom will never be changed.  */
 const unsigned int c64meminit_io_config[32] = {
     0, 0, 0, 0, 0, 1, 1, 1,
-    0, 0, 0, 0, 0, 1, 1, 1,
-    2, 2, 2, 2, 2, 2, 2, 2,
-    0, 0, 0, 0, 0, 1, 1, 1
-};
-
-/* ROML is enabled at memory configs 11, 15, 27, 31 and Ultimax.  */
-static const unsigned int c64meminit_roml_config[32] = {
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 1, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    0, 0, 0, 1, 0, 0, 0, 1
-};
-
-/* ROMH is enabled at memory configs 10, 11, 14, 15, 26, 27, 30, 31
-   and Ultimax.  */
-static const unsigned int c64meminit_romh_config[32] = {
     0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    0, 0, 1, 1, 0, 0, 1, 1
-};
-
-/* ROMH is mapped to $A000-$BFFF at memory configs 10, 11, 14, 15, 26,
-   27, 30, 31.  If Ultimax is enabled it is mapped to $E000-$FFFF.  */
-static const unsigned int c64meminit_romh_mapping[32] = {
-    0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00,
-    0xe0, 0xe0, 0xe0, 0xe0,
-    0xe0, 0xe0, 0xe0, 0xe0,
-    0x00, 0x00, 0xa0, 0xa0,
-    0x00, 0x00, 0xa0, 0xa0
+    0, 0, 0, 0, 0, 0, 0, 0
 };
 
 BYTE vsid_io_read(WORD addr)
@@ -165,28 +106,17 @@ void vsid_io_store(WORD addr, BYTE val)
 void c64meminit(unsigned int base)
 {
     unsigned int i, j;
-    int board = 0;
 
-    if (machine_class != VICE_MACHINE_C128) {
-        resources_get_int("BoardType", &board);
-    }
-
-    if (board != 1) {
-        /* Setup BASIC ROM at $A000-$BFFF (memory configs 3, 7, 11, 15).  */
-        for (i = 0xa0; i <= 0xbf; i++) {
-            mem_read_tab_set(base + 3, i, c64memrom_basic64_read);
-            mem_read_tab_set(base + 7, i, c64memrom_basic64_read);
-            mem_read_tab_set(base + 11, i, c64memrom_basic64_read);
-            mem_read_tab_set(base + 15, i, c64memrom_basic64_read);
-            mem_read_base_set(base + 3, i, c64memrom_basic64_rom - 0xa000);
-            mem_read_base_set(base + 7, i, c64memrom_basic64_rom - 0xa000);
-            mem_read_base_set(base + 11, i, c64memrom_basic64_rom - 0xa000);
-            mem_read_base_set(base + 15, i, c64memrom_basic64_rom - 0xa000);
-        }
+    /* Setup BASIC ROM at $A000-$BFFF (memory configs 3 and 7).  */
+    for (i = 0xa0; i <= 0xbf; i++) {
+        mem_read_tab_set(base + 3, i, c64memrom_basic64_read);
+        mem_read_tab_set(base + 7, i, c64memrom_basic64_read);
+        mem_read_base_set(base + 3, i, c64memrom_basic64_rom - 0xa000);
+        mem_read_base_set(base + 7, i, c64memrom_basic64_rom - 0xa000);
     }
 
     /* Setup I/O at $D000-$DFFF (memory configs 5, 6, 7).  */
-    for (j = 0; j < 32; j++) {
+    for (j = 0; j < 8; j++) {
         if (c64meminit_io_config[j] == 1) {
             mem_read_tab_set(base + j, 0xd0, vicii_read);
             mem_set_write_hook(base + j, 0xd0, vicii_store);
@@ -211,10 +141,8 @@ void c64meminit(unsigned int base)
 
             mem_read_tab_set(base + j, 0xdc, cia1_read);
             mem_set_write_hook(base + j, 0xdc, cia1_store);
-            if (board != 1) {
-                mem_read_tab_set(base + j, 0xdd, cia2_read);
-                mem_set_write_hook(base + j, 0xdd, cia2_store);
-            }
+            mem_read_tab_set(base + j, 0xdd, cia2_read);
+            mem_set_write_hook(base + j, 0xdd, cia2_store);
 
             mem_read_tab_set(base + j, 0xde, vsid_io_read);
             mem_set_write_hook(base + j, 0xde, vsid_io_store);
@@ -225,127 +153,17 @@ void c64meminit(unsigned int base)
                 mem_read_base_set(base + j, i, NULL);
             }
         }
-        if (c64meminit_io_config[j] == 2) {
-            for (i = 0xd0; i <= 0xdf; i++) {
-                mem_read_tab_set(base + j, i, ultimax_d000_dfff_read);
-                mem_set_write_hook(base + j, i, ultimax_d000_dfff_store);
-                mem_read_base_set(base + j, i, NULL);
-            }
-        }
     }
 
-    if (board != 1) {
-        /* Setup Kernal ROM at $E000-$FFFF (memory configs 2, 3, 6, 7, 10,
-        11, 14, 15, 26, 27, 30, 31).  */
-        for (i = 0xe0; i <= 0xff; i++) {
-            mem_read_tab_set(base + 2, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 3, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 6, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 7, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 10, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 11, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 14, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 15, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 26, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 27, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 30, i, c64memrom_kernal64_read);
-            mem_read_tab_set(base + 31, i, c64memrom_kernal64_read);
-            mem_read_base_set(base + 2, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 3, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 6, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 7, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 10, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 11, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 14, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 15, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 26, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 27, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 30, i, c64memrom_kernal64_trap_rom - 0xe000);
-            mem_read_base_set(base + 31, i, c64memrom_kernal64_trap_rom - 0xe000);
-        }
-    }
-
-    /* Setup ROML at $8000-$9FFF.  */
-    for (j = 0; j < 32; j++) {
-        if (c64meminit_roml_config[j]) {
-            for (i = 0x80; i <= 0x9f; i++) {
-                mem_read_tab_set(base + j, i, roml_read);
-                mem_read_base_set(base + j, i, NULL);
-                mem_set_write_hook(base + j, i, roml_no_ultimax_store);
-            }
-        }
-    }
-
-    /* Setup write Hook for when ROML is NOT selected at $8000-$9FFF in 8K Game, 16K Game */
-    for (j = 8; j < 16; j++) {
-        if (!c64meminit_roml_config[j]) {
-            for (i = 0x80; i <= 0x9f; i++) {
-                mem_set_write_hook(base + j, i, raml_no_ultimax_store);
-            }
-        }
-    }
-    for (j = 24; j < 32; j++) {
-        if (!c64meminit_roml_config[j]) {
-            for (i = 0x80; i <= 0x9f; i++) {
-                mem_set_write_hook(base + j, i, raml_no_ultimax_store);
-            }
-        }
-    }
-    /* Setup write Hook for when ROML is NOT selected at $8000-$9FFF when cart is off */
-    for (j = 0; j < 8; j++) {
-        if (!c64meminit_roml_config[j]) {
-            for (i = 0x80; i <= 0x9f; i++) {
-                mem_set_write_hook(base + j, i, raml_no_ultimax_store);
-            }
-        }
-    }
-    /* Setup ROMH at $A000-$BFFF */
-    for (j = 24; j < 32; j++) {
-        if (c64meminit_romh_config[j]) {
-            for (i = c64meminit_romh_mapping[j]; i <= c64meminit_romh_mapping[j] + 0x1f; i++) {
-                mem_read_tab_set(base + j, i, romh_read);
-                mem_read_base_set(base + j, i, NULL);
-                mem_set_write_hook(base + j, i, romh_no_ultimax_store);
-            }
-        }
-    }
-    /* Setup ROMH at $E000-$FFFF (ultimax)  */
-    for (j = 16; j < 24; j++) {
-        if (c64meminit_romh_config[j]) {
-            for (i = c64meminit_romh_mapping[j]; i <= c64meminit_romh_mapping[j] + 0x1f; i++) {
-                if (j & 2) {
-                    mem_read_tab_set(base + j, i, ultimax_romh_read_hirom);
-                } else {
-                    mem_read_tab_set(base + j, i, romh_read);
-                }
-                mem_read_base_set(base + j, i, NULL);
-            }
-        }
-    }
-
-
-    /* Setup Ultimax configuration.  */
-    for (j = 16; j < 24; j++) {
-        for (i = 0x10; i <= 0x7f; i++) {
-            mem_read_tab_set(base + j, i, ultimax_1000_7fff_read);
-            mem_set_write_hook(base + j, i, ultimax_1000_7fff_store);
-            mem_read_base_set(base + j, i, NULL);
-        }
-        for (i = 0x80; i <= 0x9f; i++) {
-            mem_set_write_hook(base + j, i, roml_store);
-        }
-        for (i = 0xa0; i <= 0xbf; i++) {
-            mem_read_tab_set(base + j, i, ultimax_a000_bfff_read);
-            mem_set_write_hook(base + j, i, ultimax_a000_bfff_store);
-            mem_read_base_set(base + j, i, NULL);
-        }
-        for (i = 0xc0; i <= 0xcf; i++) {
-            mem_read_tab_set(base + j, i, ultimax_c000_cfff_read);
-            mem_set_write_hook(base + j, i, ultimax_c000_cfff_store);
-            mem_read_base_set(base + j, i, NULL);
-        }
-        for (i = 0xe0; i <= 0xff; i++) {
-            mem_set_write_hook(base + j, i, romh_store);
-        }
+    /* Setup Kernal ROM at $E000-$FFFF (memory configs 2, 3, 6, 7).  */
+    for (i = 0xe0; i <= 0xff; i++) {
+        mem_read_tab_set(base + 2, i, c64memrom_kernal64_read);
+        mem_read_tab_set(base + 3, i, c64memrom_kernal64_read);
+        mem_read_tab_set(base + 6, i, c64memrom_kernal64_read);
+        mem_read_tab_set(base + 7, i, c64memrom_kernal64_read);
+        mem_read_base_set(base + 2, i, c64memrom_kernal64_trap_rom - 0xe000);
+        mem_read_base_set(base + 3, i, c64memrom_kernal64_trap_rom - 0xe000);
+        mem_read_base_set(base + 6, i, c64memrom_kernal64_trap_rom - 0xe000);
+        mem_read_base_set(base + 7, i, c64memrom_kernal64_trap_rom - 0xe000);
     }
 }
