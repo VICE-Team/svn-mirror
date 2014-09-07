c106240ac5637bed5ccee1b40538ba53686ea710
loggedoubt <loggedoubt@gmail.com>
2014-09-07 23:44:01 +0000

    Major rewrite of the BeVICE Drive Settings dialog, making it auto-adjust to machine-specific number of drive types, expansions and parallel cable types (which get radio buttons now.)
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@28356 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/beos/ui_drive.cc
+++ vice/src/arch/beos/ui_drive.cc
@@ -4,6 +4,7 @@
  * Written by
  *  Andreas Matthies <andreas.matthies@gmx.net>
  *  Marco van den Heuvel <blackystardust68@yahoo.com>
+ *  Marcus Sutton <loggedoubt@gmail.com>
  *
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
@@ -42,40 +43,55 @@ extern "C" {
 
 static ui_drive_type_t *drive_list;
 
-static int drive_machine_parallel_capable;
+static int machine_drive_type_count;
 
-struct _drive_extendimagepolicy {
+static int machine_drive_expansion_count;
+
+static int machine_parallel_cable_type_count;
+
+static struct _extend_image_policy {
     const char *name;
     int id;
-    BRadioButton *radiobutton; /* we have to remeber for enable/disable them */
-} drive_extendimagepolicy[] = {
-    { "Never extend", DRIVE_EXTEND_NEVER, NULL },
-    { "Ask on extend", DRIVE_EXTEND_ASK, NULL },
-    { "Extend on access", DRIVE_EXTEND_ACCESS, NULL },
-    { NULL, 0, NULL }
+} extend_image_policy[] = {
+    { "Never extend", DRIVE_EXTEND_NEVER },
+    { "Ask on extend", DRIVE_EXTEND_ASK },
+    { "Extend on access", DRIVE_EXTEND_ACCESS },
+    { NULL, 0 }
 };
 
-struct _drive_expansion {
+static struct _expansion {
     const char *name;
     const char *resource_name;
-} drive_expansion[] = {
-    { "$2000-$3FFF RAM", "Drive%dRAM2000" },
-    { "$4000-$5FFF RAM", "Drive%dRAM4000" },
-    { "$6000-$7FFF RAM", "Drive%dRAM6000" },
-    { "$8000-$9FFF RAM", "Drive%dRAM8000" },
-    { "$A000-$BFFF RAM", "Drive%dRAMA000" },
-    { "Professional DOS", "Drive%dProfDOS" },
-    { "SuperCard+", "Drive%dSuperCard" },
-    { NULL, NULL }
+    int (*check_func) (int drive_type);
+} expansion[] = {
+    { "$2000-$3FFF RAM", "Drive%dRAM2000", drive_check_expansion2000 },
+    { "$4000-$5FFF RAM", "Drive%dRAM4000", drive_check_expansion4000 },
+    { "$6000-$7FFF RAM", "Drive%dRAM6000", drive_check_expansion6000 },
+    { "$8000-$9FFF RAM", "Drive%dRAM8000", drive_check_expansion8000 },
+    { "$A000-$BFFF RAM", "Drive%dRAMA000", drive_check_expansionA000 },
+    { "Professional DOS", "Drive%dProfDOS", drive_check_profdos },
+    { "SuperCard+", "Drive%dSuperCard", drive_check_supercard },
+    { NULL, NULL, NULL }
 };
 
-struct _drive_idlemethod {
+static struct _idle_method {
     const char *name;
     int id;
-} drive_idlemethod[] = {
+} idle_method[] = {
     { "None", DRIVE_IDLE_NO_IDLE },
     { "Trap idle", DRIVE_IDLE_TRAP_IDLE },
     { "Skip cycles", DRIVE_IDLE_SKIP_CYCLES },
+    { NULL, 0 }
+};
+
+static struct _parallel_cable {
+    const char *name;
+    int id;
+} parallel_cable[] = {
+    { "None", DRIVE_PC_NONE },
+    { "Standard", DRIVE_PC_STANDARD },
+    { "Dolphin DOS 3", DRIVE_PC_DD3 },
+    { "Formel 64", DRIVE_PC_FORMEL64 },
     { NULL, 0 }
 };
 
@@ -87,8 +103,8 @@ class DriveView : public BView {
         /* we have to remember some controls to enable/disable them */
         BRadioButton *rb_extendimagepolicy[3];
         BRadioButton *rb_idlemethod[3];
-        BCheckBox *cb_expansion[5];
-        BCheckBox *cb_parallelcable;
+        BRadioButton *rb_parallelcable[4];
+        BCheckBox *cb_expansion[7];
 };
 
 void DriveView::EnableControlsForDriveSettings(int type_index)
@@ -99,56 +115,34 @@ void DriveView::EnableControlsForDriveSettings(int type_index)
 
     current_drive_type = drive_list[type_index].id;
 
-    for (i = 0; drive_extendimagepolicy[i].name; i++) {
+    for (i = 0; extend_image_policy[i].name; i++) {
         rb_extendimagepolicy[i]->SetEnabled(drive_check_extend_policy(current_drive_type));
     }
-    for (i = 0; drive_idlemethod[i].name; i++) {
+    for (i = 0; idle_method[i].name; i++) {
         rb_idlemethod[i]->SetEnabled(drive_check_idle_method(current_drive_type));
     }
-    cb_parallelcable->SetEnabled(drive_check_parallel_cable(current_drive_type) && drive_machine_parallel_capable);
-    for (i = 0; drive_expansion[i].name; i++) {
-        switch (i) {
-            case 0:
-                expand_is_possible = drive_check_expansion2000(current_drive_type);
-                break;
-            case 1:
-                expand_is_possible = drive_check_expansion4000(current_drive_type);
-                break;
-            case 2:
-                expand_is_possible = drive_check_expansion6000(current_drive_type);
-                break;
-            case 3:
-                expand_is_possible = drive_check_expansion8000(current_drive_type);
-                break;
-            case 4:
-                expand_is_possible = drive_check_expansionA000(current_drive_type);
-                break;
-            default:
-                expand_is_possible = false;
-        }
+    for (i = 0; i < machine_drive_expansion_count; i++) {
+        expand_is_possible = expansion[i].check_func(current_drive_type);
         cb_expansion[i]->SetEnabled(expand_is_possible);
     }
+    for (i = 0; i < machine_parallel_cable_type_count; i++) {
+        rb_parallelcable[i]->SetEnabled(drive_check_parallel_cable(current_drive_type));
+    }
+
 }
 
 DriveView::DriveView(BRect r, int drive_num) : BView(r, "drive_view", B_FOLLOW_NONE, B_WILL_DRAW)
 {
-    int i;
-    int current_type;
-    int current_extendimagepolicy;
-    int current_idlemethod;
-    int current_expansion;
-    int current_parallelcable;
-    BMessage *msg;
-
     BBox *box;
     BRadioButton *radiobutton;
     BCheckBox *checkbox;
+    BMessage *msg;
+
+    int i;
+    int current_value;
+    char resname[32];
 
     BView::SetViewColor(220, 220, 220, 0);
-
-    resources_get_int_sprintf("Drive%dType", &current_type, drive_num);
-    resources_get_int_sprintf("Drive%dExtendImagePolicy", &current_extendimagepolicy, drive_num);
-    resources_get_int_sprintf("Drive%dIdleMethod", &current_idlemethod, drive_num);
 
     /* extend image policy */
     r.OffsetTo(90, 0);
@@ -158,47 +152,40 @@ DriveView::DriveView(BRect r, int drive_num) : BView(r, "drive_view", B_FOLLOW_N
     box->SetLabel("40 track handling");
     AddChild(box);
     r.InsetBy(10, 10);
-    for (i = 0; drive_extendimagepolicy[i].name; i++) {
+    sprintf(resname, "Drive%dExtendImagePolicy", drive_num);
+    resources_get_int(resname, &current_value);
+    for (i = 0; extend_image_policy[i].name; i++) {
         msg = new BMessage(MESSAGE_DRIVE_EXTENDIMAGEPOLICY);
-        msg->AddInt32("drive_num", drive_num);
+        msg->AddString("resname", resname);
         msg->AddInt32("resource_index", i);
-        radiobutton = new BRadioButton(BRect(10, 20 + i * 25, 120, 30 + i * 25), drive_extendimagepolicy[i].name, drive_extendimagepolicy[i].name, msg);
+        radiobutton = new BRadioButton(BRect(10, 20 + i * 25, 120, 30 + i * 25), extend_image_policy[i].name, extend_image_policy[i].name, msg);
         box->AddChild(radiobutton);
         rb_extendimagepolicy[i] = radiobutton;
-        if (drive_extendimagepolicy[i].id == current_extendimagepolicy) {
+        if (extend_image_policy[i].id == current_value) {
             radiobutton->SetValue(1);
         }
     }
 
     /* drive expansion */
-    r.OffsetTo(90, 100);
-    r.right = 220;
-    r.bottom = 295;
-    box = new BBox(r);
-    box->SetLabel("Drive expansion");
-    AddChild(box);
-    r.InsetBy(10, 10);
-    for (i = 0; drive_expansion[i].name; i++) {
-        msg = new BMessage(MESSAGE_DRIVE_EXPANSION);
-        msg->AddInt32("drive_num", drive_num);
-        msg->AddInt32("resource_index", i);
-        checkbox = new BCheckBox(BRect(10, 20 + i * 25, 120, 30 + i * 25), drive_expansion[i].name, drive_expansion[i].name, msg);
-        box->AddChild(checkbox);
-        cb_expansion[i] = checkbox;
-        resources_get_int_sprintf(drive_expansion[i].resource_name,
-        &current_expansion, drive_num);
-        checkbox->SetValue(current_expansion);
+    if (machine_drive_expansion_count > 0) {
+        r.left = 90;
+        r.top = 100;
+        r.right = 220;
+        r.bottom = r.top + 20 + machine_drive_expansion_count * 25; /* 245 or 295 */
+        box = new BBox(r);
+        box->SetLabel("Drive expansion");
+        AddChild(box);
+        for (i = 0; i < machine_drive_expansion_count; i++) {
+            sprintf(resname, expansion[i].resource_name, drive_num);
+            msg = new BMessage(MESSAGE_DRIVE_EXPANSION);
+            msg->AddString("resname", resname);
+            checkbox = new BCheckBox(BRect(10, 20 + i * 25, 120, 30 + i * 25), resname, expansion[i].name, msg);
+            box->AddChild(checkbox);
+            cb_expansion[i] = checkbox;
+            resources_get_int(resname, &current_value);
+            checkbox->SetValue(current_value);
+        }
     }
-
-    /* parallel cable */
-    msg = new BMessage(MESSAGE_DRIVE_PARALLELCABLE);
-    msg->AddInt32("drive_num", drive_num);
-    msg->AddInt32("resource_index", i);
-    checkbox = new BCheckBox(BRect(240, 120, 360, 130), "Parallel Cable", "Parallel Cable", msg);
-    AddChild(checkbox);
-    cb_parallelcable = checkbox;
-    resources_get_int_sprintf("Drive%dParallelCable", &current_parallelcable, drive_num);
-    checkbox->SetValue(current_parallelcable);
 
     /* idle method */
     r.OffsetTo(230, 0);
@@ -208,35 +195,64 @@ DriveView::DriveView(BRect r, int drive_num) : BView(r, "drive_view", B_FOLLOW_N
     box->SetLabel("Idle method");
     AddChild(box);
     r.InsetBy(10,10);
-    for (i = 0; drive_idlemethod[i].name; i++) {
+    sprintf(resname, "Drive%dIdleMethod", drive_num);
+    resources_get_int(resname, &current_value);
+    for (i = 0; idle_method[i].name; i++) {
         msg = new BMessage(MESSAGE_DRIVE_IDLEMETHOD);
-        msg->AddInt32("drive_num", drive_num);
+        msg->AddString("resname", resname);
         msg->AddInt32("resource_index", i);
-        radiobutton = new BRadioButton(BRect(10, 20 + i * 25, 90, 30 + i * 25), drive_idlemethod[i].name, drive_idlemethod[i].name, msg);
+        radiobutton = new BRadioButton(BRect(10, 20 + i * 25, 90, 30 + i * 25), idle_method[i].name, idle_method[i].name, msg);
         box->AddChild(radiobutton);
         rb_idlemethod[i] = radiobutton;
-        if (drive_idlemethod[i].id == current_idlemethod) {
+        if (idle_method[i].id == current_value) {
             radiobutton->SetValue(1);
+        }
+    }
+
+    /* parallel cable */
+    if (machine_parallel_cable_type_count > 0) {
+        r.left = 230;
+        r.top = 100;
+        r.right = 330;
+        r.bottom = r.top + 20 + machine_parallel_cable_type_count * 25; /* 170 or 220 */
+        box = new BBox(r);
+        box->SetLabel("Parallel Cable");
+        AddChild(box);
+        sprintf(resname, "Drive%dParallelCable", drive_num);
+        resources_get_int(resname, &current_value);
+        for (i = 0; i < machine_parallel_cable_type_count; i++) {
+            msg = new BMessage(MESSAGE_DRIVE_PARALLELCABLE);
+            msg->AddString("resname", resname);
+            msg->AddInt32("resource_index", i);
+            radiobutton = new BRadioButton(BRect(10, 20 + i * 25, 90, 30 + i * 25), parallel_cable[i].name, parallel_cable[i].name, msg);
+            box->AddChild(radiobutton);
+            rb_parallelcable[i] = radiobutton;
+            if (parallel_cable[i].id == current_value) {
+                radiobutton->SetValue(1);
+            }
         }
     }
 
     /* at last drive type, so we can enable/disable other controls */
     r.OffsetTo(0, 0);
     r.right = 80;
-    r.bottom = 345;
+    r.bottom = 20 + machine_drive_type_count * 25; /* max 420 */
     box = new BBox(r);
     box->SetLabel("Drive type");
     AddChild(box);
     r.InsetBy(10, 10);
+    sprintf(resname, "Drive%dType", drive_num);
+    resources_get_int(resname, &current_value);
     for (i = 0; drive_list[i].name; i++) {
         msg = new BMessage(MESSAGE_DRIVE_TYPE);
+        msg->AddString("resname", resname);
         msg->AddInt32("drive_num", drive_num);
         msg->AddInt32("resource_index", i);
         radiobutton = new BRadioButton(BRect(10, 20 + i * 25, 70, 30 + i * 25), drive_list[i].name, drive_list[i].name, msg);
         radiobutton->SetEnabled(drive_check_type(drive_list[i].id, drive_num - 8));
         box->AddChild(radiobutton);
 
-        if (drive_list[i].id == current_type) {
+        if (drive_list[i].id == current_value) {
             radiobutton->SetValue(1);
             EnableControlsForDriveSettings(i);
         }
@@ -245,19 +261,18 @@ DriveView::DriveView(BRect r, int drive_num) : BView(r, "drive_view", B_FOLLOW_N
 
 class DriveWindow : public BWindow {
     public:
-        DriveWindow();
+        DriveWindow(BRect r);
         ~DriveWindow();
         virtual void MessageReceived(BMessage *msg);
     private:
-        DriveView *dv[DRIVE_NUM]; /* pointers to the DriveViews 8 and 9 */
+        DriveView *dv[DRIVE_NUM]; /* pointers to the DriveViews */
 };
 
 static DriveWindow *drivewindow = NULL;
 
-DriveWindow::DriveWindow()
-    : BWindow(BRect(50, 50, 400, 435), "Drive settings", B_TITLED_WINDOW_LOOK, B_MODAL_APP_WINDOW_FEEL, B_NOT_ZOOMABLE | B_NOT_RESIZABLE)
+DriveWindow::DriveWindow(BRect r)
+    : BWindow(r, "Drive settings", B_TITLED_WINDOW_LOOK, B_MODAL_APP_WINDOW_FEEL, B_NOT_ZOOMABLE | B_NOT_RESIZABLE)
 {
-    BRect r;
     BTabView *tabview;
     BTab *tab;
     int i;
@@ -291,31 +306,30 @@ DriveWindow::~DriveWindow()
 
 void DriveWindow::MessageReceived(BMessage *msg)
 {
+    const char *resname;
     int32 drive_num;
     int32 resource_index;
-    int dummy;
 
-    msg->FindInt32("drive_num", &drive_num);
+    msg->FindString("resname", &resname);
     msg->FindInt32("resource_index", &resource_index);
 
     switch (msg->what) {
         case MESSAGE_DRIVE_TYPE:
-            resources_set_int_sprintf("Drive%dType", drive_list[resource_index].id, drive_num);
+            msg->FindInt32("drive_num", &drive_num);
+            resources_set_int(resname, drive_list[resource_index].id);
             dv[drive_num - 8]->EnableControlsForDriveSettings(resource_index);
             break;
         case MESSAGE_DRIVE_EXTENDIMAGEPOLICY:
-            resources_set_int_sprintf("Drive%dExtendImagePolicy", drive_extendimagepolicy[resource_index].id, drive_num);
+            resources_set_int(resname, extend_image_policy[resource_index].id);
             break;
         case MESSAGE_DRIVE_IDLEMETHOD:
-            resources_set_int_sprintf("Drive%dIdleMethod", drive_idlemethod[resource_index].id, drive_num);
+            resources_set_int(resname, idle_method[resource_index].id);
             break;
         case MESSAGE_DRIVE_EXPANSION:
-            resources_get_int_sprintf(drive_expansion[resource_index].resource_name, &dummy, drive_num);
-            resources_set_int_sprintf(drive_expansion[resource_index].resource_name, !dummy, drive_num);
+            resources_toggle(resname, NULL);
             break;
         case MESSAGE_DRIVE_PARALLELCABLE:
-            resources_get_int_sprintf("Drive%dParallelCable", &dummy, drive_num);
-            resources_set_int_sprintf("Drive%dParallelCable", !dummy, drive_num);
+            resources_set_int(resname, parallel_cable[resource_index].id);
             break;
         default:
             BWindow::MessageReceived(msg);
@@ -326,15 +340,39 @@ void ui_drive(ui_drive_type_t *drive_types, int caps)
 {
     thread_id drivethread;
     status_t exit_value;
+    BRect r;
+    int i;
 
     if (drivewindow != NULL) {
         return;
     }
 
     drive_list = drive_types;
-    drive_machine_parallel_capable = (caps & HAS_PARA_CABLE) ? 1 : 0;
+    machine_drive_expansion_count = 0;
+    for (i = 0; drive_types[i].name; i++) {
+        if (drive_types[i].id >= DRIVE_TYPE_1541 &&
+            drive_types[i].id <= DRIVE_TYPE_1571CR &&
+            drive_types[i].id != DRIVE_TYPE_1551) {
+            machine_drive_expansion_count = (caps & HAS_PROFDOS) ? 7 : 5;
+        }
+    }
+    machine_drive_type_count = i;
 
-    drivewindow = new DriveWindow;
+    if (caps == HAS_PARA_CABLE) {
+        /* This hack deals with Plus4 only having the Standard parallel cable. */
+        machine_parallel_cable_type_count = 2;
+    } else {
+        machine_parallel_cable_type_count = (caps & HAS_PARA_CABLE) ? 4 : 0;
+    }
+
+    r.Set(50, 50, 400, 310);
+    if (machine_drive_type_count > 8) {
+        r.bottom = 110 + machine_drive_type_count * 25;  /* max 510 */
+    } else if (machine_drive_expansion_count > 0) {
+        r.bottom = 210 + machine_drive_expansion_count * 25;  /* 335 or 385 */
+    }
+
+    drivewindow = new DriveWindow(r);
 
     vsync_suspend_speed_eval();
 
