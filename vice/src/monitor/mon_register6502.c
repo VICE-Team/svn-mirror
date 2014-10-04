0ae5027c0faa8d22da6dad06421cd4218246d78e
gpz <groepaz@gmx.net>
2014-10-04 22:11:28 +0000

    fix flags
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@28508 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/monitor/mon_register6502.c
+++ vice/src/monitor/mon_register6502.c
@@ -295,6 +295,8 @@ static mon_reg_list_t *mon_register_list_get6502(int mem)
     do {
         if (regs->flags & MON_REGISTER_IS_MEMORY) {
             regs->val = (unsigned int)mon_get_mem_val(mem, regs->extra);
+        } else if (regs->flags & MON_REGISTER_IS_FLAGS) {
+            regs->val = (unsigned int)mon_register_get_val(mem, regs->id) | 0x20;
         } else {
             regs->val = (unsigned int)mon_register_get_val(mem, regs->id);
         }
