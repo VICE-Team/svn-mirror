5b04e899b9e1763bbaf087abc87d35fd8b93ff05
blackystardust <blackystardust68@yahoo.com>
2014-10-06 02:56:08 +0000

    Final fix.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@28533 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/po/gen_amiga_intl_c.c
+++ vice/po/gen_amiga_intl_c.c
@@ -115,7 +115,7 @@ static void close_all(void)
 
 static void write_converted_text(char *text1, char *prefix_text, char *trail_text)
 {
-    char *text2 = malloc(strlen(text1) * 3);
+    char *text2 = malloc(strlen(text1) * 4);
     int i;
     int counter = 0;
 
