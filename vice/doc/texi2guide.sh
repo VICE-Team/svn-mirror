6926ca650d7ff3441006bfb9a85ccee7dedad7b6
blackystardust <blackystardust68@yahoo.com>
2014-12-07 03:32:25 +0000

    Fixed guide generation.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@28831 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/doc/texi2guide.sh
+++ vice/doc/texi2guide.sh
@@ -3,13 +3,45 @@
 #
 # written by Marco van den Heuvel <blackystardust68@yahoo.com>
 #
-# texi2guide.sh <makeguide> <guide-file> <texi-file>
-#               $1          $2           $3
+# texi2guide.sh <makeguide> <guide-file> <texi-file> <host-cpu> <host-os>
+#               $1          $2           $3          $4         $5
 
 MAKEGUIDE=$1
 GUIDEFILE=$2
 TEXIFILE=$3
+HOSTCPU=$4
+HOSTOS=$5
+
+AMIGAFLAVOR=""
+
+if test x"$HOSTOS" = "xaros"; then
+  AMIGAFLAVOR=-Dplatformaros
+fi
+
+if test x"$HOSTOS" = "xmorphos"; then
+  AMIGAFLAVOR=-Dplatformmorphos
+fi
+
+if test x"$HOSTOS" = "xwarpos"; then
+  AMIGAFLAVOR=-Dplatformamigaos3
+fi
+
+if test x"$HOSTOS" = "xpowerup"; then
+  AMIGAFLAVOR=-Dplatformamigaos3
+fi
+
+if test x"$HOSTOS" = "xamigaos" -a x"$HOSTCPU" = "xm68k"; then
+  AMIGAFLAVOR=-Dplatformamigaos3
+fi
+
+if test x"$HOSTOS" = "xamigaos" -a x"$AMIGAFLAVOR" != "x-Dplatformamigaos3"; then
+  AMIGAFLAVOR=-Dplatformamigaos4
+fi
+
+if test x"$AMIGAFLAVOR" = "x"; then
+  AMIGAFLAVOR="-Dplatformaros -Dplatformmorphos -Dplatformamigaos3 -Dplatformamigaos4"
+fi
 
 echo generating amigaguide
-$MAKEGUIDE --error-limit 10000 --amiga -o $GUIDEFILE $TEXIFILE
+$MAKEGUIDE -Dplatformamiga $AMIGAFLAVOR --error-limit 10000 --amiga -o $GUIDEFILE $TEXIFILE
 echo done
