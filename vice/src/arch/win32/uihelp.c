d28dd2570c66b02ed2a9cac880ff7c58212f5e78
blackystardust <blackystardust68@yahoo.com>
2015-01-19 00:45:00 +0000

    Changed the windows gui to use the new automatically generated team list.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@29134 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/uihelp.c
+++ vice/src/arch/win32/uihelp.c
@@ -48,46 +48,21 @@
 #include "vicefeatures.h"
 #include "winmain.h"
 
+#define ID_TEXT 200
+#define ID_MEMBERS_TEXT 300
+
 #ifdef USE_SVN_REVISION
 #include "svnversion.h"
 #endif
 
-int CALLBACK about_dialog_proc(HWND dialog, UINT msg, UINT wparam, LONG lparam)
+LPWORD lpdwAlign(LPWORD lpIn)
 {
-    char *version;
-    TCHAR *st_version;
-
-    switch (msg) {
-        case WM_INITDIALOG:
-#ifdef UNSTABLE
-#ifdef USE_SVN_REVISION
-            version = lib_msprintf(translate_text(IDS_VERSION_S_REV_S_UNSTABLE), VERSION, VICE_SVN_REV_STRING, PLATFORM);
-#else
-            version = lib_msprintf(translate_text(IDS_VERSION_S_UNSTABLE), VERSION, PLATFORM);
-#endif
-#else /* #ifdef UNSTABLE */
-#ifdef USE_SVN_REVISION
-            version = lib_msprintf(translate_text(IDS_VERSION_S_REV_S), VERSION, VICE_SVN_REV_STRING, PLATFORM);
-#else
-            version = lib_msprintf(translate_text(IDS_VERSION_S), VERSION, PLATFORM);
-#endif
-#endif /* #ifdef UNSTABLE */
-            st_version = system_mbstowcs_alloc(version);
-            SetDlgItemText(dialog, IDC_ABOUT_VERSION, st_version);
-            system_mbstowcs_free(st_version);
-            lib_free(version);
-            return TRUE;
-        case WM_CLOSE:
-            EndDialog(dialog, 0);
-            return TRUE;
-        case WM_COMMAND:
-            if ((wparam == IDOK) || (wparam == IDCANCEL)) {
-                EndDialog(dialog, 0);
-                return TRUE;
-            }
-            break;
-    }
-    return FALSE;
+    ULONG ul;
+    ul = (ULONG)lpIn;
+    ul += 3;
+    ul >>= 2;
+    ul <<= 2;
+    return (LPWORD)ul;
 }
 
 static char *get_compiletime_features(void)
@@ -112,6 +87,192 @@ static char *get_compiletime_features(void)
     return str;
 }
 
+static BOOL CALLBACK AboutDialogProc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    HWND element;
+    char *version;
+    char *tmp;
+    TCHAR *st_version;
+    int i;
+
+    switch (iMsg) {
+        case WM_INITDIALOG:
+            SetWindowText(hDlg, "About VICE");
+            element = GetDlgItem(hDlg, IDOK);
+            SetWindowText(element, "OK");
+            element = GetDlgItem(hDlg, ID_TEXT);
+            SetWindowText(element, "VICE");
+            element = GetDlgItem(hDlg, ID_TEXT + 1);
+            SetWindowText(element, "Versatile Commodore Emulator");
+            element = GetDlgItem(hDlg, ID_TEXT + 2);
+            SetWindowText(element, "Version 0.0");
+#ifdef UNSTABLE
+#  ifdef USE_SVN_REVISION
+            version = lib_msprintf(translate_text(IDS_VERSION_S_REV_S_UNSTABLE), VERSION, VICE_SVN_REV_STRING, PLATFORM);
+#  else
+            version = lib_msprintf(translate_text(IDS_VERSION_S_UNSTABLE), VERSION, PLATFORM);
+#  endif
+#else /* #ifdef UNSTABLE */
+#  ifdef USE_SVN_REVISION
+            version = lib_msprintf(translate_text(IDS_VERSION_S_REV_S), VERSION, VICE_SVN_REV_STRING, PLATFORM);
+#  else
+            version = lib_msprintf(translate_text(IDS_VERSION_S), VERSION, PLATFORM);
+#  endif
+#endif /* #ifdef UNSTABLE */
+            st_version = system_mbstowcs_alloc(version);
+            SetDlgItemText(hDlg, ID_TEXT + 2, st_version);
+            system_mbstowcs_free(st_version);
+            lib_free(version);
+            for (i = 0; core_team[i].name; i++) {
+                    tmp = util_concat("Copyright (c) ", core_team[i].years, " ", core_team[i].name, NULL);
+                    element = GetDlgItem(hDlg, ID_MEMBERS_TEXT + i);
+                    SetWindowText(element, tmp);
+                    lib_free(tmp);
+            }
+            return TRUE ;
+        case WM_COMMAND:
+            switch (LOWORD(wParam)) {
+                case IDOK:
+                    EndDialog(hDlg, 0) ;
+                    return TRUE ;
+            }
+            break ;
+    }
+    return FALSE ;
+}
+
+static LRESULT DisplayAboutBox(HINSTANCE hinst, HWND hwndOwner)
+{
+    HGLOBAL hgbl;
+    LPDLGTEMPLATE lpdt;
+    LPDLGITEMTEMPLATE lpdit;
+    LPWORD lpw;
+    LPWSTR lpwsz;
+    LRESULT ret;
+    int nchar;
+    int i;
+    int amount = 0;
+
+    hgbl = GlobalAlloc(GMEM_ZEROINIT, 32768);
+
+    if (!hgbl) {
+       return -1;
+    }
+
+    for (i = 0; core_team[i].name; i++) {
+        amount++;
+    }
+
+    lpdt = (LPDLGTEMPLATE)GlobalLock(hgbl);
+
+    /* Dialog Box */
+    lpdt->style = WS_POPUP | WS_BORDER | WS_SYSMENU | DS_MODALFRAME | WS_CAPTION | DS_SETFONT;
+    lpdt->cdit = 4 + amount;
+    lpdt->x = 0;
+    lpdt->y = 0;
+    lpdt->cx = 225;
+    lpdt->cy = 50 + ((amount + 1) * 9);
+
+    lpw = (LPWORD)(lpdt + 1);
+    *lpw++ = 0;
+    *lpw++ = 0;
+    *lpw++ = 0;
+    *lpw++ = 8;
+
+    lpwsz = (LPWSTR)lpw;
+    nchar = 1 + MultiByteToWideChar(CP_ACP, 0, "MS Sans Serif", -1, lpwsz, 50);
+    lpw += nchar;
+
+    /* OK Button */
+    lpw = lpdwAlign(lpw);
+
+    lpdit = (LPDLGITEMTEMPLATE)lpw;
+    lpdit->x = 132;
+    lpdit->y = 14;
+    lpdit->cx = 50;
+    lpdit->cy = 14;
+    lpdit->id = IDOK;
+    lpdit->style = WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON;
+
+    lpw = (LPWORD)(lpdit + 1);
+    *lpw++ = 0xFFFF;
+    *lpw++ = 0x0080;
+    *lpw++ = 0;
+    *lpw++ = 0;
+
+    /* "VICE" */
+    lpw = lpdwAlign(lpw);
+    lpdit = (LPDLGITEMTEMPLATE)lpw;
+    lpdit->x = 7;
+    lpdit->y = 5;
+    lpdit->cx = 118;
+    lpdit->cy = 8;
+    lpdit->id = ID_TEXT;
+    lpdit->style = WS_CHILD | WS_VISIBLE | SS_CENTER;
+
+    lpw = (LPWORD)(lpdit + 1);
+    *lpw++ = 0xFFFF;
+    *lpw++ = 0x0082;
+    *lpw++ = 0;
+    *lpw++ = 0;
+
+    /* "Versatile Commodore Emulator" */
+    lpw = lpdwAlign(lpw);
+    lpdit = (LPDLGITEMTEMPLATE)lpw;
+    lpdit->x = 7;
+    lpdit->y = 18;
+    lpdit->cx = 118;
+    lpdit->cy = 8;
+    lpdit->id = ID_TEXT + 1;
+    lpdit->style = WS_CHILD | WS_VISIBLE | SS_CENTER;
+
+    lpw = (LPWORD)(lpdit + 1);
+    *lpw++ = 0xFFFF;
+    *lpw++ = 0x0082;
+    *lpw++ = 0;
+    *lpw++ = 0;
+
+    /* "Version" */
+    lpw = lpdwAlign(lpw);
+    lpdit = (LPDLGITEMTEMPLATE)lpw;
+    lpdit->x = 7;
+    lpdit->y = 31;
+    lpdit->cx = 180;
+    lpdit->cy = 8;
+    lpdit->id = ID_TEXT + 2;
+    lpdit->style = WS_CHILD | WS_VISIBLE | SS_CENTER;
+
+    lpw = (LPWORD)(lpdit + 1);
+    *lpw++ = 0xFFFF;
+    *lpw++ = 0x0082;
+    *lpw++ = 0;
+    *lpw++ = 0;
+
+    for (i = 0; core_team[i].name; i++) {
+        lpw = lpdwAlign(lpw);
+        lpdit = (LPDLGITEMTEMPLATE)lpw;
+        lpdit->x = 5;
+        lpdit->y = 50 + (i * 9);
+        lpdit->cx = 180;
+        lpdit->cy = 8;
+        lpdit->id = ID_MEMBERS_TEXT + i;
+        lpdit->style = WS_CHILD | WS_VISIBLE | SS_CENTER;
+        lpw = (LPWORD)(lpdit + 1);
+        *lpw++ = 0xFFFF;
+        *lpw++ = 0x0082;
+        *lpw++ = 0;
+        *lpw++ = 0;
+    }
+
+    GlobalUnlock(hgbl);
+
+    ret = DialogBoxIndirect(hinst, (LPDLGTEMPLATE)hgbl, hwndOwner, (DLGPROC)AboutDialogProc);
+
+    GlobalFree(hgbl);
+
+    return ret;
+}
+
 void uihelp_dialog(HWND hwnd, WPARAM wparam)
 {
     char *fname;
@@ -126,7 +287,7 @@ void uihelp_dialog(HWND hwnd, WPARAM wparam)
 
     switch (wparam) {
         case IDM_ABOUT:
-            DialogBox(winmain_instance, MAKEINTRESOURCE(IDD_ABOUT), hwnd, (DLGPROC)about_dialog_proc);
+            DisplayAboutBox(winmain_instance, hwnd);
             break;
         case IDM_HELP:
             fname = util_concat(archdep_boot_path(), "\\DOC\\vice.chm", NULL);
