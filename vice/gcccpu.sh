f9764af25f3d93aa32d08e7aa09baf0d7c22175c
loggedoubt <loggedoubt@gmail.com>
2015-01-22 09:10:35 +0000

    Fixed gcccpu.sh so that it only compiles cpu files (including z80.c) with -O0 or -O1 that would really be hard to compile otherwise. It now also prints messages about what it is doing.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@29166 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/gcccpu.sh
+++ vice/gcccpu.sh
@@ -2,21 +2,52 @@
 # gcccpu.sh helper
 #
 # written by Marco van den Heuvel <blackystardust68@yahoo.com>
+#            Marcus Sutton <loggedoubt@gmail.com>
 #
-# This script compiles the *cpu.c files with -O0
+# This script compiles certain *cpu.c files with -O0 or -O1
 
 cpufile=no
+cpuopt=no
+oldopt=no
+filename=no
 
 for i in $*
 do
   case "$i" in
-    *cpu.c)
+    *c64cpu.c|*c64dtvcpu.c|*z80.c)
       cpufile=yes
+      cpuopt=-O1
+      ;;
+    *scpu64cpu.c)
+      cpufile=yes
+      cpuopt=-O0
+      ;;
+  esac
+
+  case "$i" in
+    -O2|-O3)
+      oldopt=$i
+      ;;
+  esac
+
+  case "$i" in
+    *.c|*.cc)
+      filename=$i
       ;;
   esac
 done
 
 newcmdline="gcc"
+
+if test x"$filename" != "xno"; then
+  if test x"$cpufile" = "xyes"; then
+    echo "compiling $filename with $cpuopt"
+  elif test x"$oldopt" != "xno"; then
+    echo "compiling $filename with $oldopt"
+  else
+    echo "compiling $filename normally"
+  fi
+fi
 
 for i in $*
 do
@@ -28,7 +59,7 @@ do
   esac
 
   if test x"$cpufile" = "xyes" -a x"$isopt" = "xyes"; then
-    newcmdline="$newcmdline -O0"
+    newcmdline="$newcmdline $cpuopt"
   else
     newcmdline="$newcmdline $i"
   fi
