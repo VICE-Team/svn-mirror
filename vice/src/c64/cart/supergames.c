c5666e4d557464bef2be30c9298727543cb36515
gpz <groepaz@gmx.net>
2015-01-28 18:48:09 +0000

    fix snapshot
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@29230 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/cart/supergames.c
+++ vice/src/c64/cart/supergames.c
@@ -194,7 +194,7 @@ void supergames_detach(void)
 /* ---------------------------------------------------------------------*/
 
 #define CART_DUMP_VER_MAJOR   0
-#define CART_DUMP_VER_MINOR   0
+#define CART_DUMP_VER_MINOR   1
 #define SNAP_MODULE_NAME  "CARTSUPERGAMES"
 
 int supergames_snapshot_write_module(snapshot_t *s)
@@ -209,6 +209,7 @@ int supergames_snapshot_write_module(snapshot_t *s)
 
     if (0
         || (SMW_B(m, (BYTE)currbank) < 0)
+        || (SMW_B(m, (BYTE)reglatched) < 0)
         || (SMW_BA(m, roml_banks, 0x8000) < 0)
         || (SMW_BA(m, romh_banks, 0x8000) < 0)) {
         snapshot_module_close(m);
@@ -236,6 +237,7 @@ int supergames_snapshot_read_module(snapshot_t *s)
 
     if (0
         || (SMR_B_INT(m, &currbank) < 0)
+        || (SMR_B_INT(m, &reglatched) < 0)
         || (SMR_BA(m, roml_banks, 0x8000) < 0)
         || (SMR_BA(m, romh_banks, 0x8000) < 0)) {
         snapshot_module_close(m);
