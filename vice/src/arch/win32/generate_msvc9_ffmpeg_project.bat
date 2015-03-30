bcdd0b276ac612f3b397621a590b5c51a2543d66
blackystardust <blackystardust68@yahoo.com>
2015-03-30 04:45:22 +0000

    Added msvc9 ffmpeg project generation batch file.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@29554 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/win32/generate_msvc9_ffmpeg_project.bat
@@ -0,0 +1,25 @@
+@echo off
+if not exist vs_tmpl\mkmsvc.exe goto missingmkmsvc
+echo generating MSVC9 project as vs9-ffmpeg
+if exist vs9-ffmpeg goto removedir
+:makemsvcdir
+mkdir vs9-ffmpeg
+cd vs_tmpl
+mkmsvc.exe -native -9 vice
+cd ..
+copy msvc\make-msvc-bindist-native.bat.proto vs9-ffmpeg\make-msvc-bindist.bat
+copy msvc\winid*.bat vs9-ffmpeg
+echo MSVC9 project files generated in vs9-ffmpeg
+goto end
+:missingmkmsvc
+echo vs_tmpl\mkmsvc.exe is missing
+goto end
+:removedir
+echo y | del vs9-ffmpeg\*.* /S
+rd vs9-ffmpeg /s /q
+if exist vs9-ffmpeg goto cannotremovedir
+goto makemsvcdir
+:cannotremovedir
+echo can't delete the vs9-ffmpeg directory, please remove vs9-ffmpeg manually and run this batch file again.
+:end
+pause
