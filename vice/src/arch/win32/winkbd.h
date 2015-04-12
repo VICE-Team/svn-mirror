0c91dd302be00856bc63edd29507160f6ea2440a
blackystardust <blackystardust68@yahoo.com>
2015-04-12 12:08:27 +0000

    Add some 'backup' language defines in case they are not defined in the windows headers.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@29611 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/winkbd.h
+++ vice/src/arch/win32/winkbd.h
@@ -31,4 +31,56 @@
 
 extern const TCHAR *kbd_code_to_string(kbd_code_t kcode);
 
+#ifndef LANG_ENGLISH
+#define LANG_ENGLISH 0x09
+#endif
+
+#ifndef SUBLANG_ENGLISH_US
+#define SUBLANG_ENGLISH_US 0x01
+#endif
+
+#ifndef SUBLANG_ENGLISH_UK
+#define SUBLANG_ENGLISH_UK 0x02
+#endif
+
+#ifndef LANG_GERMAN
+#define LANG_GERMAN 0x07
+#endif
+
+#ifndef SUBLANG_GERMAN
+#define SUBLANG_GERMAN 0x01
+#endif
+
+#ifndef LANG_DANISH
+#define LANG_DANISH 0x06
+#endif
+
+#ifndef SUBLANG_DANISH_DENMARK
+#define SUBLANG_DANISH_DENMARK 0x01
+#endif
+
+#ifndef LANG_NORWEGIAN
+#define LANG_NORWEGIAN  0x14
+#endif
+
+#ifndef SUBLANG_NORWEGIAN_BOKMAL
+#define SUBLANG_NORWEGIAN_BOKMAL 0x01
+#endif
+
+#ifndef LANG_FINNISH
+#define LANG_FINNISH 0x0b
+#endif
+
+#ifndef SUBLANG_FINNISH_FINLAND
+#define SUBLANG_FINNISH_FINLAND 0x01
+#endif
+
+#ifndef LANG_ITALIAN
+#define LANG_ITALIAN 0x10
+#endif
+
+#ifndef SUBLANG_ITALIAN
+#define SUBLANG_ITALIAN 0x01
+#endif
+
 #endif
