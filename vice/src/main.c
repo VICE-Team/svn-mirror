f72892f3a9d5793f43ab9af7e137cb8fe1e4ec4c
gpz <groepaz@gmx.net>
2015-05-04 23:00:03 +0000

    fix warning
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@29673 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/main.c
+++ vice/src/main.c
@@ -227,7 +227,7 @@ int main_program(int argc, char **argv)
     for (i = 0; core_team[i].name; i++) {
         n += strlen(core_team[i].name);
         if (n > 74) {
-            log_message(LOG_DEFAULT, tmp);
+            log_message(LOG_DEFAULT, "%s", tmp);
             n = 0; *tmp = 0;
         }
         strcat(tmp, core_team[i].name);
@@ -235,7 +235,7 @@ int main_program(int argc, char **argv)
             strcat(tmp, ", ");
         } else {
             strcat(tmp, ".");
-            log_message(LOG_DEFAULT, tmp);
+            log_message(LOG_DEFAULT, "%s", tmp);
         }
     }
     lib_free(tmp);
