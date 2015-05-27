c0ca195f183857c278e123a87f36ad74496e5555
gpz <groepaz@gmx.net>
2015-05-27 02:15:02 +0000

    added missing option for iee488 interface
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@29778 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/unix/gui/vic20ui.c
+++ vice/src/arch/unix/gui/vic20ui.c
@@ -475,6 +475,8 @@ static ui_menu_entry_t iocollision_submenu[] = {
     { NULL }
 };
 
+UI_MENU_DEFINE_TOGGLE(IEEE488)
+
 static ui_menu_entry_t io_extensions_submenu[] = {
     { CARTRIDGE_VIC20_NAME_MEGACART, UI_MENU_TYPE_NORMAL,
       NULL, NULL, megacart_submenu },
@@ -484,6 +486,8 @@ static ui_menu_entry_t io_extensions_submenu[] = {
       NULL, NULL, vicflash_submenu },
     { N_("SID cartridge"), UI_MENU_TYPE_NORMAL,
       NULL, NULL, sidcart_submenu },
+    { N_("VIC-1112 IEEE488 interface"), UI_MENU_TYPE_TICK,
+      (ui_callback_t)toggle_IEEE488, NULL, NULL },
     { "--", UI_MENU_TYPE_SEPARATOR },
     { CARTRIDGE_NAME_DIGIMAX " (MasC=uerade)", UI_MENU_TYPE_NORMAL,
       NULL, NULL, digimax_vic20_submenu },
