83fff97865c1666a232286469483d4c5b56be73a
blackystardust <blackystardust68@yahoo.com>
2015-10-06 03:05:08 +0000

    Fixed for 'PIC' x86_64/amd64 compile.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@29908 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/platform/platform_x86_gcc_cpuid.h
+++ vice/src/platform/platform_x86_gcc_cpuid.h
@@ -27,7 +27,7 @@
 #ifndef PLATFORM_X86_GCC_CPUID_H
 #define PLATFORM_X86_GCC_CPUID_H
 
-#if defined(__PIC__) && !defined(OpenBSD5_7) && !defined(__CYGWIN32__) && !defined(__CYGWIN__) && !defined(WIN32_COMPILE)
+#if defined(__PIC__) && !defined(OpenBSD5_7) && !defined(__CYGWIN32__) && !defined(__CYGWIN__) && !defined(WIN32_COMPILE) && !defined(__x86_64__)
 
 #define cpuid(func, ax, bx, cx, dx)                                    \
     __asm__ __volatile__ ("pushl %%ebx\n\t"                            \
