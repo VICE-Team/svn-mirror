de8315355738d0de088c2b4cc83f65ed731596a4
blackystardust <blackystardust68@yahoo.com>
2015-11-18 05:44:40 +0000

    More ow fixes.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@30192 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/watcom/genwt.bat
+++ vice/src/arch/win32/watcom/genwt.bat
@@ -6,17 +6,23 @@ copy ..\resstrings.txt ress.txt
 copy ..\..\..\translate.txt trans.txt
 copy ..\..\..\..\doc\vice.texi vice.tex
 copy ..\..\..\infocontrib.sed ic.sed
+copy ..\..\..\..\AUTHORS authors.txt
+copy ..\..\unix\macosx\Resources\Credits.html credits.htm
+copy ..\..\..\..\README readme.txt
+copy ..\..\..\..\doc\html\index.html index.htm
+copy ..\..\os2\dialogs\dialogs.rc dlgs.rc
+copy ..\..\..\..\man\vice.1 vice1.txt
 if Windows_NT == %OS% goto winnt
 goto use16
 :winnt
 if defined ProgramFiles(x86) GOTO use32
 :use16
-genic vice.tex ic.h ic.sed
+genic ..\ ..\ watcom\vice.tex watcom\ic.h watcom\ic.sed watcom\authors.txt watcom\credits.htm watcom\readme.txt watcom\index.htm watcom\dlgs.rc watcom\vice1.txt
 genwr res.h menuid.h strid.h res.txt resd.txt resm.txt ress.txt
 gentr trans.txt trans.h transtab.h
 goto after
 :use32
-geninfocontrib32 vice.tex ic.h ic.sed
+geninfocontrib32 ..\ ..\ watcom\vice.tex watcom\ic.h watcom\ic.sed watcom\authors.txt watcom\credits.htm watcom\readme.txt watcom\index.htm watcom\dlgs.rc watcom\vice1.txt
 genwinres32 res.h menuid.h strid.h res.txt resd.txt resm.txt ress.txt
 gentranslate32 trans.txt trans.h transtab.h
 :after
@@ -26,4 +32,10 @@ copy menuid.h ..\menuid.h
 copy strid.h ..\stringid.h
 copy trans.h ..\..\..\translate.h
 copy transtab.h ..\..\..\translate_table.h
-del ic.h vice.tex ic.sed res.txt resd.txt resm.txt ress.txt trans.txt res.h menuid.h strid.h trans.h transtab.h
+copy authors.txt ..\..\..\..\AUTHORS
+copy credits.htm ..\..\unix\macosx\Resources\Credits.html
+copy readme.txt ..\..\..\..\README
+copy index.htm ..\..\..\..\doc\html\index.html
+copy dlgs.rc ..\..\os2\dialogs\dialogs.rc
+copy vice1.txt ..\..\..\..\man\vice.1
+del ic.h vice.tex ic.sed res.txt resd.txt resm.txt ress.txt trans.txt res.h menuid.h strid.h trans.h transtab.h authors.txt credits.htm readme.txt index.htm dlgs.rc vice1.txt
