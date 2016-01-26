1f328cec6ef5d41f2ca9f25476b17e8d6d323589
blackystardust <blackystardust68@yahoo.com>
2016-01-26 01:57:59 +0000

    Attempt to fix os2 native ow compile.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@30449 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/os2/watcom/genwt.cmd
@@ -0,0 +1,22 @@
+@echo off
+copy ..\..\..\translate.txt trans.txt
+copy ..\..\..\..\doc\vice.texi vice.tex
+copy ..\..\..\infocontrib.sed ic.sed
+copy ..\..\..\..\AUTHORS authors.txt
+copy ..\..\unix\macosx\Resources\Credits.html credits.htm
+copy ..\..\..\..\README readme.txt
+copy ..\..\..\..\doc\html\index.html index.htm
+copy ..\..\os2\dialogs\dialogs.rc dlgs.rc
+copy ..\..\..\..\man\vice.1 vice1.txt
+geninfocontribos2 ..\ ..\ watcom\vice.tex watcom\ic.h watcom\ic.sed watcom\authors.txt watcom\credits.htm watcom\readme.txt watcom\index.htm watcom\dlgs.rc watcom\vice1.txt
+gentranslateos2 trans.txt trans.h transtab.h
+copy ic.h ..\..\..\infocontrib.h 
+copy trans.h ..\..\..\translate.h
+copy transtab.h ..\..\..\translate_table.h
+copy authors.txt ..\..\..\..\AUTHORS
+copy credits.htm ..\..\unix\macosx\Resources\Credits.html
+copy readme.txt ..\..\..\..\README
+copy index.htm ..\..\..\..\doc\html\index.html
+copy dlgs.rc ..\..\os2\dialogs\dialogs.rc
+copy vice1.txt ..\..\..\..\man\vice.1
+del ic.h vice.tex ic.sed trans.txt trans.h transtab.h authors.txt credits.htm readme.txt index.htm dlgs.rc vice1.txt
