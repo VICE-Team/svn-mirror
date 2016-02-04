06d575b6d4eb55be4633c2ddb0b14b5f1c761e6c
blackystardust <blackystardust68@yahoo.com>
2016-02-04 03:05:43 +0000

    VICE 2.4.25
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@30501 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/version.h
+++ vice/src/version.h
@@ -28,11 +28,11 @@
 #define VICE__VERSION_H__
 
 #ifndef VERSION
-#define VERSION "2.4.24"
+#define VERSION "2.4.25"
 #endif
 
 #ifndef VERSION_RC_NUMBER
-#define VERSION_RC_NUMBER 2,4,24,0
+#define VERSION_RC_NUMBER 2,4,25,0
 #endif
 
 #ifndef PACKAGE
