7af2f795c23db2e08575c98eb0bbff630d1d2c21
blackystardust <blackystardust68@yahoo.com>
2016-02-22 18:55:35 +0000

    Made the dos c64 cart menus dynamically generated.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@30584 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/msdos/uic64cart.c
+++ vice/src/arch/msdos/uic64cart.c
@@ -88,63 +88,49 @@ static TUI_MENU_CALLBACK(cartridge_callback)
     }
 }
 
-static tui_menu_item_def_t attach_cartridge_submenu_items[] = {
+static tui_menu_item_def_t *attach_generic_cartridge_submenu_items = NULL;
+static tui_menu_item_def_t *attach_ramex_cartridge_submenu_items = NULL;
+static tui_menu_item_def_t *attach_freezer_cartridge_submenu_items = NULL;
+static tui_menu_item_def_t *attach_game_cartridge_submenu_items = NULL;
+static tui_menu_item_def_t *attach_util1_cartridge_submenu_items = NULL;
+static tui_menu_item_def_t *attach_util2_cartridge_submenu_items = NULL;
+
+static tui_menu_item_def_t attach_c64_cartridge_menu_items[] = {
+    { "--" },
     { "Attach _CRT image...",
       "Attach a CRT image, autodetecting its type",
       attach_cartridge_callback, (void *)CARTRIDGE_CRT, 0,
       TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach generic _8KB image...",
-      "Attach a generic 8KB cartridge dump",
-      attach_cartridge_callback, (void *)CARTRIDGE_GENERIC_8KB, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach Generic _16KB image...",
-      "Attach a generic 16KB cartridge dump",
-      attach_cartridge_callback, (void *)CARTRIDGE_GENERIC_16KB, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach _Action Replay image...",
-      "Attach an Action Replay cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_ACTION_REPLAY, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach _Action Replay MK3 image...",
-      "Attach an Action Replay MK3 cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_ACTION_REPLAY3, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach _Action Replay MK4 image...",
-      "Attach an Action Replay MK4 cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_ACTION_REPLAY4, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach _Stardos image...",
-      "Attach a Stardos cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_STARDOS, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach Atomic _Power image...",
-      "Attach an Atomic Power cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_ATOMIC_POWER, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach _Epyx FastLoad image...",
-      "Attach an Epyx FastLoad cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_EPYX_FASTLOAD, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach _IEEE-488 Interface image...",
-      "Attach an IEEE-488 interface cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_IEEE488, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach _Retro Replay image...",
-      "Attach a Retro Replay cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_RETRO_REPLAY, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach IDE_64 interface image...",
-      "Attach an IDE64 interface cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_IDE64, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach Super Snapshot V_4 image...",
-      "Attach an Super Snapshot V4 cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_SUPER_SNAPSHOT, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
-    { "Attach Super Snapshot V_5 image...",
-      "Attach an Super Snapshot V5 cartridge image",
-      attach_cartridge_callback, (void *)CARTRIDGE_SUPER_SNAPSHOT_V5, 0,
-      TUI_MENU_BEH_CLOSE, NULL, NULL },
+    { "_Generic cartridges:",
+      "Attach a generic cartridge image",
+      cartridge_callback, NULL, 30,
+      TUI_MENU_BEH_CONTINUE, NULL,
+      "Attach generic cartridge" },
+    { "_RAM expansion cartridges:",
+      "Attach a RAM expansion cartridge image",
+      cartridge_callback, NULL, 30,
+      TUI_MENU_BEH_CONTINUE, NULL,
+      "Attach RAM expansion cartridge" },
+    { "_Freezer cartridges:",
+      "Attach a freezer cartridge image",
+      cartridge_callback, NULL, 30,
+      TUI_MENU_BEH_CONTINUE, NULL,
+      "Attach freezer cartridge" },
+    { "G_ame cartridges:",
+      "Attach a game cartridge image",
+      cartridge_callback, NULL, 30,
+      TUI_MENU_BEH_CONTINUE, NULL,
+      "Attach game cartridge" },
+    { "_Utility cartridges (part 1):",
+      "Attach a utility cartridge image",
+      cartridge_callback, NULL, 30,
+      TUI_MENU_BEH_CONTINUE, NULL,
+      "Attach utility cartridge" },
+    { "_Utility cartridges (part 2):",
+      "Attach a utility cartridge image",
+      cartridge_callback, NULL, 30,
+      TUI_MENU_BEH_CONTINUE, NULL,
+      "Attach utility cartridge" },
     { "--" },
     { "Set cartridge as _default",
       "Save the current cartridge to the settings",
@@ -157,13 +143,41 @@ static tui_menu_item_def_t attach_cartridge_submenu_items[] = {
     { NULL }
 };
 
-static tui_menu_item_def_t attach_cartridge_menu_items[] = {
+static tui_menu_item_def_t attach_scpu64_cartridge_menu_items[] = {
     { "--" },
-    { "_Cartridge:",
-      "Attach a cartridge image",
+    { "Attach _CRT image...",
+      "Attach a CRT image, autodetecting its type",
+      attach_cartridge_callback, (void *)CARTRIDGE_CRT, 0,
+      TUI_MENU_BEH_CLOSE, NULL, NULL },
+    { "_Generic cartridge:",
+      "Attach a generic cartridge image",
       cartridge_callback, NULL, 30,
-      TUI_MENU_BEH_CONTINUE, attach_cartridge_submenu_items,
-      "Attach cartridge" },
+      TUI_MENU_BEH_CONTINUE, NULL,
+      "Attach generic cartridge" },
+    { "_RAM expansion cartridge:",
+      "Attach a RAM expansion cartridge image",
+      cartridge_callback, NULL, 30,
+      TUI_MENU_BEH_CONTINUE, NULL,
+      "Attach RAM expansion cartridge" },
+    { "G_ame cartridge:",
+      "Attach a game cartridge image",
+      cartridge_callback, NULL, 30,
+      TUI_MENU_BEH_CONTINUE, NULL,
+      "Attach game cartridge" },
+    { "_Utility cartridge:",
+      "Attach a utility cartridge image",
+      cartridge_callback, NULL, 30,
+      TUI_MENU_BEH_CONTINUE, NULL,
+      "Attach utility cartridge" },
+    { "--" },
+    { "Set cartridge as _default",
+      "Save the current cartridge to the settings",
+      cartridge_set_default_callback, NULL, 0,
+      TUI_MENU_BEH_CLOSE, NULL, NULL },
+    { "Reset on cart change",
+      "Reset the machine when a cart is inserted or detached",
+      toggle_CartridgeReset_callback, NULL, 3,
+      TUI_MENU_BEH_CONTINUE, NULL, NULL },
     { NULL }
 };
 
@@ -213,11 +227,105 @@ static tui_menu_item_def_t freeze_cartridge_menu_items[] = {
     { NULL }
 };
 
+static int cart_count(cartridge_info_t *cartlist, int group)
+{
+    int count = 0;
+
+    while (cartlist->name) {
+        if (cartlist->flags & group) {
+            ++count;
+        }
+        ++cartlist;
+    }
+    return count;
+}
+
+static void cart_menu_set(cartridge_info_t *cartlist, int group, tui_menu_item_def_t *submenu_items, int items_offset, int max_items)
+{
+    int i = 0;
+    int done = 0;
+
+    while (cartlist->name && i < max_items) {
+        if (cartlist->flags & group) {
+            if (done >= items_offset) {
+                submenu_items[i].label = cartlist->name;
+                submenu_items[i].help_string = cartlist->name;
+                submenu_items[i].callback = attach_cartridge_callback;
+                submenu_items[i].callback_param = (void *)cartlist->crtid;
+                submenu_items[i].par_string_max_len = 0;
+                submenu_items[i].behavior = TUI_MENU_BEH_CLOSE;
+                submenu_items[i].submenu = NULL;
+                submenu_items[i].submenu_title = NULL;
+                ++i;
+            }
+            ++done;
+        }
+        ++cartlist;
+    }
+    submenu_items[i].label = NULL;
+    submenu_items[i].help_string = NULL;
+    submenu_items[i].callback = NULL;
+    submenu_items[i].callback_param = NULL;
+    submenu_items[i].par_string_max_len = 0;
+    submenu_items[i].behavior = 0;
+    submenu_items[i].submenu = NULL;
+    submenu_items[i].submenu_title = NULL;
+}
+
+static void uicart_menu_create(void)
+{
+    int num;
+    cartridge_info_t *cartlist = cartridge_get_info_list();
+
+    num = cart_count(cartlist, CARTRIDGE_GROUP_GENERIC);
+    attach_generic_cartridge_submenu_items = lib_malloc(sizeof(tui_menu_item_def_t) * (num + 1));
+    cart_menu_set(cartlist, CARTRIDGE_GROUP_GENERIC, attach_generic_cartridge_submenu_items, 0, num);
+    
+    num = cart_count(cartlist, CARTRIDGE_GROUP_RAMEX);
+    attach_ramex_cartridge_submenu_items = lib_malloc(sizeof(tui_menu_item_def_t) * (num + 1));
+    cart_menu_set(cartlist, CARTRIDGE_GROUP_RAMEX, attach_ramex_cartridge_submenu_items, 0, num);
+
+    num = cart_count(cartlist, CARTRIDGE_GROUP_FREEZER);
+    attach_freezer_cartridge_submenu_items = lib_malloc(sizeof(tui_menu_item_def_t) * (num + 1));
+    cart_menu_set(cartlist, CARTRIDGE_GROUP_FREEZER, attach_freezer_cartridge_submenu_items, 0, num);
+
+    num = cart_count(cartlist, CARTRIDGE_GROUP_GAME);
+    attach_game_cartridge_submenu_items = lib_malloc(sizeof(tui_menu_item_def_t) * (num + 1));
+    cart_menu_set(cartlist, CARTRIDGE_GROUP_GAME, attach_game_cartridge_submenu_items, 0, num);
+
+    num = cart_count(cartlist, CARTRIDGE_GROUP_UTIL);
+    attach_util1_cartridge_submenu_items = lib_malloc(sizeof(tui_menu_item_def_t) * (num + 1));
+    cart_menu_set(cartlist, CARTRIDGE_GROUP_UTIL, attach_util1_cartridge_submenu_items, 0, num / 2);
+
+    attach_util2_cartridge_submenu_items = lib_malloc(sizeof(tui_menu_item_def_t) * (num + 1));
+    cart_menu_set(cartlist, CARTRIDGE_GROUP_UTIL, attach_util2_cartridge_submenu_items, num / 2, num - (num / 2));
+
+    attach_c64_cartridge_menu_items[2].submenu = attach_generic_cartridge_submenu_items;
+    attach_scpu64_cartridge_menu_items[2].submenu = attach_generic_cartridge_submenu_items;
+
+    attach_c64_cartridge_menu_items[3].submenu = attach_ramex_cartridge_submenu_items;
+    attach_scpu64_cartridge_menu_items[3].submenu = attach_ramex_cartridge_submenu_items;
+
+    attach_c64_cartridge_menu_items[4].submenu = attach_freezer_cartridge_submenu_items;
+
+    attach_c64_cartridge_menu_items[5].submenu = attach_game_cartridge_submenu_items;
+    attach_scpu64_cartridge_menu_items[4].submenu = attach_game_cartridge_submenu_items;
+
+    attach_c64_cartridge_menu_items[6].submenu = attach_util1_cartridge_submenu_items;
+    attach_scpu64_cartridge_menu_items[5].submenu = attach_util1_cartridge_submenu_items;
+
+    attach_c64_cartridge_menu_items[7].submenu = attach_util2_cartridge_submenu_items;
+    attach_scpu64_cartridge_menu_items[6].submenu = attach_util2_cartridge_submenu_items;
+}
+
 void uic64cart_init(struct tui_menu *parent_submenu)
 {
-    tui_menu_add(ui_attach_submenu, attach_cartridge_menu_items);
+    uicart_menu_create();
     tui_menu_add(ui_detach_submenu, detach_cartridge_menu_items);
     if (machine_class != VICE_MACHINE_SCPU64) {
+        tui_menu_add(ui_attach_submenu, attach_c64_cartridge_menu_items);
         tui_menu_add(ui_reset_submenu, freeze_cartridge_menu_items);
+    } else {
+        tui_menu_add(ui_attach_submenu, attach_scpu64_cartridge_menu_items);
     }
 }
