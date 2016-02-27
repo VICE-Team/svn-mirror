28c3a509065b5446af12d92860045007c2edf502
blackystardust <blackystardust68@yahoo.com>
2016-02-27 19:15:53 +0000

    Fixed.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@30646 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/os2/watcom/genwt.bat
+++ vice/src/arch/os2/watcom/genwt.bat
@@ -5,7 +5,7 @@ copy ..\..\..\..\doc\vice.texi vice.tex
 copy ..\..\..\infocontrib.sed ic.sed
 copy ..\..\..\..\AUTHORS authors.txt
 copy ..\..\unix\macosx\Resources\Credits.html credits.htm
-copy ..\..\..\..\README readme.txt
+copy ..\..\..\..\README readme.tx1
 copy ..\..\..\..\doc\html\index.html index.htm
 copy ..\..\os2\dialogs\dialogs.rc dlgs.rc
 copy ..\..\..\..\man\vice.1 vice1.txt
@@ -14,11 +14,11 @@ goto use16
 :winnt
 if defined ProgramFiles(x86) GOTO use32
 :use16
-genic ..\ ..\ watcom\vice.tex watcom\ic.h watcom\ic.sed watcom\authors.txt watcom\credits.htm watcom\readme.txt watcom\index.htm watcom\dlgs.rc watcom\vice1.txt
+genic ..\ ..\ watcom\vice.tex watcom\ic.h watcom\ic.sed watcom\authors.txt watcom\credits.htm watcom\readme.tx1 watcom\index.htm watcom\dlgs.rc watcom\vice1.txt
 gentr trans.txt trans.h transtab.h
 goto after
 :use32
-geninfocontrib32 ..\ ..\ watcom\vice.tex watcom\ic.h watcom\ic.sed watcom\authors.txt watcom\credits.htm watcom\readme.txt watcom\index.htm watcom\dlgs.rc watcom\vice1.txt
+geninfocontrib32 ..\ ..\ watcom\vice.tex watcom\ic.h watcom\ic.sed watcom\authors.txt watcom\credits.htm watcom\readme.tx1 watcom\index.htm watcom\dlgs.rc watcom\vice1.txt
 gentranslate32 trans.txt trans.h transtab.h
 :after
 copy ic.h ..\..\..\infocontrib.h 
@@ -26,8 +26,8 @@ copy trans.h ..\..\..\translate.h
 copy transtab.h ..\..\..\translate_table.h
 copy authors.txt ..\..\..\..\AUTHORS
 copy credits.htm ..\..\unix\macosx\Resources\Credits.html
-copy readme.txt ..\..\..\..\README
+copy readme.tx1 ..\..\..\..\README
 copy index.htm ..\..\..\..\doc\html\index.html
 copy dlgs.rc ..\..\os2\dialogs\dialogs.rc
 copy vice1.txt ..\..\..\..\man\vice.1
-del ic.h vice.tex ic.sed trans.txt trans.h transtab.h authors.txt credits.htm readme.txt index.htm dlgs.rc vice1.txt
+del ic.h vice.tex ic.sed trans.txt trans.h transtab.h authors.txt credits.htm readme.tx1 index.htm dlgs.rc vice1.txt
