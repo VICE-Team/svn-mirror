fb6f11fdf26c19731ff9c54eb4c7e1043b4c6858
blackystardust <blackystardust68@yahoo.com>
2016-03-02 02:33:25 +0000

    Fixed.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@30666 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/beos/cbm2ui.cc
+++ vice/src/arch/beos/cbm2ui.cc
@@ -68,7 +68,7 @@ ui_menu_toggle cbm2_ui_menu_toggles[] = {
     { "CartridgeReset", MENU_CART_CBM2_RESET_ON_CHANGE },
     { "Mouse", MENU_TOGGLE_MOUSE },
     { "UserportRTC58321a", MENU_TOGGLE_USERPORT_RTC_58321A },
-    { "UserportRTC58321aSave", MENU_TOGGLE_USERPORT_RTC_SAVE_58321A },
+    { "UserportRTC58321aSave", MENU_TOGGLE_USERPORT_RTC_58321A_SAVE },
     { NULL, 0 }
 };
 
