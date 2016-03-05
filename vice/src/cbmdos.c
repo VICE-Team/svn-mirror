cdf16da2d87298c7fe113db1180588625545720d
soci <soci@c64.rulez.org>
2016-03-05 15:44:36 +0000

    Use "prg" for unspecified secondary 0 open too
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@30699 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/cbmdos.c
+++ vice/src/cbmdos.c
@@ -250,7 +250,8 @@ unsigned int cbmdos_command_parse(cbmdos_cmd_parse_t *cmd_parse)
     log_debug("CBMDOS parsed cmd: '%s'", cmd_parse->parsecmd);
 #endif
 
-    cmd_parse->filetype = 0;
+    /* Set filetype according secondary address, if it was not specified */
+    cmd_parse->filetype = (cmd_parse->secondary < 2) ? CBMDOS_FT_PRG : CBMDOS_FT_SEQ;
 
     /*
      * Change modes ?
@@ -321,13 +322,6 @@ unsigned int cbmdos_command_parse(cbmdos_cmd_parse_t *cmd_parse)
     }
     if (cmd_parse->secondary == 1) {
         cmd_parse->readmode = CBMDOS_FAM_WRITE;
-    }
-
-    /* Set filetype according secondary address, if it was not specified
-        and if we are in write mode */
-    if (cmd_parse->filetype == 0 && cmd_parse->readmode == CBMDOS_FAM_WRITE) {
-        cmd_parse->filetype = (cmd_parse->secondary < 2)
-                              ? CBMDOS_FT_PRG : CBMDOS_FT_SEQ;
     }
 
     return CBMDOS_IPE_OK;
