62dcc69692ee531290c492a86dc26c47981febc0
gpz <groepaz@gmx.net>
2016-03-21 19:55:38 +0000

    channel > 1 always opens the file, fixes bug #728. patch by greg king
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@30828 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/cbmdos.c
+++ vice/src/cbmdos.c
@@ -250,11 +250,11 @@ unsigned int cbmdos_command_parse(cbmdos_cmd_parse_t *cmd_parse)
     log_debug("CBMDOS parsed cmd: '%s'", cmd_parse->parsecmd);
 #endif
 
-    /* Set filetype according secondary address, if it was not specified */
-    cmd_parse->filetype = (cmd_parse->secondary < 2) ? CBMDOS_FT_PRG : CBMDOS_FT_SEQ;
+    /* Preset the file-type if the LOAD/SAVE secondary addresses are used. */
+    cmd_parse->filetype = (cmd_parse->secondary < 2) ? CBMDOS_FT_PRG : 0;
 
     /*
-     * Change modes ?
+     * Change type or mode?
      */
     while (cmdlen > 0) {
         cmdlen--;
