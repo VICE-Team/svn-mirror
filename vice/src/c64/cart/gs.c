0d04d27339c1d6b88553e1c61a9c721a6fd83baa
blackystardust <blackystardust68@yahoo.com>
2016-04-13 23:11:49 +0000

    Fixed gs snapshot support.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@31003 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/cart/gs.c
+++ vice/src/c64/cart/gs.c
@@ -174,7 +174,7 @@ void gs_detach(void)
 /* ---------------------------------------------------------------------*/
 
 #define CART_DUMP_VER_MAJOR   1
-#define CART_DUMP_VER_MINOR   0
+#define CART_DUMP_VER_MINOR   1
 #define SNAP_MODULE_NAME  "CARTGS"
 
 int gs_snapshot_write_module(snapshot_t *s)
@@ -188,6 +188,7 @@ int gs_snapshot_write_module(snapshot_t *s)
     }
 
     if (0
+        || (SMW_B(m, regval) < 0)
         || (SMW_B(m, (BYTE)currbank) < 0)
         || (SMW_BA(m, roml_banks, 0x2000 * 64) < 0)) {
         snapshot_module_close(m);
@@ -214,6 +215,7 @@ int gs_snapshot_read_module(snapshot_t *s)
     }
 
     if (0
+        || (SMR_B(m, &regval) < 0)
         || (SMR_B_INT(m, &currbank) < 0)
         || (SMR_BA(m, roml_banks, 0x2000 * 64) < 0)) {
         snapshot_module_close(m);
