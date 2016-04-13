849c1d977056d4d9cf36644b23c64926b0f3dc40
blackystardust <blackystardust68@yahoo.com>
2016-04-13 22:00:25 +0000

    Fixed dela ep 7x8 snapshot support.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@30992 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/cart/delaep7x8.c
+++ vice/src/c64/cart/delaep7x8.c
@@ -220,7 +220,7 @@ void delaep7x8_detach(void)
 /* ---------------------------------------------------------------------*/
 
 #define CART_DUMP_VER_MAJOR   0
-#define CART_DUMP_VER_MINOR   0
+#define CART_DUMP_VER_MINOR   1
 #define SNAP_MODULE_NAME  "CARTDELAEP7X8"
 
 int delaep7x8_snapshot_write_module(snapshot_t *s)
@@ -234,6 +234,7 @@ int delaep7x8_snapshot_write_module(snapshot_t *s)
     }
 
     if (0
+        || (SMW_B(m, regval) < 0)
         || (SMW_B(m, (BYTE)currbank) < 0)
         || (SMW_BA(m, roml_banks, 0x2000 * 8) < 0)) {
         snapshot_module_close(m);
@@ -260,6 +261,7 @@ int delaep7x8_snapshot_read_module(snapshot_t *s)
     }
 
     if (0
+        || (SMR_B(m, &regval) < 0)
         || (SMR_B_INT(m, &currbank) < 0)
         || (SMR_BA(m, roml_banks, 0x2000 * 8) < 0)) {
         snapshot_module_close(m);
