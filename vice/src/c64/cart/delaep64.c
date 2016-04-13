f4772541cb6e86966025ca4c2e8aabfba105c7d1
blackystardust <blackystardust68@yahoo.com>
2016-04-13 22:03:45 +0000

    FixedFixed dela ep 64 snapshot support.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@30993 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/cart/delaep64.c
+++ vice/src/c64/cart/delaep64.c
@@ -277,7 +277,7 @@ void delaep64_detach(void)
 /* ---------------------------------------------------------------------*/
 
 #define CART_DUMP_VER_MAJOR   0
-#define CART_DUMP_VER_MINOR   0
+#define CART_DUMP_VER_MINOR   1
 #define SNAP_MODULE_NAME  "CARTDELAEP64"
 
 int delaep64_snapshot_write_module(snapshot_t *s)
@@ -291,6 +291,7 @@ int delaep64_snapshot_write_module(snapshot_t *s)
     }
 
     if (0
+        || (SMW_B(m, regval) < 0)
         || (SMW_B(m, (BYTE)currbank) < 0)
         || (SMW_BA(m, roml_banks, 0x2000 * 9) < 0)) {
         snapshot_module_close(m);
@@ -317,6 +318,7 @@ int delaep64_snapshot_read_module(snapshot_t *s)
     }
 
     if (0
+        || (SMR_B(m, &regval) < 0)
         || (SMR_B_INT(m, &currbank) < 0)
         || (SMR_BA(m, roml_banks, 0x2000 * 9) < 0)) {
         snapshot_module_close(m);
