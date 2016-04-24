51410f7643a1b43348b4b906ef20ddf2c1abb735
gpz <groepaz@gmx.net>
2016-04-24 21:17:21 +0000

    added dummy functions
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@31073 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/pet/petiec.c
+++ vice/src/pet/petiec.c
@@ -54,6 +54,22 @@ iecbus_t *iecbus_drive_port(void)
     return NULL;
 }
 
+BYTE iec_drive_read(unsigned int dnr)
+{
+    /* FIXME: unused */
+    return 0;
+}
+
+void iec_drive_write(BYTE data, unsigned int dnr)
+{
+    /* FIXME: unused */
+    iec_update_ports();
+}
+
+BYTE parallel_cable_drive_read(int type, int handshake)
+{
+}
+
 void parallel_cable_drive_write(int port, BYTE data, int handshake, unsigned int dnr)
 {
 }
@@ -61,4 +77,8 @@ void parallel_cable_drive_write(int port, BYTE data, int handshake, unsigned int
 int iec_available_busses(void)
 {
     return IEC_BUS_IEEE;
+}
+
+void iec_fast_drive_direction(int direction, unsigned int dnr)
+{
 }
