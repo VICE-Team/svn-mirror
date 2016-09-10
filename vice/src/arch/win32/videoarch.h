c81413a6b7ec58363701351cd66500a8c5bf4660
areascout <areascout@gmx.at>
2016-09-10 16:00:23 +0000

    Add: Common Controls 6.0 visuals for msvc win32/64 port - styled buttons etc.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@31792 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/win32/videoarch.h
+++ vice/src/arch/win32/videoarch.h
@@ -121,4 +121,14 @@ extern int dx_primary_surface_rendering;
 extern LPDIRECT3D9 d3d;
 #endif
 
+#if _MSC_VER > 1400 && _WIN32_WINNT >= 0x0501
+#if defined _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+#endif
+
 #endif
