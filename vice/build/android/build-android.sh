c4dffd4c84e07ede7efa8faea804b454ea065118
blackystardust <blackystardust68@yahoo.com>
2016-09-23 19:56:47 +0000

    Fixed here too.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@31873 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/build/android/build-android.sh
+++ vice/build/android/build-android.sh
@@ -408,7 +408,7 @@ echo generating src/translate.h
 . ./gentranslate_h.sh <translate.txt >translate.h
 
 echo generating src/infocontrib.h
-. ./geninfocontrib_h.sh <../doc/vice.texi | sed -f infocontrib.sed >infocontrib.h
+. ./geninfocontrib_h.sh infocontrib.h <../doc/vice.texi | sed -f infocontrib.sed >infocontrib.h
 
 cd arch/android/AnVICE/jni
 
