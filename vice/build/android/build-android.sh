ceed6d055c422908a79cd8f6985420c44991dc9b
blackystardust <blackystardust68@yahoo.com>
2016-09-24 23:04:59 +0000

    Fix 'infocontrib.h' generation problem.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@31887 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/build/android/build-android.sh
+++ vice/build/android/build-android.sh
@@ -444,13 +444,13 @@ fi
 cd src
 
 echo generating src/translate_table.h
-. ./gentranslatetable.sh <translate.txt >translate_table.h
+${SHELL} ./gentranslatetable.sh <translate.txt >translate_table.h
 
 echo generating src/translate.h
-. ./gentranslate_h.sh <translate.txt >translate.h
+${SHELL} ./gentranslate_h.sh <translate.txt >translate.h
 
 echo generating src/infocontrib.h
-. ./geninfocontrib_h.sh infocontrib.h <../doc/vice.texi | sed -f infocontrib.sed >infocontrib.h
+${SHELL} ./geninfocontrib_h.sh infocontrib.h <../doc/vice.texi | sed -f infocontrib.sed >infocontrib.h
 
 cd arch/android/AnVICE/jni
 
