0e468b3d93871176841ba891eb7baed563fbdd78
areascout <areascout@gmx.at>
2016-10-01 16:46:22 +0000

    Fix: sid menu creation for xcbm5x0
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@31930 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/sdl/xcbm2_ui.c
+++ vice/src/arch/sdl/xcbm2_ui.c
@@ -334,6 +334,7 @@ int cbm5x0ui_init(void)
     uidrive_menu_create();
     uikeyboard_menu_create();
     uipalette_menu_create("VICII", NULL);
+    uisid_menu_create();
 
     sdl_ui_set_menu_font(mem_chargen_rom + 0x800, 8, 8);
     sdl_ui_set_main_menu(xcbm5x0_main_menu);
