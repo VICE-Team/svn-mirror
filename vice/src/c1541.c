9b947a3f84eac266811c89155e9fc413b435ebc0
blackystardust <blackystardust68@yahoo.com>
2016-10-18 16:30:43 +0000

    Fixed to use base 10.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@32024 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c1541.c
+++ vice/src/c1541.c
@@ -768,7 +768,7 @@ static int extract_unit_from_file_name_compyx(char *name, char **endptr)
     }
 
     /* try to parse an integer between '@' and ':' */
-    result = strtol(name + 1, endptr);
+    result = strtol(name + 1, endptr, 10);
     if (*endptr != NULL && **endptr == ':') {
         /* got something */
         (*endptr)++;
