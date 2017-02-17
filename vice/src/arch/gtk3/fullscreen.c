66a95bd82dffcf996da31ce89f98266ad5b2230d
compyx <b.wassink@ziggo.nl>
2017-02-17 11:00:00 +0000

    Gtk3: add fullscreen stubs
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@32883 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/fullscreen.c
+++ vice/src/arch/gtk3/fullscreen.c
@@ -31,9 +31,62 @@
 #include "not_implemented.h"
 
 #include "fullscreen.h"
+#include "video.h"
+
+
+static int fullscreen_enable(struct video_canvas_s *canvas, int enable)
+{
+    NOT_IMPLEMENTED();
+    return 0;
+}
+
+
+static int fullscreen_statusbar(struct video_canvas_s *canvas, int enable)
+{
+    NOT_IMPLEMENTED();
+    return 0;
+}
+
+
+static int fullscreen_double_size(struct video_canvas_s *canvas, int enable)
+{
+    NOT_IMPLEMENTED();
+    return 0;
+}
+
+
+static int fullscreen_double_scan(struct video_canvas_s *canvas, int enable)
+{
+    NOT_IMPLEMENTED();
+    return 0;
+}
+
+
+static int fullscreen_device(struct video_canvas_s *canvas, const char *device)
+{
+    NOT_IMPLEMENTED();
+    return 0;
+}
+
+
+static int fullscreen_mode_gtk3(struct video_canvas_s *canvas, int mode)
+{
+    NOT_IMPLEMENTED();
+    return 0;
+}
+
+
+
 
 void fullscreen_capability(struct cap_fullscreen_s *cap_fullscreen)
 {
-    NOT_IMPLEMENTED();
+    cap_fullscreen->device_num = 0;
+    cap_fullscreen->device_name[cap_fullscreen->device_num] = "GTK3";
+    cap_fullscreen->enable = fullscreen_enable;
+    cap_fullscreen->statusbar = fullscreen_statusbar;
+    cap_fullscreen->double_size = fullscreen_double_size;
+    cap_fullscreen->double_scan = fullscreen_double_scan;
+    cap_fullscreen->device = fullscreen_device;
+    cap_fullscreen->mode[cap_fullscreen->device_num] = fullscreen_mode_gtk3;
 }
 
