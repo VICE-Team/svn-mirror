34e5f08750c113050fffbe7ecce668076fe93b0a
blackystardust <blackystardust68@yahoo.com>
2017-03-07 21:29:54 +0000

    Updated.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@32956 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/platform/platform_qnx6_version.h
+++ vice/src/platform/platform_qnx6_version.h
@@ -65,6 +65,18 @@
 #define PLATFORM_OS "QNX 6.4.0"
 #endif
 
+#if (_NTO_VERSION==641)
+#define PLATFORM_OS "QNX 6.4.1"
+#endif
+ 
+#if (_NTO_VERSION==650)
+#define PLATFORM_OS "QNX 6.5"
+#endif
+
+#if (_NTO_VERSION==660)
+#define PLATFORM_OS "QNX 6.6"
+#endif
+
 #ifndef PLATFORM_OS
 #define PLATFORM_OS "QNX 6.x"
 #endif
