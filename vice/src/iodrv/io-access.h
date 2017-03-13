1d7d4be117a7d44cc839cf1d32f1c5a4207f2f8d
compyx <b.wassink@ziggo.nl>
2017-03-13 19:48:36 +0000

    Add missing prototypes for io-unix-access.c
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@32979 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/iodrv/io-access.h
+++ vice/src/iodrv/io-access.h
@@ -36,4 +36,11 @@ extern DWORD io_access_read_long(WORD addr);
 extern int io_access_init(void);
 extern void io_access_shutdown(void);
 
+# ifdef UNIX_COMPILE
+extern void io_access_store(WORD addr, BYTE value);
+extern BYTE io_access_read(WORD addr);
+int io_access_map(WORD addr, WORD space);
+void io_access_unmap(WORD addr, WORD space);
+# endif
+
 #endif
