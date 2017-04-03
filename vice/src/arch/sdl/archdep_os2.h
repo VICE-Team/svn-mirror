c690a9364ab54b7408ac630c50616ae1160cf42d
blackystardust <blackystardust68@yahoo.com>
2017-04-03 21:19:23 +0000

    Fixed.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33104 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/sdl/archdep_os2.h
+++ vice/src/arch/sdl/archdep_os2.h
@@ -44,13 +44,13 @@
 #define ARCHDEP_FINDPATH_SEPARATOR_STRING ";"
 
 /* Modes for fopen().  */
-#define MODE_READ              "r"
-#define MODE_READ_TEXT         "rt"
-#define MODE_READ_WRITE        "r+"
-#define MODE_WRITE             "w"
-#define MODE_WRITE_TEXT        "wt"
-#define MODE_APPEND            "a"
-#define MODE_APPEND_READ_WRITE "a+"
+#define MODE_READ              "rb"
+#define MODE_READ_TEXT         "r"
+#define MODE_READ_WRITE        "rb+"
+#define MODE_WRITE             "wb"
+#define MODE_WRITE_TEXT        "w"
+#define MODE_APPEND            "ab"
+#define MODE_APPEND_READ_WRITE "ab+"
 
 /* Printer default devices.  */
 #define ARCHDEP_PRINTER_DEFAULT_DEV1 "viceprnt.out"
