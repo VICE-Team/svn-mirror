3e1d9616e22243b03717b17353b8bba06eec16ec
blackystardust <blackystardust68@yahoo.com>
2017-04-11 18:25:31 +0000

    Change default menu key for sdl amiga based platforms to 'F10'.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33147 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/sdl/archdep_amiga.h
+++ vice/src/arch/sdl/archdep_amiga.h
@@ -107,20 +107,10 @@ extern void close_libs(void);
 
 extern int pci_lib_loaded;
 
-#ifdef AMIGA_OS4
-extern int amigainput_lib_loaded;
-#else
-extern int lowlevel_lib_loaded;
-#endif
-
-#if (defined(AMIGA_M68K) && defined(HAVE_PROTO_CYBERGRAPHICS_H)) || defined(AMIGA_MORPHOS) || defined(AMIGA_AROS)
-#ifdef HAVE_XVIDEO
-extern int xvideo_lib_loaded;
-#endif
-#endif
-
 #if defined(HAVE_PROTO_OPENPCI_H) || defined(AMIGA_OS4)
 extern int pci_lib_loaded;
 #endif
+
+#define DEFAULT_MENU_KEY SDLK_F10
 
 #endif
