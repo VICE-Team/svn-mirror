56db00feb8220355cb79a80098879a97ab031461
blackystardust <blackystardust68@yahoo.com>
2017-05-30 18:03:23 +0000

    Removed 'emulator' from the title bar text.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33305 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/raster/raster.c
+++ vice/src/raster/raster.c
@@ -482,9 +482,9 @@ void raster_set_title(raster_t *raster, const char *name)
     char *extra_title_text = archdep_extra_title_text();
 
     if (extra_title_text) {
-        title = util_concat("VICE: ", name, " emulator", extra_title_text, NULL);
+        title = util_concat("VICE: ", name, extra_title_text, NULL);
     } else {
-        title = util_concat("VICE: ", name, " emulator", NULL);
+        title = util_concat("VICE: ", name, NULL);
     }
     video_viewport_title_set(raster->canvas, title);
 
