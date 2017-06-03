31125bf2905e98e8cbdd3556942bd4928657616d
compyx <b.wassink@ziggo.nl>
2017-06-03 14:54:34 +0000

    Fix warnings
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33309 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/imagecontents/imagecontents.c
+++ vice/src/imagecontents/imagecontents.c
@@ -200,7 +200,7 @@ image_contents_screencode_t *image_contents_to_screencode(image_contents_t
  */
 char *image_contents_to_string(image_contents_t * contents, char out_charset)
 {
-    BYTE *str = lib_msprintf("0 \"%s\" %s", contents->name, contents->id);
+    BYTE *str = (BYTE *)lib_msprintf("0 \"%s\" %s", contents->name, contents->id);
 
     if (out_charset == IMAGE_CONTENTS_STRING_PETSCII) {
         return (char *)str;
@@ -267,7 +267,7 @@ char *image_contents_filename_to_string(image_contents_file_list_t * p,
     if (out_charset == IMAGE_CONTENTS_STRING_PETSCII) {
         return lib_stralloc(print_name);
     } else {
-        return (char *)charset_petconv_stralloc(print_name, out_charset);
+        return (char *)charset_petconv_stralloc((BYTE *)print_name, out_charset);
     }
 }
 
@@ -306,7 +306,7 @@ char *image_contents_file_to_string(image_contents_file_list_t *p,
     BYTE *str;
 
     print_name = image_contents_get_filename(p);
-    str = lib_msprintf("%-5d %s %s", p->size, print_name, p->type);
+    str = (BYTE *)lib_msprintf("%-5d %s %s", p->size, print_name, p->type);
 
     if (out_charset == IMAGE_CONTENTS_STRING_PETSCII) {
         return (char *)str;
