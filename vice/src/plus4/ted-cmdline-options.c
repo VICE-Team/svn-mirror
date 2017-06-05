ab2ffc006d4724fb48b959ba6d85eac0c019ff1a
compyx <b.wassink@ziggo.nl>
2017-06-05 14:08:20 +0000

    More missing initializer fixes
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33320 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/plus4/ted-cmdline-options.c
+++ vice/src/plus4/ted-cmdline-options.c
@@ -66,7 +66,10 @@ static const cmdline_option_t cmdline_options[] =
     { "-TEDborders", CALL_FUNCTION, 1,
       border_set_func, NULL, "TEDBorderMode", NULL,
       USE_PARAM_ID, USE_DESCRIPTION_ID,
-      IDCLS_P_MODE, IDCLS_SET_BORDER_MODE },
+      IDCLS_P_MODE, IDCLS_SET_BORDER_MODE,
+      "mode",               /* added, was missing, feel free to alter /Compyx */
+      "TED border mode"     /* same here */
+    },
     CMDLINE_LIST_END
 };
 
