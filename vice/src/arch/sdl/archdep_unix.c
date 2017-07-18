f3acd1168fbd5396e37df5f0a316d6b05531a3ad
compyx <b.wassink@ziggo.nl>
2017-07-18 19:02:22 +0000

    Remove duplicate archdep_file_is_gzip() implementations from arch/sdl
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33358 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/sdl/archdep_unix.c
+++ vice/src/arch/sdl/archdep_unix.c
@@ -592,15 +592,6 @@ FILE *archdep_mkstemp_fd(char **filename, const char *mode)
 #endif
 }
 
-int archdep_file_is_gzip(const char *name)
-{
-    size_t l = strlen(name);
-
-    if ((l < 4 || strcasecmp(name + l - 3, ".gz")) && (l < 3 || strcasecmp(name + l - 2, ".z")) && (l < 4 || toupper((int)(name[l - 1])) != 'Z' || name[l - 4] != '.')) {
-        return 0;
-    }
-    return 1;
-}
 
 int archdep_file_set_gzip(const char *name)
 {
