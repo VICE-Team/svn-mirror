cfbb232affcbb38738f4f9d8f6d1df7ed0ff5116
gpz <groepaz@gmx.net>
2017-08-08 18:30:59 +0000

    use UINT32_MAX
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33449 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/rs232-win32-dev.c
+++ vice/src/arch/gtk3/rs232-win32-dev.c
@@ -187,7 +187,7 @@ int rs232dev_open(int device)
          * ensure that a read will always terminate and only return
          * what is already in the buffers
          */
-        comm_timeouts.ReadIntervalTimeout = UINT_MAX;
+        comm_timeouts.ReadIntervalTimeout = UINT32_MAX;
         comm_timeouts.ReadTotalTimeoutMultiplier = 0;
         comm_timeouts.ReadTotalTimeoutConstant = 0;
 
