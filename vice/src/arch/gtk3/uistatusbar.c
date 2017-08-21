8f9d9687676e6591446388d83002be48eb495485
mcmartin <mcmartin@gmail.com>
2017-08-21 02:17:18 +0000

    Put all possible drives in GTK3 status bar
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33536 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/uistatusbar.c
+++ vice/src/arch/gtk3/uistatusbar.c
@@ -43,8 +43,6 @@
  *
  * These vars are used in NOT_IMPLEMENTED_WARN_X_TIMES() macro calls.
  */
-static int led_msgs = 0;
-static int track_msgs = 0;
 static int tape_status_msgs = 0;
 static int tape_counter_msgs = 0;
 static int tape_motor_msgs = 0;
@@ -52,17 +50,19 @@ static int joyport_msgs = 0;
 
 #define MAX_STATUS_BARS 3
 
-/* TODO: Multiple LEDs, or possibly no LEDs for the non-TDE case */
-typedef struct ui_statusbar_drive_s {
-    GtkLabel *number, *track;
-    GtkDrawingArea *led;
-} ui_statusbar_drive_t;
+/* Global data that custom status bar widgets base their rendering
+ * on. */
+static struct ui_sb_state_s {
+    /* TODO: does not cover two-unit drives */
+    int drive_led_types[4];
+    unsigned int current_drive_leds[4][2];
+} sb_state;
 
 typedef struct ui_statusbar_s {
     GtkWidget *bar;
     GtkLabel *msg;
     /* TODO: Tape, Joystick */
-    ui_statusbar_drive_t drives[4];
+    GtkWidget *drives[4];
 } ui_statusbar_t;
 
 static ui_statusbar_t allocated_bars[MAX_STATUS_BARS];
@@ -75,10 +75,14 @@ void ui_statusbar_init(void)
         allocated_bars[i].bar = NULL;
         allocated_bars[i].msg = NULL;
         for (j = 0; j < 4; ++j) {
-            allocated_bars[i].drives[j].number = NULL;
-            allocated_bars[i].drives[j].track = NULL;
-            allocated_bars[i].drives[j].led = NULL;
+            allocated_bars[i].drives[j] = NULL;
         }
+    }
+
+    for (i = 0; i < 4; ++i) {
+        sb_state.drive_led_types[i] = 0;
+        sb_state.current_drive_leds[i][0] = 0;
+        sb_state.current_drive_leds[i][1] = 0;
     }
 }
 
@@ -87,24 +91,62 @@ void ui_statusbar_shutdown(void)
     /* Any universal resources we allocate get cleaned up here */
 }
 
-static GtkWidget *ui_drive_widget_create(void)
+static gboolean draw_drive_led_cb(GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+    int width, height, drive, i;
+    double red = 0.0, green = 0.0, x, y, w, h;
+
+    width = gtk_widget_get_allocated_width(widget);
+    height = gtk_widget_get_allocated_height(widget);
+    drive = *((int *)data);
+    for (i = 0; i < 2; ++i) {
+        int led_color = sb_state.drive_led_types[drive] & (1 << i);
+        if (led_color) {
+            green += sb_state.current_drive_leds[drive][i] / 1000.0;
+        } else {
+            red += sb_state.current_drive_leds[drive][i] / 1000.0;
+        }
+    }    
+    /* Cairo clamps these for us */
+    cairo_set_source_rgb(cr, red, green, 0);
+    /* LED is half text height and aims for a 2x1 aspect ratio */
+    h = height / 2.0;
+    w = 2.0 * h;
+    x = (width / 2.0) - h;
+    y = height / 4.0;
+    cairo_rectangle(cr, x, y, w, h);
+    cairo_fill(cr);
+    return FALSE;
+}
+
+static void destroy_drive_led_cb(GtkWidget *drive, gpointer data)
+{
+    lib_free(data);
+}
+
+static GtkWidget *ui_drive_widget_create(int unit)
 {
     GtkWidget *grid, *number, *track, *led;
+    char drive_id[4];
+    int *drive_state;
+    
     grid = gtk_grid_new();
     gtk_orientable_set_orientation(GTK_ORIENTABLE(grid), GTK_ORIENTATION_HORIZONTAL);
-    /* TODO: Fit this into the general status bar creation code, and
-     *       keep them all hidden at first. */
-    number = gtk_label_new(NULL);
-    track = gtk_label_new(NULL);
+    snprintf(drive_id, 4, "%d:", unit+8);
+    drive_id[3]=0;
+    number = gtk_label_new(drive_id);
+    track = gtk_label_new("18.5");
     led = gtk_drawing_area_new();
+    gtk_widget_set_size_request(led, 30, 15);
     gtk_container_add(GTK_CONTAINER(grid), number);
     gtk_container_add(GTK_CONTAINER(grid), track);
     gtk_container_add(GTK_CONTAINER(grid), led);
-    /* TODO: LED drawing code. Color is imported from setup, and the
-     * LED itself was 45*16 on Win32, and much shorter on GTK2. */
+    drive_state = lib_malloc(sizeof(int));
+    *drive_state = unit;
+    g_signal_connect(led, "destroy", G_CALLBACK(destroy_drive_led_cb), drive_state);
+    g_signal_connect(led, "draw", G_CALLBACK(draw_drive_led_cb), drive_state);
     return grid;
 }
-    
 
 static void destroy_statusbar_cb(GtkWidget *sb, gpointer ignored)
 {
@@ -116,9 +158,7 @@ static void destroy_statusbar_cb(GtkWidget *sb, gpointer ignored)
             allocated_bars[i].bar = NULL;
             allocated_bars[i].msg = NULL;
             for (j = 0; j < 4; ++j) {
-                allocated_bars[i].drives[j].number = NULL;
-                allocated_bars[i].drives[j].track = NULL;
-                allocated_bars[i].drives[j].led = NULL;
+                allocated_bars[i].drives[j] = NULL;
             }
         }
     }
@@ -126,8 +166,8 @@ static void destroy_statusbar_cb(GtkWidget *sb, gpointer ignored)
 
 GtkWidget *ui_statusbar_create(void)
 {
-    GtkWidget *sb;
-    int i;
+    GtkWidget *sb, *msg;
+    int i, j;
 
     TEMPORARY_IMPLEMENTATION();
 
@@ -142,15 +182,28 @@ GtkWidget *ui_statusbar_create(void)
         return NULL;
     }
 
-    /* TODO: Create a real bar */
     /* TODO: Create all elements independently and keep them
      *       referenced by this code, even when not part of any
-     *       immediate display. */
-    sb = gtk_label_new(NULL);
-    gtk_label_set_xalign(GTK_LABEL(sb), 0.0);
+     *       immediate display. Procedurally lay out all active
+     *       devices based on system status. */
+    sb = gtk_grid_new();
+    msg = gtk_label_new(NULL);
+    gtk_label_set_xalign(GTK_LABEL(msg), 0.0);
+    gtk_widget_set_hexpand(msg, TRUE);
     g_signal_connect(sb, "destroy", G_CALLBACK(destroy_statusbar_cb), NULL);
     allocated_bars[i].bar = sb;
-    allocated_bars[i].msg = GTK_LABEL(sb);
+    allocated_bars[i].msg = GTK_LABEL(msg);
+    gtk_grid_attach(GTK_GRID(sb), msg, 0, 0, 1, 2);
+    for (j = 0; j < 4; ++j) {
+        GtkWidget *drive = ui_drive_widget_create(j);
+        int row = j % 2;
+        int column = (j / 2) * 2 + 2;
+        if (row == 0) {
+            gtk_grid_attach(GTK_GRID(sb), gtk_separator_new(GTK_ORIENTATION_VERTICAL), column-1, 0, 1, 2);
+        }
+        gtk_grid_attach(GTK_GRID(sb), drive, column, row, 1, 1);
+        allocated_bars[i].drives[j] = drive;
+    }
 
     return sb;
 }
@@ -244,16 +297,47 @@ void ui_display_tape_current_image(const char *image)
  */
 void ui_display_drive_led(int drive_number, unsigned int pwm1, unsigned int led_pwm2)
 {
-    /* printf ("%d: [%u/%u]\n", drive_number, pwm1, led_pwm2); */
-    NOT_IMPLEMENTED_WARN_X_TIMES(led_msgs, 3);
+    int i;
+    if (drive_number < 0 || drive_number > 3) {
+        /* TODO: Fatal error? */
+        return;
+    }
+    sb_state.current_drive_leds[drive_number][0] = pwm1;
+    sb_state.current_drive_leds[drive_number][1] = led_pwm2;
+    for (i = 0; i < MAX_STATUS_BARS; ++i) {
+        if (allocated_bars[i].bar) {
+            GtkWidget *drive, *led;
+            drive = allocated_bars[i].drives[i];
+            led = gtk_grid_get_child_at(GTK_GRID(drive), 2, 0);
+            if (led) {
+                gtk_widget_queue_draw(led);
+            }
+        }
+    }
 }
 
 void ui_display_drive_track(unsigned int drive_number,
                             unsigned int drive_base,
                             unsigned int half_track_number)
 {
-    /* printf("%u/%u: %.1lf\n", drive_base, drive_number, half_track_number / 2.0); */
-    NOT_IMPLEMENTED_WARN_X_TIMES(track_msgs, 3);
+    int i, unit = drive_base-8;
+    if (unit < 0 || unit > 3) {
+        /* TODO: Fatal error? */
+        return;
+    }
+    for (i = 0; i < MAX_STATUS_BARS; ++i) {
+        if (allocated_bars[i].bar) {
+            GtkWidget *drive, *track;
+            drive = allocated_bars[i].drives[i];
+            track = gtk_grid_get_child_at(GTK_GRID(drive), 1, 0);
+            if (track) {
+                char track_str[16];
+                snprintf(track_str, 16, "%.1lf", half_track_number / 2.0);
+                track_str[15] = 0;
+                gtk_label_set_text(GTK_LABEL(track), track_str);
+            }
+        }
+    }
 }
 
 void ui_enable_drive_status(ui_drive_enable_t state, int *drive_led_color)
@@ -262,10 +346,11 @@ void ui_enable_drive_status(ui_drive_enable_t state, int *drive_led_color)
     /* 4 is based on drive/drive.h's DRIVE_NUM, but drive.h can't be
      * included here for some reason */
     /* TODO: Fix that */
-    /* static const char *led_types[] = { "Red", "Green/Red", "Red/Green", "Green" }; */
     for (i = 0; i < 4; ++i) {
         if (state & 1) {
-            /* printf("Drive %d enabled, LED color: %s\n", i+8, led_types[drive_led_color[i]]); */
+            sb_state.drive_led_types[i] = drive_led_color[i];
+            sb_state.current_drive_leds[i][0] = 0;
+            sb_state.current_drive_leds[i][1] = 0;
         }
         state >>= 1;
     }
