d0a194604d7833f505e86dbca5d504717e9b0712
compyx <b.wassink@ziggo.nl>
2017-08-29 16:23:50 +0000

    Gtk3: add speed and warp mmode to settings dialog
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33591 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/uisettings.c
+++ vice/src/arch/gtk3/uisettings.c
@@ -24,6 +24,23 @@
  */
 
 
+/* The layout is supposed to become this:
+ *
+ * +------------+-------------------------+
+ * | treeview   |                         |
+ * |  with      |                         |
+ * |   settings |   central widget,       |
+ * |  more      |   depending on which    |
+ * |   foo      |   item is selected in   |
+ * |   bar      |   the treeview          |
+ * |    whatever|                         |
+ * | burp       |                         |
+ * +------------+-------------------------+
+ * | load  save  load...  save...  close  |
+ * +--------------------------------------+
+ */
+
+
 #include "vice.h"
 
 #include <gtk/gtk.h>
@@ -31,10 +48,13 @@
 #include <stdlib.h>
 
 #include "lib.h"
+#include "resources.h"
 #include "vsync.h"
 
 /* widgets */
 #include "refreshratewidget.h"
+#include "speedwidget.h"
+
 
 #include "uisettings.h"
 
@@ -42,11 +62,115 @@
 #define NUM_COLUMNS 1
 
 
-static char *topitems[] = { "Refresh rate", "maximum speed", "warp" };
+
+static void on_load_clicked(GtkWidget *widget, gpointer data)
+{
+#ifdef HAVE_DEBUG_GTK3UI
+    g_print("[debug-gtk3ui] %s() called\n", __func__);
+#endif
+}
+
+
+static void on_save_clicked(GtkWidget *widget, gpointer data)
+{
+#ifdef HAVE_DEBUG_GTK3UI
+    g_print("[debug-gtk3ui] %s() called\n", __func__);
+#endif
+}
+
+
+static void on_load_file_clicked(GtkWidget *widget, gpointer data)
+{
+#ifdef HAVE_DEBUG_GTK3UI
+    g_print("[debug-gtk3ui] %s() called\n", __func__);
+#endif
+}
+
+
+static void on_save_file_clicked(GtkWidget *widget, gpointer data)
+{
+#ifdef HAVE_DEBUG_GTK3UI
+    g_print("[debug-gtk3ui] %s() called\n", __func__);
+#endif
+}
+
+
+static void on_close_clicked(GtkWidget *widget, gpointer data)
+{
+    GtkWidget *window = GTK_WIDGET(data);
+    gtk_widget_destroy(window);
+}
+
+
+static void warp_callback(GtkWidget *widget, gpointer data)
+{
+    int warp = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
+
+    resources_set_int("WarpMode", warp);
+}
+
+
+static GtkWidget *create_warp_checkbox(void)
+{
+    GtkWidget *check;
+
+    check = gtk_check_button_new_with_label("Warp mode");
+    g_signal_connect(check, "toggled", G_CALLBACK(warp_callback), NULL);
+    gtk_widget_show(check);
+    return check;
+}
+
+
+static GtkWidget *create_button_box(GtkWidget *window)
+{
+    GtkWidget *box;
+
+    GtkWidget *btn_load;
+    GtkWidget *btn_save;
+    GtkWidget *btn_load_file;
+    GtkWidget *btn_save_file;
+    GtkWidget *btn_close;
+
+    box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+
+    btn_load = gtk_button_new_with_label("Load");
+    g_signal_connect(btn_load, "clicked", G_CALLBACK(on_load_clicked),
+            window);
+    gtk_widget_show(btn_load);
+    gtk_container_add(GTK_CONTAINER(box), btn_load);
+
+    btn_save = gtk_button_new_with_label("Save");
+    g_signal_connect(btn_save, "clicked", G_CALLBACK(on_save_clicked),
+            window);
+    gtk_widget_show(btn_save);
+    gtk_container_add(GTK_CONTAINER(box), btn_save);
+
+    btn_load_file = gtk_button_new_with_label("Load from file ...");
+    g_signal_connect(btn_load_file, "clicked", G_CALLBACK(on_load_file_clicked),
+            window);
+    gtk_widget_show(btn_load_file);
+    gtk_container_add(GTK_CONTAINER(box), btn_load_file);
+
+    btn_save_file = gtk_button_new_with_label("Save to file ...");
+    g_signal_connect(btn_save_file, "clicked", G_CALLBACK(on_save_file_clicked),
+            window);
+    gtk_widget_show(btn_save_file);
+    gtk_container_add(GTK_CONTAINER(box), btn_save_file);
+
+    btn_close = gtk_button_new_with_label("Close");
+    g_signal_connect(btn_close, "clicked", G_CALLBACK(on_close_clicked),
+            window);
+    gtk_widget_show(btn_close);
+    gtk_container_add(GTK_CONTAINER(box), btn_close);
+
+    gtk_widget_show(box);
+    return box;
+}
+
 
 
 /* TODO:    find a non-convoluted way to add items with callbacks setting the
- *          proper 'main widget'
+ *          proper 'central widget'
  */
 static GtkWidget *create_treeview(void)
 {
@@ -92,18 +216,26 @@ void ui_settings_dialog_callback(GtkWidget *widget, gpointer user_data)
 #ifdef HAVE_DEBUG_GTK3UI
     g_print("[debug-gtk3ui] %s() called\n", __func__);
 #endif
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_modal(GTK_WINDOW(window), TRUE);
+    gtk_window_set_title(GTK_WINDOW(window), "VICE settings");
 
     layout = gtk_grid_new();
     tree = create_treeview();
-    gtk_grid_attach(GTK_GRID(layout), tree, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(layout), tree, 0, 0, 1, 2);
 
-    gtk_grid_attach(GTK_GRID(layout), create_refreshrate_widget(), 1, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(layout), create_refreshrate_widget(), 1, 0, 1, 2);
+    gtk_grid_attach(GTK_GRID(layout), create_speed_widget(), 2, 0, 1, 1);
+
+    gtk_grid_attach(GTK_GRID(layout), create_button_box(window), 0, 2, 3, 1);
+
+    gtk_grid_attach(GTK_GRID(layout), create_warp_checkbox(), 2, 1, 1, 1);
+
 
     gtk_widget_show(layout);
     gtk_widget_show(tree);
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_modal(GTK_WINDOW(window), TRUE);
+
     gtk_container_add(GTK_CONTAINER(window), layout);
     gtk_widget_show(window);
 }
