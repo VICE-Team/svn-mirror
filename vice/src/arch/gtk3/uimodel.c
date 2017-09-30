860b2cb5167fbad843fc4da2100253ebe0e64bf6
compyx <b.wassink@ziggo.nl>
2017-09-30 16:34:58 +0000

    Gtk3: refactor uimodel to use separate layout function for machines.
    The old code using one massive layout setup got messy since the 'model' settings vary widly from machine to machine. Currently only C64/C64SC produce a proper layout. More to come.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33821 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/uimodel.c
+++ vice/src/arch/gtk3/uimodel.c
@@ -2,6 +2,8 @@
  * \brief   Model settings dialog
  *
  * Controls the following resource(s):
+ *  IECReset    (c64, c64sc)
+ *  GlueLogic   (c64sc)
  *
  *  (for more, see used widgets)
  *
@@ -38,6 +40,7 @@
 #include "debug_gtk3.h"
 #include "not_implemented.h"
 #include "machine.h"
+#include "resources.h"
 #include "machinemodelwidget.h"
 #include "videomodelwidget.h"
 #include "vdcmodelwidget.h"
@@ -49,35 +52,190 @@
 #include "uimodel.h"
 
 
+/** \brief  Machine model widget
+ *
+ * Used by all machines
+ */
+static GtkWidget *machine_widget = NULL;
+
+/** \brief  CIA model widget
+ *
+ * Used by C64, C64SC, SCPU64, C64DTV, C128, CBM5x0, CBM-II, VSID
+ */
 static GtkWidget *cia_widget = NULL;
 
+/** \brief  Video model widget
+ */
+static GtkWidget *video_widget = NULL;
+static GtkWidget *vdc_widget = NULL;
+static GtkWidget *sid_widget = NULL;
+static GtkWidget *kernal_widget = NULL;
 
 
-/** \brief  Create 'Model' widget for the settings UI
+/** \brief  Handler for the "toggled" event of the Reset-to-IEC radio buttons
  *
- * \param[in]   parent  parent widget
+ * \param[in]   widget      radio button triggering the event
+ * \param[in]   user_data   data for event (unused)
+ */
+static void on_reset_to_iec_toggle(GtkWidget *widget, gpointer user_data)
+{
+    int state = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
+    debug_gtk3("setting IECReset to %s\n", state ? "ON" : "OFF");
+    resources_set_int("IECReset", state);
+}
+
+
+/** \brief  Handler for the "toggled" event of the C64SC Glue Logic radio buttons
+ *
+ * \param[in]   widget      radio button triggering the event
+ * \param[in]   user_data   glue value (int)
+ */
+static void on_c64_glue_toggled(GtkWidget *widget, gpointer user_data)
+{
+    int glue = GPOINTER_TO_INT(user_data);
+
+    if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget))) {
+        debug_gtk3("setting GlueLogic to %s\n",
+                glue == 0 ? "discrete" : "252535-01");
+        resources_set_int("GlueLogic", glue);
+    }
+}
+
+
+/** \brief  Create widget to toggle "Reset-to-IEC"
  *
  * \return  GtkGrid
  */
-GtkWidget *uimodel_create_central_widget(GtkWidget *parent)
+static GtkWidget *create_reset_to_iec_widget(void)
 {
-    GtkWidget *layout;
+    int state;
+    GtkWidget *check;
 
-    GtkWidget *model_widget;
-    GtkWidget *video_widget = NULL;
-    GtkWidget *vdc_widget = NULL;       /* for the C128's VDC widget */
+    resources_get_int("IECReset", &state);
+    check = gtk_check_button_new_with_label("Reset goes to IEC");
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check), state);
+    g_signal_connect(check, "toggled", G_CALLBACK(on_reset_to_iec_toggle),
+            NULL);
+    gtk_widget_show(check);
+    return check;
+}
 
-    GtkWidget *video_wrapper = NULL;    /* wrapper to have two video model
-                                           widgets in case of the C128 */
-    GtkWidget *sid_widget = NULL;
 
-    GtkWidget *kernal_widget = NULL;
+/** \brief  Create widget to select C64SC Glue Logic
+ *
+ * \return  GtkGrid
+ */
+static GtkWidget *create_c64_glue_widget(void)
+{
+    GtkWidget *grid;
+    GtkWidget *label;
+    GtkWidget *discrete_radio;
+    GtkWidget *custom_radio;
+    GtkWidget *active;
+    GSList *group = NULL;
+
+    int glue;
+
+    resources_get_int("GlueLogic", &glue);
+
+    grid = gtk_grid_new();
+    gtk_grid_set_column_spacing(GTK_GRID(grid), 8);
+
+    label = gtk_label_new("Glue logic");
+    g_object_set(label, "margin-left", 16, NULL);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
+
+    discrete_radio = gtk_radio_button_new_with_label(group, "Discrete");
+    custom_radio = gtk_radio_button_new_with_label(group, "Custom IC");
+    gtk_radio_button_join_group(GTK_RADIO_BUTTON(custom_radio),
+            GTK_RADIO_BUTTON(discrete_radio));
+
+    active = glue == 0 ? discrete_radio : custom_radio;
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(active), TRUE);
+
+    g_signal_connect(discrete_radio, "toggled",
+            G_CALLBACK(on_c64_glue_toggled), GINT_TO_POINTER(0));
+    g_signal_connect(custom_radio, "toggled",
+            G_CALLBACK(on_c64_glue_toggled), GINT_TO_POINTER(1));
+
+    gtk_grid_attach(GTK_GRID(grid), discrete_radio, 1, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), custom_radio, 2, 0, 1, 1);
 
 
-    layout = gtk_grid_new();
+    gtk_widget_show_all(grid);
+    return grid;
+}
 
-    model_widget = machine_model_widget_create();
-    gtk_grid_attach(GTK_GRID(layout), model_widget, 0, 0, 1, 1);
+
+
+static GtkWidget *create_c64_misc_widget(void)
+{
+    GtkWidget *grid;
+    GtkWidget *iec_widget;
+    GtkWidget *glue_widget = NULL;
+
+    grid = uihelpers_create_grid_with_label("Miscellaneous", 1);
+
+    iec_widget = create_reset_to_iec_widget();
+    g_object_set(iec_widget, "margin-left", 16, NULL);
+    gtk_grid_attach(GTK_GRID(grid), iec_widget, 0, 1, 1, 1);
+    if (machine_class == VICE_MACHINE_C64SC) {
+        glue_widget = create_c64_glue_widget();
+        gtk_grid_attach(GTK_GRID(grid), glue_widget, 0, 2, 1, 1);
+    }
+
+    gtk_widget_show_all(grid);
+    return grid;
+}
+
+
+/** \brief  Create widget layout for C64/C64SC
+ *
+ * \param[in,out]   grid    GtkGrid to use for layout
+ *
+ * \return  \a grid
+ */
+static GtkWidget *create_c64_layout(GtkWidget *grid)
+{
+    /* add machine widget */
+    gtk_grid_attach(GTK_GRID(grid), machine_widget, 0, 0, 1, 2);
+
+    /* VIC-II model widget */
+    video_widget = video_model_widget_create();
+    gtk_grid_attach(GTK_GRID(grid), video_widget, 1, 0, 1, 1);
+
+    /* SID widget */
+    sid_widget = sid_model_widget_create(machine_widget);
+    gtk_grid_attach(GTK_GRID(grid), sid_widget, 1, 1, 1, 1);
+
+    /* CIA1 & CIA2 widget */
+    cia_widget = cia_model_widget_create(machine_widget, 2);
+    gtk_grid_attach(GTK_GRID(grid), cia_widget, 0, 2, 2, 1);
+
+    /* Kernal revision widget */
+    kernal_widget = kernal_revision_widget_create();
+    gtk_grid_attach(GTK_GRID(grid), kernal_widget, 2, 0, 1, 1);
+
+    /* Reset-to-IEC check button */
+    gtk_grid_attach(GTK_GRID(grid), create_c64_misc_widget(),
+            2, 1, 1, 1);
+
+    return grid;
+}
+
+static GtkWidget *create_scpu64_layout(GtkWidget *grid)
+{
+    return grid;
+}
+
+static GtkWidget *create_c128_layout(GtkWidget *grid)
+{
+    GtkWidget *video_wrapper;
+
+    /* add machine widget */
+    gtk_grid_attach(GTK_GRID(grid), machine_widget, 0, 0, 1, 1);
+
+#if 0
 
     if (machine_class != VICE_MACHINE_CBM6x0 &&
             machine_class != VICE_MACHINE_PET) {
@@ -95,10 +253,123 @@ GtkWidget *uimodel_create_central_widget(GtkWidget *parent)
 
         gtk_grid_attach(GTK_GRID(layout), video_wrapper, 1, 0, 1, 1);
     }
+#endif
+
+    return grid;
+}
+
+static GtkWidget *create_c64dtv_layout(GtkWidget *grid)
+{
+    return grid;
+}
+
+static GtkWidget *create_vic20_layout(GtkWidget *grid)
+{
+    return grid;
+}
+
+static GtkWidget *create_plus4_layout(GtkWidget *grid)
+{
+    return grid;
+}
+
+static GtkWidget *create_pet_layout(GtkWidget *grid)
+{
+    return grid;
+}
+
+static GtkWidget *create_cbm5x0_layout(GtkWidget *grid)
+{
+    return grid;
+}
+
+static GtkWidget *create_cbm6x0_layout(GtkWidget *grid)
+{
+    return grid;
+}
+
+static GtkWidget *create_vsid_layout(GtkWidget *grid)
+{
+    return grid;
+}
+
+
+
+/** \brief  Create machine-specific layout
+ *
+ * Creates a machine-specific layout, including creating the required widgets
+ *
+ * \return  GtkGrid
+ */
+static GtkWidget *create_layout(void)
+{
+    GtkWidget *grid = gtk_grid_new();
+
+    /* Can't add the machine model widget here, since it differs a lot in size
+     * depending on the machine, which may result in odd layouts. So we need
+     * to determine per machine how many grid rows the machine model widget
+     * needs to occupy */
+
+    switch (machine_class) {
+        case VICE_MACHINE_C64:      /* fall through */
+        case VICE_MACHINE_C64SC:
+            return create_c64_layout(grid);
+        case VICE_MACHINE_SCPU64:
+            return create_scpu64_layout(grid);
+        case VICE_MACHINE_C64DTV:
+            return create_c64dtv_layout(grid);
+        case VICE_MACHINE_C128:
+            return create_c128_layout(grid);
+        case VICE_MACHINE_VIC20:
+            return create_vic20_layout(grid);
+        case VICE_MACHINE_PLUS4:
+            return create_plus4_layout(grid);
+        case VICE_MACHINE_PET:
+            return create_pet_layout(grid);
+        case VICE_MACHINE_CBM5x0:
+            return create_cbm5x0_layout(grid);
+        case VICE_MACHINE_CBM6x0:
+            return create_cbm6x0_layout(grid);
+        case VICE_MACHINE_VSID:
+            return create_vsid_layout(grid);
+        default:
+            /* shouldn't get here */
+            fprintf(stderr, "Aargs! machine %d does not exist!", machine_class);
+            exit(1);
+    }
+}
+
+
+/** \brief  Create 'Model' widget for the settings UI
+ *
+ * \param[in]   parent  parent widget
+ *
+ * \return  GtkGrid
+ */
+GtkWidget *uimodel_create_central_widget(GtkWidget *parent)
+{
+    GtkWidget *layout;
+    GtkWidget *video_wrapper = NULL;    /* wrapper to have two video model
+                                           widgets in case of the C128 */
+
+    machine_widget = NULL;
+    cia_widget = NULL;
+    video_widget = NULL;
+    vdc_widget = NULL;
+    sid_widget = NULL;
+    kernal_widget = NULL;
+
+    /* every machine has a machine model widget */
+    machine_widget = machine_model_widget_create();
+
+    /* create machine-specific layout */
+    layout = create_layout();
+
+#if 0
 
     /* create SID widget: every machine either has a SID built-in or can have
      * one via an expansion card, so no need to check machine_class here: */
-    sid_widget = sid_model_widget_create(model_widget);
+    sid_widget = sid_model_widget_create(machine_widget);
     gtk_grid_attach(GTK_GRID(layout), sid_widget, 2, 0, 1, 1);
 
 
@@ -111,13 +382,13 @@ GtkWidget *uimodel_create_central_widget(GtkWidget *parent)
         case VICE_MACHINE_SCPU64:   /* fall through */
         case VICE_MACHINE_C128:     /* fall through */
         case VICE_MACHINE_VSID:
-            cia_widget = cia_model_widget_create(model_widget, 2);
+            cia_widget = cia_model_widget_create(machine_widget, 2);
             break;
 
         /* 1x CIA */
         case VICE_MACHINE_CBM5x0:   /* fall through */
         case VICE_MACHINE_CBM6x0:
-            cia_widget = cia_model_widget_create(model_widget, 1);
+            cia_widget = cia_model_widget_create(machine_widget, 1);
             break;
 
         /* no CIA */
@@ -137,7 +408,7 @@ GtkWidget *uimodel_create_central_widget(GtkWidget *parent)
     /*
      * Connect signals that were not connected in the previous calls
      */
-    machine_model_widget_connect_signals(model_widget);
+    machine_model_widget_connect_signals(machine_widget);
     if (machine_class != VICE_MACHINE_CBM6x0 &&
             machine_class != VICE_MACHINE_PET) {
         video_model_widget_connect_signals(video_widget);
@@ -145,7 +416,7 @@ GtkWidget *uimodel_create_central_widget(GtkWidget *parent)
     if (machine_class == VICE_MACHINE_C128) {
         vdc_model_widget_connect_signals(vdc_widget);
     }
-
+#endif
     gtk_widget_show_all(layout);
     return layout;
 }
