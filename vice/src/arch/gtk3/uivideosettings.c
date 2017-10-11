bf9cee671019a29522d8de954fcad1ba37c86497
compyx <b.wassink@ziggo.nl>
2017-10-11 13:09:55 +0000

    Gtk3: Enable 'video settings' to control two video chips
    Since the C128 has both a VICII and a VDC, the widget now allows to have two settings sub-widgets. Still needs additional checks agains 'machine_class' to, for example, disable sprite-collision check boxes for Crtc/VDC.
    And it needs a few more checkboxes per video-settings sub-widget.
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33870 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/uivideosettings.c
+++ vice/src/arch/gtk3/uivideosettings.c
@@ -47,67 +47,56 @@
 
 #include "uivideosettings.h"
 
+static char *widget_title[2] = { NULL, NULL };
+static const char *chip_name[2] = { NULL, NULL };
 
 
-static GtkWidget *double_size_widget = NULL;
-static GtkWidget *double_scan_widget = NULL;
-static GtkWidget *video_cache_widget = NULL;
-static GtkWidget *vert_stretch_widget = NULL;
-static GtkWidget *video_render_widget = NULL;
-static GtkWidget *audio_leak_widget = NULL;
-static GtkWidget *sprite_sprite_widget = NULL;
-static GtkWidget *sprite_background_widget = NULL;
-static GtkWidget *vsp_bug_widget = NULL;
-
-static char *double_size_resname = NULL;
-static char *double_scan_resname = NULL;
-static char *video_cache_resname = NULL;
-static char *vert_stretch_resname = NULL;
-static char *audio_leak_resname = NULL;
-static char *sprite_sprite_resname = NULL;
-static char *sprite_background_resname = NULL;
-static char *vsp_bug_resname = NULL;
+static char *double_size_resname[2] = { NULL, NULL };
+static char *double_scan_resname[2] = { NULL, NULL };
+static char *video_cache_resname[2] = { NULL, NULL };
+static char *vert_stretch_resname[2] = { NULL, NULL };
+static char *audio_leak_resname[2] = { NULL, NULL };
+static char *sprite_sprite_resname[2] = { NULL, NULL };
+static char *sprite_background_resname[2] = { NULL, NULL };
+static char *vsp_bug_resname[2] = { NULL, NULL };
 
 static void on_destroy(GtkWidget *widget)
 {
-    double_size_widget = NULL;
-    double_scan_widget = NULL;
-    video_cache_widget = NULL;
-    vert_stretch_widget = NULL;
-    video_render_widget = NULL;
-    audio_leak_widget = NULL;
-    sprite_sprite_resname = NULL;
-    sprite_background_resname = NULL;
-    vsp_bug_widget = NULL;
+    int i;
 
-    if (double_size_resname != NULL) {
-        lib_free(double_size_resname);
-    }
-    if (double_scan_resname != NULL) {
-        lib_free(double_scan_resname);
-    }
-    if (video_cache_resname != NULL) {
-        lib_free(video_cache_resname);
-    }
-    if (vert_stretch_resname != NULL) {
-        lib_free(vert_stretch_resname);
-    }
-    if (audio_leak_resname != NULL) {
-        lib_free(audio_leak_resname);
-    }
-    if (sprite_sprite_resname != NULL) {
-        lib_free(sprite_sprite_resname);
-    }
-    if (sprite_background_resname != NULL) {
-        lib_free(sprite_background_resname);
-    }
-    if (vsp_bug_resname != NULL) {
-        lib_free(vsp_bug_resname);
+    for (i = 0; i < 2; i++) {
+        if (double_size_resname[i] != NULL) {
+            lib_free(double_size_resname[i]);
+        }
+        if (double_scan_resname[i] != NULL) {
+            lib_free(double_scan_resname[i]);
+        }
+        if (video_cache_resname[i] != NULL) {
+            lib_free(video_cache_resname[i]);
+        }
+        if (vert_stretch_resname[i] != NULL) {
+            lib_free(vert_stretch_resname[i]);
+        }
+        if (audio_leak_resname[i] != NULL) {
+            lib_free(audio_leak_resname[i]);
+        }
+        if (sprite_sprite_resname[i] != NULL) {
+            lib_free(sprite_sprite_resname[i]);
+        }
+        if (sprite_background_resname[i] != NULL) {
+            lib_free(sprite_background_resname[i]);
+        }
+        if (vsp_bug_resname[i] != NULL) {
+            lib_free(vsp_bug_resname[i]);
+        }
+        if (widget_title[i] != NULL) {
+            lib_free(widget_title[i]);
+        }
     }
 }
 
 
-static const char *video_chip_prefix(void)
+static const char *get_video_chip_name(void)
 {
     switch (machine_class) {
         /* VIC */
@@ -142,99 +131,109 @@ static const char *video_chip_prefix(void)
 }
 
 
-static GtkWidget *create_double_size_widget(void)
+static GtkWidget *create_double_size_widget(int index)
 {
-    double_size_resname = lib_msprintf("%sDoubleSize", video_chip_prefix());
+    double_size_resname[index] = lib_msprintf("%sDoubleSize", chip_name[index]);
     return uihelpers_create_resource_checkbox(
-            "Double size", double_size_resname);
+            "Double size", double_size_resname[index]);
 }
 
 
-static GtkWidget *create_double_scan_widget(void)
+static GtkWidget *create_double_scan_widget(int index)
 {
-    double_scan_resname = lib_msprintf("%sDoubleScan", video_chip_prefix());
+    double_scan_resname[index] = lib_msprintf("%sDoubleScan", chip_name[index]);
     return uihelpers_create_resource_checkbox(
-            "Double scan", double_scan_resname);
+            "Double scan", double_scan_resname[index]);
 }
 
 
-static GtkWidget *create_video_cache_widget(void)
+static GtkWidget *create_video_cache_widget(int index)
 {
-    video_cache_resname = lib_msprintf("%sVideoCache", video_chip_prefix());
+    video_cache_resname[index] = lib_msprintf("%sVideoCache", chip_name[index]);
     return uihelpers_create_resource_checkbox(
-        "Video cache", video_cache_resname);
+        "Video cache", video_cache_resname[index]);
 }
 
 
-static GtkWidget *create_vert_stretch_widget(void)
+static GtkWidget *create_vert_stretch_widget(int index)
 {
-    vert_stretch_resname = lib_msprintf("%sStretchVertical",
-            video_chip_prefix());
+    vert_stretch_resname[index] = lib_msprintf("%sStretchVertical",
+            chip_name[index]);
     return uihelpers_create_resource_checkbox(
-        "Stretch vertically", vert_stretch_resname);
+        "Stretch vertically", vert_stretch_resname[index]);
 }
 
 
-static GtkWidget *create_audio_leak_widget(void)
+static GtkWidget *create_audio_leak_widget(int index)
 {
-    audio_leak_resname = lib_msprintf("%sAudioLeak", video_chip_prefix());
+    audio_leak_resname[index] = lib_msprintf("%sAudioLeak", chip_name[index]);
     return uihelpers_create_resource_checkbox(
-            "Audio leak emulation", audio_leak_resname);
+            "Audio leak emulation", audio_leak_resname[index]);
 }
 
 
-static GtkWidget *create_sprite_sprite_widget(void)
-{
-    /* really fucked up resource name by the way */
-    sprite_sprite_resname = lib_msprintf("%sCheckSsColl", video_chip_prefix());
-    return uihelpers_create_resource_checkbox(
-            "Sprite-sprite collisions", sprite_sprite_resname);
-}
-
-
-static GtkWidget *create_sprite_background_widget(void)
+static GtkWidget *create_sprite_sprite_widget(int index)
 {
     /* really fucked up resource name by the way */
-    sprite_background_resname = lib_msprintf("%sCheckSbColl",
-            video_chip_prefix());
+    sprite_sprite_resname[index] = lib_msprintf("%sCheckSsColl",
+            chip_name[index]);
     return uihelpers_create_resource_checkbox(
-            "Sprite-background collisions", sprite_background_resname);
+            "Sprite-sprite collisions", sprite_sprite_resname[index]);
 }
 
 
-static GtkWidget *create_vsp_bug_widget(void)
+static GtkWidget *create_sprite_background_widget(int index)
 {
-    vsp_bug_resname = lib_msprintf("%sVSPBug",
-            video_chip_prefix());
+    /* really fucked up resource name by the way */
+    sprite_background_resname[index] = lib_msprintf("%sCheckSbColl",
+            chip_name[index]);
     return uihelpers_create_resource_checkbox(
-            "VSP bug emulation", vsp_bug_resname);
+            "Sprite-background collisions", sprite_background_resname[index]);
 }
 
 
+static GtkWidget *create_vsp_bug_widget(int index)
+{
+    vsp_bug_resname[index] = lib_msprintf("%sVSPBug", chip_name[index]);
+    return uihelpers_create_resource_checkbox(
+            "VSP bug emulation", vsp_bug_resname[index]);
+}
 
 
-
-GtkWidget *uivideosettings_widget_create(GtkWidget *parent)
+static GtkWidget *create_layout(GtkWidget *parent, const char *chip, int index)
 {
     GtkWidget *layout;
     GtkWidget *wrapper;
+    GtkWidget *double_size_widget = NULL;
+    GtkWidget *double_scan_widget = NULL;
+    GtkWidget *video_cache_widget = NULL;
+    GtkWidget *vert_stretch_widget = NULL;
+    GtkWidget *video_render_widget = NULL;
+    GtkWidget *audio_leak_widget = NULL;
+    GtkWidget *sprite_sprite_widget = NULL;
+    GtkWidget *sprite_background_widget = NULL;
+    GtkWidget *vsp_bug_widget = NULL;
 
-    layout = gtk_grid_new();
+    widget_title[index] = lib_msprintf("%s Settings", chip);
+    chip_name[index] = chip;
+
+    /* row 0, columns 0-2 */
+    layout = uihelpers_create_grid_with_label(widget_title[index], 3);
     gtk_grid_set_column_spacing(GTK_GRID(layout), 8);
     g_object_set(layout, "margin-left", 16, NULL);
 
     wrapper = gtk_grid_new();
     gtk_grid_set_column_spacing(GTK_GRID(wrapper), 8);
-/*    g_object_set(wrapper, "margin-left", 16, NULL);*/
 
-    double_size_widget = create_double_size_widget();
-    double_scan_widget = create_double_scan_widget();
-    video_cache_widget = create_video_cache_widget();
-    vert_stretch_widget = create_vert_stretch_widget();
-    audio_leak_widget = create_audio_leak_widget();
-    sprite_sprite_widget = create_sprite_sprite_widget();
-    sprite_background_widget = create_sprite_background_widget();
-    vsp_bug_widget = create_vsp_bug_widget();
+    double_size_widget = create_double_size_widget(index);
+    g_object_set(double_size_widget, "margin-left", 16, NULL);
+    double_scan_widget = create_double_scan_widget(index);
+    video_cache_widget = create_video_cache_widget(index);
+    vert_stretch_widget = create_vert_stretch_widget(index);
+    audio_leak_widget = create_audio_leak_widget(index);
+    sprite_sprite_widget = create_sprite_sprite_widget(index);
+    sprite_background_widget = create_sprite_background_widget(index);
+    vsp_bug_widget = create_vsp_bug_widget(index);
 
 
     gtk_widget_set_vexpand(audio_leak_widget, FALSE);
@@ -244,27 +243,27 @@ GtkWidget *uivideosettings_widget_create(GtkWidget *parent)
     gtk_grid_attach(GTK_GRID(wrapper), video_cache_widget, 2, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(wrapper), vert_stretch_widget, 3, 0, 1, 1);
 
-    /* row 0, columns 0-2 */
-    gtk_grid_attach(GTK_GRID(layout), wrapper, 0, 0, 3, 1);
-
     /* row 1, columns 0-2 */
-    gtk_grid_attach(GTK_GRID(layout),
-            video_palette_widget_create(video_chip_prefix()),
-            0, 1, 3, 1);
+    gtk_grid_attach(GTK_GRID(layout), wrapper, 0, 1, 3, 1);
 
-    /* row 2, column 0 */
+    /* row 2, columns 0-2 */
     gtk_grid_attach(GTK_GRID(layout),
-            video_render_filter_widget_create(video_chip_prefix()),
-            0, 2, 1, 1);
-    /* row 2, column 1 */
+            video_palette_widget_create(chip),
+            0, 2, 3, 1);
+
+    /* row 3, column 0 */
+    gtk_grid_attach(GTK_GRID(layout),
+            video_render_filter_widget_create(chip),
+            0, 3, 1, 1);
+    /* row 3, column 1 */
     if (machine_class != VICE_MACHINE_PET
             && machine_class != VICE_MACHINE_CBM6x0) {
         /* add border mode widget */
         gtk_grid_attach(GTK_GRID(layout),
-                video_border_mode_widget_create(video_chip_prefix()),
-                1, 2, 1, 1);
+                video_border_mode_widget_create(chip),
+                1, 3, 1, 1);
     }
-    /* row 2, column2 */
+    /* row 3, column2 */
     wrapper = uihelpers_create_grid_with_label("Other shit", 1);
     gtk_grid_set_column_spacing(GTK_GRID(wrapper), 8);
 
@@ -276,12 +275,37 @@ GtkWidget *uivideosettings_widget_create(GtkWidget *parent)
             && machine_class != VICE_MACHINE_SCPU64) {
         gtk_widget_set_sensitive(vsp_bug_widget, FALSE);
     }
-    gtk_grid_attach(GTK_GRID(layout), wrapper, 2, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(layout), wrapper, 2, 3, 1, 1);
 
     gtk_widget_show_all(layout);
 
-    g_signal_connect(layout, "destroy", G_CALLBACK(on_destroy), NULL);
     return layout;
+}
 
+
+
+
+
+
+GtkWidget *uivideosettings_widget_create(GtkWidget *parent)
+{
+    GtkWidget *grid = gtk_grid_new();
+    const char *chip = get_video_chip_name();
+
+    gtk_grid_set_row_spacing(GTK_GRID(grid), 16);
+
+    gtk_grid_attach(GTK_GRID(grid),
+            create_layout(parent, chip, 0),
+            0, 0, 1, 1);
+    if (machine_class == VICE_MACHINE_C128) {
+        gtk_grid_attach(GTK_GRID(grid),
+                create_layout(parent, "VDC", 1),
+                0, 1, 1, 1);
+    }
+
+
+    g_signal_connect(grid, "destroy", G_CALLBACK(on_destroy), NULL);
+    gtk_widget_show_all(grid);
+    return grid;
 }
 
