cf3e7864f66155cacf4db3ec48525a38b1e24bd2
compyx <b.wassink@ziggo.nl>
2017-10-21 18:35:02 +0000

    Gtk3: Add SID settings widgets to the soundchip settings UI
    The resources "SidEngine", "SidStereo", "SidResidSampling", "SidFilters", "Sid[Stereo|Triple|Quad]AddressStart resources are now handled. Next up are ReSID specific resources "SidResidPassband", "SidResidGain" and "SidResidFilterBias".
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@33935 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/sidsoundwidget.c
+++ vice/src/arch/gtk3/widgets/sidsoundwidget.c
@@ -4,6 +4,15 @@
  * Written by
  *  Bas Wassink <b.wassink@ziggo.nl>
  *
+ * Controls the following resource(s):
+ *  SidEngine
+ *  SidStereo (int (0-3), enabled extra SIDs)
+ *  SidResidSampling
+ *  SidFilters
+ *  SidStereoAddressStart
+ *  SidTripleAddressStart
+ *  SidQuadAddressStart
+ *
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
  *
@@ -29,6 +38,7 @@
 #include <gtk/gtk.h>
 
 #include "lib.h"
+#include "machine.h"
 #include "resources.h"
 #include "basewidgets.h"
 #include "debug_gtk3.h"
@@ -36,6 +46,8 @@
 #include "sidsoundwidget.h"
 
 
+/** \brief  Values for the "SidEngine" resource
+ */
 static ui_radiogroup_entry_t sid_engines[] = {
     { "FastSID", 0 },
     { "ReSID", 1 },
@@ -47,7 +59,84 @@ static ui_radiogroup_entry_t sid_engines[] = {
     { NULL, -1 }
 };
 
+/** \brief  Values for the "SidResidSampling" resource
+ */
+static ui_radiogroup_entry_t resid_sampling_modes[] = {
+    { "Fast", 0 },
+    { "Interpolation", 1 },
+    { "Resampling", 2 },
+    { "Fast resampling", 3 },
+    { NULL, -1 }
+};
 
+
+/** \brief  Values for the "number of sids" widget
+ */
+static ui_radiogroup_entry_t num_sids[] = {
+    { "One", 0 },
+    { "Two", 1 },
+    { "Three", 2 },
+    { "Four" , 3 },
+    { NULL, -1 }
+};
+
+
+/** \brief  I/O addresses for extra SID's for the C64
+ *
+ * \note    Yes, I know I can generate this table
+ */
+static ui_combo_entry_int_t sid_address_c64[] = {
+    { "$d420", 0xd420 }, { "$d440", 0xd440 }, { "$d460", 0xd460 },
+    { "$d480", 0xd480 }, { "$d4a0", 0xd4a0 }, { "$d4c0", 0xd4c0 },
+    { "$d4e0", 0xd4e0 },
+
+    { "$d500", 0xd500 }, { "$d520", 0xd520 }, { "$d540", 0xd540 },
+    { "$d560", 0xd560 }, { "$d580", 0xd580 }, { "$d5a0", 0xd5a0 },
+    { "$d5c0", 0xd5c0 }, { "$d5e0", 0xd5e0 },
+
+    { "$d600", 0xd600 }, { "$d620", 0xd620 }, { "$d640", 0xd640 },
+    { "$d660", 0xd660 }, { "$d680", 0xd680 }, { "$d6a0", 0xd6a0 },
+    { "$d6c0", 0xd6c0 }, { "$d6e0", 0xd6e0 },
+
+    { "$d700", 0xd700 }, { "$d720", 0xd720 }, { "$d740", 0xd740 },
+    { "$d760", 0xd760 }, { "$d780", 0xd780 }, { "$d7a0", 0xd7a0 },
+    { "$d7c0", 0xd7c0 }, { "$d7e0", 0xd7e0 },
+
+    { "$de00", 0xde00 }, { "$de20", 0xde20 }, { "$de40", 0xde40 },
+    { "$de60", 0xde60 }, { "$de80", 0xde80 }, { "$dea0", 0xdea0 },
+    { "$dec0", 0xdec0 }, { "$dee0", 0xdee0 },
+
+    { "$df00", 0xdf00 }, { "$df20", 0xdf20 }, { "$df40", 0xdf40 },
+    { "$df60", 0xdf60 }, { "$df80", 0xdf80 }, { "$dfa0", 0xdfa0 },
+    { "$dfc0", 0xdfc0 }, { "$dfe0", 0xdfe0 }
+};
+
+
+/** \brief  I/O addresses for extra SID's for the C128
+ */
+static ui_combo_entry_int_t sid_address_c128[] = {
+    { "$d420", 0xd420 }, { "$d440", 0xd440 }, { "$d460", 0xd460 },
+    { "$d480", 0xd480 }, { "$d4a0", 0xd4a0 }, { "$d4c0", 0xd4c0 },
+    { "$d4e0", 0xd4e0 },
+
+    { "$d700", 0xd700 }, { "$d720", 0xd720 }, { "$d740", 0xd740 },
+    { "$d760", 0xd760 }, { "$d780", 0xd780 }, { "$d7a0", 0xd7a0 },
+    { "$d7c0", 0xd7c0 }, { "$d7e0", 0xd7e0 },
+
+    { "$de00", 0xde00 }, { "$de20", 0xde20 }, { "$de40", 0xde40 },
+    { "$de60", 0xde60 }, { "$de80", 0xde80 }, { "$dea0", 0xdea0 },
+    { "$dec0", 0xdec0 }, { "$dee0", 0xdee0 },
+
+    { "$df00", 0xdf00 }, { "$df20", 0xdf20 }, { "$df40", 0xdf40 },
+    { "$df60", 0xdf60 }, { "$df80", 0xdf80 }, { "$dfa0", 0xdfa0 },
+    { "$dfc0", 0xdfc0 }, { "$dfe0", 0xdfe0 }
+};
+
+
+/** \brief  Create widget to control the SID engine
+ *
+ * \return  GtkGrid
+ */
 static GtkWidget *create_sid_engine_widget(void)
 {
     GtkWidget *grid;
@@ -73,12 +162,107 @@ static GtkWidget *create_sid_engine_widget(void)
 }
 
 
+/** \brief  Create widget to control ReSID sampling method
+ *
+ * \return  GtkGrid
+ */
+static GtkWidget *create_resid_sampling_widget(void)
+{
+    GtkWidget *grid;
+    GtkWidget *label;
+    GtkWidget *radio_group;
 
+    grid = gtk_grid_new();
+    g_object_set(grid, "margin-left", 8, NULL);
+
+    label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), "<b>ReSID sampling method</b>");
+    gtk_widget_set_halign(label, GTK_ALIGN_START);
+    g_object_set(label, "margin-bottom", 8, NULL);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
+
+    radio_group = resource_radiogroup_create("SidResidSampling",
+            resid_sampling_modes, GTK_ORIENTATION_VERTICAL);
+    g_object_set(radio_group, "margin-left", 16, NULL);
+    gtk_grid_attach(GTK_GRID(grid), radio_group, 0, 1, 1, 1);
+
+    gtk_widget_show_all(grid);
+    return grid;
+}
+
+
+/** \brief  Create widget to set the number of emulated SID's
+ *
+ * \return  GtkGrid
+ */
+static GtkWidget *create_num_sids_widget(void)
+{
+    GtkWidget *grid;
+    GtkWidget *label;
+    GtkWidget *radio_group;
+
+    grid = gtk_grid_new();
+    g_object_set(grid, "margin-left", 8, NULL);
+
+    label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), "<b>Number of SIDs</b>");
+    gtk_widget_set_halign(label, GTK_ALIGN_START);
+    g_object_set(label, "margin-bottom", 8, NULL);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
+
+    radio_group = resource_radiogroup_create("SidStereo",
+            num_sids, GTK_ORIENTATION_VERTICAL);
+    g_object_set(radio_group, "margin-left", 16, NULL);
+    gtk_grid_attach(GTK_GRID(grid), radio_group, 0, 1, 1, 1);
+
+    gtk_widget_show_all(grid);
+    return grid;
+}
+
+
+/** \brief  Create widget for extra SID addresses
+ *
+ * \param[in]   sid     extra SID number (1-3)
+ *
+ * \return  GtkGrid
+ */
+static GtkWidget *create_extra_sid_address_widget(int sid)
+{
+    GtkWidget *widget;
+    ui_combo_entry_int_t *entries;
+    char label[256];
+    const char *resource[3] = {
+        "SidStereoAddressStart",
+        "SidTripleAddressStart",
+        "SidQuadAddressStart"
+    };
+
+    g_snprintf(label, 256, "SID #%d address", sid + 1);
+    entries = machine_class == VICE_MACHINE_C128
+        ? sid_address_c128 : sid_address_c64;
+
+    widget = resource_combo_box_int_create_with_label(resource[sid - 1],
+            entries, label);
+    gtk_widget_show_all(widget);
+    return widget;
+}
+
+
+/** \brief  Create widget to control SID settings
+ *
+ * \param[in]   parent  parent widget
+ *
+ * \return  GtkGrid
+ */
 GtkWidget *sid_sound_widget_create(GtkWidget *parent)
 {
     GtkWidget *layout;
     GtkWidget *label;
     GtkWidget *engine;
+    GtkWidget *resid_sampling;
+    GtkWidget *num_sids;
+    GtkWidget *filters;
+    int i;
 
     layout = gtk_grid_new();
     gtk_grid_set_column_spacing(GTK_GRID(layout), 8);
@@ -91,6 +275,22 @@ GtkWidget *sid_sound_widget_create(GtkWidget *parent)
 
     engine = create_sid_engine_widget();
     gtk_grid_attach(GTK_GRID(layout), engine, 0, 1, 1,1);
+
+    resid_sampling = create_resid_sampling_widget();
+    gtk_grid_attach(GTK_GRID(layout), resid_sampling, 1, 1, 1, 1);
+
+    num_sids = create_num_sids_widget();
+    gtk_grid_attach(GTK_GRID(layout), num_sids, 2, 1, 1, 1);
+
+    for (i = 1; i < 4; i++) {
+        gtk_grid_attach(GTK_GRID(layout), create_extra_sid_address_widget(i),
+                i - 1, 2, 1, 1);
+    }
+
+    filters = resource_check_button_create("SidFilters",
+            "Enable SID filter emulation");
+    gtk_grid_attach(GTK_GRID(layout), filters, 0, 3, 3, 1);
+
 
     gtk_widget_show_all(layout);
     return layout;
