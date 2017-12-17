667c0571f37d1a6a1bb52df2e5d3b98626cd9f30
compyx <b.wassink@ziggo.nl>
2017-12-17 14:02:39 +0000

    Gtk3: Update ffmpeg settings widget
    Update video/audio codec lists based on selected format, add resource getters/setters.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@34188 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/ffmpegwidget.c
+++ vice/src/arch/gtk3/widgets/ffmpegwidget.c
@@ -1,8 +1,16 @@
-/** \file   ffmpegwidget.c
+/** \file   src/arch/gtk3/widgets/ffmpegwidget.c
  * \brief   FFMPEG media recording options dialog
  *
  * Written by
  *  Bas Wassink <b.wassink@ziggo.nl>
+ *
+ * Controls the following resource(s):
+ *  FFMPEGFormat
+ *  FFMPEGAudioBitrate
+ *  FFMPEGVideoBitrate
+ *  FFMPEGAudioCodec
+ *  FFMPEGVideoCodec
+ *  FFMPEGVideoHalveFramerate
  *
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
@@ -36,10 +44,6 @@
 #include "machine.h"
 #include "widgethelpers.h"
 #include "basewidgets.h"
-#include "basedialogs.h"
-#include "openfiledialog.h"
-#include "savefiledialog.h"
-#include "selectdirectorydialog.h"
 #include "ui.h"
 #include "gfxoutput.h"
 
@@ -53,41 +57,170 @@
  */
 static gfxoutputdrv_t *driver_info = NULL;
 
-
+/* references to combo boxes, used in various event handlers */
 static GtkWidget *format_widget = NULL;
 static GtkWidget *video_widget = NULL;
 static GtkWidget *audio_widget = NULL;
 
 
-/** \brief  Create a GtkListStore for the FFMPEG output formats
+static GtkListStore *create_video_model(int fmt);
+static GtkListStore *create_audio_model(int fmt);
+static void update_video_combo_box(int id);
+static void update_audio_combo_box(int id);
+
+
+/*****************************************************************************
+ *                              Event handlers                               *
+ ****************************************************************************/
+
+/** \brief  Handler for the "changed" event of the format combo box
+ *
+ * \param[in]   widget  format combo box
+ * \param[in]   data    extra event data (unused)
+ */
+static void on_format_changed(GtkWidget *widget, gpointer data)
+{
+    GtkListStore *video;
+    GtkListStore *audio;
+    GtkTreeIter iter;
+    int fmt_id;
+    const char *fmt_name = NULL;
+    int vc;
+    int ac;
+
+    fmt_id = gtk_combo_box_get_active(GTK_COMBO_BOX(widget));
+    /* convoluted way to get the displayed text of the combo box */
+    if (gtk_combo_box_get_active_iter(GTK_COMBO_BOX(widget), &iter)) {
+        GtkTreeModel *model = gtk_combo_box_get_model(GTK_COMBO_BOX(widget));
+        gtk_tree_model_get(model, &iter, 0, &fmt_name, -1);
+    }
+
+    debug_gtk3("called, index %d: '%s'\n", fmt_id, fmt_name);
+    if (fmt_name != NULL && fmt_name != '\0') {
+        resources_set_string("FFMPEGFormat", fmt_name);
+    }
+
+
+    video = create_video_model(fmt_id);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(video_widget), 0);
+    gtk_combo_box_set_model(GTK_COMBO_BOX(video_widget), GTK_TREE_MODEL(video));
+    if (resources_get_int("FFMPEGVideoCodec", &vc) < 0) {
+        vc = 0;
+    }
+    update_video_combo_box(vc);
+
+    audio = create_audio_model(fmt_id);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(audio_widget), 0);
+    gtk_combo_box_set_model(GTK_COMBO_BOX(audio_widget), GTK_TREE_MODEL(audio));
+    if (resources_get_int("FFMPEGAudioCodec", &ac) < 0) {
+        ac = 0;
+    }
+    update_audio_combo_box(ac);
+}
+
+
+/** \brief  Handler for the "changed" event of the video codec combo box
+ *
+ * \param[in]   combo   video codec combo box
+ * \param[in]   data    extra event data (unused)
+ */
+static void on_video_codec_changed(GtkComboBox *combo, gpointer data)
+{
+    GtkTreeModel *model;
+    GtkTreeIter iter;
+
+    if (gtk_combo_box_get_active(combo) < 0) {
+        return;
+    }
+
+    model = gtk_combo_box_get_model(combo);
+    if (gtk_combo_box_get_active_iter(combo, &iter)) {
+        int codec;
+
+        gtk_tree_model_get(model, &iter, 0, &codec, -1);
+        debug_gtk3("setting FFMPEGVideoCodec to %d\n", codec);
+        resources_set_int("FFMPEGVideoCodec", codec);
+    }
+}
+
+
+/** \brief  Handler for the "changed" event of the audio codec combo box
+ *
+ * \param[in]   combo   audio codec combo box
+ * \param[in]   data    extra event data (unused)
+ */
+static void on_audio_codec_changed(GtkComboBox *combo, gpointer data)
+{
+    GtkTreeModel *model;
+    GtkTreeIter iter;
+
+    if (gtk_combo_box_get_active(combo) < 0) {
+        return;
+    }
+
+    model = gtk_combo_box_get_model(combo);
+    if (gtk_combo_box_get_active_iter(combo, &iter)) {
+        int codec;
+
+        gtk_tree_model_get(model, &iter, 0, &codec, -1);
+        debug_gtk3("setting FFMPEGAudioCodec to %d\n", codec);
+        resources_set_int("FFMPEGAudioCodec", codec);
+    }
+}
+
+
+/*****************************************************************************
+ *                              Helper functions                             *
+ ****************************************************************************/
+
+/** \brief  Get index in format list of \a fmt
+ *
+ * \param[in]   fmt format name
+ *
+ * \return  index in list or -1 when not found
+ */
+static int get_format_index_by_name(const char *fmt)
+{
+    int i;
+
+    for (i = 0; driver_info->formatlist[i].name != NULL; i++) {
+        if (strcmp(driver_info->formatlist[i].name, fmt) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+/** \brief  Create a model for the FFMPEG output formats
  *
  * \return  GtkListStore
  */
-static GtkListStore *create_format_store(void)
+static GtkListStore *create_format_model(void)
 {
-    GtkListStore *store;
+    GtkListStore *model;
     GtkTreeIter iter;
     int i;
 
-    store = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_INT);
+    model = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_INT);
 
     for (i = 0; driver_info->formatlist[i].name != NULL; i++) {
         const char *name = driver_info->formatlist[i].name;
 
-        debug_gtk3("adding FFMPEG format '%s'\n", name);
-        gtk_list_store_append(store, &iter);
-        gtk_list_store_set(store, &iter, 0, name, 1, i, -1);
+        /*debug_gtk3("adding FFMPEG format '%s'\n", name);*/
+        gtk_list_store_append(model, &iter);
+        gtk_list_store_set(model, &iter, 0, name, 1, i, -1);
     }
 
-    return store;
+    return model;
 }
 
 
-/** \brief  Create a GtkListStore for the video codecs of \a fmt
+/** \brief  Create a model for the video codecs of \a fmt
  *
  * \return  GtkListStore
  */
-static GtkListStore *create_video_store(int fmt)
+static GtkListStore *create_video_model(int fmt)
 {
     GtkListStore *store;
     GtkTreeIter iter;
@@ -98,6 +231,9 @@ static GtkListStore *create_video_store(int fmt)
     codec_list = driver_info->formatlist[fmt].video_codecs;
 
     store = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_INT);
+    if (codec_list == NULL) {
+        return store;
+    }
 
     for (i = 0; codec_list[i].name != NULL; i++) {
         const char *name = codec_list[i].name;
@@ -116,7 +252,7 @@ static GtkListStore *create_video_store(int fmt)
  *
  * \return  GtkListStore
  */
-static GtkListStore *create_audio_store(int fmt)
+static GtkListStore *create_audio_model(int fmt)
 {
     GtkListStore *store;
     GtkTreeIter iter;
@@ -128,11 +264,15 @@ static GtkListStore *create_audio_store(int fmt)
 
     store = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_INT);
 
+    if (codec_list == NULL) {
+        return store;
+    }
+
     for (i = 0; codec_list[i].name != NULL; i++) {
         const char *name = codec_list[i].name;
         int id = codec_list[i].id;
 
-        debug_gtk3("adding FFMPEG audio codec '%s' (%d)\n", name, id);
+        /*debug_gtk3("adding FFMPEG audio codec '%s' (%d)\n", name, id);*/
         gtk_list_store_append(store, &iter);
         gtk_list_store_set(store, &iter, 0, name, 1, id, -1);
     }
@@ -164,13 +304,13 @@ static GtkWidget *create_indented_label(const gchar *text)
 static GtkWidget *create_format_combo_box(void)
 {
     GtkWidget *combo;
-    GtkListStore *store;
+    GtkListStore *model;
     GtkCellRenderer *renderer;
 
-    store = create_format_store();
-    combo = gtk_combo_box_new_with_model(GTK_TREE_MODEL(store));
+    model = create_format_model();
+    combo = gtk_combo_box_new_with_model(GTK_TREE_MODEL(model));
     /* combo takes ownership, so we can unref the store now */
-    g_object_unref(store);
+    g_object_unref(model);
 
     renderer = gtk_cell_renderer_text_new();
     gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(combo), renderer, TRUE);
@@ -192,16 +332,17 @@ static void update_format_combo_box(const char * fmt)
 
     /* get the model and get an iterator to its first element */
     model = gtk_combo_box_get_model(GTK_COMBO_BOX(format_widget));
-    gtk_tree_model_get_iter_first(model, &iter);
+    if (!gtk_tree_model_get_iter_first(model, &iter)) {
+        return;
+    }
 
     do {
         const gchar *s;
 
         gtk_tree_model_get(model, &iter, 0, &s, -1);
-
         if (strcmp(s, fmt) == 0) {
             gtk_combo_box_set_active(GTK_COMBO_BOX(format_widget), index);
-            break;
+            return;
         }
         index++;
     } while (gtk_tree_model_iter_next(model, &iter));
@@ -210,6 +351,67 @@ static void update_format_combo_box(const char * fmt)
     gtk_combo_box_set_active(GTK_COMBO_BOX(format_widget), 0);
 }
 
+
+/** \brief  Update the video codec combo box
+ *
+ * \param[in]   id  video codec ID
+ */
+static void update_video_combo_box(int id)
+{
+    GtkTreeModel *model;
+    GtkTreeIter iter;
+    int index = 0;
+
+    /* get the model and get an iterator to its first element */
+    model = gtk_combo_box_get_model(GTK_COMBO_BOX(video_widget));
+    if (!gtk_tree_model_get_iter_first(model, &iter)) {
+        return;
+    }
+
+    do {
+        int codec_id;
+
+        gtk_tree_model_get(model, &iter, 1, &codec_id, -1);
+        if (codec_id == id) {
+            gtk_combo_box_set_active(GTK_COMBO_BOX(video_widget), index);
+        }
+        index++;
+    } while (gtk_tree_model_iter_next(model, &iter));
+
+    /* not found, set index to 0 (shouldn't happen) */
+    gtk_combo_box_set_active(GTK_COMBO_BOX(video_widget), 0);
+}
+
+
+/** \brief  Update the audio codec combo box
+ *
+ * \param[in]   id  audio codec ID
+ */
+static void update_audio_combo_box(int id)
+{
+    GtkTreeModel *model;
+    GtkTreeIter iter;
+    int index = 0;
+
+    /* get the model and get an iterator to its first element */
+    model = gtk_combo_box_get_model(GTK_COMBO_BOX(audio_widget));
+    if (!gtk_tree_model_get_iter_first(model, &iter)) {
+        return;
+    }
+
+    do {
+        int codec_id;
+
+        gtk_tree_model_get(model, &iter, 1, &codec_id, -1);
+        if (codec_id == id) {
+            gtk_combo_box_set_active(GTK_COMBO_BOX(audio_widget), index);
+        }
+        index++;
+    } while (gtk_tree_model_iter_next(model, &iter));
+
+    /* not found, set index to 0 (shouldn't happen) */
+    gtk_combo_box_set_active(GTK_COMBO_BOX(audio_widget), 0);
+}
 
 
 /** \brief  Create combo box with supported video codecs for \a fmt
@@ -220,20 +422,19 @@ static void update_format_combo_box(const char * fmt)
 static GtkWidget *create_video_combo_box(int fmt)
 {
     GtkWidget *combo;
-    GtkListStore *store;
+    GtkListStore *model;
     GtkCellRenderer *renderer;
 
-    store = create_video_store(fmt);
-    combo = gtk_combo_box_new_with_model(GTK_TREE_MODEL(store));
+    model = create_video_model(fmt);
+    combo = gtk_combo_box_new_with_model(GTK_TREE_MODEL(model));
     /* combo takes ownership, so we can unref the store now */
-    g_object_unref(store);
+    g_object_unref(model);
 
     renderer = gtk_cell_renderer_text_new();
     gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(combo), renderer, TRUE);
     gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(combo), renderer,
             "text", 0, NULL);
 
-    /* XXX: replace with resource lookup */
     gtk_combo_box_set_active(GTK_COMBO_BOX(combo), 0);
     return combo;
 }
@@ -246,24 +447,28 @@ static GtkWidget *create_video_combo_box(int fmt)
 static GtkWidget *create_audio_combo_box(int fmt)
 {
     GtkWidget *combo;
-    GtkListStore *store;
+    GtkListStore *model;
     GtkCellRenderer *renderer;
 
-    store = create_audio_store(fmt);
-    combo = gtk_combo_box_new_with_model(GTK_TREE_MODEL(store));
+    model = create_audio_model(fmt);
+    combo = gtk_combo_box_new_with_model(GTK_TREE_MODEL(model));
     /* combo takes ownership, so we can unref the store now */
-    g_object_unref(store);
+    g_object_unref(model);
 
     renderer = gtk_cell_renderer_text_new();
     gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(combo), renderer, TRUE);
     gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(combo), renderer,
             "text", 0, NULL);
 
-    /* XXX: replace with resource lookup */
     gtk_combo_box_set_active(GTK_COMBO_BOX(combo), 0);
     return combo;
 }
 
+
+
+/*****************************************************************************
+ *                              Public functions                             *
+ ****************************************************************************/
 
 /** \brief  Create widget to control FFMPEG output options
  *
@@ -275,6 +480,9 @@ GtkWidget *ffmpeg_widget_create(void)
     GtkWidget *label;
     GtkWidget *fps;
     const char *current_format = NULL;
+    int fmt_index;
+    int current_vc;
+    int current_ac;
 
     /* retrieve FFMPEG driver info */
     driver_info = gfxoutput_get_driver("FFMPEG");
@@ -283,6 +491,8 @@ GtkWidget *ffmpeg_widget_create(void)
     if (resources_get_string("FFMPEGFormat", &current_format) < 0) {
         current_format = "avi"; /* hope this works out */
     }
+    /* get index in table of format */
+    fmt_index = get_format_index_by_name(current_format);
 
     grid = gtk_grid_new();
     gtk_grid_set_column_spacing(GTK_GRID(grid), 16);
@@ -297,15 +507,23 @@ GtkWidget *ffmpeg_widget_create(void)
 
     /* video codec selection */
     label = create_indented_label("video codec");
-    video_widget = create_video_combo_box(0);
+    video_widget = create_video_combo_box(fmt_index);
     gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), video_widget, 1, 1, 1, 1);
+    if (resources_get_int("FFMPEGVideoCodec", &current_vc) < 0) {
+        current_vc = 0;
+    }
+    update_video_combo_box(current_vc);
 
     /* audio codec selection */
     label = create_indented_label("audio codec");
-    audio_widget = create_audio_combo_box(0);
+    audio_widget = create_audio_combo_box(fmt_index);
     gtk_grid_attach(GTK_GRID(grid), label, 2, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), audio_widget, 3, 1, 1, 1);
+    if (resources_get_int("FFMPEGAudioCodec", &current_ac) < 0) {
+        current_ac = 0;
+    }
+    update_audio_combo_box(current_ac);
 
     /* video codec bitrate */
     label = create_indented_label("video bitrate");
@@ -331,6 +549,14 @@ GtkWidget *ffmpeg_widget_create(void)
     gtk_widget_set_halign(fps, GTK_ALIGN_START);
     g_object_set(fps, "margin-left", 16, NULL);
     gtk_grid_attach(GTK_GRID(grid), fps, 0, 3, 4, 1);
+
+    /* connect event handlers */
+    g_signal_connect(format_widget, "changed",
+            G_CALLBACK(on_format_changed), NULL);
+    g_signal_connect(video_widget, "changed",
+            G_CALLBACK(on_video_codec_changed), NULL);
+    g_signal_connect(audio_widget, "changed",
+            G_CALLBACK(on_audio_codec_changed), NULL);
 
     gtk_widget_show_all(grid);
     return grid;
