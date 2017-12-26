43a51b0178ac28a525278c9cd73469d2ef9e6d85
compyx <b.wassink@ziggo.nl>
2017-12-26 19:48:46 +0000

    Gtk3: Fix Double Quick Brown Box widget
    Disconnect 'enable' widget from the other widgets.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@34222 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/dqbbwidget.c
+++ vice/src/arch/gtk3/widgets/dqbbwidget.c
@@ -47,24 +47,6 @@
 #include "dqbbwidget.h"
 
 
-/* list of widgets, used to enable/disable depending on Double Quick Brown Box
- * resources */
-static GtkWidget *dqbb_enable_widget = NULL;
-static GtkWidget *dqbb_image = NULL;
-
-
-/** \brief  Handler for the "toggled" event of the dqbb_enable widget
- *
- * \param[in]   widget      check button
- * \param[in]   user_data   unused
- */
-static void on_enable_toggled(GtkWidget *widget, gpointer user_data)
-{
-    gboolean state = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
-
-    gtk_widget_set_sensitive(dqbb_image, state);
-}
-
 
 /** \brief  Create Double Quick Brown Box enable check button
  *
@@ -102,22 +84,18 @@ static GtkWidget *create_dqbb_image_widget(GtkWidget *parent)
 GtkWidget *dqbb_widget_create(GtkWidget *parent)
 {
     GtkWidget *grid;
+    GtkWidget *dqbb_enable;
+    GtkWidget *dqbb_image;
 
     grid = gtk_grid_new();
     gtk_grid_set_column_spacing(GTK_GRID(grid), 8);
     gtk_grid_set_row_spacing(GTK_GRID(grid), 8);
 
-    dqbb_enable_widget = create_dqbb_enable_widget();
-    gtk_grid_attach(GTK_GRID(grid), dqbb_enable_widget, 0, 0, 1, 1);
+    dqbb_enable = create_dqbb_enable_widget();
+    gtk_grid_attach(GTK_GRID(grid), dqbb_enable, 0, 0, 1, 1);
 
     dqbb_image = create_dqbb_image_widget(parent);
     gtk_grid_attach(GTK_GRID(grid), dqbb_image, 0, 1, 1, 1);
-
-    g_signal_connect(dqbb_enable_widget, "toggled", G_CALLBACK(on_enable_toggled),
-            NULL);
-
-    /* enable/disable widget based on dqbb-enable (dirty trick, I know) */
-    on_enable_toggled(dqbb_enable_widget, NULL);
 
     gtk_widget_show_all(grid);
     return grid;
