9f6acb35baf60fb5b59675c4e897f0291096c500
compyx <b.wassink@ziggo.nl>
2017-12-28 15:22:38 +0000

    Gtk3: Fix IEEE-488 adapter widget
    The widget now refuses to enable the adapter unless an EEPROM image is provided.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@34236 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/ieee488widget.c
+++ vice/src/arch/gtk3/widgets/ieee488widget.c
@@ -42,17 +42,40 @@
 #include "ieee488widget.h"
 
 
-static GtkWidget *entry = NULL;
-static GtkWidget *browse = NULL;
-
-
-
-static void on_enable_toggled(GtkWidget *widget, gpointer user_data)
+/** \brief  Handler for the "toggled" event of the 'enable' check button
+ *
+ * Toggles the 'enabled' state of the IEEE-488 adapter/cart, but only if an
+ * EEPROM image has been specified, otherwise when trying to set the check
+ * button to 'true', an error message is displayed and the check button is
+ * reverted to 'off'.
+ *
+ * \param[in,out]   widget  check button
+ * \param[in]       data    unused
+ */
+static void on_enable_toggled(GtkWidget *widget, gpointer data)
 {
-    int state = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
+    int state;
+    const char *image;
 
-    gtk_widget_set_sensitive(entry, state);
-    gtk_widget_set_sensitive(browse, state);
+    state = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
+    if (state) {
+        if (resources_get_string("IEEE488Image", &image) < 0) {
+            image = NULL;
+        }
+
+        if (image == NULL || *image == '\0') {
+            /* no image */
+            ui_message_error(widget, "VICE core",
+                    "Cannot enable IEEE-488 adapter, no image specified.");
+            gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(widget), FALSE);
+            state = 0;
+        }
+    }
+
+    if (resources_set_int("IEEE488", state) < 0) {
+        debug_gtk3("failed to set 'IEEE488' to '%s'", state ? "True" : "False");
+    }
+
 }
 
 
@@ -74,6 +97,14 @@ static void on_browse_clicked(GtkWidget *widget, gpointer user_data)
         GtkEntry *entry = GTK_ENTRY(user_data);
         debug_gtk3("setting IEEE488Image to '%s'\n", filename);
         gtk_entry_set_text(entry, filename);
+        /* required, since setting the text of the entry doesn't trigger an
+         * update of the connected resource (it only responds to focus-out and
+         * pressing 'Enter' */
+        if (resources_set_string("IEEE488Image", filename) < 0) {
+            ui_message_error(widget, "VICE core",
+                    "Failed to set '%s' as IEEE-488 EEPROM image.",
+                    filename);
+        }
         g_free(filename);
     }
 }
@@ -88,20 +119,39 @@ static void on_browse_clicked(GtkWidget *widget, gpointer user_data)
 GtkWidget *ieee488_widget_create(GtkWidget *parent)
 {
     GtkWidget *grid;
-    GtkWidget *enable;
+    GtkWidget *enable_widget;
     GtkWidget *label;
+    GtkWidget *entry;
+    GtkWidget *browse;
+
+    const char *image;
+    int enable_state;
+
+    if (resources_get_string("IEEE488Image", &image) < 0) {
+        image = NULL;
+    }
+    if (resources_get_int("IEEE488", &enable_state) < 0) {
+        enable_state = 0;
+    }
 
     grid = gtk_grid_new();
     gtk_grid_set_column_spacing(GTK_GRID(grid), 16);
     gtk_grid_set_row_spacing(GTK_GRID(grid), 8);
 
-    enable = resource_check_button_create("IEEE488",
-            "Enable IEEE-488 interface");
-    gtk_grid_attach(GTK_GRID(grid), enable, 0, 0, 3, 1);
+    /* we can't use a `resource_check_button` here, since toggling the resource
+     * depends on whether an image file is specified
+     */
+    enable_widget = gtk_check_button_new_with_label("Enable IEEE-488 interface");
+    gtk_grid_attach(GTK_GRID(grid), enable_widget, 0, 0, 3, 1);
 
-    label = gtk_label_new("IEEE-488 ROM");
+    /* only set state to true if both the state is true and an image is given */
+    if (enable_state && (image != NULL && *image != '\0')) {
+        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(enable_widget), TRUE);
+    }
+
+    label = gtk_label_new("IEEE-488 image");
     gtk_widget_set_halign(label, GTK_ALIGN_START);
-    entry = resource_entry_create("IEEE488Image");
+    entry = resource_entry_full_create("IEEE488Image");
     gtk_widget_set_hexpand(entry, TRUE);
     browse = gtk_button_new_with_label("Browse ...");
     g_signal_connect(browse, "clicked", G_CALLBACK(on_browse_clicked),
@@ -111,10 +161,7 @@ GtkWidget *ieee488_widget_create(GtkWidget *parent)
     gtk_grid_attach(GTK_GRID(grid), entry, 1, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), browse, 2, 1, 1, 1);
 
-    g_signal_connect(enable, "toggled", G_CALLBACK(on_enable_toggled), NULL);
-
-    /*hack*/
-    on_enable_toggled(enable, NULL);
+    g_signal_connect(enable_widget, "toggled", G_CALLBACK(on_enable_toggled), NULL);
 
     gtk_widget_show_all(grid);
     return grid;
