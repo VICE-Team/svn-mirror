c37c1ebf74c9e094469c1b178d52da93874d2bee
compyx <b.wassink@ziggo.nl>
2018-01-31 15:32:40 +0000

    Gtk3: RS232 settings: add Userport RS232 emulation widgets
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@34387 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/settings_rs232.c
+++ vice/src/arch/gtk3/widgets/settings_rs232.c
@@ -9,6 +9,9 @@
  *  Acia1Base       (x64/x64sc/xscpu64/x128/xvic)
  *  Acia1Irq        (x64/x64sc/xscpu64/x128/xvic)
  *  Acia1Mode       (x64/x64sc/xscpu64/x128/xvic)
+ *  RsUserEnable    (x64/x64sc/xscpu64/x128/xvic)
+ *  RsUserBaud      (x64/x64sc/xscpu64/x128/xvic)
+ *  RsUserDev       (x64/x64sc/xscpu64/x128/xvic)
  */
 
 /*
@@ -45,6 +48,8 @@
 
 
 /** \brief  List of ACIA devices
+ *
+ * \note    Also used by the Userport RS232 Device list combo box
  */
 static const vice_gtk3_combo_entry_int_t acia_devices[] = {
     { "Serial 1", 0 },
@@ -99,6 +104,19 @@ static const vice_gtk3_radiogroup_entry_t acia_modes[] = {
     { "Normal",     0 },
     { "Swiftlink",  1 },
     { "Turbo232",   2 },
+    { NULL, -1 }
+};
+
+
+/** \brief  List of user-port RS232 baud rates (x64/x64sc/xscpu64/x128/xvic)
+ */
+static const vice_gtk3_combo_entry_int_t rsuser_baud_rates[] = {
+    { "300",    300 },
+    { "600",    600 },
+    { "1200",   1200 },
+    { "2400",   2400 },
+    { "4800",   4800 },
+    { "9600",   9600 },
     { NULL, -1 }
 };
 
@@ -274,7 +292,7 @@ static GtkWidget *create_acia_widget(void)
     }
 
     /* Acia1Dev */
-    label = create_indented_label("ACIA device");
+    label = create_indented_label("Device");
     acia_device_widget = vice_gtk3_resource_combo_box_int_create(
             "Acia1Dev", acia_devices);
     gtk_grid_attach(GTK_GRID(grid), label, 0, row, 1, 1);
@@ -284,7 +302,7 @@ static GtkWidget *create_acia_widget(void)
     /* Acia1Base */
     acia_base_widget = create_acia_base_widget();
     if (acia_base_widget != NULL) {
-        label = create_indented_label("ACIA base address");
+        label = create_indented_label("Base address");
         gtk_grid_attach(GTK_GRID(grid), label, 0, row, 1, 1);
         gtk_grid_attach(GTK_GRID(grid), acia_base_widget, 1, row, 1, 1);
         row++;
@@ -293,7 +311,7 @@ static GtkWidget *create_acia_widget(void)
     /* Acia1Irq */
     acia_irq_widget = create_acia_irq_widget();
     if (acia_irq_widget != NULL) {
-        label = create_indented_label("ACIA IRQ");
+        label = create_indented_label("IRQ");
         gtk_grid_attach(GTK_GRID(grid), label, 0, row, 1, 1);
         gtk_grid_attach(GTK_GRID(grid), acia_irq_widget, 1, row, 1, 1);
         row++;
@@ -302,11 +320,56 @@ static GtkWidget *create_acia_widget(void)
     /* Acia1Mode*/
     acia_mode_widget = create_acia_mode_widget();
     if (acia_mode_widget != NULL) {
-        label = create_indented_label("ACIA emulation mode");
+        label = create_indented_label("Emulation mode");
         gtk_grid_attach(GTK_GRID(grid), label, 0, row, 1, 1);
         gtk_grid_attach(GTK_GRID(grid), acia_mode_widget, 1, row, 1, 1);
         row++;
     }
+
+
+    gtk_widget_show_all(grid);
+    return grid;
+}
+
+
+/** \brief  Create user-port RS233 emulation settings widget
+ *
+ * \note    C64,C128 and VIC-20 only
+ *
+ * \return  GtkGrid
+ */
+static GtkWidget *create_userport_widget(void)
+{
+    GtkWidget *grid;
+    GtkWidget *label;
+    GtkWidget *rsuser_enable_widget;
+    GtkWidget *rsuser_baud_widget;
+    GtkWidget *rsuser_device_widget;
+
+    grid = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, VICE_GTK3_DEFAULT);
+    label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), "<b>Userport RS232 settings</b>");
+    gtk_widget_set_halign(label, GTK_ALIGN_START);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 2, 1);
+
+    rsuser_enable_widget = vice_gtk3_resource_check_button_create(
+            "RsUserEnable", "Enable Userport RS232 emulation");
+    gtk_widget_set_halign(rsuser_enable_widget, GTK_ALIGN_START);
+    g_object_set(rsuser_enable_widget, "margin-left", 16, NULL);
+    gtk_grid_attach(GTK_GRID(grid), rsuser_enable_widget, 0, 1, 2, 1);
+
+    label = create_indented_label("Baud rate");
+    rsuser_baud_widget = vice_gtk3_resource_combo_box_int_create(
+            "RsUserBaud", rsuser_baud_rates);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), rsuser_baud_widget, 1, 2, 1, 1);
+
+    label = create_indented_label("Device");
+    rsuser_device_widget = vice_gtk3_resource_combo_box_int_create(
+            "RsUserDev", acia_devices);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 3, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), rsuser_device_widget, 1, 3, 1, 1);
+
 
 
     gtk_widget_show_all(grid);
@@ -325,10 +388,27 @@ static GtkWidget *create_acia_widget(void)
 GtkWidget *settings_rs232_widget_create(GtkWidget *parent)
 {
     GtkWidget *grid;
+    GtkWidget *userport_widget;
+    int row = 1;
 
-    grid = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, VICE_GTK3_DEFAULT);
+    grid = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, 32);
     gtk_grid_attach(GTK_GRID(grid), create_acia_widget(), 0, 0, 1, 1);
     gtk_widget_show_all(grid);
+
+    switch (machine_class) {
+        case VICE_MACHINE_C64:      /* fall through */
+        case VICE_MACHINE_C64SC:    /* fall through */
+        case VICE_MACHINE_SCPU64:   /* fall through */
+        case VICE_MACHINE_C128:     /* fall through */
+        case VICE_MACHINE_VIC20:
+            userport_widget = create_userport_widget();
+            gtk_grid_attach(GTK_GRID(grid), userport_widget, 0, row, 1, 1);
+            row++;
+            break;
+        default:
+            break;
+    }
+
     return grid;
 }
 
