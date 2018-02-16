6c5338f3dae65f9ccf64d41ec7f3f0d5d708c4e4
blackystardust <blackystardust68@yahoo.com>
2018-02-16 17:15:03 +0000

    Make (windows) msys console monitor mode work for gtk3.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@34470 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/uimon-fallback.c
+++ vice/src/arch/gtk3/uimon-fallback.c
@@ -34,12 +34,18 @@
 #include <dirent.h>
 #include <ctype.h>
 #include <unistd.h>
+
 #ifdef HAVE_SYS_IOCTL_H
 #include <sys/ioctl.h>
 #endif
 
 #if (defined(sun) || defined(__sun)) && (defined(__SVR4) || defined(__svr4__))
 #include <sys/stat.h>
+#endif
+
+#ifdef WIN32_COMPILE
+#include <windows.h>
+#include <winternl.h>
 #endif
 
 #include "console.h"
@@ -74,18 +80,91 @@ int console_out(console_t *log, const char *format, ...)
 }
 #endif
 
+#ifdef WIN32_COMPILE
+static int vice_ismsystty(int fd)
+{
+    intptr_t h_stdin = _get_osfhandle(fd);
+    char ntfn_bytes[sizeof(OBJECT_NAME_INFORMATION) + 256 * sizeof(WCHAR)];
+    OBJECT_NAME_INFORMATION *ntfn = (OBJECT_NAME_INFORMATION*) ntfn_bytes;
+    NTSTATUS status;
+    ULONG ntfn_size = sizeof(ntfn_bytes);
+    USHORT i, l;
+    wchar_t c, *s0;
+
+    memset(ntfn, 0, ntfn_size);
+    status = NtQueryObject((HANDLE)h_stdin, ObjectNameInformation, ntfn, ntfn_size, &ntfn_size);
+
+    if (!NT_SUCCESS(status)) {
+        return 0;
+    }
+
+    l = ntfn->Name.Length;
+    s0 = ntfn->Name.Buffer;
+    /* Check for "\Device\NamedPipe" */
+    {
+        USHORT l1 = l;
+        wchar_t *s1 = s0;
+        wchar_t expect[] = L"\\Device\\NamedPipe\\";
+
+        if (s0[0] == '\\' && s0[1] == '\\' && s0[2] == '?' && s0[3] == '\\') {
+            l1 -= 4;
+            s1 += 4;
+        }
+        for (i = 0; i < l1; i++) {
+            wchar_t e = expect[i];
+            c = s1[i];
+            if (!e) {
+                break;
+            }
+            if (c != e) {
+                return 0;
+            }
+        }
+    }
+    /* Look for "-pty%d-" */
+    for (i = 0; i < l; i++) {
+        c = s0[i];
+        if (c == '-') {
+            wchar_t *s = s0 + i + 1;
+            if (s[0] == 'p' && s[1] == 't' && s[2] == 'y' && (c = s[3]) && (c >= '0') && (c <= '9'))
+            {
+                s += 4;
+                while ((c = *s) && (c >= '0') && (c <= '9')) {
+                    s++;
+                }
+                if (c == '-' || c == 0) {
+                    return 1;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+static int vice_isatty(int fd)
+{
+    if (!isatty(fileno(stdin))) {
+        return vice_ismsystty(fileno(stdin));
+    }
+    return 1;
+}
+#else
+#define vice_isatty isatty
+#endif
+
 console_t *uimon_window_open(void)
 {
 #ifdef HAVE_SYS_IOCTL_H
     struct winsize w;
 #endif
 
-    if (!isatty(fileno(stdin))) {
+    if (!vice_isatty(fileno(stdin))) {
         log_error(LOG_DEFAULT, "console_open: stdin is not a tty.");
         console_log_local = NULL;
         return NULL;
     }
-    if (!isatty(fileno(stdout))) {
+    if (!vice_isatty(fileno(stdout))) {
         log_error(LOG_DEFAULT, "console_open: stdout is not a tty.");
         console_log_local = NULL;
         return NULL;
