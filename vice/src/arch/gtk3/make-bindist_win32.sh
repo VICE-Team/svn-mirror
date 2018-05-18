33576ac8041506d5ca50d091d17db583e65816c4
compyx <b.wassink@ziggo.nl>
2018-05-18 16:36:18 +0000

    Gtk3: apply the Windows make bindist[zip] patch by Greg King
    I had to change some rm's in rm -f and remove -e from the $(SHELL) -e in the
    Makefile.am to make it work on my Windows VM (building docs fails on that one,
    will fix it)
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@34824 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/make-bindist_win32.sh
+++ vice/src/arch/gtk3/make-bindist_win32.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 #
-# make-bindist.sh - make binary distribution for the windows GTK3 port
+# make-bindist_win32.sh -- Make a binary distribution for the Windows GTK3 port.
 #
 # Written by
 #  Marco van den Heuvel <blackystardust68@yahoo.com>
+#  Greg King <gregdk@users.sf.net>
 #
 # This file is part of VICE, the Versatile Commodore Emulator.
 # See README for copyright notice.
@@ -24,88 +25,96 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 #  02111-1307  USA.
 #
-# Usage: make-bindist.sh <strip> <vice-version> <--enable-arch> <zip|nozip> <x64sc-included> <top-srcdir> <cpu>
-#                         $1      $2             $3              $4          $5               $6           $7
+# Usage: make-bindist.sh <strip> <vice-version> <--enable-arch> <zip|nozip> <x64sc-included> <top-srcdir> <cpu> <abs-top-builddir>
+#                         $1      $2             $3              $4          $5               $6           $7    $8
 #
 
 STRIP=$1
 VICEVERSION=$2
 ENABLEARCH=$3
 ZIPKIND=$4
-X64SC=$5
 TOPSRCDIR=$6
-CPU=$7
-
-if test x"$X64SC" = "xyes"; then
-  SCFILE="x64sc"
-else
-  SCFILE=""
+TOPBUILDDIR=$8
+if test x"$7" = "xx86_64" -o x"$7" = "xamd64"
+then WINXX="win64"
+else WINXX="win32"
+fi
+if test x"$5" = "xyes"
+then SCFILE="x64sc"
+else SCFILE=""
 fi
 
 EMULATORS="x64 xscpu64 x64dtv $SCFILE x128 xcbm2 xcbm5x0 xpet xplus4 xvic vsid"
 CONSOLE_TOOLS="c1541 cartconv petcat"
 EXECUTABLES="$EMULATORS $CONSOLE_TOOLS"
+unset CONSOLE_TOOLS EMULATORS SCFILE
 
-for i in $EXECUTABLES
-do
-  if [ ! -e src/$i.exe ]
-  then
-    echo Error: executable file\(s\) not found, do a \"make\" first
+for i in $EXECUTABLES; do
+  if [ ! -x $TOPBUILDDIR/src/$i.exe ]; then
+    echo 'Error: executable files not found; do a "make" first.'
     exit 1
   fi
 done
 
-if test x"$CPU" = "xx86_64" -o x"$CPU" = "xamd64"; then
-  WINXX="win64"
-else
-  WINXX="win32"
-fi
-
-echo Generating $WINXX GTK3 port binary distribution.
 GTK3NAME="GTK3VICE"
+BUILDPATH="$TOPBUILDDIR/$GTK3NAME-$VICEVERSION-$WINXX"
 
-rm -f -r $GTK3NAME-$VICEVERSION-$WINXX
-mkdir $GTK3NAME-$VICEVERSION-$WINXX
-for i in $EXECUTABLES
-do
-  $STRIP src/$i.exe
-  cp src/$i.exe $GTK3NAME-$VICEVERSION-$WINXX
+echo "Removing an old $BUILDPATH ..."
+rm -r -f $BUILDPATH
+echo "Generating a $WINXX GTK3 port binary distribution..."
+mkdir $BUILDPATH
+
+for i in $EXECUTABLES; do
+  cp $TOPBUILDDIR/src/$i.exe $BUILDPATH
+  $STRIP $BUILDPATH/$i.exe
 done
-cp -a $TOPSRCDIR/data/C128 $TOPSRCDIR/data/C64 $GTK3NAME-$VICEVERSION-$WINXX
-cp -a $TOPSRCDIR/data/C64DTV $TOPSRCDIR/data/CBM-II $GTK3NAME-$VICEVERSION-$WINXX
-cp -a $TOPSRCDIR/data/DRIVES $TOPSRCDIR/data/PET $GTK3NAME-$VICEVERSION-$WINXX
-cp -a $TOPSRCDIR/data/PLUS4 $TOPSRCDIR/data/PRINTER $GTK3NAME-$VICEVERSION-$WINXX
-cp -a $TOPSRCDIR/data/SCPU64 $TOPSRCDIR/data/VIC20 $GTK3NAME-$VICEVERSION-$WINXX
-cp -a $TOPSRCDIR/doc/html $GTK3NAME-$VICEVERSION-$WINXX
-rm $GTK3NAME-$VICEVERSION-$WINXX/html/checklinks.sh
-cp $TOPSRCDIR/FEEDBACK $TOPSRCDIR/README $GTK3NAME-$VICEVERSION-$WINXX
-cp $TOPSRCDIR/COPYING $TOPSRCDIR/NEWS $GTK3NAME-$VICEVERSION-$WINXX
-cp $TOPSRCDIR/doc/readmes/Readme-GTK3.txt $GTK3NAME-$VICEVERSION-$WINXX
-rm `find $GTK3NAME-$VICEVERSION-$WINXX -name "Makefile*"`
-rm `find $GTK3NAME-$VICEVERSION-$WINXX -name "amiga_*.vkm"`
-rm `find $GTK3NAME-$VICEVERSION-$WINXX -name "dos_*.vkm"`
-rm `find $GTK3NAME-$VICEVERSION-$WINXX -name "osx*.vkm"`
-rm `find $GTK3NAME-$VICEVERSION-$WINXX -name "beos_*.vkm"`
-rm `find $GTK3NAME-$VICEVERSION-$WINXX -name "sdl_*.vkm"`
-rm `find $GTK3NAME-$VICEVERSION-$WINXX -name "x11_*.vkm"`
-rm `find $GTK3NAME-$VICEVERSION-$WINXX -name "win*.v*"`
-rm `find $GTK3NAME-$VICEVERSION-$WINXX -name "*.vsc"`
-rm $GTK3NAME-$VICEVERSION-$WINXX/html/texi2html
-mkdir $GTK3NAME-$VICEVERSION-$WINXX/doc
-cp $TOPSRCDIR/doc/vice.chm $GTK3NAME-$VICEVERSION-$WINXX/doc
-cp $TOPSRCDIR/doc/vice.hlp $GTK3NAME-$VICEVERSION-$WINXX/doc
-cp $TOPSRCDIR/doc/vice.pdf $GTK3NAME-$VICEVERSION-$WINXX/doc
+
+# The following lines assume that this script is run by MSYS2.
+cp `ntldd -R $BUILDPATH/x64.exe|gawk '/\\\\bin\\\\/{print $3;}'|cygpath -f -` $BUILDPATH
+cp $MINGW_PREFIX/bin/lib{croco-0.6-3,lzma-5,rsvg-2-2,xml2-2}.dll $BUILDPATH
+cd $MINGW_PREFIX
+cp --parents lib/gdk-pixbuf-2.0/2.*/loaders.cache lib/gdk-pixbuf-2.0/2.*/loaders/libpixbufloader-{png,svg,xpm}.dll $BUILDPATH
+cp --parents share/glib-2.0/schemas/{gschema*,org.gtk.Settings.FileChooser.gschema.xml} $BUILDPATH
+cp --parents -a share/icons/Adwaita $BUILDPATH
+cd - >/dev/null
+
+cp -a $TOPSRCDIR/data/C128 $TOPSRCDIR/data/C64 $BUILDPATH
+cp -a $TOPSRCDIR/data/C64DTV $TOPSRCDIR/data/CBM-II $BUILDPATH
+cp -a $TOPSRCDIR/data/DRIVES $TOPSRCDIR/data/PET $BUILDPATH
+cp -a $TOPSRCDIR/data/PLUS4 $TOPSRCDIR/data/PRINTER $BUILDPATH
+cp -a $TOPSRCDIR/data/SCPU64 $TOPSRCDIR/data/VIC20 $BUILDPATH
+rm -f `find $BUILDPATH -name "Makefile*"`
+rm -f `find $BUILDPATH -name "amiga_*.vkm"`
+rm -f `find $BUILDPATH -name "dos_*.vkm"`
+rm -f `find $BUILDPATH -name "osx*.vkm"`
+rm -f `find $BUILDPATH -name "beos_*.vkm"`
+rm -f `find $BUILDPATH -name "sdl_*.v*"`
+rm -f `find $BUILDPATH -name "x11_*.vkm"`
+rm -f `find $BUILDPATH -name "win*.v*"`
+#cp $TOPSRCDIR/data/gtk3-vice-logo.svg $BUILDPATH
+cp -a $TOPSRCDIR/doc/html $BUILDPATH
+rm -f $BUILDPATH/html/Makefile* $BUILDPATH/html/checklinks.sh $BUILDPATH/html/texi2html
+rm -f $BUILDPATH/html/robots.txt $BUILDPATH/html/sitemap.xml
+rm -f $BUILDPATH/html/COPYING $BUILDPATH/html/NEWS
+cp $TOPSRCDIR/COPYING $TOPSRCDIR/FEEDBACK $TOPSRCDIR/NEWS $TOPSRCDIR/README $BUILDPATH
+cp $TOPSRCDIR/doc/readmes/Readme-GTK3.txt $BUILDPATH
+mkdir $BUILDPATH/doc
+cp $TOPSRCDIR/doc/vice.{chm,hlp,pdf} $BUILDPATH/doc
 if test x"$ZIPKIND" = "xzip"; then
-  if test x"$ZIP" = "x"; then
-    zip -r -9 -q $GTK3NAME-$VICEVERSION-$WINXX.zip $GTK3NAME-$VICEVERSION-$WINXX
-  else
-    $ZIP $GTK3NAME-$VICEVERSION-$WINXX.zip $GTK3NAME-$VICEVERSION-$WINXX
+  rm -f $BUILDPATH.zip
+  cd $BUILDPATH/..
+  if test x"$ZIP" = "x"
+  then zip -r -9 -q $BUILDPATH.zip $GTK3NAME-$VICEVERSION-$WINXX
+  else $ZIP $BUILDPATH.zip $GTK3NAME-$VICEVERSION-$WINXX
   fi
-  rm -f -r $GTK3NAME-$VICEVERSION-$WINXX
-  echo $WINXX GTK3 port binary distribution archive generated as $GTK3NAME-$VICEVERSION-$WINXX.zip
-else
-  echo $WINXX GTK3 port binary distribution directory generated as $GTK3NAME-$VICEVERSION-$WINXX
+  rm -r -f $BUILDPATH
+  echo "$WINXX GTK3 port binary distribution archive generated as $BUILDPATH.zip"
+else echo "$WINXX GTK3 port binary distribution directory generated as $BUILDPATH"
 fi
 if test x"$ENABLEARCH" = "xyes"; then
-  echo Warning: binaries are optimized for your system and might not run on a different system, use --enable-arch=no to avoid this
+  echo ''
+  echo 'Warning: The binaries are optimized for your system.'
+  echo 'They might not run on a different system.'
+  echo 'Configure with --enable-arch=no to avoid that.'
+  echo ''
 fi
