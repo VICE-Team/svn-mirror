609ff2554f42f13a5e017daf5977121f1e954e68
compyx <b.wassink@ziggo.nl>
2018-06-01 15:08:58 +0000

    Gtk3: implement Windows archdep_spawn()
    Stolen from arc/sdl/archdep_win32.c and untested.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@34969 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/archdep_win32.c
+++ vice/src/arch/gtk3/archdep_win32.c
@@ -206,11 +206,81 @@ void archdep_shutdown(void)
     INCOMPLETE_IMPLEMENTATION();
 }
 
+
+/** \brief  Spawn new process
+ *
+ * Shamelessly stolen from arch/sdl/archdep_win32.c
+ */
 int archdep_spawn(const char *name, char **argv, char **pstdout_redir, const char *stderr_redir)
 {
-    /* Only zfile uses this */
-    NOT_IMPLEMENTED();
-    return 0;
+    int new_stdout, new_stderr;
+    int old_stdout_mode, old_stderr_mode;
+    int old_stdout, old_stderr;
+    int retval;
+    char *stdout_redir = NULL;
+
+    if (pstdout_redir != NULL) {
+        if (*pstdout_redir == NULL) {
+            *pstdout_redir = archdep_tmpnam();
+        }
+        stdout_redir = *pstdout_redir;
+    }
+
+    new_stdout = new_stderr = old_stdout = old_stderr = -1;
+
+    /* Make sure we are in binary mode.  */
+    old_stdout_mode = _setmode(STDOUT_FILENO, _O_BINARY);
+    old_stderr_mode = _setmode(STDERR_FILENO, _O_BINARY);
+
+    /* Redirect stdout and stderr as requested, saving the old
+       descriptors.  */
+    if (stdout_redir != NULL) {
+        old_stdout = _dup(STDOUT_FILENO);
+        new_stdout = _open(stdout_redir, _O_WRONLY | _O_TRUNC | _O_CREAT, _S_IWRITE | _S_IREAD);
+        if (new_stdout == -1) {
+            log_error(LOG_DEFAULT, "open(\"%s\") failed: %s.", stdout_redir, strerror(errno));
+            retval = -1;
+            goto cleanup;
+        }
+        _dup2(new_stdout, STDOUT_FILENO);
+    }
+    if (stderr_redir != NULL) {
+        old_stderr = _dup(STDERR_FILENO);
+        new_stderr = _open(stderr_redir, _O_WRONLY | _O_TRUNC | _O_CREAT, _S_IWRITE | _S_IREAD);
+        if (new_stderr == -1) {
+            log_error(LOG_DEFAULT, "open(\"%s\") failed: %s.", stderr_redir, strerror(errno));
+            retval = -1;
+            goto cleanup;
+        }
+        _dup2(new_stderr, STDERR_FILENO);
+    }
+
+    /* Spawn the child process.  */
+    retval = (int)_spawnvp(_P_WAIT, name, (const char **)argv);
+
+cleanup:
+    if (old_stdout >= 0) {
+        _dup2(old_stdout, STDOUT_FILENO);
+        _close(old_stdout);
+    }
+    if (old_stderr >= 0) {
+        _dup2(old_stderr, STDERR_FILENO);
+        _close(old_stderr);
+    }
+    if (old_stdout_mode >= 0) {
+        _setmode(STDOUT_FILENO, old_stdout_mode);
+    }
+    if (old_stderr_mode >= 0) {
+        _setmode(STDERR_FILENO, old_stderr_mode);
+    }
+    if (new_stdout >= 0) {
+        _close(new_stdout);
+    }
+    if (new_stderr >= 0) {
+        _close(new_stderr);
+    }
+
+    return retval;
 }
 
 
