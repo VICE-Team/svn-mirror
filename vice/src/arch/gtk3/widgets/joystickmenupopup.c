88121904365f8dc6e832c25f4e4cc17453738809
compyx <b.wassink@ziggo.nl>
2018-07-30 15:57:05 +0000

    Gtk3:	Make the joystick popup menu consistent
    A) Only show menu items when they are possible
    2) Only enable "show userport joysticks" when an adapter is active
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@35329 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/joystickmenupopup.c
+++ vice/src/arch/gtk3/widgets/joystickmenupopup.c
@@ -32,9 +32,97 @@
 
 #include <gtk/gtk.h>
 #include "debug_gtk3.h"
+#include "machine.h"
+#include "resources.h"
 #include "uicommands.h"
 
 #include "joystickmenupopup.h"
+
+
+/** \brief  Determine if joystick swapping is possible
+ *
+ * \return  bool
+ */
+static gboolean joystick_swap_possible(void)
+{
+    switch (machine_class) {
+
+        /* always TRUE */
+        case VICE_MACHINE_C64:      /* fall through */
+        case VICE_MACHINE_C64SC:    /* fall through */
+        case VICE_MACHINE_C128:     /* fall through */
+        /* x64dtv emulates the second joystick hack by degault */
+        case VICE_MACHINE_C64DTV:   /* fall through */
+        case VICE_MACHINE_SCPU64:   /* fall through */
+        case VICE_MACHINE_PLUS4:    /* fall through */
+        case VICE_MACHINE_CBM5x0:
+            return TRUE;
+
+        /* always FALSE */
+        case VICE_MACHINE_VIC20:    /* fall through */
+        case VICE_MACHINE_PET:      /* fall through */
+        case VICE_MACHINE_CBM6x0:
+            return FALSE;
+
+        /* should never get here */
+        default:
+            return FALSE;
+    }
+}
+
+
+/** \brief  Determine if userport joystick swapping is possible
+ *
+ * This function only checks if userport joystick swapping is possible,
+ * ignoring if an adapter is active to actually allow userport joysticks (and
+ * thus swapping)
+ *
+ * \return  bool
+ */
+static gboolean userport_joystick_swap_possible(void)
+{
+    switch (machine_class) {
+        /* these all support userport joystick adapters with two ports */
+        case VICE_MACHINE_C64:      /* fall through */
+        case VICE_MACHINE_C64SC:    /* fall through */
+        case VICE_MACHINE_C128:     /* fall through */
+        case VICE_MACHINE_SCPU64:   /* fall through */
+        case VICE_MACHINE_PLUS4:    /* fall through */
+        case VICE_MACHINE_VIC20:    /* fall through */
+        case VICE_MACHINE_PET:      /* fall through */
+        case VICE_MACHINE_CBM6x0:
+            return TRUE;
+
+        case VICE_MACHINE_C64DTV:
+            /* DTV only supports a hack for a single userport joystick */
+            return FALSE;
+
+        case VICE_MACHINE_CBM5x0:
+            /* CBM-II 5x0 models don't support userport joystick adapters */
+            return FALSE;
+
+        default:
+            /* shouldn't get here */
+            return FALSE;
+    }
+}
+
+
+/** \brief  Determine if a userport joystick adapter is enabled
+ *
+ * \return  bool
+ */
+static gboolean userport_joystick_adapter_enabled(void)
+{
+    int enabled;
+
+    if (resources_get_int("UserportJoy", &enabled) < 0) {
+        debug_gtk3("failed to get value for resource 'UserportJoy',"
+                " assuming FALSE");
+        enabled = 0;
+    }
+    return (gboolean)enabled;
+}
 
 
 /** \brief  Create joystick menu popup for the statusbar
@@ -50,16 +138,22 @@ GtkWidget *joystick_menu_popup_create(void)
 
     menu = gtk_menu_new();
 
-    item = gtk_menu_item_new_with_label("Swap joysticks");
-    gtk_container_add(GTK_CONTAINER(menu), item);
-    g_signal_connect(item, "activate",
-            G_CALLBACK(ui_swap_joysticks_callback), NULL);
+    if (joystick_swap_possible()) {
+        item = gtk_menu_item_new_with_label("Swap joysticks");
+        gtk_container_add(GTK_CONTAINER(menu), item);
+        g_signal_connect(item, "activate",
+                G_CALLBACK(ui_swap_joysticks_callback), NULL);
+    }
 
-    item = gtk_menu_item_new_with_label("Swap userport joysticks");
-    gtk_container_add(GTK_CONTAINER(menu), item);
-    g_signal_connect(item, "activate",
-            G_CALLBACK(ui_swap_userport_joysticks_callback), NULL);
+    if (userport_joystick_swap_possible()) {
+        item = gtk_menu_item_new_with_label("Swap userport joysticks");
+        gtk_container_add(GTK_CONTAINER(menu), item);
+        g_signal_connect(item, "activate",
+                G_CALLBACK(ui_swap_userport_joysticks_callback), NULL);
+        gtk_widget_set_sensitive(GTK_WIDGET(item),
+                userport_joystick_adapter_enabled());
 
+    }
     gtk_widget_show_all(menu);
     return menu;
 }
