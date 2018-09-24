ee374c5074303d9e4f5757d11546fd337c17b37a
compyx <b.wassink@ziggo.nl>
2018-09-24 12:09:59 +0000

    Gtk3: Update JAM dialog, should fix bug #1049
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@35515 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/jamdialog.c
+++ vice/src/arch/gtk3/widgets/jamdialog.c
@@ -1,11 +1,12 @@
-/**
+/** \file   jam_dialog.c
  * \brief   Gtk3 CPU jam dialog
  *
  * GTK3 CPU Jam dialog
  *
- * Written by
- *  Bas Wassink <b.wassink@ziggo.nl>
- *
+ * \author  Bas Wassink <b.wassink@ziggo.nl>
+ */
+
+/*
  * This file is part of VICE, the Versatile Commodore Emulator.
  * See README for copyright notice.
  *
@@ -35,19 +36,33 @@
 #include "jamdialog.h"
 
 
+/** \brief  Custom dialog response codes
+ *
+ * These are used to enumerate the response codes for the custom buttons of the
+ * dialog, in turn determining which UI_JAM_ACTION to return to the emulator.
+ */
 enum {
-    RESPONSE_NONE = 1,
-    RESPONSE_RESET_SOFT,
-    RESPONSE_RESET_HARD,
-    RESPONSE_MONITOR,
+    RESPONSE_NONE = 1,      /**< don't do anything */
+    RESPONSE_RESET_SOFT,    /**< trigger soft reset */
+    RESPONSE_RESET_HARD,    /**< trigger hard reset */
+    RESPONSE_MONITOR,       /**< open monitor */
 };
 
 
-
+/** \brief  Create CPU JAM dialog
+ *
+ * \param[in]   parent  parent widget
+ * \param[in]   msg     message to display
+ *
+ * \return  JAM action response
+ */
 ui_jam_action_t jam_dialog(GtkWidget *parent, const char *msg)
 {
     GtkWidget *dialog;
+    GtkWidget *content;
+    GtkWidget *label;
     ui_jam_action_t result = UI_JAM_NONE;
+
 
     /*
      * No point in making this asynchronous I think, the emulation is paused
@@ -58,8 +73,19 @@ ui_jam_action_t jam_dialog(GtkWidget *parent, const char *msg)
             "None", RESPONSE_NONE,
             "Soft reset", RESPONSE_RESET_SOFT,
             "Hard reset", RESPONSE_RESET_HARD,
-            "Active monitor", RESPONSE_MONITOR,
+            "Activate monitor", RESPONSE_MONITOR,
             NULL);
+
+    /*
+     * Add label with message to the content area and make the label
+     * wrap at word boundaries
+     */
+    content = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+    label = gtk_label_new(msg);
+    gtk_label_set_line_wrap(GTK_LABEL(label), TRUE);
+    gtk_label_set_line_wrap_mode(GTK_LABEL(label), PANGO_WRAP_WORD);
+    gtk_box_pack_start(GTK_BOX(content), label, FALSE, FALSE, 16);
+    gtk_widget_show_all(content);
 
     switch (gtk_dialog_run(GTK_DIALOG(dialog))) {
         case RESPONSE_NONE:
