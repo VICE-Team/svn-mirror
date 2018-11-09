f2ae6723718bd8b38d979077f83b715c7ed60ead
compyx <b.wassink@ziggo.nl>
2018-11-09 18:52:24 +0000

    Gtk3: Remove more deprecated code
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@35751 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/petvideosizewidget.c
+++ vice/src/arch/gtk3/widgets/petvideosizewidget.c
@@ -33,6 +33,7 @@
 
 #include <gtk/gtk.h>
 
+#include "basewidgets.h"
 #include "widgethelpers.h"
 #include "debug_gtk3.h"
 #include "resources.h"
@@ -55,62 +56,18 @@ static const vice_gtk3_radiogroup_entry_t video_sizes[] = {
 static void (*user_callback)(int) = NULL;
 
 
-/** \brief  Get index in list of video sizes of \a size
- *
- * \param[in]   size    video size
- *
- * \return  index in list, or -1 when not found
- */
-static int get_video_sizes_index(int size)
-{
-    int i = 0;
-
-    while (video_sizes[i].id >= 0) {
-        if (video_sizes[i].id == size) {
-            return i;
-        }
-        i++;
-    }
-    return -1;
-}
-
-
-/** \brief  Look up the index of \a size int the video sizes array
- *
- * \param[in]   size    video size value (0, 40 or 80)
- *
- * \return  index in the array or -1 when \a size is not found
- */
-static int get_video_size_index(int size)
-{
-    return vice_gtk3_radiogroup_get_list_index(video_sizes, size);
-}
-
-
 /** \brief  Handler for the "toggled" event of the radio buttons
  *
  * Sets the VideoSize resource when it has been changed.
  *
  * \param[in]   widget      radio button triggering the event
- * \param[in]   user_data   value for the resource (`int`)
+ * \param[in]   new_val     value for the resource (`int`)
  */
-static void on_video_size_toggled(GtkWidget *widget, gpointer user_data)
+static void on_video_size_toggled(GtkWidget *widget, int new_val)
 {
-    int old_val;
-    int new_val;
-
-    resources_get_int("VideoSize", &old_val);
-    new_val = GPOINTER_TO_INT(user_data);
-
-    if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget))
-            && (new_val != old_val)) {
-        debug_gtk3("setting VideoSize to %d.", new_val);
-        resources_set_int("VideoSize", new_val);
-
-        if (user_callback != NULL) {
-            debug_gtk3("call user_callback with %d.", new_val);
-            user_callback(new_val);
-        }
+    if (user_callback != NULL) {
+        debug_gtk3("call user_callback with %d.", new_val);
+        user_callback(new_val);
     }
 }
 
@@ -124,20 +81,30 @@ static void on_video_size_toggled(GtkWidget *widget, gpointer user_data)
 GtkWidget *pet_video_size_widget_create(void)
 {
     GtkWidget *grid;
-    int size;
-    int index;
+    GtkWidget *group;
+    GtkWidget *label;
 
-    /* disable old callback, if any */
-    user_callback = NULL;
+    grid = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, VICE_GTK3_DEFAULT);
+    g_object_set(G_OBJECT(grid), "margin-left", 8, NULL);
+    label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), "<b>Bidet size</b>");
+    gtk_widget_set_halign(label, GTK_ALIGN_START);
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
 
-    resources_get_int("VideoSize", &size);
-    index = get_video_size_index(size);
-
-    grid = uihelpers_radiogroup_create(
-            "Video size",
+    group = vice_gtk3_resource_radiogroup_new(
+            "VideoSize",
             video_sizes,
-            on_video_size_toggled,
-            index);
+            GTK_ORIENTATION_VERTICAL);
+    /* TODO: should probably be 'set_callback` since I only support a single
+     *       callback, 'add' indicates it would be possible to add multiple
+     *       callbacks, which isn't possible (not yet anyway, and shouldn't
+     *       be required)
+     */
+    vice_gtk3_resource_radiogroup_add_callback(group, on_video_size_toggled);
+    g_object_set(G_OBJECT(group), "margin-left", 16, NULL);
+    gtk_grid_attach(GTK_GRID(grid), group, 0, 1, 1, 1);
+
+
     gtk_widget_show_all(grid);
     return grid;
 }
@@ -161,13 +128,10 @@ void pet_video_size_widget_set_callback(GtkWidget *widget,
  */
 void pet_video_size_widget_sync(GtkWidget *widget)
 {
-    int size;
-    int index;
+    GtkWidget *group;
 
-    resources_get_int("VideoSize", &size);
-    index = get_video_sizes_index(size);
-    if (index >= 0) {
-        /* got valid index in radio button list */
-        vice_gtk3_radiogroup_set_index(widget, index);
+    group = gtk_grid_get_child_at(GTK_GRID(widget), 0, 1);
+    if (group != NULL && GTK_IS_GRID(group)) {
+        vice_gtk3_resource_radiogroup_sync(group);
     }
 }
