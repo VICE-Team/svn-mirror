8f33b087697bb044e57ea6769ab54507102cd3fd
compyx <b.wassink@ziggo.nl>
2019-02-16 23:16:21 +0000

    Gtk3: Add time display for audio/video recording on the statusbar
    Since the audio/video recording code don't expose the time they've beem recording, I've added some g_timeout*() code which seems to be accurate enough.
    Of course having the audio/video recording code passing off their timestamp to the UI would be preffered.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@36073 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/statusbarrecordingwidget.c
+++ vice/src/arch/gtk3/widgets/statusbarrecordingwidget.c
@@ -78,6 +78,35 @@ static const gchar *rec_types[] = {
 };
 
 
+/** \brief  Callback for the g_timeout
+ *
+ * \param[in,out]   data    statusbar recording widget
+ *
+ * \return  boolean (TRUE == keep running, FALSE == delete timer source)
+ */
+static gboolean update_timer(gpointer data)
+{
+    guint time;
+    int status;
+
+    /* do we have status == stop? */
+    status = GPOINTER_TO_INT(g_object_get_data(G_OBJECT(data), "Status"));
+    if (status == 0) {
+        return FALSE;   /* unregister timer */
+    }
+
+    /* get time in seconds, but add one because the timer counts down 1 second,
+     * so after one second we actually have 1 second */
+    time = GPOINTER_TO_UINT(g_object_get_data(G_OBJECT(data), "Seconds"));
+    statusbar_recording_widget_set_time(GTK_WIDGET(data), time + 1, 0);
+
+    /* update time, again adjusting to the timer countdown behaviour */
+    g_object_set_data(G_OBJECT(data), "Seconds", GUINT_TO_POINTER(time + 1));
+
+    return TRUE;
+}
+
+
 /** \brief  Event handler for the 'clicked' event of the STOP button
  *
  * Stops all recordings.
@@ -114,14 +143,17 @@ GtkWidget *statusbar_recording_widget_create(void)
     grid = vice_gtk3_grid_new_spaced(8, 8);
     gtk_widget_set_hexpand(grid, TRUE);
 
+    g_object_set_data(G_OBJECT(grid), "Seconds", GINT_TO_POINTER(0));
+    g_object_set_data(G_OBJECT(grid), "Status", GINT_TO_POINTER(0));
+
     /* recording status label */
-    label = gtk_label_new("Recording widget, not quite finished.");
+    label = gtk_label_new("Recording inactive");
     gtk_grid_attach(GTK_GRID(grid), label, RW_COL_TEXT, RW_ROW_TEXT, 1, 1);
     gtk_widget_set_halign(label, GTK_ALIGN_FILL);
     gtk_widget_set_hexpand(label, TRUE);
 
     /* recording timestamp label */
-    label = gtk_label_new("00:00 / 00:00");
+    label = gtk_label_new("Time: --:--");
     gtk_widget_set_halign(label, GTK_ALIGN_FILL);
     gtk_widget_set_hexpand(label, TRUE);
     gtk_grid_attach(GTK_GRID(grid), label, RW_COL_TIME, RW_ROW_TIME, 1, 1);
@@ -157,14 +189,13 @@ void statusbar_recording_widget_set_recording_status(GtkWidget *widget,
     GtkWidget *button;
     int type = 0;   /* set recording type to 'inactive' */
 
+    g_object_set_data(G_OBJECT(widget), "Status", GINT_TO_POINTER(status));
+    if (status == 0) {
+        g_object_set_data(G_OBJECT(widget), "Seconds", GINT_TO_POINTER(0));
+    }
+
     /* determine recording type */
     if (event_record_active()) {
-        /* XXX: doesn't work for some obscure reason
-         *
-         * When triggering the menu item 'start event recording' again, we do
-         * get "Recording events", so it looks like the event recording state
-         * is set after events.c calls ui_display_recording().
-         */
         type = RW_TYPE_EVENTS;
     } else if (sound_is_recording()) {
         type = RW_TYPE_AUDIO;
@@ -172,6 +203,9 @@ void statusbar_recording_widget_set_recording_status(GtkWidget *widget,
         type = RW_TYPE_VIDEO;
     }
 
+    if (type != RW_TYPE_NONE && type != RW_TYPE_EVENTS && status) {
+        g_timeout_add_seconds(1, update_timer, (gpointer)widget);
+    }
 
     /* update recording status text */
     label = gtk_grid_get_child_at(GTK_GRID(widget), RW_COL_TEXT, RW_ROW_TEXT);
@@ -205,7 +239,12 @@ void statusbar_recording_widget_set_time(GtkWidget *widget,
     gchar buffer[256];
 
     label = gtk_grid_get_child_at(GTK_GRID(widget), RW_COL_TIME, RW_ROW_TIME);
-    g_snprintf(buffer, 256, "Time: %02u:%02u / %02u:%02u",
-            current / 60, current % 60, total / 60, total % 60);
+
+    if (total > 0) {
+        g_snprintf(buffer, 256, "Time: %02u:%02u / %02u:%02u",
+                current / 60, current % 60, total / 60, total % 60);
+    } else {
+        g_snprintf(buffer, 256, "Time: %02u:%02u", current / 60, current % 60);
+    }
     gtk_label_set_text(GTK_LABEL(label), buffer);
 }
