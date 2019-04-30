392d3fa413e10cd2111e50da589b56378a226dbc
compyx <b.wassink@ziggo.nl>
2019-04-30 15:55:05 +0000

    Gtk3: Add fallback on Win32 when vice.pdf cannot be found
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@36269 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/uicommands.c
+++ vice/src/arch/gtk3/uicommands.c
@@ -310,6 +310,10 @@ void ui_open_manual_callback(GtkWidget *widget, gpointer user_data)
         vice_gtk3_message_error(
                 "Failed to load PDF: %s.",
                 error != NULL ? error->message : "<no message>");
+#ifdef WIN32_COMPILE
+        gtk_show_uri_on_window(NULL, "https://youtu.be/pkJG53d7p3U?t=173",
+                GDK_CURRENT_TIME, NULL);
+#endif
     }
     lib_free(uri);
     g_free(final_uri);
