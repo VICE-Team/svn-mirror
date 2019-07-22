85a6eac31a6273ec8c94275986e8676b4da79666
compyx <b.wassink@ziggo.nl>
2019-07-22 16:32:38 +0000

    AdÐ kéÿmáp sàvé tó Gtk3
    Voeg een knop toe die de huidige keymap opslaat.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@36795 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/settings_keyboard.c
+++ vice/src/arch/gtk3/widgets/settings_keyboard.c
@@ -32,13 +32,16 @@
 #include "vice.h"
 
 #include <gtk/gtk.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
+#include "vice_gtk3.h"
+#include "keyboard.h"
 #include "lib.h"
 #include "ui.h"
 #include "resources.h"
 #include "vsync.h"
-#include "basewidgets.h"
-#include "widgethelpers.h"
 
 #include "kbdmappingwidget.h"
 #include "kbdlayoutwidget.h"
@@ -47,10 +50,47 @@
 #include "settings_keyboard.h"
 
 
-
+/** \brief  Toggle statusbar keyboard debugging widget display
+ *
+ *
+ * \param[in]   widget  toggle button
+ * \param[in]   data    extra event data (unused)
+ */
 static void on_kbd_debug_toggled(GtkWidget *widget, gpointer data)
 {
-    ui_statusbar_set_kbd_debug(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget)));
+    ui_statusbar_set_kbd_debug(
+            gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget)));
+}
+
+
+/** \brief  Write current keymap to host system
+ *
+ * \param[in]   widget  button triggering the event (ignored)
+ * \param[in]   data    extra event data (ignored)
+ */
+static void on_save_custom_keymap_clicked(GtkWidget *widget, gpointer data)
+{
+    gchar *path;
+
+/*    vice_gtk3_message_info("Save current keymap", "Nothing to see here..."); */
+    path = vice_gtk3_save_file_dialog(
+            "Save current keymap",  /* title */
+            NULL,                   /* proposed filename: might use this later */
+            TRUE,                   /* query user before overwrite */
+            NULL                    /* base path, maybe ~ ?) */
+         );
+
+    if (path != NULL && *path != '\0') {
+        /* we got something at least */
+        int oops = keyboard_keymap_dump(path);
+        if (oops == 0) {
+            vice_gtk3_message_info("Succesfully saved current keymap",
+                    "Wrote current keymap as '%s'.", path);
+        } else {
+            vice_gtk3_message_error("Failed to save custom keymap",
+                    "Error %d: %s", errno, strerror(errno));
+        }
+    }
 }
 
 
@@ -66,6 +106,7 @@ GtkWidget *settings_keyboard_widget_create(GtkWidget *widget)
     GtkWidget *mapping_widget;
     GtkWidget *layout_widget;
     GtkWidget *kbdstatusbar;
+    GtkWidget *custom_button;
 
     layout = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, VICE_GTK3_DEFAULT);
 
@@ -77,10 +118,16 @@ GtkWidget *settings_keyboard_widget_create(GtkWidget *widget)
 
     gtk_grid_attach(GTK_GRID(layout), layout_widget, 0, 1, 1, 1);
 
+    /* Add button to save current (perhaps altered) keymap */
+    custom_button = gtk_button_new_with_label("Save current keymap");
+    g_signal_connect(custom_button, "clicked",
+            G_CALLBACK(on_save_custom_keymap_clicked), NULL);
+    gtk_grid_attach(GTK_GRID(layout), custom_button, 0, 2, 1, 1);
+    g_object_set(custom_button, "margin-top", 16, NULL);
 
     kbdstatusbar = vice_gtk3_resource_check_button_new("KbdStatusbar",
             "Enable keyboard debugging on statusbar");
-    gtk_grid_attach(GTK_GRID(layout), kbdstatusbar, 0, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(layout), kbdstatusbar, 0, 3, 1, 1);
     g_signal_connect(kbdstatusbar, "toggled", G_CALLBACK(on_kbd_debug_toggled),
             NULL);
     g_object_set(kbdstatusbar, "margin-top", 16, NULL);
