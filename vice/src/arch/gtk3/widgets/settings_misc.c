9657ae74c13ae76ae41df5174de350eae2fb6523
compyx <b.wassink@ziggo.nl>
2019-09-01 20:20:33 +0000

    Gtk3: Remove Apple Monitor Stand support
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@36966 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/settings_misc.c
+++ vice/src/arch/gtk3/widgets/settings_misc.c
@@ -45,16 +45,6 @@
 #include "settings_misc.h"
 
 
-static void on_fuck_apple(GtkWidget *widget, gpointer data)
-{
-    debug_gtk3("got triggered!");
-    vice_gtk3_message_info("You're a fucking idiot",
-            "Since you actually clicked on this, "
-            "you're either a complete idiot or you have a sense of humor");
-    abort();
-
-}
-
 /** \brief  Create miscellaneous settings widget
  *
  * Basically a widget to contain (hopefully temporarily) widgets controlling
@@ -72,7 +62,6 @@ GtkWidget *settings_misc_widget_create(GtkWidget *widget)
     GtkWidget *jam_widget = jam_action_widget_create();
     GtkWidget *backend_widget = canvas_render_backend_widget_create();
     GtkWidget *filter_widget = canvas_render_filter_widget_create();
-    GtkWidget *apple;
 
     grid = gtk_grid_new();
 
@@ -90,11 +79,6 @@ GtkWidget *settings_misc_widget_create(GtkWidget *widget)
         g_object_set(filter_widget, "margin-left",8, NULL);
         gtk_grid_attach(GTK_GRID(grid), backend_widget, 1, 4, 2, 1);
         g_object_set(backend_widget, "margin-left",8, NULL);
-
-        apple = gtk_check_button_new_with_label("Enable Apple monitor stand emulation");
-        g_object_set(apple, "margin-top", 16, NULL);
-        gtk_grid_attach(GTK_GRID(grid), apple, 0, 5, 1, 1);
-        g_signal_connect(apple, "toggled", G_CALLBACK(on_fuck_apple), NULL);
 
     } else {
          gtk_grid_attach(GTK_GRID(grid), jam_widget, 0, 0, 1, 1);
