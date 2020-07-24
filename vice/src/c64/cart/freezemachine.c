f5efc324194a6e3e85ad6563ce34e702c049083e
gpz <groepaz@gmx.net>
2020-07-24 10:59:19 +0000

    fix -Wshorten-64-to-32 warnings
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@38160 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/cart/freezemachine.c
+++ vice/src/c64/cart/freezemachine.c
@@ -278,7 +278,8 @@ int freezemachine_bin_attach(const char *filename, uint8_t *rawcart)
  */
 int freezemachine_crt_attach(FILE *fd, uint8_t *rawcart)
 {
-    int i, pos, banks, chips;
+    int i, banks, chips;
+    size_t pos;
     crt_chip_header_t chip;
 
     /* find out how many banks and chips are in the file */
