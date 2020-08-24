0a565b74acb532eda8165607b4e86d7850b4f272
compyx <b.wassink@ziggo.nl>
2020-08-24 19:25:12 +0000

    Gtk3: Generate .desktop files for the emus, don't install them
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@38456 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/src/arch/gtk3/data/mkdesktopfile.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Script to generate .desktop files for the emu binaries on Linux
+#
+#
+
+show_help()
+{
+    echo "Usage: `basename $0` <top_srcdir> <top_builddir> <template> <prefix> <version> <binary> [<desc> [<icon>]]"
+}
+
+
+# Entry point, check for -h/--help
+if [ -z "$1" ]; then
+    show_help
+    exit 1
+fi
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+    show_help
+    exit 0
+fi
+
+
+# grab params
+TOPSRCDIR="$1"
+TOPBUILDDIR="$2"
+TEMPLATE="$3"
+PREFIX="$4"
+VERSION="$5"
+BINARY="$6"
+COMMENT="$7"
+ICON="$8"
+
+if [ -z "$COMMENT" ]; then
+    COMMENT="$BINARY"
+fi
+if [ -z "$ICON" ]; then
+    ICON="icon.png"
+fi
+
+
+# check for SVN revision, and if present, add to version number
+if [ -e "$TOPSRCDIR/src/svnversion.h" ]; then
+    SVNREVISION=`grep -oP 'VICE_SVN_REV_NUMBER\s+\K\d+' "$TOPSRCDIR/src/svnversion.h"`
+    VERSION="$VERSION-r$SVNREVISION"
+fi
+
+#echo "top_srcdir   : $TOPSRCDIR"
+#echo "top_builddir : $TOPBUILDDIR"
+#echo "template     : $TEMPLATE"
+#echo "prefix       : $PREFIX"
+#echo "version      : $VERSION"
+#echo "binary       : $BINARY"
+#echo "cmment       : $COMMENT"
+#echo "icon         : $ICON"
+
+
+cat "${TEMPLATE}" | \
+    sed -e "s@__PREFIX__@${PREFIX}@g;s@__VICE_EMU_BIN__@${BINARY}@g;s@__ICON__@${ICON}@g;s@__VICE_EMU_COMMENT__@${COMMENT}@g;s@__ICON__@${ICON}@g;s@__VICE_VERSION__@${VERSION}@g;s@__VICE_EMU_NAME__@${BINARY}@g" > "${BINARY}.desktop"
+
