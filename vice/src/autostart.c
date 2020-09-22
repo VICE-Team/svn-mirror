a8a2c22198cd5f42786ae755b6b8a4f447b3456e
dqh <david.q.hogan@gmail.com>
2020-09-22 12:57:17 +0000

    Re-enable autostart main lock asserts, but allow the vice thread to autostart too because the monitor can do that.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@38592 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/autostart.c
+++ vice/src/autostart.c
@@ -1369,10 +1369,9 @@ int autostart_disk(const char *file_name, const char *program_name,
     char *name = NULL;
 
     DBG(("autostart_disk"));
-    /* FIXME Causes monitor autostart command to crash
-    mainlock_assert_is_not_vice_thread();
-    mainlock_assert_lock_obtained();
-    */
+    if (!mainlock_is_vice_thread()) {
+        mainlock_assert_lock_obtained();
+    }
 
     if (network_connected() || event_record_active() || event_playback_active()
         || !file_name || !autostart_enabled) {
@@ -1677,10 +1676,9 @@ static void set_tapeport_device(int datasette, int tapecart)
 int autostart_autodetect(const char *file_name, const char *program_name,
                          unsigned int program_number, unsigned int runmode)
 {
-    /* FIXME Causes monitor autostart command to crash
-    mainlock_assert_is_not_vice_thread();
-    mainlock_assert_lock_obtained();
-    */
+    if (!mainlock_is_vice_thread()) {
+        mainlock_assert_lock_obtained();
+    }
 
     if (network_connected() || event_record_active() || event_playback_active()
         || file_name == NULL) {
@@ -1758,10 +1756,9 @@ int autostart_autodetect(const char *file_name, const char *program_name,
 /* Autostart the image attached to device `num'.  */
 int autostart_device(int num)
 {
-    /* FIXME Causes monitor autostart command to crash
-    mainlock_assert_is_not_vice_thread();
-    mainlock_assert_lock_obtained();
-    */
+    if (!mainlock_is_vice_thread()) {
+        mainlock_assert_lock_obtained();
+    }
 
     if (network_connected() || event_playback_active() || event_record_active()
         || !autostart_enabled) {
