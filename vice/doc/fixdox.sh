3cea03ec2405fd264e6edc1dafb426f491da5550
gpz <groepaz@gmx.net>
2020-10-08 17:36:17 +0000

    cleanup, remove stuff we do no more support
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@38724 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/doc/fixdox.sh
+++ vice/doc/fixdox.sh
@@ -30,29 +30,6 @@
 
 FORMAT=$1
 
-table4chm()
-{
-  rm -f chmtmp*.txt
-  read data
-  newdata=""
-  for i in $data
-  do
-    if test x"$i" != "x@item"; then
-      newdata="$newdata $i"
-    fi
-  done
-  echo >>chmtmp.txt "@multitable $newdata"
-  read data
-  while test x"$data" != "x@end multitable"
-  do
-    echo >>chmtmp.txt "$data"
-    read data
-  done
-  sed 's/@item/@rowstart/g' <chmtmp.txt >chmtmp2.txt
-  cat chmtmp2.txt
-  rm -f chmtmp*.txt
-}
-
 fixtxt()
 {
   while read data
@@ -78,91 +55,24 @@ fixtxt()
   done
 }
 
-fixchm()
-{
-  outputok=yes
-  while read data
-  do
-    case x"${data}" in
-      "x@multitable"*)
-        table4chm
-        ;;
-      "x@ifset"*)
-        outputok=no
-        ;;
-      "x@ifclear"*)
-        outputok=no
-        ;;
-    esac
-
-    if test x"$data" = "x@ifinfo"; then
-      outputok=no
-    fi
-
-    if test x"$outputok" = "xyes"; then
-      echo $data
-    fi
-
-    if test x"$data" = "x@ifset platformwindows"; then
-      outputok=yes
-    fi
-
-    if test x"$data" = "x@end ifinfo"; then
-      outputok=yes
-    fi
-
-    if test x"$data" = "x@end ifset"; then
-      outputok=yes
-    fi
-
-    if test x"$data" = "x@end ifclear"; then
-      outputok=yes
-    fi
-  done
-}
-
-fixhlp()
-{
-  sed <tmp.texi >vicetmp.texi -e 's/@firstparagraphindent none//g' -e 's/@exampleindent 0//g' -e 's/@verbatim/@smallexample/g' -e 's/@end verbatim/@end smallexample/g'
-  rm -f -r tmp.texi
-}
-
-fixguide()
-{
-  sed <tmp.texi >vicetmp.texi -e 's/@ifinfo//g' -e 's/@end ifinfo//g' -e 's/@format//g' -e 's/@end format//g' -e 's/@firstparagraphindent none//g' -e 's/@exampleindent 0//g'
-}
-
 fixpdf()
 {
   echo not implemented yet
 }
 
-fixipf()
+fixhtml()
 {
-  sed <tmp.texi >vicetmp.texi -e 's/@firstparagraphindent//g' -e 's/@paragraphindent//g' -e 's/@exampleindent//g' -e 's/@ifcommanddefined//g' -e 's/@end ifcommanddefined//g'
-  rm -f -r tmp.texi
+  echo not implemented yet
 }
 
 if test x"$FORMAT" = "xtxt"; then
   fixtxt
 fi
 
-if test x"$FORMAT" = "xchm"; then
-  fixchm
-fi
-
-if test x"$FORMAT" = "xhlp"; then
-  fixhlp
-fi
-
-if test x"$FORMAT" = "xguide"; then
-  fixguide
-fi
-
 if test x"$FORMAT" = "xpdf"; then
   fixpdf
 fi
 
-if test x"$FORMAT" = "xipf"; then
-  fixipf
+if test x"$FORMAT" = "xhtml"; then
+  fixhtml
 fi
