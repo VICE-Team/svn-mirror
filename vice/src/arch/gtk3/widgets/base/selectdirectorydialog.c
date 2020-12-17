47ec3534c677ec70e9daa02d7aad8a93364a32a3
compyx <b.wassink@ziggo.nl>
2020-12-17 11:42:21 +0000

    Gtk3: Hopefully fix the non-active 'Select' button in the directory selection dialog
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@39333 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/base/selectdirectorydialog.c
+++ vice/src/arch/gtk3/widgets/base/selectdirectorydialog.c
@@ -64,9 +64,7 @@ gchar *vice_gtk3_select_directory_dialog(
     dialog = gtk_file_chooser_dialog_new(
             title,
             ui_get_active_window(),
-            allow_create
-                ? GTK_FILE_CHOOSER_ACTION_CREATE_FOLDER
-                : GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
+            GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
             "Select", GTK_RESPONSE_ACCEPT,
             "Cancel", GTK_RESPONSE_REJECT,
             NULL);
@@ -86,6 +84,7 @@ gchar *vice_gtk3_select_directory_dialog(
     filter = gtk_file_filter_new ();
     gtk_file_filter_add_mime_type (filter, "inode/directory");
     gtk_file_chooser_set_filter(GTK_FILE_CHOOSER(dialog), filter);
+    gtk_file_chooser_set_create_folders(GTK_FILE_CHOOSER(dialog), allow_create);
 
     result = gtk_dialog_run(GTK_DIALOG(dialog));
     if (result == GTK_RESPONSE_ACCEPT) {
