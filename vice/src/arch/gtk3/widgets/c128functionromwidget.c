25361d5940542375de60f089849b9f553d95326d
compyx <compyx@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2020-12-29 16:28:54 +0000

    Gtk3: Fix C128 function ROM settings
    * Remove use of deprecated function
    * Fix a long-standing bug where browsing for an 'internal' ROM would actually set the 'external' ROM
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@39466 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/c128functionromwidget.c
+++ vice/src/arch/gtk3/widgets/c128functionromwidget.c
@@ -36,11 +36,8 @@
 #include "vice.h"
 #include <gtk/gtk.h>
 
-#include "basewidgets.h"
-#include "debug_gtk3.h"
+#include "vice_gtk3.h"
 #include "functionrom.h"
-#include "openfiledialog.h"
-#include "widgethelpers.h"
 #include "ui.h"
 
 #include "c128functionromwidget.h"
@@ -61,16 +58,11 @@ static const vice_gtk3_radiogroup_entry_t rom_types[] = {
 };
 
 
-/** \brief  Reference to the text entry for the filename
- */
-static GtkWidget *entry_widget;
-
-
 /** \brief  Callback for the open-file dialog
  *
  * \param[in,out]   dialog      open-file dialog
  * \param[in]       filename    filename or NULL on cancel
- * \param[in]       data        extra data (unused)
+ * \param[in]       data        entry widget reference
  */
 static void browse_filename_callback(GtkDialog *dialog,
                                      gchar *filename,
@@ -79,7 +71,11 @@ static void browse_filename_callback(GtkDialog *dialog,
     debug_gtk3("got filename '%s'.", filename);
 
     if (filename != NULL) {
-        vice_gtk3_resource_entry_full_set(entry_widget, filename);
+        GtkWidget *entry = data;
+        if (entry == NULL) {
+            debug_gtk3("Got NULL as entry!!!");
+        }
+        vice_gtk3_resource_entry_full_set(entry, filename);
         g_free(filename);
     }
     gtk_widget_destroy(GTK_WIDGET(dialog));
@@ -99,9 +95,36 @@ static void on_browse_clicked(GtkWidget *widget, gpointer data)
             NULL,
             NULL,
             browse_filename_callback,
-            NULL);
+            data);  /* resourceentry widget for the filename */
 }
 
+
+/*
+ * This was supposed to be used to grey-out the ROM filename entry when the
+ * type was None. But the callback only gets triggered when there already is
+ * a valid ROM file, so we can't do the grey-out since an empty resource means
+ * setting the ROM type fails.
+ * This has to do with the base 'resourceradiogroup' widget triggering the
+ * callback when setting the resource fails.
+ *
+ * I could change the resourceradiogroup widget to trigger the callback even
+ * on errors, but that might break other things.
+ *
+ * Another option would be custom code, not using the base resource widgets,
+ * but that's a lot of work for something that appears to basically never get
+ * used.
+ */
+#if 0
+/** \brief  Extra callback for the ROM type radio buttons
+ *
+ * \param[in,out]   widget  resource radiogroup
+ * \param[in]       id      current select ID
+ */
+static void rom_type_callback(GtkWidget *widget, int id)
+{
+    debug_gtk3("Got ID %d", id);
+}
+#endif
 
 /** \brief  Create ROM type widget
  *
@@ -130,18 +153,20 @@ static GtkWidget *create_rom_file_widget(const char *prefix)
 {
     GtkWidget *grid;
     GtkWidget *browse;
+    GtkWidget *entry;
 
     grid = gtk_grid_new();
     gtk_grid_set_column_spacing(GTK_GRID(grid), 8);
 
-    entry_widget = vice_gtk3_resource_entry_full_new_sprintf("%sFunctionName",
-                                                             prefix);
-    gtk_widget_set_hexpand(entry_widget, TRUE);
-    gtk_grid_attach(GTK_GRID(grid), entry_widget, 0, 0, 1, 1);
+    entry = vice_gtk3_resource_entry_full_new_sprintf("%sFunctionName",
+                                                      prefix);
+    gtk_widget_set_hexpand(entry, TRUE);
+    gtk_grid_attach(GTK_GRID(grid), entry, 0, 0, 1, 1);
 
     browse = gtk_button_new_with_label("Browse ...");
     gtk_grid_attach(GTK_GRID(grid), browse, 1, 0, 1,1);
-    g_signal_connect(browse, "clicked", G_CALLBACK(on_browse_clicked), NULL);
+    g_signal_connect(browse, "clicked", G_CALLBACK(on_browse_clicked),
+            entry);
 
     gtk_widget_show_all(grid);
     return grid;
@@ -158,22 +183,24 @@ static GtkWidget *create_rom_file_widget(const char *prefix)
 static GtkWidget *create_rom_widget(GtkWidget *parent, const char *prefix)
 {
     GtkWidget *grid;
+    GtkWidget *type;
     GtkWidget *label;
     GtkWidget *rtc;
     char buffer[256];
 
     g_snprintf(buffer, sizeof(buffer), "%s Function ROM", prefix);
-    grid = uihelpers_create_grid_with_label(buffer, 1);
-    gtk_grid_set_column_spacing(GTK_GRID(grid), 16);
-    gtk_grid_set_row_spacing(GTK_GRID(grid), 8);
+    grid = vice_gtk3_grid_new_spaced_with_label(-1, -1, buffer, 1);
 
-    gtk_widget_show_all(grid);
 
     label = gtk_label_new("ROM type");
     gtk_widget_set_halign(label, GTK_ALIGN_START);
     g_object_set(label, "margin-left", 16, NULL);
     gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), create_rom_type_widget(prefix), 1, 1, 1, 1);
+    type = create_rom_type_widget(prefix);
+#if 0
+    vice_gtk3_resource_radiogroup_add_callback(type, rom_type_callback);
+#endif
+    gtk_grid_attach(GTK_GRID(grid), type, 1, 1, 1, 1);
 
     label = gtk_label_new("ROM file");
     gtk_widget_set_halign(label, GTK_ALIGN_START);
@@ -186,6 +213,7 @@ static GtkWidget *create_rom_widget(GtkWidget *parent, const char *prefix)
     g_object_set(rtc, "margin-left", 16, NULL);
     gtk_grid_attach(GTK_GRID(grid), rtc, 0, 3, 3, 1);
 
+    gtk_widget_show_all(grid);
     return grid;
 }
 
@@ -199,13 +227,15 @@ static GtkWidget *create_rom_widget(GtkWidget *parent, const char *prefix)
 GtkWidget *c128_function_rom_widget_create(GtkWidget *parent)
 {
     GtkWidget *grid;
+    GtkWidget *internal_widget;
+    GtkWidget *external_widget;
 
-    grid = gtk_grid_new();
+    grid = vice_gtk3_grid_new_spaced(16, 32);
 
-    gtk_grid_attach(GTK_GRID(grid), create_rom_widget(parent, "Internal"),
-            0, 0, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), create_rom_widget(parent, "External"),
-            0, 1, 1, 1);
+    internal_widget = create_rom_widget(parent, "Internal");
+    gtk_grid_attach(GTK_GRID(grid), internal_widget, 0, 0, 1, 1);
+    external_widget = create_rom_widget(parent, "External");
+    gtk_grid_attach(GTK_GRID(grid), external_widget, 0, 1, 1, 1);
 
     gtk_widget_show_all(grid);
     return grid;
