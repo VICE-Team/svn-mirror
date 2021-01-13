c6d1459fe3e5550fcd281a37e11018c7bd90d081
compyx <compyx@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2021-01-13 14:32:37 +0000

    Gtk3: About dialog: Remove custom responses
    Runtime info is already shown in the default panel of the dialog, compile time features are listed in a separate dialog.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@39537 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/uiabout.c
+++ vice/src/arch/gtk3/uiabout.c
@@ -51,14 +51,6 @@
  */
 #define VERSION_STRING_MAX 8192
 
-/** \brief  Custom response ID's for the dialog
- */
-enum {
-    RESPONSE_RUNTIME = 1,   /**< response ID for 'Runtime' button */
-    RESPONSE_COMPILE_TIME   /**< response ID for 'Compile time' button */
-};
-
-
 
 /** \brief  List of current team members
  *
@@ -142,14 +134,6 @@ static void about_response_callback(GtkWidget *widget, gint response_id,
         case GTK_RESPONSE_DELETE_EVENT:
             gtk_widget_destroy(widget);
             break;
-        case RESPONSE_RUNTIME:
-            debug_gtk3("Got RUNTIME! (TODO)");
-            break;
-#if 0
-        case RESPONSE_COMPILE_TIME:
-            debug_gtk3("Got COMPILE TIME! (TODO)");
-            break;
-#endif
         default:
             debug_gtk3("Warning: Unsupported response ID %d", response_id);
             break;
@@ -245,9 +229,7 @@ gboolean ui_about_dialog_callback(GtkWidget *widget, gpointer user_data)
         gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(about), logo);
         g_object_unref(logo);
     }
-#if 0
-    gtk_dialog_add_button(GTK_DIALOG(about), "Runtime info", RESPONSE_RUNTIME);
-#endif
+
     /*
      * hook up event handlers
      */
