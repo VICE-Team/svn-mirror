a32a8f46fb8422fd680982247ceda00e52959ea5
dqh <dqh@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2021-02-17 10:29:44 +0000

    Static Analysis page at https://vice-team.github.io/svn-mirror/ will now be updated on each commit. Takes about 40 minutes to run and there’s a 10 minute or so web cache in front of it as well.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@39731 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ vice/build/macOS/github-actions-build.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+cd "$(dirname $0)"/../..
+
+UI="$1"
+BUILD_TYPE="$2"
+
+ARGS="\
+    --disable-arch \
+    --disable-pdf-docs \
+    --enable-ethernet \
+    --with-jpeg \
+    --with-png \
+    --with-gif \
+    --with-vorbis \
+    --with-flac \
+    --enable-lame \
+    --enable-midi \
+    --enable-cpuhistory \
+    --enable-external-ffmpeg \
+    "
+
+case "$UI" in
+GTK3)
+    ARGS="--enable-native-gtk3ui $ARGS"
+    ;;
+
+SDL2)
+    ARGS="--enable-sdlui2 $ARGS"
+    ;;
+
+*)
+    echo "Bad UI: $UI"
+    exit 1
+    ;;
+esac
+
+case "$BUILD_TYPE" in
+bindist)
+    ./autogen.sh
+    ./configure $ARGS || ( echo -e "\n**** CONFIGURE FAILED ****\n" ; cat config.log ; exit 1 )
+    make -j $(sysctl -n hw.ncpu) -s
+    make bindistzip
+    ;;
+
+analyse)
+    OUTPUT="../gh-pages/analysis/$UI"
+    ./autogen.sh
+    scan-build ./configure $ARGS || ( echo -e "\n**** CONFIGURE FAILED ****\n" ; cat config.log ; exit 1 )
+    scan-build -o $OUTPUT make -j $(sysctl -n hw.ncpu)
+
+    # scan-build -o still creates a silly folder name
+    mv $(dirname $(find $OUTPUT/scan-build-* -name index.html))/* $OUTPUT/
+    rm -rf $OUTPUT/scan-build-*
+    ;;
+
+*)
+    echo "Bad Build Type: $BUILD_TYPE"
+    exit 1
+    ;;
+esac
