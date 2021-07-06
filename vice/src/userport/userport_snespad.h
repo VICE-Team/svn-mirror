7847eebed253bc6e977bac03f22c2bb4ba8e2d9d
blackystardust <blackystardust68@yahoo.com>
2021-07-06 00:45:26 +0000

    Fixed userport snespad stream output.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@40304 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/userport/userport_snespad.h
+++ vice/src/userport/userport_snespad.h
@@ -29,22 +29,6 @@
 
 #include "types.h"
 
-#define USERPORT_SNESPAD_BUTTON_B        0
-#define USERPORT_SNESPAD_BUTTON_Y        1
-#define USERPORT_SNESPAD_BUTTON_SELECT   2
-#define USERPORT_SNESPAD_BUTTON_START    3
-#define USERPORT_SNESPAD_UP              4
-#define USERPORT_SNESPAD_DOWN            5
-#define USERPORT_SNESPAD_LEFT            6
-#define USERPORT_SNESPAD_RIGHT           7
-#define USERPORT_SNESPAD_BUTTON_A        8
-#define USERPORT_SNESPAD_BUTTON_X        9
-#define USERPORT_SNESPAD_BUMPER_LEFT    10
-#define USERPORT_SNESPAD_BUMPER_RIGHT   11
-
-/* End Of Sequence */
-#define USERPORT_SNESPAD_EOS            12
-
 extern int userport_snespad_resources_init(void);
 extern int userport_snespad_cmdline_options_init(void);
 
