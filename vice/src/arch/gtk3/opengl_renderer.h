11fbdc12ddf8b7e2a339aba0c0a7696a8101bfb9
dqh <david.q.hogan@gmail.com>
2021-07-07 05:52:29 +0000

    Fixed minimum window size calculation on OpenGL builds. Also refactored OpenGL rendering into multiple functions.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@40314 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/opengl_renderer.h
+++ vice/src/arch/gtk3/opengl_renderer.h
@@ -131,10 +131,16 @@ typedef struct vice_opengl_renderer_context_s {
     GLuint vao;
 
     /** \brief The texture identifier for the GPU's copy of our  machine display. */
-    GLuint texture;
+    GLuint current_frame_texture;
+    unsigned int current_frame_width;
+    unsigned int current_frame_height;
+    float current_frame_pixel_aspect_ratio;
 
     /** \brief The texture identifier for the GPU's copy of our  machine display. */
     GLuint previous_frame_texture;
+    unsigned int previous_frame_width;
+    unsigned int previous_frame_height;
+    float previous_frame_pixel_aspect_ratio;
 
     /** \brief size of the next frame to be emulated */
     unsigned int emulated_width_next;
@@ -144,15 +150,6 @@ typedef struct vice_opengl_renderer_context_s {
 
     /** \brief pixel aspect ratio of the next frame to be emulated */
     float pixel_aspect_ratio_next;
-
-    /** \brief size of the last frame to be rendered on host */
-    unsigned int emulated_width_last_rendered;
-    
-    /** \brief size of the last frame to be rendered on host */
-    unsigned int emulated_height_last_rendered;
-
-    /** \brief pixel aspect ratio of the last frame to be rendered on host */
-    float pixel_aspect_ratio_last_rendered;
 
     /** \brief when the last frame was rendered */
     unsigned long last_render_time;
