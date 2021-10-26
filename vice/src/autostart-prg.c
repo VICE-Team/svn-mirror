33ebf29b04bb55f1cdb911172dd37847d360de34
compyx <b.wassink@ziggo.nl>
2021-10-26 13:46:51 +0000

    Remove lingering debugging printf() call
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@40975 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/autostart-prg.c
+++ vice/src/autostart-prg.c
@@ -270,7 +270,6 @@ int autostart_prg_with_disk_image(int unit, int drive, const char *file_name,
         /* create empty image */
         if (vdrive_internal_create_format_disk_image(image_name, (char *)"AUTOSTART", disk_image_type) < 0) {
             log_error(log, "Error creating autostart disk image: %s", image_name);
-            printf("FOOFOOFOO!\n\n\n");
             ui_error("Error creating autostart disk image '%s'.\n"
                      "(%d: %s)\n"
                      "\n"
