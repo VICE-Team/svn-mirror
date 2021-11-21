2083c03b5da7a6b9ac647753226b1e854ee4ef2a
gpz <groepaz@gmx.net>
2021-11-21 18:34:10 +0000

    updated to handle all systems, put implied clock rate into the tap struct so it can be used to scale the pulses in the tape code
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@41173 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/tap.h
+++ vice/src/tap.h
@@ -42,10 +42,14 @@
 #define TAP_HDR_SYSTEM_C64      0
 #define TAP_HDR_SYSTEM_VIC20    1
 #define TAP_HDR_SYSTEM_C16      2
+#define TAP_HDR_SYSTEM_PET      3
+#define TAP_HDR_SYSTEM_C500     4
+#define TAP_HDR_SYSTEM_C600     5
 
 #define TAP_HDR_VIDEO_PAL       0
 #define TAP_HDR_VIDEO_NTSC      1
 #define TAP_HDR_VIDEO_NTSCOLD   2
+#define TAP_HDR_VIDEO_PALN      3
 
 struct tape_init_s;
 struct tape_file_record_s;
@@ -68,6 +72,9 @@ typedef struct tap_s {
 
     /* Videostandard the image is made for.  */
     uint8_t video;
+
+    /* clock speed the tap file was created with */
+    int tap_clock;
 
     /* Tape name.  */
     uint8_t name[12];
