2e74ab7b6a44e71e882ea5f233c49dc3d4a1a870
compyx <b.wassink@ziggo.nl>
2021-12-05 10:20:34 +0000

    Gtk3: VSID: Move the state object/threading stuff into separate files
    This should make it easier to use from multiple other files, and force devs not to use the state object directly but via the vsid_state_lock()/vsid_state_unlock() accessor functions.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@41309 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/uivsidwindow.c
+++ vice/src/arch/gtk3/uivsidwindow.c
@@ -51,6 +51,7 @@
 #include "vsidcontrolwidget.h"
 #include "vsidmainwidget.h"
 #include "vsidmainwidget.h"
+#include "vsidstate.h"
 #include "vsidtuneinfowidget.h"
 #include "vsidtuneinfowidget.h"
 #include "vsidui.h"
@@ -59,209 +60,14 @@
 #include "uivsidwindow.h"
 
 
-/** \brief  VSID UI state object
- *
- * Used to handle communication between the main (VICE) thread and the UI thread.
- *
- * The `_pending` flags indicate a new value has arrived for a widget. There
- * are no flags for string values since non-NULL means a new value has arrived,
- * and no flag for the play time update, since we keep track of the old time
- * to avoid rendering the related widgets (play time/progress bar) again for
- * the same time.
- */
-typedef struct vsid_state_s {
-    /* PSID data */
-
-    /** \brief  Author field of a PSID file */
-    char *author;
-
-    /** \brief  Copyright field of a PSID file
-     *
-     * This should be called 'released' in the UI.
-     */
-    char *copyright;
-
-    /** \brief  IRQ type */
-    char *irq;
-
-    /** \brief  PSID tune name */
-    char *name;
-
-    /** \brief  Number of subtunes */
-    int  tune_count;
-    /** \brief  Number of subtunes needs to be updated in the UI */
-    bool tune_count_pending;
-
-    /** \brief  Current tune number */
-    int  tune_current;
-    /** \brief  Current tune number needs to be updated the UI */
-    bool tune_current_pending;
-
-    /** \brief  Default tune number */
-    int  tune_default;
-    /** \brief  Default tune number needs to be updated in the UI */
-    bool tune_default_pending;
-
-    /** \brief  SID model (0 = 6581, 1 = 8580) */
-    int  model;
-    /** \brief  SID model needs to be updated in the UI */
-    bool model_pending;
-
-    /** \brief  Sync factor (0 = 60Hz, 1 = 50Hz) */
-    int  sync;
-    /** \brief  Sync factor needs to be updated in the UI */
-    bool sync_pending;
-
-    /** \brief  Load address */
-    uint16_t load_addr;
-    /** \brief  Load address needs to be updated in the UI */
-    bool     load_addr_pending;
-
-    /** \brief  Init address */
-    uint16_t init_addr;
-    /** \brief  Init address needs to be updated in the UI */
-    bool     init_addr_pending;
-
-    /** \brief  Play address */
-    uint16_t play_addr;
-    /** \brief  Play address needs to be updated in the UI */
-    bool     play_addr_pending;
-
-    /** \brief  Size of the SID data, excluding PSID header */
-    uint16_t data_size;
-    /** \brief  Size needs to be updated in the UI */
-    bool     data_size_pending;
-
-    /*
-     * VICE's driver
-     */
-
-    /** \brief  Current displayed play time
-     *
-     * Set in the UI thread.
-     */
-    unsigned int current_time;
-
-    /** \brief  New play time
-     *
-     * Set in the main thread.
-     *
-     * When this differs from current_time the UI thread will update the time
-     * widget and set current_time to this.
-     */
-    unsigned int new_time;
-
-    /** \brief  Driver address */
-    uint16_t driver_addr;
-    /** \brief  Driver address needs to be updated in the UI */
-    bool     driver_addr_pending;
-
-} vsid_state_t;
-
-
 /** \brief  Main widget of VSID
  */
 static GtkWidget *main_widget = NULL;
 
+/** \brief  ID of the frame clock update event handler
+ */
 static gulong frame_clock_update_handler;
 
-/** \brief  UI state object for threading, do NOT use directly
- *
- * Do not use directly, but use lock_vsid_state()/unlock_vsid_state() to access.
- */
-vsid_state_t vsid_state_hands_off;
-
-/** \brief  Mutex to handle UI update requests from the VICE thread.
- */
-static pthread_mutex_t vsid_state_lock = PTHREAD_MUTEX_INITIALIZER;
-
-
-
-/** \brief  Get the lock on the VSID state
- *
- * \return  VSID state reference
- */
-static vsid_state_t *lock_vsid_state(void)
-{
-    pthread_mutex_lock(&vsid_state_lock);
-    return &vsid_state_hands_off;
-}
-
-
-/** \brief  Unlock the VSID state
- */
-static void unlock_vsid_state(void)
-{
-    pthread_mutex_unlock(&vsid_state_lock);
-}
-
-
-/** \brief  Initialize UI state object
- */
-static void init_vsid_state(void)
-{
-    /* during setup we can use the object directly */
-    vsid_state_t *state = &vsid_state_hands_off;
-
-    /* Let's be explicit and not rely on automatic initialization to 0/NULL,
-     * when the meaning of 0/NULL for a member changes we're in for some weird
-     * bugs.
-     */
-    state->name = NULL;
-    state->author = NULL;
-    state->copyright = NULL;
-    state->irq = NULL;
-    state->tune_count = 0;
-    state->tune_count_pending = false;
-    state->tune_current = 0;
-    state->tune_current_pending = false;
-    state->tune_default = 0;
-    state->tune_default_pending = false;
-    state->model = 0;
-    state->model_pending = false;
-    state->sync = 0;
-    state->sync_pending = false;
-    state->load_addr = 0;
-    state->load_addr_pending = false;
-    state->init_addr = 0;
-    state->init_addr_pending = false;
-    state->play_addr = 0;
-    state->play_addr_pending = false;
-    state->data_size = 0;
-    state->data_size_pending = false;
-
-    state->current_time = ~0;   /*  force rendering of play time once after */
-    state->new_time = 0;        /*+ UI realization */
-    state->driver_addr = 0;
-    state->driver_addr_pending = false;
-}
-
-
-/** \brief  Clean up any resources used by the VSID state object
- */
-static void shutdown_vsid_state(void)
-{
-    vsid_state_t *state = lock_vsid_state();
-
-    if (state->name != NULL) {
-        lib_free(state->name);
-        state->name = NULL;
-    }
-    if (state->author != NULL) {
-        lib_free(state->author);
-        state->author = NULL;
-    }
-    if (state->copyright != NULL) {
-        lib_free(state->copyright);
-        state->copyright = NULL;
-    }
-    if (state->irq != NULL) {
-        lib_free(state->irq);
-        state->irq = NULL;
-    }
-
-    unlock_vsid_state();
-}
 
 
 /** \brief  UI update handler
@@ -269,7 +75,7 @@ static void shutdown_vsid_state(void)
  */
 static void vsid_ui_update(void)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     /* play time */
     if (state->current_time != state->new_time) {
@@ -355,9 +161,8 @@ static void vsid_ui_update(void)
         state->data_size_pending = false;
     }
 
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
-
 
 
 /** \brief  Called once per frame for frequent asynchronous UI updates.
@@ -391,7 +196,7 @@ static void on_widget_unrealized(GtkWidget *widget, gpointer data)
     frame_clock = gdk_window_get_frame_clock(gtk_widget_get_window(widget));
     g_signal_handler_disconnect(frame_clock, frame_clock_update_handler);
     gdk_frame_clock_end_updating(frame_clock);
-    shutdown_vsid_state();
+    vsid_state_shutdown();
 }
 
 
@@ -451,7 +256,7 @@ int ui_vsid_window_load_psid(const char *filename)
  */
 void ui_vsid_window_init(void)
 {
-    init_vsid_state();
+    vsid_state_init();
     ui_set_create_window_func(vsid_window_create);
     ui_set_handle_dropped_files_func(ui_vsid_window_load_psid);
 }
@@ -467,12 +272,12 @@ void ui_vsid_window_init(void)
  */
 void vsid_ui_display_name(const char *name)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     if (state->name == NULL) {
         state->name = lib_strdup(name);
     }
-    unlock_vsid_state();
+    vsid_state_unlock();
 
 }
 
@@ -483,12 +288,12 @@ void vsid_ui_display_name(const char *name)
  */
 void vsid_ui_display_author(const char *author)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     if (state->author == NULL) {
         state->author = lib_strdup(author);
     }
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -498,12 +303,12 @@ void vsid_ui_display_author(const char *author)
  */
 void vsid_ui_display_copyright(const char *copyright)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     if (state->copyright == NULL) {
         state->copyright = lib_strdup(copyright);
     }
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -513,12 +318,12 @@ void vsid_ui_display_copyright(const char *copyright)
  */
 void vsid_ui_display_irqtype(const char *irq)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     if (state->irq == NULL) {
         state->irq = lib_strdup(irq);
     }
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -528,11 +333,11 @@ void vsid_ui_display_irqtype(const char *irq)
  */
 void vsid_ui_display_sid_model(int model)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->model = model;
     state->model_pending = true;
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -542,11 +347,11 @@ void vsid_ui_display_sid_model(int model)
  */
 void vsid_ui_display_sync(int sync)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->sync = sync;
     state->sync_pending = true;
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -556,10 +361,10 @@ void vsid_ui_display_sync(int sync)
  */
 void vsid_ui_display_time(unsigned int dsec)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->new_time = dsec;
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -569,11 +374,11 @@ void vsid_ui_display_time(unsigned int dsec)
  */
 void vsid_ui_display_nr_of_tunes(int count)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->tune_count = count;
     state->tune_count_pending = true;
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -583,11 +388,11 @@ void vsid_ui_display_nr_of_tunes(int count)
  */
 void vsid_ui_set_default_tune(int nr)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->tune_default = nr;
     state->tune_default_pending = true;
-    unlock_vsid_state();
+    vsid_state_unlock();
 
     vsid_main_widget_set_tune_default(nr);
 }
@@ -599,11 +404,11 @@ void vsid_ui_set_default_tune(int nr)
  */
 void vsid_ui_display_tune_nr(int nr)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->tune_current = nr;
     state->tune_current_pending = true;
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -623,11 +428,11 @@ void vsid_ui_setdrv(char *driver_info_text)
  */
 void vsid_ui_set_driver_addr(uint16_t addr)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->driver_addr = addr;
     state->driver_addr_pending = true;
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -637,11 +442,11 @@ void vsid_ui_set_driver_addr(uint16_t addr)
  */
 void vsid_ui_set_load_addr(uint16_t addr)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->load_addr = addr;
     state->load_addr_pending = true;
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -651,11 +456,11 @@ void vsid_ui_set_load_addr(uint16_t addr)
  */
 void vsid_ui_set_init_addr(uint16_t addr)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->init_addr = addr;
     state->init_addr_pending = true;
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -665,11 +470,11 @@ void vsid_ui_set_init_addr(uint16_t addr)
  */
 void vsid_ui_set_play_addr(uint16_t addr)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->play_addr = addr;
     state->play_addr_pending = true;
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
 
 
@@ -679,9 +484,9 @@ void vsid_ui_set_play_addr(uint16_t addr)
  */
 void vsid_ui_set_data_size(uint16_t size)
 {
-    vsid_state_t *state = lock_vsid_state();
+    vsid_state_t *state = vsid_state_lock();
 
     state->data_size = size;
     state->data_size_pending = true;
-    unlock_vsid_state();
+    vsid_state_unlock();
 }
---
+++ vice/src/arch/gtk3/vsidstate.c
@@ -0,0 +1,118 @@
+/** \file   vsidstate.c
+ * \brief   VSID state module
+ *
+ * This module is a central place to keep track of the state of the VSID UI and
+ * handle communication between the VICE thread and the UI thread.
+ *
+ * \author  Bas Wassink <b.wassink@ziggo.nl>
+ */
+
+#include "vice.h"
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include "lib.h"
+
+#include "vsidstate.h"
+
+
+/** \brief  UI state object for threading, do NOT use directly
+ *
+ * Do not use directly, but use lock_vsid_state()/unlock_vsid_state() to access.
+ */
+vsid_state_t vsid_state_hands_off;
+
+
+/** \brief  Mutex to handle UI update requests from the VICE thread.
+ */
+static pthread_mutex_t vsid_lock = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+/** \brief  Get the lock on the VSID state
+ *
+ * \return  VSID state reference
+ */
+vsid_state_t *vsid_state_lock(void)
+{
+    pthread_mutex_lock(&vsid_lock);
+    return &vsid_state_hands_off;
+}
+
+
+/** \brief  Unlock the VSID state
+ */
+void vsid_state_unlock(void)
+{
+    pthread_mutex_unlock(&vsid_lock);
+}
+
+
+/** \brief  Initialize UI state object
+ */
+void vsid_state_init(void)
+{
+    /* during setup we can use the object directly */
+    vsid_state_t *state = &vsid_state_hands_off;
+
+    /* Let's be explicit and not rely on automatic initialization to 0/NULL,
+     * when the meaning of 0/NULL for a member changes we're in for some weird
+     * bugs.
+     */
+    state->name = NULL;
+    state->author = NULL;
+    state->copyright = NULL;
+    state->irq = NULL;
+    state->tune_count = 0;
+    state->tune_count_pending = false;
+    state->tune_current = 0;
+    state->tune_current_pending = false;
+    state->tune_default = 0;
+    state->tune_default_pending = false;
+    state->model = 0;
+    state->model_pending = false;
+    state->sync = 0;
+    state->sync_pending = false;
+    state->load_addr = 0;
+    state->load_addr_pending = false;
+    state->init_addr = 0;
+    state->init_addr_pending = false;
+    state->play_addr = 0;
+    state->play_addr_pending = false;
+    state->data_size = 0;
+    state->data_size_pending = false;
+
+    state->current_time = ~0;   /*  force rendering of play time once after */
+    state->new_time = 0;        /*+ UI realization */
+    state->driver_addr = 0;
+    state->driver_addr_pending = false;
+}
+
+
+/** \brief  Clean up any resources used by the VSID state object
+ */
+void vsid_state_shutdown(void)
+{
+    vsid_state_t *state = vsid_state_lock();
+
+    if (state->name != NULL) {
+        lib_free(state->name);
+        state->name = NULL;
+    }
+    if (state->author != NULL) {
+        lib_free(state->author);
+        state->author = NULL;
+    }
+    if (state->copyright != NULL) {
+        lib_free(state->copyright);
+        state->copyright = NULL;
+    }
+    if (state->irq != NULL) {
+        lib_free(state->irq);
+        state->irq = NULL;
+    }
+
+    vsid_state_unlock();
+}
