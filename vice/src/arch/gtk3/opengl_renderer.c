a8ed434b1312a821addb5d419d8a3b710c8361ab
dqh <david.q.hogan@gmail.com>
2022-02-05 00:58:17 +0000

    Fixed macOS resize performance when emu fps > host display Hz. And this time, without rewriting half the rendering code in the universe
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@41737 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/opengl_renderer.c
+++ vice/src/arch/gtk3/opengl_renderer.c
@@ -241,10 +241,13 @@ static void on_widget_resized(GtkWidget *widget, GtkAllocation *allocation, gpoi
         context->native_view_bg_b = 0.5f;
     }
 
+    RENDER_LOCK();
+    CANVAS_UNLOCK();
+    
     /* Update the size of the native child window to match the gtk drawing area */
     vice_opengl_renderer_resize_child_view(context);
-
-    CANVAS_UNLOCK();
+    
+    RENDER_UNLOCK();
 }
 
 static void on_widget_monitors_changed(GdkScreen *screen, gpointer data)
