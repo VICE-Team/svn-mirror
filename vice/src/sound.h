243cec9a14c3af539fa69204a3f7555b66867cc3
compyx <b.wassink@ziggo.nl>
2022-03-26 17:08:57 +0000

    Remove unused sound_init_X_device() prototypes
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@42036 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/sound.h
+++ vice/src/sound.h
@@ -179,23 +179,16 @@ extern int sound_cmdline_options_init(void);
 
 
 /* device initialization prototypes */
-extern int sound_init_allegro_device(void);
 extern int sound_init_alsa_device(void);
-extern int sound_init_sb_device(void);
 extern int sound_init_dummy_device(void);
 extern int sound_init_dump_device(void);
 extern int sound_init_fs_device(void);
 extern int sound_init_wav_device(void);
-extern int sound_init_midas_device(void);
 extern int sound_init_sdl_device(void);
 extern int sound_init_sun_device(void);
 extern int sound_init_uss_device(void);
 extern int sound_init_dx_device(void);
-extern int sound_init_ce_device(void);
-extern int sound_init_vidc_device(void);
-extern int sound_init_mmos2_device(void);
 extern int sound_init_dart_device(void);
-extern int sound_init_dart2_device(void);
 extern int sound_init_beos_device(void);
 extern int sound_init_bsp_device(void);
 extern int sound_init_arts_device(void);
