0508866ff1e9552a97afd65ffa272797e3ceddec
compyx <b.wassink@ziggo.nl>
2022-03-27 12:51:08 +0000

    Archdep: archdep_program_path(): Disable debug message in sysctl fallback on FreeBSD
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@42048 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/shared/archdep_program_path.c
+++ vice/src/arch/shared/archdep_program_path.c
@@ -233,8 +233,11 @@ const char *archdep_program_path(void)
 
     /* /proc may not be available on FreeBSD */
     if (readlink("/proc/curproc/file", buffer, sizeof(buffer) - 1) < 0) {
+		/* testing with FreeBSD 13.0 indicates it is indeed not present */
+#if 0
         printf("%s(): failed to read /proc/curproc/file: %d: %s\n",
                 __func__, errno, strerror(errno));
+#endif
         /* try sysctl call */
         mib[0] = CTL_KERN;
         mib[1] = KERN_PROC;
