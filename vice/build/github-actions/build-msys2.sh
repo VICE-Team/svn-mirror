7c672537f651b6e7965a7da5ebe29325ab595967
gpz <groepaz@gmx.net>
2022-04-17 16:53:30 +0000

    do the stylecheck early before the headless build, not in the msys script
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@42250 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/build/github-actions/build-msys2.sh
+++ vice/build/github-actions/build-msys2.sh
@@ -70,7 +70,6 @@ else
     ./configure $ARGS SVN_REVISION_OVERRIDE=$(echo "$2" | sed 's/^r//') || ( echo -e "\n**** CONFIGURE FAILED ****\n" ; cat config.log ; exit 1 )
 fi
 
-make -j $(( $NUMBER_OF_PROCESSORS )) -s stylecheck
 make -j $(( $NUMBER_OF_PROCESSORS )) -s
 make bindistzip
 make bindist7zip
