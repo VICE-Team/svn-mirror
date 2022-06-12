c9db3db5ea19c06437595ab72291b7a9924214e8
dqh <david.q.hogan@gmail.com>
2022-06-12 09:07:44 +0000

    Automatic Cmake generation of Xcode schemes for all executable targets
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@42459 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/cmake-bootstrap.sh
+++ vice/cmake-bootstrap.sh
@@ -463,7 +463,9 @@ function generate_executable_target {
 	
 	cat <<-HEREDOC
 
+		set(CMAKE_XCODE_GENERATE_SCHEME ON)
 		add_executable($executable)
+		set(CMAKE_XCODE_GENERATE_SCHEME OFF)
 
 		target_compile_definitions(
 		    $executable
