0d510651f5776541b06bc78173c71de1cb2245b6
compyx <b.wassink@ziggo.nl>
2022-11-12 18:45:41 +0000

    Gtk3: VSID: Remove some dead code
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@42610 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/uivsidwindow.c
+++ vice/src/arch/gtk3/uivsidwindow.c
@@ -109,7 +109,7 @@ static void vsid_ui_update(void)
     }
     /* current tune number */
     if (state->tune_current_pending) {
-        vsid_main_widget_set_tune_current(state->tune_current);
+        vsid_tune_info_widget_set_tune_current(state->tune_current);
         state->tune_current_pending = false;
     }
     /* default tune number */
