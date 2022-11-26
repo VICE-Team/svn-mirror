bc67e70b7245d3b4c733ee9c5ce73d0e0212c311
compyx <b.wassink@ziggo.nl>
2022-11-26 19:03:03 +0000

    Archdep: archdep_stat(): allow ignoring either `len` or `isdir` argument
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@42660 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/shared/archdep_stat.c
+++ vice/src/arch/shared/archdep_stat.c
@@ -37,15 +37,14 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-
 #include "archdep_stat.h"
 
 
 /** \brief  Determine the size of \a path and whether it's a directory
  *
  * \param[in]   path    pathname
- * \param[out]  len     length of file \a pathname
- * \param[out]  isdir   pathname is a directory
+ * \param[out]  len     length of file \a path (pass `NULL` to ignore)
+ * \param[out]  isdir   \a path is a directory (pass `NULL` to ignore)
  *
  * \return  0 on success, -1 on failure
  */
@@ -54,11 +53,19 @@ int archdep_stat(const char *path, size_t *len, unsigned int *isdir)
     struct stat statbuf;
 
     if (stat(path, &statbuf) < 0) {
-        *len = -1;
-        *isdir = 0;
+        if (len != NULL) {
+            *len = -1;
+        }
+        if (isdir != NULL) {
+            *isdir = 0;
+        }
         return -1;
     }
-    *len = statbuf.st_size;
-    *isdir = S_ISDIR(statbuf.st_mode);
+    if (len != NULL) {
+        *len = statbuf.st_size;
+    }
+    if (isdir != NULL) {
+        *isdir = S_ISDIR(statbuf.st_mode);
+    }
     return 0;
 }
