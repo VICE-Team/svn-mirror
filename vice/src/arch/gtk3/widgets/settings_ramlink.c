7257c68330e6ba6c7a0721033f6915b384a9f5f8
compyx <b.wassink@ziggo.nl>
2023-01-31 09:40:15 +0000

    Gtk3: Settings dialog: Refactor "cartridges/ramlink"
    Try to fix layout as much as possible. Still a layout issue left with the
    "Browse..." buttons for the ROM and RAM images: Due to the presence of the
    (slightly wider) "Save as..."/"Save image" buttons the "Browse..." button for
    the ROM image is slightly less wide than the other "Browse..." button. No
    amount of get_allocated_size()/get_size_allocation()/set_size)_request can fix
    that.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@42985 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/settings_ramlink.c
+++ vice/src/arch/gtk3/widgets/settings_ramlink.c
@@ -49,9 +49,9 @@
 /** \brief  RAMLINK modes
  */
 static const vice_gtk3_radiogroup_entry_t ramlink_modes[] = {
-    { "Normal",     1 },
-    { "Disable",    0 },
-    { NULL,         -1 }
+    { "Normal",   1 },
+    { "Disable",  0 },
+    { NULL,      -1 }
 };
 
 
@@ -64,6 +64,7 @@ static const vice_gtk3_radiogroup_entry_t ramlink_modes[] = {
 GtkWidget *settings_ramlink_widget_create(GtkWidget *parent)
 {
     GtkWidget *grid;
+    GtkWidget *wrapper;
     GtkWidget *label;
     GtkWidget *enable;
     GtkWidget *rtc_save;
@@ -73,61 +74,101 @@ GtkWidget *settings_ramlink_widget_create(GtkWidget *parent)
     GtkWidget *rom_widget;
 
     /* use three columns for the label */
-    grid = vice_gtk3_grid_new_spaced_with_label(-1, -1, CARTRIDGE_NAME_RAMLINK " settings", 2);
+    grid = vice_gtk3_grid_new_spaced_with_label(8, 8, CARTRIDGE_NAME_RAMLINK " settings", 4);
 
     /* create 'enable ramlink' checkbox */
-    enable = vice_gtk3_resource_check_button_new("RAMLINK", "Enable " CARTRIDGE_NAME_RAMLINK);
-    gtk_widget_set_margin_start(enable, 16);
+    enable = vice_gtk3_resource_check_button_new("RAMLINK",
+                                                 "Enable " CARTRIDGE_NAME_RAMLINK);
+    gtk_widget_set_margin_start(enable, 8);
     gtk_grid_attach(GTK_GRID(grid), enable, 0, 1, 1, 1);
 
     /* create 'RTC Save' checkbox */
-    rtc_save = vice_gtk3_resource_check_button_new("RAMLINKRTCSave", "RTC Save");
-    gtk_widget_set_margin_start(rtc_save, 16);
+    rtc_save = vice_gtk3_resource_check_button_new("RAMLINKRTCSave",
+                                                   "RTC Save");
+    gtk_widget_set_margin_start(rtc_save, 8);
+    gtk_widget_set_valign(rtc_save, GTK_ALIGN_START);
     gtk_grid_attach(GTK_GRID(grid), rtc_save, 0, 2, 1, 1);
 
     /* create mode widget */
-    mode = vice_gtk3_resource_radiogroup_new(
-            "RAMLINKmode",
-            ramlink_modes,
-            GTK_ORIENTATION_HORIZONTAL);
+    mode = vice_gtk3_resource_radiogroup_new("RAMLINKmode",
+                                             ramlink_modes,
+                                             GTK_ORIENTATION_VERTICAL);
     /* create mode label */
     label = gtk_label_new("Mode");
     gtk_widget_set_halign(label, GTK_ALIGN_START);
-    gtk_widget_set_margin_start(label, 16);
+    gtk_widget_set_valign(label, GTK_ALIGN_START);
+    gtk_widget_set_margin_start(label, 8);
     gtk_grid_attach(GTK_GRID(grid), label, 2, 2, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), mode, 3, 2, 1, 1);
 
     /* create size widget */
-    size = vice_gtk3_resource_spin_int_new(
-            "RAMLINKsize",
-            0, 16, 1);
+    size = vice_gtk3_resource_spin_int_new("RAMLINKsize",
+                                           0, 16, 1);
     label = gtk_label_new("Size (MiB)");
     gtk_widget_set_halign(label, GTK_ALIGN_START);
-    gtk_widget_set_margin_start(label, 16);
+    gtk_widget_set_margin_start(label, 8);
     gtk_grid_attach(GTK_GRID(grid), label, 2, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), size, 3, 1, 1, 1);
     /* fix size of the spin button */
     gtk_widget_set_hexpand(size, FALSE);
     gtk_widget_set_halign(size, GTK_ALIGN_START);
 
-    /* ROM image browser widget */
-    rom_widget = vice_gtk3_resource_browser_new(
-            "RAMLINKBIOSfilename",
-            NULL,
-            NULL,
-            "Select Ramlink ROM image",
-            CARTRIDGE_NAME_RAMLINK " ROM",
-            NULL);
-    gtk_grid_attach(GTK_GRID(grid), rom_widget, 0, 4, 4, 1);
+    /* ROM image browser widget (mimic the cart image layout */
+    wrapper = vice_gtk3_grid_new_spaced_with_label(8, 0, "RAMLink ROM Image", 1);
+    vice_gtk3_grid_set_title_margin(wrapper, 8);
+    rom_widget = vice_gtk3_resource_browser_new("RAMLINKBIOSfilename",
+                                                NULL,
+                                                NULL,
+                                                "Select Ramlink ROM image",
+                                                "File name:",
+                                                NULL);
+    gtk_widget_set_margin_start(rom_widget, 8);
+    gtk_grid_attach(GTK_GRID(wrapper), rom_widget, 0, 1, 1, 1);
+    gtk_widget_set_margin_top(wrapper, 16);
+    gtk_widget_set_margin_bottom(wrapper, 24);
+    gtk_grid_attach(GTK_GRID(grid), wrapper, 0, 4, 4, 1);
 
     /* create RAMlink image browser */
-    cart_widget = cart_image_widget_create(
-            parent,
-            CARTRIDGE_NAME_RAMLINK " RAM Image",
-            "RAMLINKfilename", "RAMLINKImageWrite",
-            CARTRIDGE_NAME_RAMLINK, CARTRIDGE_RAMLINK);
-
+    cart_widget = cart_image_widget_create(parent,
+                                           CARTRIDGE_NAME_RAMLINK " RAM Image",
+                                           "RAMLINKfilename",
+                                           "RAMLINKImageWrite",
+                                           CARTRIDGE_NAME_RAMLINK,
+                                           CARTRIDGE_RAMLINK);
     gtk_grid_attach(GTK_GRID(grid), cart_widget, 0, 5, 4, 1);
+
     gtk_widget_show_all(grid);
+
+    /* Try to make the two "Browse ..." buttons the same width
+     * Doesn't work of course, get_allocated_width() returns 1, and
+     * get_size_allocation() returns allocation.width == 1.
+     * So although we request the widgets get allocated by calling
+     * gtk_widget_show_all(), at this point they're not drawn yet.
+     * No idea if this is normal Gtk behaviour or if it's a consequence of our
+     * threading code/gdk main loop handling.
+     *
+     * An alternate method to align the buttons would be to remove the widgets
+     * from the ROM grid (label, entry, button) with something like:
+     *
+     *      button = gtk_grid_get_child_at(cart_widget, 2, 1);
+     *      g_object_ref(button);
+     *      gtk_container_remove(rom_widget, button);
+     *      gtk_grid_insert_row(rom_widget, 0);
+     *      gtk_grid_attach(rom_widget, button, .. , ..);
+     *      g_object_unref(button)
+     *
+     * for title, label, entry and button =)
+     */
+#if 0
+    GtkAllocation allocation;
+    GtkWidget *rom_browse = gtk_grid_get_child_at(GTK_GRID(rom_widget), 2, 0);
+    GtkWidget *ram_browse = gtk_grid_get_child_at(GTK_GRID(cart_widget), 2, 1);
+
+    int width = gtk_widget_get_allocated_width(ram_browse);
+    gtk_widget_get_allocated_size(ram_browse, &allocation, NULL);
+    g_print("Width = %d, allocation.width = %d\n", width, allocation.width);
+    gtk_widget_set_size_request(rom_browse, allocation.width, -1);
+#endif
+
     return grid;
 }
