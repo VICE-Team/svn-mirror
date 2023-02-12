2f1474e3de11b9334df36910bae5ab54856933c3
compyx <b.wassink@ziggo.nl>
2023-02-12 14:40:54 +0000

    Gtk3: IEEE-488 settings: refactor to use the resource file chooser widget
    Use the resource file chooser instead of custom code with entry, button and
    dialog.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@43152 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/settings_ieee488.c
+++ vice/src/arch/gtk3/widgets/settings_ieee488.c
@@ -31,24 +31,14 @@
  */
 
 #include "vice.h"
-
 #include <gtk/gtk.h>
 
-#include "vice_gtk3.h"
 #include "cartridge.h"
 #include "log.h"
-#include "machine.h"
 #include "resources.h"
-#include "ui.h"
+#include "vice_gtk3.h"
 
 #include "settings_ieee488.h"
-
-
-/** \brief  Reference to the entry widget used for the IEEE-488 ROM
- *
- * TODO:    Check if we can refactor this code to use a 'base widget'
- */
-static GtkWidget *entry_widget;
 
 
 /** \brief  Handler for the 'toggled' event of the 'enable' check button
@@ -63,21 +53,20 @@ static GtkWidget *entry_widget;
  */
 static void on_enable_toggled(GtkWidget *widget, gpointer data)
 {
-    int state;
-    const char *image;
+    gboolean state;
 
     state = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
     if (state) {
-        if (resources_get_string("IEEE488Image", &image) < 0) {
-            image = NULL;
-        }
+        const char *image = NULL;
 
+        resources_get_string("IEEE488Image", &image);
         if (image == NULL || *image == '\0') {
             /* no image */
-            vice_gtk3_message_error("VICE core",
-                    "Cannot enable IEEE-488 adapter, no image specified.");
+            vice_gtk3_message_error("IEEE-488 Error",
+                                    "Cannot enable the IEEE-488 adapter,"
+                                    " no IEEE-488 image has been selected.");
             gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(widget), FALSE);
-            state = 0;
+            state = FALSE;
         }
     }
 
@@ -93,52 +82,6 @@ static void on_enable_toggled(GtkWidget *widget, gpointer data)
 }
 
 
-/** \brief  Callback for the open-file dialog
- *
- * \param[in,out]   dialog      open-file dialog
- * \param[in]       filename    filename or NULL on cancel
- * \param[in]       data        extra data (unused)
- */
-static void browse_filename_callback(GtkDialog *dialog,
-                                     gchar *filename,
-                                     gpointer data)
-{
-    if (filename != NULL) {
-        gtk_entry_set_text(GTK_ENTRY(entry_widget), filename);
-
-        /* required, since setting the text of the entry doesn't trigger an
-         * update of the connected resource (it only responds to focus-out and
-         * pressing 'Enter' */
-        if (resources_set_string("IEEE488Image", filename) < 0) {
-            vice_gtk3_message_error("VICE core",
-                    "Failed to set '%s' as IEEE-488 EEPROM image.",
-                    filename);
-        }
-        g_free(filename);
-    }
-
-    gtk_widget_destroy(GTK_WIDGET(dialog));
-}
-
-
-/** \brief  Handler for the "clicked" event of the browse button
- *
- * Activates a file-open dialog and stores the file name in the GtkEntry passed
- * in \a user_data if valid, triggering a resource update.
- *
- * \param[in]   widget      button
- * \param[in]   user_data   entry to store filename in
- */
-static void on_browse_clicked(GtkWidget *widget, gpointer user_data)
-{
-    vice_gtk3_open_file_dialog(
-            "Open IEEE-488 image",
-            NULL, NULL, NULL,
-            browse_filename_callback,
-            NULL);
-}
-
-
 /** \brief  Create widget to control IEEE-488 adapter
  *
  * \param[in]   parent  parent widget
@@ -147,44 +90,45 @@ static void on_browse_clicked(GtkWidget *widget, gpointer user_data)
  */
 GtkWidget *settings_ieee488_widget_create(GtkWidget *parent)
 {
-    GtkWidget *grid;
-    GtkWidget *enable_widget;
-    GtkWidget *label;
-    GtkWidget *browse;
+    GtkWidget  *grid;
+    GtkWidget  *enable;
+    GtkWidget  *chlabel;
+    GtkWidget  *chooser;
+    const char *patterns[] = { "*.bin", "*.rom", NULL };
+    const char *image = NULL;
+    int         cart_enabled;
 
-    const char *image;
-    int enable_state;
+    resources_get_string("IEEE488Image", &image);
+    cart_enabled = cartridge_type_enabled(CARTRIDGE_IEEE488);
 
-    if (resources_get_string("IEEE488Image", &image) < 0) {
-        image = NULL;
-    }
-    enable_state = cartridge_type_enabled(CARTRIDGE_IEEE488);
-
-    grid = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, VICE_GTK3_DEFAULT);
+    grid = vice_gtk3_grid_new_spaced(8, 16);
 
     /* we can't use a `resource_check_button` here, since toggling the resource
      * depends on whether an image file is specified
      */
-    enable_widget = gtk_check_button_new_with_label("Enable IEEE-488 interface");
-    gtk_grid_attach(GTK_GRID(grid), enable_widget, 0, 0, 3, 1);
-
+    enable = gtk_check_button_new_with_label("Enable IEEE-488 interface");
     /* only set state to true if both the state is true and an image is given */
-    if (enable_state && (image != NULL && *image != '\0')) {
-        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(enable_widget), TRUE);
+    if (cart_enabled && (image != NULL && *image != '\0')) {
+        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(enable), TRUE);
     }
+    g_signal_connect(enable,
+                     "toggled",
+                     G_CALLBACK(on_enable_toggled),
+                     NULL);
 
-    label = gtk_label_new("IEEE-488 image");
-    gtk_widget_set_halign(label, GTK_ALIGN_START);
-    entry_widget = vice_gtk3_resource_entry_new("IEEE488Image");
-    gtk_widget_set_hexpand(entry_widget, TRUE);
-    browse = gtk_button_new_with_label("Browse ...");
-    g_signal_connect(browse, "clicked", G_CALLBACK(on_browse_clicked), NULL);
+    chlabel = gtk_label_new("IEEE-488 Image");
+    gtk_widget_set_halign(chlabel, GTK_ALIGN_START);
+    chooser = vice_gtk3_resource_filechooser_new("IEEE488Image",
+                                                 GTK_FILE_CHOOSER_ACTION_OPEN);
+    vice_gtk3_resource_filechooser_set_filter(chooser,
+                                              "ROM files",
+                                              patterns,
+                                              TRUE);
 
-    gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), entry_widget, 1, 1, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), browse, 2, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), enable,  0, 0, 2, 1);
+    gtk_grid_attach(GTK_GRID(grid), chlabel, 0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), chooser, 1, 1, 1, 1);
 
-    g_signal_connect(enable_widget, "toggled", G_CALLBACK(on_enable_toggled), NULL);
 
     gtk_widget_show_all(grid);
     return grid;
