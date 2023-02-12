35c90286eccc62d7f64e9bf9e2fe877f4bb04e9e
compyx <b.wassink@ziggo.nl>
2023-02-12 20:55:51 +0000

    Gtk3: IEEE Flash 64! settings: simplify code and tweak layout
    Use resource file chooser instead of custom code, tweak spacing between widgets.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@43157 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/settings_ieeeflash64.c
+++ vice/src/arch/gtk3/widgets/settings_ieeeflash64.c
@@ -2,6 +2,7 @@
  * \brief   Settings widget to control IEEE Flash! 64 resources
  *
  * \author  Christopher Bongaarts <cab@bongalow.net>
+ * \author  Bas Wassink <b.wassink@ziggo.nl>
  */
 
 /*
@@ -36,20 +37,13 @@
 #include "vice.h"
 #include <gtk/gtk.h>
 
-#include "vice_gtk3.h"
-#include "machine.h"
-#include "resources.h"
 #include "cartridge.h"
 #include "log.h"
+#include "resources.h"
+#include "vice_gtk3.h"
 
 #include "settings_ieeeflash64.h"
 
-
-/** \brief  Reference to the entry widget used for the IEEE-488 ROM
- *
- * TODO:    Check if we can refactor this code to use a 'base widget'
- */
-static GtkWidget *entry_widget;
 
 /** \brief  Handler for the "toggled" event of the 'enable' check button
  *
@@ -63,19 +57,18 @@ static GtkWidget *entry_widget;
  */
 static void on_enable_toggled(GtkWidget *widget, gpointer data)
 {
-    int state;
-    const char *image;
+    gboolean state;
 
     state = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
     if (state) {
-        if (resources_get_string("IEEEFlash64Image", &image) < 0) {
-            image = NULL;
-        }
+        const char *image = NULL;
 
+        resources_get_string("IEEEFlash64Image", &image);
         if (image == NULL || *image == '\0') {
             /* no image */
-            vice_gtk3_message_error("VICE core",
-                    "Cannot enable IEEE Flash! 64 adapter, no image specified.");
+            vice_gtk3_message_error(CARTRIDGE_NAME_IEEEFLASH64 " Error",
+                                    "Cannot enable " CARTRIDGE_NAME_IEEEFLASH64
+                                    ", no image specified.");
             gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(widget), FALSE);
             state = 0;
         }
@@ -92,89 +85,6 @@ static void on_enable_toggled(GtkWidget *widget, gpointer data)
     }
 }
 
-/** \brief  Callback for the open-file dialog
- *
- * \param[in,out]   dialog      open-file dialog
- * \param[in]       filename    filename or NULL on cancel
- * \param[in]       data        extra data (unused)
- */
-static void browse_filename_callback(GtkDialog *dialog,
-                                     gchar *filename,
-                                     gpointer data)
-{
-    if (filename != NULL) {
-        gtk_entry_set_text(GTK_ENTRY(entry_widget), filename);
-
-        /* required, since setting the text of the entry doesn't trigger an
-         * update of the connected resource (it only responds to focus-out and
-         * pressing 'Enter' */
-        if (resources_set_string("IEEEFlash64Image", filename) < 0) {
-            vice_gtk3_message_error("VICE core",
-                    "Failed to set '%s' as IEEE Flash! 64 EEPROM image.",
-                    filename);
-        }
-        g_free(filename);
-    }
-
-    gtk_widget_destroy(GTK_WIDGET(dialog));
-}
-
-/** \brief  Handler for the "clicked" event of the browse button
- *
- * Activates a file-open dialog and stores the file name in the GtkEntry passed
- * in \a user_data if valid, triggering a resource update.
- *
- * \param[in]   widget      button
- * \param[in]   user_data   entry to store filename in
- */
-static void on_browse_clicked(GtkWidget *widget, gpointer user_data)
-{
-    vice_gtk3_open_file_dialog(
-            "Open IEEE Flash! 64 image",
-            NULL, NULL, NULL,
-            browse_filename_callback,
-            NULL);
-}
-
-/** \brief  Create device 8 switch checkbutton
- *
- * \return  GtkCheckButton
- */
-static GtkWidget *create_ieeeflash64_dev8_widget(void)
-{
-    GtkWidget *check;
-
-    check = vice_gtk3_resource_check_button_new(
-            "IEEEFlash64Dev8", "Route device 8 to IEEE bus");
-    return check;
-}
-
-/** \brief  Create device 9/10 switch checkbutton
- *
- * \return  GtkCheckButton
- */
-static GtkWidget *create_ieeeflash64_dev910_widget(void)
-{
-    GtkWidget *check;
-
-    check = vice_gtk3_resource_check_button_new(
-            "IEEEFlash64Dev910", "Route devices 9/10 to IEEE bus");
-    return check;
-}
-
-/** \brief  Create device 4 switch checkbutton
- *
- * \return  GtkCheckButton
- */
-static GtkWidget *create_ieeeflash64_dev4_widget(void)
-{
-    GtkWidget *check;
-
-    check = vice_gtk3_resource_check_button_new(
-            "IEEEFlash64Dev4", "Route device 4 to IEEE bus");
-    return check;
-}
-
 
 /** \brief  Create widget to control IEEE Flash! 64 resources
  *
@@ -184,56 +94,60 @@ static GtkWidget *create_ieeeflash64_dev4_widget(void)
  */
 GtkWidget *settings_ieeeflash64_widget_create(GtkWidget *parent)
 {
-    GtkWidget *grid;
-    GtkWidget *ieeeflash64_dev8_widget;
-    GtkWidget *ieeeflash64_dev910_widget;
-    GtkWidget *ieeeflash64_dev4_widget;
+    GtkWidget  *grid;
+    GtkWidget  *enable;
+    GtkWidget  *chlabel;
+    GtkWidget  *chooser;
+    GtkWidget  *dev8_route;
+    GtkWidget  *dev910_route;
+    GtkWidget  *dev4_route;
+    const char *patterns[] = { "*.bin", "*.rom", NULL };
+    const char *image = NULL;
+    int         cart_enabled;
 
-    GtkWidget *enable_widget;
-    GtkWidget *label;
-    GtkWidget *browse;
+    resources_get_string("IEEEFlash64Image", &image);
+    cart_enabled = cartridge_type_enabled(CARTRIDGE_IEEEFLASH64);
 
-    const char *image;
-    int enable_state;
-
-    if (resources_get_string("IEEEFlash64Image", &image) < 0) {
-        image = NULL;
-    }
-    enable_state = cartridge_type_enabled(CARTRIDGE_IEEEFLASH64);
-
-    grid = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, VICE_GTK3_DEFAULT);
+    grid = gtk_grid_new();
+    gtk_grid_set_column_spacing(GTK_GRID(grid), 8);
 
     /* we can't use a `resource_check_button` here, since toggling the resource
      * depends on whether an image file is specified
      */
-    enable_widget = gtk_check_button_new_with_label("Enable IEEE Flash! 64 interface");
-    gtk_grid_attach(GTK_GRID(grid), enable_widget, 0, 0, 3, 1);
-
+    enable = gtk_check_button_new_with_label("Enable " CARTRIDGE_NAME_IEEEFLASH64);
+    gtk_widget_set_margin_bottom(enable, 16);
     /* only set state to true if both the state is true and an image is given */
-    if (enable_state && (image != NULL && *image != '\0')) {
-        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(enable_widget), TRUE);
+    if (cart_enabled && (image != NULL && *image != '\0')) {
+        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(enable), TRUE);
     }
+    g_signal_connect(G_OBJECT(enable),
+                     "toggled",
+                     G_CALLBACK(on_enable_toggled),
+                     NULL);
 
-    label = gtk_label_new("IEEE Flash! 64 KERNAL ROM image");
-    gtk_widget_set_halign(label, GTK_ALIGN_START);
-    entry_widget = vice_gtk3_resource_entry_new("IEEEFlash64Image");
-    gtk_widget_set_hexpand(entry_widget, TRUE);
-    browse = gtk_button_new_with_label("Browse ...");
-    g_signal_connect(browse, "clicked", G_CALLBACK(on_browse_clicked), NULL);
+    chlabel = gtk_label_new(CARTRIDGE_NAME_IEEEFLASH64 " KERNAL ROM Image");
+    gtk_widget_set_halign(chlabel, GTK_ALIGN_START);
+    chooser = vice_gtk3_resource_filechooser_new("IEEEFlash64Image",
+                                                 GTK_FILE_CHOOSER_ACTION_OPEN);
+    vice_gtk3_resource_filechooser_set_filter(chooser,
+                                              "ROM files",
+                                              patterns,
+                                              TRUE);
 
-    gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), entry_widget, 1, 1, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), browse, 2, 1, 1, 1);
+    dev8_route   = vice_gtk3_resource_check_button_new("IEEEFlash64Dev8",
+                                                       "Route device 8 to IEEE bus");
+    dev910_route = vice_gtk3_resource_check_button_new("IEEEFlash64Dev910",
+                                                       "Route devices 9/10 to IEEE bus");
+    dev4_route   = vice_gtk3_resource_check_button_new("IEEEFlash64Dev4",
+                                                       "Route device 4 to IEEE bus");
+    gtk_widget_set_margin_top(dev8_route, 16);
 
-    g_signal_connect(enable_widget, "toggled", G_CALLBACK(on_enable_toggled), NULL);
-
-    ieeeflash64_dev8_widget   = create_ieeeflash64_dev8_widget();
-    ieeeflash64_dev910_widget = create_ieeeflash64_dev910_widget();
-    ieeeflash64_dev4_widget   = create_ieeeflash64_dev4_widget();
-
-    gtk_grid_attach(GTK_GRID(grid), ieeeflash64_dev8_widget,   0, 2, 3, 1);
-    gtk_grid_attach(GTK_GRID(grid), ieeeflash64_dev910_widget, 0, 3, 3, 1);
-    gtk_grid_attach(GTK_GRID(grid), ieeeflash64_dev4_widget,   0, 4, 3, 1);
+    gtk_grid_attach(GTK_GRID(grid), enable,       0, 0, 2, 1);
+    gtk_grid_attach(GTK_GRID(grid), chlabel,      0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), chooser,      1, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), dev8_route,   0, 2, 2, 1);
+    gtk_grid_attach(GTK_GRID(grid), dev910_route, 0, 3, 2, 1);
+    gtk_grid_attach(GTK_GRID(grid), dev4_route,   0, 4, 2, 1);
 
     gtk_widget_show_all(grid);
     return grid;
