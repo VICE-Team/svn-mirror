958c9a5c0c1afa7efe26d56e2188101023da503b
compyx <b.wassink@ziggo.nl>
2023-02-14 19:31:33 +0000

    Gtk3: Settings dialog: refactor Magic Voice settings
    Tweak layout, use resource filechooser for ROM selection.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@43173 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/settings_magicvoice.c
+++ vice/src/arch/gtk3/widgets/settings_magicvoice.c
@@ -34,9 +34,6 @@
 #include <gtk/gtk.h>
 
 #include "cartridge.h"
-#include "debug_gtk3.h"
-#include "machine.h"
-#include "resources.h"
 #include "vice_gtk3.h"
 
 #include "settings_magicvoice.h"
@@ -52,25 +49,34 @@ GtkWidget *settings_magicvoice_widget_create(GtkWidget *parent)
 {
     GtkWidget *grid;
     GtkWidget *enable;
-    GtkWidget *browser;
+    GtkWidget *label;
+    GtkWidget *chooser;
+    const char *title;
+    const char *patterns[] = { "*.bin", "*.rom", NULL };
 
-    grid = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, VICE_GTK3_DEFAULT);
+    grid = gtk_grid_new();
+    gtk_grid_set_column_spacing(GTK_GRID(grid), 8);
+    gtk_grid_set_row_spacing(GTK_GRID(grid), 8);
 
     /* enable checkbutton */
-    enable = carthelpers_create_enable_check_button(
-            CARTRIDGE_NAME_MAGIC_VOICE,
-            CARTRIDGE_MAGIC_VOICE);
-    gtk_grid_attach(GTK_GRID(grid), enable, 0, 0, 1, 1);
+    enable = carthelpers_create_enable_check_button(CARTRIDGE_NAME_MAGIC_VOICE,
+                                                    CARTRIDGE_MAGIC_VOICE);
+    gtk_grid_attach(GTK_GRID(grid), enable, 0, 0, 2, 1);
 
     /* image browser widget */
-    browser = vice_gtk3_resource_browser_new(
-            "MagicVoiceImage",
-            NULL,
-            NULL,
-            "Select Magic Voice ROM image",
-            "Magic Voice ROM",
-            NULL);
-    gtk_grid_attach(GTK_GRID(grid), browser, 0, 1, 1, 1);
+    label = gtk_label_new("ROM image file");
+    gtk_widget_set_margin_start(label, GTK_ALIGN_START);
+    title   = "Select " CARTRIDGE_NAME_MAGIC_VOICE " ROM image file";
+    chooser = vice_gtk3_resource_filechooser_new("MagicVoiceImage",
+                                                 GTK_FILE_CHOOSER_ACTION_OPEN);
+    vice_gtk3_resource_filechooser_set_custom_title(chooser, title);
+    vice_gtk3_resource_filechooser_set_filter(chooser,
+                                              "ROM images",
+                                              patterns,
+                                              TRUE);
+
+    gtk_grid_attach(GTK_GRID(grid), label,   0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), chooser, 1, 1, 1, 1);
 
     gtk_widget_show_all(grid);
     return grid;
