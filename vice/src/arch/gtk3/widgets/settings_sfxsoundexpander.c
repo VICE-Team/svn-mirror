a0dfa5ad6e739a3c94f296642e4ca7dc64da08f9
compyx <b.wassink@ziggo.nl>
2023-02-15 22:01:47 +0000

    Gtk3: Settings dialog: Update SFX Sound Expander settings
    Tweak layout (keep check buttons together), use function that checks the result
    of cartridge_enable() before enabling the check button. Use separate IDs for
    the c64 and vic20 "variants" from cartridge.h.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@43179 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/settings_sfxsoundexpander.c
+++ vice/src/arch/gtk3/widgets/settings_sfxsoundexpander.c
@@ -34,9 +34,8 @@
 #include "vice.h"
 #include <gtk/gtk.h>
 
+#include "cartridge.h"
 #include "machine.h"
-#include "resources.h"
-#include "debug_gtk3.h"
 #include "vice_gtk3.h"
 
 #include "settings_sfxsoundexpander.h"
@@ -47,7 +46,7 @@
 static const vice_gtk3_radiogroup_entry_t chip_models[] = {
     { "3526", 3526 },
     { "3812", 3812 },
-    { NULL, -1 }
+    { NULL,     -1 }
 };
 
 
@@ -63,6 +62,18 @@ static GtkWidget *chip_group = NULL;
 static GtkWidget *io_swap = NULL;
 
 
+/** \brief  Set sensitivity of the IO swap and YM Chip selection widgets
+ *
+ * \param[in]   sensitive   sensitivity
+ */
+static void set_widgets_sensitivity(gboolean sensitive)
+{
+    gtk_widget_set_sensitive(chip_group, sensitive);
+    if (io_swap != NULL) {
+        gtk_widget_set_sensitive(io_swap, sensitive);
+    }
+}
+
 /** \brief  Handler for the 'toggled' event of the SFXSE check button
  *
  * \param[in]       widget      check button
@@ -70,14 +81,10 @@ static GtkWidget *io_swap = NULL;
  */
 static void on_sfx_sound_expander_toggled(GtkWidget *widget, gpointer user_data)
 {
-    int state = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
+    gboolean active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
 
-    gtk_widget_set_sensitive(chip_group, state);
-    if (machine_class == VICE_MACHINE_VIC20) {
-        gtk_widget_set_sensitive(io_swap, state);
-    }
+    set_widgets_sensitivity(active);
 }
-
 
 /** \brief  Create YM chip selection widget
  *
@@ -85,22 +92,12 @@ static void on_sfx_sound_expander_toggled(GtkWidget *widget, gpointer user_data)
  */
 static GtkWidget *create_sfx_chip_widget(void)
 {
-    GtkWidget *grid;
-    GtkWidget *label;
-
-    grid = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, VICE_GTK3_DEFAULT);
-
-    label = gtk_label_new("YM chip model");
-    gtk_widget_set_margin_start(label, 16);
-    gtk_widget_set_halign(label, GTK_ALIGN_START);
-    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
-
-    chip_group = vice_gtk3_resource_radiogroup_new("SFXSoundExpanderChip",
-            chip_models, GTK_ORIENTATION_HORIZONTAL);
-    gtk_grid_attach(GTK_GRID(grid), chip_group, 1, 0, 1, 1);
-
-    gtk_widget_show_all(grid);
-    return grid;
+    GtkWidget *group;
+    group = vice_gtk3_resource_radiogroup_new("SFXSoundExpanderChip",
+                                              chip_models,
+                                              GTK_ORIENTATION_HORIZONTAL);
+    gtk_grid_set_column_spacing(GTK_GRID(group), 16);
+    return group;
 }
 
 
@@ -113,27 +110,46 @@ static GtkWidget *create_sfx_chip_widget(void)
 GtkWidget *settings_sfxsoundexpander_widget_create(GtkWidget *parent)
 {
     GtkWidget *grid;
-    GtkWidget *sfx_enable;
+    GtkWidget *label;
+    GtkWidget *enable;
+    gboolean   active;
+    int        row = 0;
+    int        cart_id;
 
-    grid = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, VICE_GTK3_DEFAULT);
-
-    sfx_enable = vice_gtk3_resource_check_button_new("SFXSoundExpander",
-            "Enable SFX Sound Expander");
-    gtk_grid_attach(GTK_GRID(grid), sfx_enable, 0, 0, 1, 1);
-
-    gtk_grid_attach(GTK_GRID(grid), create_sfx_chip_widget(), 0, 1, 1, 1);
+    grid = gtk_grid_new();
+    gtk_grid_set_column_spacing(GTK_GRID(grid), 8);
+    gtk_grid_set_row_spacing(GTK_GRID(grid), 8);
 
     if (machine_class == VICE_MACHINE_VIC20) {
-        io_swap = vice_gtk3_resource_check_button_new(
-                "SFXSoundExpanderIOSwap", "Enable MasC=uerade I/O swap");
-        gtk_widget_set_margin_start(io_swap, 16);
-        gtk_grid_attach(GTK_GRID(grid), io_swap, 0, 2, 1, 1);
+        cart_id = CARTRIDGE_VIC20_SFX_SOUND_EXPANDER;
+    } else {
+        cart_id = CARTRIDGE_SFX_SOUND_EXPANDER;
+    }
+    enable = carthelpers_create_enable_check_button(CARTRIDGE_NAME_SFX_SOUND_EXPANDER,
+                                                    cart_id);
+    gtk_grid_attach(GTK_GRID(grid), enable, 0, row, 2, 1);
+    row++;
+
+    if (machine_class == VICE_MACHINE_VIC20) {
+        io_swap = vice_gtk3_resource_check_button_new("SFXSoundExpanderIOSwap",
+                                                      "Enable MasC=uerade I/O swap");
+        gtk_grid_attach(GTK_GRID(grid), io_swap, 0, row, 2, 1);
+        row++;
     }
 
-    g_signal_connect(sfx_enable, "toggled",
-            G_CALLBACK(on_sfx_sound_expander_toggled), (gpointer)chip_group);
-    /* set proper state */
-    on_sfx_sound_expander_toggled(sfx_enable, (gpointer)chip_group);
+    label = gtk_label_new("YM chip model");
+    gtk_widget_set_halign(label, GTK_ALIGN_START);
+    chip_group = create_sfx_chip_widget();
+    gtk_grid_attach(GTK_GRID(grid), label,      0, row, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), chip_group, 1, row, 1, 1);
+
+    g_signal_connect_unlocked(enable, "toggled",
+                              G_CALLBACK(on_sfx_sound_expander_toggled),
+                              NULL);
+    /* set proper widget sensitivity */
+    active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(enable));
+    set_widgets_sensitivity(active);
+
     gtk_widget_show_all(grid);
     return grid;
 }
