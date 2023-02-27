2efe596c60bc1709e2bb639754e2e352ccbe08ff
compyx <b.wassink@ziggo.nl>
2023-02-27 21:16:54 +0000

    Gtk3: PET REU settings: use RAM sizes widget and file chooser widget
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@43349 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/settings_petreu.c
+++ vice/src/arch/gtk3/widgets/settings_petreu.c
@@ -39,27 +39,30 @@
 #include "settings_petreu.h"
 
 
-/** \brief  List of REU sizes
- */
-static const vice_gtk3_radiogroup_entry_t reu_sizes[] = {
-    { "128KiB",     128 },
-    { "512KiB",     512 },
-    { "1024KiB",    1024 },
-    { "2048KiB",    2048 },
-    { NULL,         -1 }
-};
+/** \brief  List of REU sizes in KiB */
+static const int reu_sizes[] = { 128, 512, 1024, 2048, -1 };
 
 
-/* references to widgets to be able to toggle sensitive state depending on the
- * REU Enable check button
+/* References to widgets to be able to toggle sensitive state depending on the
+ * REU Enable check button.
  */
 
-/** \brief  REU file browser */
-static GtkWidget *browser = NULL;
+/** \brief  REU image file chooser */
+static GtkWidget *chooser = NULL;
 
 /** \brief  REU size radio group */
 static GtkWidget *group = NULL;
 
+
+/** \brief  Set sensitivity of RAM size and image file chooser widgets
+ *
+ * \param[in]   sensitive   new sensitivity
+ */
+static void set_widgets_sensitivity(gboolean sensitive)
+{
+    gtk_widget_set_sensitive(group,   sensitive);
+    gtk_widget_set_sensitive(chooser, sensitive);
+}
 
 /** \brief  Handler for the "toggled" event of the REU Enable check button
  *
@@ -70,10 +73,24 @@ static GtkWidget *group = NULL;
  */
 static void on_reu_toggled(GtkWidget *widget, gpointer user_data)
 {
-    int state = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
+    gboolean active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
 
-    gtk_widget_set_sensitive(browser, state);
-    gtk_widget_set_sensitive(group, state);
+    set_widgets_sensitivity(active);
+}
+
+/** \brief  Create left-aligned label using Pango markup
+ *
+ * \param[in]   text    text for label
+ *
+ * \return  GtkLabel
+ */
+static GtkWidget *label_helper(const char *text)
+{
+    GtkWidget *label = gtk_label_new(NULL);
+
+    gtk_label_set_markup(GTK_LABEL(label), text);
+    gtk_widget_set_halign(label, GTK_ALIGN_START);
+    return label;
 }
 
 
@@ -88,45 +105,40 @@ GtkWidget *settings_petreu_widget_create(GtkWidget *parent)
     GtkWidget *grid;
     GtkWidget *enable;
     GtkWidget *label;
+    gboolean   active;
 
-    grid = vice_gtk3_grid_new_spaced(VICE_GTK3_DEFAULT, VICE_GTK3_DEFAULT);
+    grid = gtk_grid_new();
+    gtk_grid_set_column_spacing(GTK_GRID(grid), 8);
+    gtk_grid_set_row_spacing(GTK_GRID(grid), 16);
 
     /* REU enable */
     enable = vice_gtk3_resource_check_button_new("PETREU",
-            "Enable PET RAM Expansion Unit");
-    g_signal_connect(enable, "toggled", G_CALLBACK(on_reu_toggled), NULL);
+                                                 "Enable PET RAM Expansion Unit");
+    g_signal_connect_unlocked(G_OBJECT(enable),
+                              "toggled",
+                              G_CALLBACK(on_reu_toggled),
+                              NULL);
     gtk_grid_attach(GTK_GRID(grid), enable, 0, 0, 3, 1);
 
     /* REU size */
-    label = gtk_label_new("REU size");
-    gtk_widget_set_halign(label, GTK_ALIGN_START);
-    gtk_widget_set_margin_start(label, 16);
-    group = vice_gtk3_resource_radiogroup_new("PETREUsize", reu_sizes,
-            GTK_ORIENTATION_HORIZONTAL);
-    gtk_grid_set_column_spacing(GTK_GRID(group), 16);
+    label = label_helper("REU size");
+    group = ram_size_radiogroup_new("PETREUsize", NULL, reu_sizes);
+    gtk_widget_set_valign(label, GTK_ALIGN_START);
     gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), group, 1, 1, 2, 1);
 
-    /* REU filename */
-
-    /* Don't use the resourcebrowser widget's label, but a separate one to
-     * properly align with the previous widgets */
-    label = gtk_label_new("REU image file");
-    gtk_widget_set_halign(label, GTK_ALIGN_START);
-    gtk_widget_set_margin_start(label, 16);
-    gtk_grid_attach(GTK_GRID(grid), label, 0, 2, 1, 1);
-
-    browser = vice_gtk3_resource_browser_new(
-            "PETREUfilename",
-            NULL,
-            NULL,
-            "Select REU image file",
-            NULL,
-            NULL);
-    gtk_grid_attach(GTK_GRID(grid), browser, 1, 2, 2, 1);
+    /* REU image file */
+    label   = label_helper("REU image file");
+    chooser = vice_gtk3_resource_filechooser_new("PETREUfilename",
+                                                 GTK_FILE_CHOOSER_ACTION_SAVE);
+    vice_gtk3_resource_filechooser_set_custom_title(chooser,
+                                                    "Select PET REU image file");
+    gtk_grid_attach(GTK_GRID(grid), label,   0, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), chooser, 1, 2, 2, 1);
 
     /* set initial sensitive state of widgets */
-    on_reu_toggled(enable, NULL);
+    active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(enable));
+    set_widgets_sensitivity(active);
 
     gtk_widget_show_all(grid);
     return grid;
