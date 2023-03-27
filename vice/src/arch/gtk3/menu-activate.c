79633269bcbcd44ef0d651857666d2332ecab2aa
fabbo <fabrizio.ge@tiscalinet.it>
2023-03-27 21:09:08 +0000

    It is possible to perform actions mapped to joystick events in the GTK port
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@43595 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/menu-activate.c
+++ vice/src/arch/gtk3/menu-activate.c
@@ -25,6 +25,7 @@
  */
 
 #include "menu-activate.h"
+#include "uiactions.h"
 #include "uisettings.h"
 
 void arch_ui_activate(void)
@@ -34,6 +35,7 @@ void arch_ui_activate(void)
 
 void arch_ui_perform_action(int action)
 {
+    ui_action_trigger(action);
 }
 
 void arch_hotkey_path_to_file(FILE* file, int action)
