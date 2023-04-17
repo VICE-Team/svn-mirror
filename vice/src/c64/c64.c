37f8ed6640e2b210136fbf86af7b59ce5bc86812
blackystardust <blackystardust68@yahoo.com>
2023-04-17 05:54:07 +0000

    Split off hummer userport joystick adapter code from userport_joystick.*
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@43682 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c64/c64.c
+++ vice/src/c64/c64.c
@@ -130,6 +130,7 @@
 #include "userport_diag_586220_harness.h"
 #include "userport_digimax.h"
 #include "userport_hks_joystick.h"
+#include "userport_hummer_joystick.h"
 #include "userport_io_sim.h"
 #include "userport_joystick.h"
 #include "userport_petscii_snespad.h"
