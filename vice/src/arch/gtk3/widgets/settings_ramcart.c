8c976150659992d2d929b0eca17dc1071205c87c
compyx <b.wassink@ziggo.nl>
2023-04-26 18:19:19 +0000

    Gtk3: RamCart settings: update code and layout
    Use new cart image widget, put "write-on-detach" and "read-only" check buttons
    inside the new widget, use define from cartridge.h for proper "RamCart" name.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@43717 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/settings_ramcart.c
+++ vice/src/arch/gtk3/widgets/settings_ramcart.c
@@ -46,52 +46,57 @@
 static int ram_sizes[] = { 64, 128, -1 };
 
 
-/** \brief  Create RAMCART enable check button
+/** \brief  Create RamCart enable check button
  *
  * \return  GtkCheckButton
  */
 static GtkWidget *create_ramcart_enable_widget(void)
 {
     return vice_gtk3_resource_check_button_new("RAMCART",
-                                               "Enable RAMCART expansion");
+                                               "Enable " CARTRIDGE_NAME_RAMCART
+                                               " emulation");
 }
 
-/** \brief  Create check button to toggle read-only mode
- *
- * \return  GtkCheckButton
- */
-static GtkWidget *create_ramcart_readonly_widget(void)
-{
-    return vice_gtk3_resource_check_button_new("RAMCART_RO",
-                                               "RAMCART contents are read only");
-}
-
-/** \brief  Create radio button group to determine RAMCART size
+/** \brief  Create radio button group to determine RamCart size
  *
  * \return  GtkGrid
  */
 static GtkWidget *create_ramcart_size_widget(void)
 {
     return ram_size_radiogroup_new("RAMCARTSize",
-                                   "RAM size",
+                                   CARTRIDGE_NAME_RAMCART " size",
                                    ram_sizes);
 }
 
-/** \brief  Create widget to load/save RAMCART image file
+/** \brief  Create widget for RamCart image file
+ *
+ * Widge to load/save image with check buttons for "write-on-detach" and
+ * "read-only".
  *
  * \return  GtkGrid
  */
 static GtkWidget *create_ramcart_image_widget(void)
 {
-    return cart_image_widget_create(NULL,
-                                    "RAMCARTfilename",
-                                    "RAMCARTImageWrite",
-                                    CARTRIDGE_NAME_RAMCART,
-                                    CARTRIDGE_RAMCART);
+    GtkWidget *image;
+
+    image = cart_image_widget_new(CARTRIDGE_RAMCART,
+                                  CARTRIDGE_NAME_RAMCART,
+                                  CART_IMAGE_PRIMARY,
+                                  "cartridge",
+                                  "RAMCARTfilename",
+                                  TRUE,
+                                  TRUE);
+    cart_image_widget_append_check(image,
+                                   "RAMCARTImageWrite",
+                                   "Write image on detach/emulator exit");
+    cart_image_widget_append_check(image,
+                                   "RAMCART_RO",
+                                   "Image contents are read-only");
+    return image;
 }
 
 
-/** \brief  Create widget to control RAM Expansion Module resources
+/** \brief  Create widget to control RamCart resources
  *
  * \param[in]   parent  parent widget (unused)
  *
@@ -100,24 +105,20 @@ static GtkWidget *create_ramcart_image_widget(void)
 GtkWidget *settings_ramcart_widget_create(GtkWidget *parent)
 {
     GtkWidget *grid;
-    GtkWidget *ramcart_enable;
-    GtkWidget *ramcart_size;
-    GtkWidget *ramcart_readonly;
-    GtkWidget *ramcart_image;
+    GtkWidget *enable;
+    GtkWidget *image;
+    GtkWidget *size;
 
-    grid = vice_gtk3_grid_new_spaced(32, 8);
+    grid = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(grid), 16);
 
-    ramcart_enable = create_ramcart_enable_widget();
-    gtk_grid_attach(GTK_GRID(grid), ramcart_enable, 0, 0, 2, 1);
+    enable = create_ramcart_enable_widget();
+    image  = create_ramcart_image_widget();
+    size   = create_ramcart_size_widget();
 
-    ramcart_size = create_ramcart_size_widget();
-    gtk_grid_attach(GTK_GRID(grid), ramcart_size, 0, 1, 1, 1);
-
-    ramcart_image = create_ramcart_image_widget();
-    gtk_grid_attach(GTK_GRID(grid), ramcart_image, 1, 1, 1, 1);
-
-    ramcart_readonly = create_ramcart_readonly_widget();
-    gtk_grid_attach(GTK_GRID(grid), ramcart_readonly, 0, 2, 2,1);
+    gtk_grid_attach(GTK_GRID(grid), enable, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), image,  0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), size,   0, 2, 1, 1);
 
     gtk_widget_show_all(grid);
     return grid;
