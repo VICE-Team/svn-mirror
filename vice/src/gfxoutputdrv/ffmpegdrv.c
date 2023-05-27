bbe386111bcd2751346a9dc93e26a0d0a7563932
gpz <groepaz@gmx.net>
2023-05-27 15:22:46 +0000

    fix memory leaks/double free, better handling of HAVE_FFMPEG vs !HAVE_FFMPEG
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@43903 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/gfxoutputdrv/ffmpegdrv.c
+++ vice/src/gfxoutputdrv/ffmpegdrv.c
@@ -107,7 +107,7 @@ static gfxoutputdrv_codec_t none_codeclist[] = {
 
 /* formatlist is filled from with available formats and codecs at init time */
 gfxoutputdrv_format_t *ffmpegdrv_formatlist = NULL;
-gfxoutputdrv_format_t formats_to_test[] =
+static gfxoutputdrv_format_t formats_to_test[] =
 {
     { "mp4", mp4_audio_codeclist, mp4_video_codeclist },
     { "ogg", ogg_audio_codeclist, ogg_video_codeclist },
