87981d3f8121769cfe2db0b4b57bf8e5c3e035a0
compyx <b.wassink@ziggo.nl>
2023-06-10 09:38:09 +0000

    Fix missing `mirror_mode` initializers for `io_source_t` objects (C128)
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@44030 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/c128/cart/comal80.c
+++ vice/src/c128/cart/comal80.c
@@ -76,35 +76,37 @@ static void c128comal80_io2_store(uint16_t addr, uint8_t value);
 static int c128comal80_dump(void);
 
 static io_source_t c128comal80_io1_device = {
-    CARTRIDGE_C128_NAME_COMAL80, /* name of the device */
-    IO_DETACH_CART,               /* use cartridge ID to detach the device when involved in a read-collision */
-    IO_DETACH_NO_RESOURCE,        /* does not use a resource for detach */
-    0xde00, 0xdeff, 0xff,         /* range for the device, address is ignored by the write functions, reg:$de00, mirrors:$de01-$deff */
-    1,                            /* read is never valid */
-    c128comal80_io1_store,       /* store function */
-    NULL,                         /* NO poke function */
-    c128comal80_io1_read,        /* read function */
-    c128comal80_io1_peek,        /* peek function */
-    c128comal80_dump,            /* device state information dump function */
-    CARTRIDGE_C128_COMAL80,  /* cartridge ID */
-    IO_PRIO_NORMAL,               /* normal priority, device read needs to be checked for collisions */
-    0                             /* insertion order, gets filled in by the registration function */
+    CARTRIDGE_C128_NAME_COMAL80,    /* name of the device */
+    IO_DETACH_CART,                 /* use cartridge ID to detach the device when involved in a read-collision */
+    IO_DETACH_NO_RESOURCE,          /* does not use a resource for detach */
+    0xde00, 0xdeff, 0xff,           /* range for the device, address is ignored by the write functions, reg:$de00, mirrors:$de01-$deff */
+    1,                              /* read is never valid */
+    c128comal80_io1_store,          /* store function */
+    NULL,                           /* NO poke function */
+    c128comal80_io1_read,           /* read function */
+    c128comal80_io1_peek,           /* peek function */
+    c128comal80_dump,               /* device state information dump function */
+    CARTRIDGE_C128_COMAL80,         /* cartridge ID */
+    IO_PRIO_NORMAL,                 /* normal priority, device read needs to be checked for collisions */
+    0,                              /* insertion order, gets filled in by the registration function */
+    IO_MIRROR_NONE                  /* NO mirroring */
 };
 
 static io_source_t c128comal80_io2_device = {
-    CARTRIDGE_C128_NAME_COMAL80, /* name of the device */
-    IO_DETACH_CART,               /* use cartridge ID to detach the device when involved in a read-collision */
-    IO_DETACH_NO_RESOURCE,        /* does not use a resource for detach */
-    0xdf00, 0xdfff, 0xff,         /* range for the device, address is ignored by the read/write functions, reg:$df00, mirrors:$df01-$dfff */
-    1,                            /* validity of the read is determined by the cartridge at read time */
-    c128comal80_io2_store,       /* store function */
-    NULL,                         /* NO poke function */
-    c128comal80_io2_read,        /* read function */
-    c128comal80_io2_peek,        /* peek function */
-    c128comal80_dump,            /* device state information dump function */
-    CARTRIDGE_C128_COMAL80,  /* cartridge ID */
-    IO_PRIO_NORMAL,               /* normal priority, device read needs to be checked for collisions */
-    0                             /* insertion order, gets filled in by the registration function */
+    CARTRIDGE_C128_NAME_COMAL80,    /* name of the device */
+    IO_DETACH_CART,                 /* use cartridge ID to detach the device when involved in a read-collision */
+    IO_DETACH_NO_RESOURCE,          /* does not use a resource for detach */
+    0xdf00, 0xdfff, 0xff,           /* range for the device, address is ignored by the read/write functions, reg:$df00, mirrors:$df01-$dfff */
+    1,                              /* validity of the read is determined by the cartridge at read time */
+    c128comal80_io2_store,          /* store function */
+    NULL,                           /* NO poke function */
+    c128comal80_io2_read,           /* read function */
+    c128comal80_io2_peek,           /* peek function */
+    c128comal80_dump,               /* device state information dump function */
+    CARTRIDGE_C128_COMAL80,         /* cartridge ID */
+    IO_PRIO_NORMAL,                 /* normal priority, device read needs to be checked for collisions */
+    0,                              /* insertion order, gets filled in by the registration function */
+    IO_MIRROR_NONE                  /* NO mirroring */
 };
 
 static io_source_list_t *c128comal80_io1_list_item = NULL;
