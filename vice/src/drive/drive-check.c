c1683b50551eb287dde26924becb59f53fe1f203
compyx <b.wassink@ziggo.nl>
2023-06-12 07:29:48 +0000

    drive_get_type_by_devnr(): don't access "IECDevice[8-11]" for VIC-20
    Although the VIC-20 does have an IEC bus, the resource "IECDevice[8-11]" isn't
    used by xvic, it has its own custom IEC code. So we check that `machine_class`
    isn't VIC-20 before accessing the resource and add a FIXME comment about why
    the check is there. Fixes unknown resource error messages from xvic when the
    status bar drive menu pops up.
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@44050 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/drive/drive-check.c
+++ vice/src/drive/drive-check.c
@@ -384,7 +384,9 @@ int drive_get_type_by_devnr(int devnr)
 
     if ((machine_class != VICE_MACHINE_CBM5x0) &&
         (machine_class != VICE_MACHINE_CBM6x0) &&
-        (machine_class != VICE_MACHINE_PET)) {
+        (machine_class != VICE_MACHINE_PET) &&
+        /* FIXME: xvic has its own IEC code and doesn't use "IECDevice[8-11]" */
+        (machine_class != VICE_MACHINE_VIC20)) {
         resources_get_int_sprintf("IECDevice%i", &iecdevice, devnr);
         resources_get_int_sprintf("FileSystemDevice%i", &fsdevice, devnr);
     }
