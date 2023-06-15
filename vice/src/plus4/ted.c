3dd2f54d75896c505642b66a513becef47eac0b3
compyx <b.wassink@ziggo.nl>
2023-06-15 06:23:40 +0000

    Use format specifiers from <inttypes.h> (PRIu64, PRIx64) to print `uint64_t`s
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@44063 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/plus4/ted.c
+++ vice/src/plus4/ted.c
@@ -31,6 +31,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
 #include "videoarch.h"
 
@@ -893,7 +894,7 @@ int ted_dump(void)
     }
 
     mon_out("Timer 1 IRQ: %s  running: %s \n",((ted.regs[0x0a] >> 3) & 0x01)? "on" : "off", (ted.timer_running[0]) ? "yes" : "no");
-    mon_out("Timer 1: $%04x (latched $%04llx)\n", (unsigned int)((ted_timer_read(0x01) << 8) | ted_timer_read(0x00)), ted.t1_start);
+    mon_out("Timer 1: $%04x (latched $%04"PRIx64")\n", (unsigned int)((ted_timer_read(0x01) << 8) | ted_timer_read(0x00)), ted.t1_start);
     mon_out("Timer 2 IRQ: %s  running: %s \n",((ted.regs[0x0a] >> 4) & 0x01)? "on" : "off", (ted.timer_running[1]) ? "yes" : "no");
     mon_out("Timer 2: $%04x\n", (unsigned int)((ted_timer_read(0x03) << 8) | ted_timer_read(0x02)));
     mon_out("Timer 3 IRQ: %s  running: %s \n",((ted.regs[0x0a] >> 6) & 0x01)? "on" : "off", (ted.timer_running[2]) ? "yes" : "no");
