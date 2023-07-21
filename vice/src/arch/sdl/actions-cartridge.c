5e60c31088f757377ff206eb63e4099e1b4caf94
compyx <b.wassink@ziggo.nl>
2023-07-21 05:02:49 +0000

    SDL: xvic: implement action handlers for attach-image-at-$2000/$4000/$6000/$A000/$B000
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@44295 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/sdl/actions-cartridge.c
+++ vice/src/arch/sdl/actions-cartridge.c
@@ -29,6 +29,7 @@
 
 #include "cartridge.h"
 #include "keyboard.h"
+#include "log.h"
 #include "uiactions.h"
 #include "uimenu.h"
 
@@ -55,6 +56,47 @@ static void cart_attach_crt_action(void *unused)
 static void cart_attach_raw_action(void *unused)
 {
     sdl_ui_menu_item_activate_by_action(ACTION_CART_ATTACH_RAW);
+}
+
+/** \brief  Attach raw cartridge at specific memory address action
+ *
+ * Show dialog to attach a raw cartridge image at \a address.
+ *
+ * \param[in]   address
+ */
+static void cart_attach_raw_address_action(void *address)
+{
+    int action = ACTION_NONE;
+    int addr   = vice_ptr_to_int(address);
+
+    switch (addr) {
+        case 0x1000:
+            action = ACTION_CART_ATTACH_RAW_1000;
+            break;
+        case 0x2000:
+            action = ACTION_CART_ATTACH_RAW_2000;
+            break;
+        case 0x4000:
+            action = ACTION_CART_ATTACH_RAW_4000;
+            break;
+        case 0x6000:
+            action = ACTION_CART_ATTACH_RAW_6000;
+            break;
+        case 0xa000:
+            action = ACTION_CART_ATTACH_RAW_A000;
+            break;
+        case 0xb000:
+            action = ACTION_CART_ATTACH_RAW_B000;
+            break;
+        default:
+            log_error(LOG_ERR,
+                      "%s(): couldn't translate address $%04x to action ID.",
+                      __func__, (unsigned int)addr);
+            return;
+    }
+    printf("%s(): action %d (%s), attach at $%04x\n",
+           __func__, action, ui_action_get_name(action), (unsigned int)addr);
+    sdl_ui_menu_item_activate_by_action(action);
 }
 
 /** \brief  Detach cartridge action
@@ -89,6 +131,36 @@ static const ui_action_map_t cartridge_actions[] = {
     },
     {   .action  = ACTION_CART_ATTACH_RAW,
         .handler = cart_attach_raw_action,
+    },
+    {   .action  = ACTION_CART_ATTACH_RAW_1000,
+        .handler = cart_attach_raw_address_action,
+        .dialog  = true,
+        .param   = int_to_void_ptr(0x1000)
+    },
+    {   .action  = ACTION_CART_ATTACH_RAW_2000,
+        .handler = cart_attach_raw_address_action,
+        .dialog  = true,
+        .param   = int_to_void_ptr(0x2000)
+    },
+    {   .action  = ACTION_CART_ATTACH_RAW_4000,
+        .handler = cart_attach_raw_address_action,
+        .dialog  = true,
+        .param   = int_to_void_ptr(0x4000)
+    },
+    {   .action  = ACTION_CART_ATTACH_RAW_6000,
+        .handler = cart_attach_raw_address_action,
+        .dialog  = true,
+        .param   = int_to_void_ptr(0x6000)
+    },
+    {   .action  = ACTION_CART_ATTACH_RAW_A000,
+        .handler = cart_attach_raw_address_action,
+        .dialog  = true,
+        .param   = int_to_void_ptr(0xa000)
+    },
+    {   .action  = ACTION_CART_ATTACH_RAW_B000,
+        .handler = cart_attach_raw_address_action,
+        .dialog  = true,
+        .param   = int_to_void_ptr(0xb000)
     },
     {   .action  = ACTION_CART_DETACH,
         .handler = cart_detach_action
