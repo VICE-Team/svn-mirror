921ff7a426ed059cbca132a04a1f8535a49a4949
compyx <b.wassink@ziggo.nl>
2023-08-15 17:16:07 +0000

    Github actions: deleted commented-out code to build and install xa65
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@44424 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/build/github-actions/build-msys2.sh
+++ vice/build/github-actions/build-msys2.sh
@@ -7,38 +7,8 @@ set -o nounset
 source $(dirname $0)/build-shared.sh
 cd "$(dirname $0)"/../..
 
-UI="$1"
-
-#
-# Build and install xa65. When this stops working, check
-# https://www.floodgap.com/retrotech/xa/dists/ to see why
-#
-
-#if [ ! -e /usr/local/bin/xa65.exe ]
-#then
-#    pushd /usr/local
-#    mkdir -p src
-#    cd src
-#    XA_VERSION=$(wget --tries=1 -O - https://www.floodgap.com/retrotech/xa/dists/ 2>/dev/null | grep '"xa-[^"]*gz"' | sed -e 's,.*xa-,,' -e 's,.tar.gz.*,,' | sort -V | tail -n1)
-#    echo "got XA version: " $XA_VERSION
-#    if [ x"$XA_VERSION"x = x""x ]; then
-#        echo "WARNING: using backup location for XA"
-#        wget https://vice-emu.sourceforge.io/xa/xa65-xa-2.3.11.tar.gz
-#        tar -xzf xa65-xa-2.3.11.tar.gz
-#        cd xa65-xa-2.3.11/xa
-#    else
-#        wget https://www.floodgap.com/retrotech/xa/dists/xa-${XA_VERSION}.tar.gz
-#        tar -xzf xa-${XA_VERSION}.tar.gz
-#        cd xa-${XA_VERSION}
-#    fi
-#    make mingw install
-#    cp /usr/local/bin/xa.exe /usr/local/bin/xa65.exe
-#    popd
-#fi
-
-
 # Set proper configure options
-set_configure_options "$UI"
+set_configure_options "$1"
 
 
 # Skip autogen.sh when building release from tarball
