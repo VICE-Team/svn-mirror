086e248e246cc1ae75a65bef6d8a2acd47b83127
compyx <b.wassink@ziggo.nl>
2023-08-15 16:34:20 +0000

    Github actions: do not build xa65 from source, install using msys2 package
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@44423 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/build/github-actions/build-msys2.sh
+++ vice/build/github-actions/build-msys2.sh
@@ -14,27 +14,27 @@ UI="$1"
 # https://www.floodgap.com/retrotech/xa/dists/ to see why
 #
 
-if [ ! -e /usr/local/bin/xa65.exe ]
-then
-    pushd /usr/local
-    mkdir -p src
-    cd src
-    XA_VERSION=$(wget --tries=1 -O - https://www.floodgap.com/retrotech/xa/dists/ 2>/dev/null | grep '"xa-[^"]*gz"' | sed -e 's,.*xa-,,' -e 's,.tar.gz.*,,' | sort -V | tail -n1)
-    echo "got XA version: " $XA_VERSION
-    if [ x"$XA_VERSION"x = x""x ]; then
-        echo "WARNING: using backup location for XA"
-        wget https://vice-emu.sourceforge.io/xa/xa65-xa-2.3.11.tar.gz
-        tar -xzf xa65-xa-2.3.11.tar.gz
-        cd xa65-xa-2.3.11/xa
-    else
-        wget https://www.floodgap.com/retrotech/xa/dists/xa-${XA_VERSION}.tar.gz
-        tar -xzf xa-${XA_VERSION}.tar.gz
-        cd xa-${XA_VERSION}
-    fi
-    make mingw install
-    cp /usr/local/bin/xa.exe /usr/local/bin/xa65.exe
-    popd
-fi
+#if [ ! -e /usr/local/bin/xa65.exe ]
+#then
+#    pushd /usr/local
+#    mkdir -p src
+#    cd src
+#    XA_VERSION=$(wget --tries=1 -O - https://www.floodgap.com/retrotech/xa/dists/ 2>/dev/null | grep '"xa-[^"]*gz"' | sed -e 's,.*xa-,,' -e 's,.tar.gz.*,,' | sort -V | tail -n1)
+#    echo "got XA version: " $XA_VERSION
+#    if [ x"$XA_VERSION"x = x""x ]; then
+#        echo "WARNING: using backup location for XA"
+#        wget https://vice-emu.sourceforge.io/xa/xa65-xa-2.3.11.tar.gz
+#        tar -xzf xa65-xa-2.3.11.tar.gz
+#        cd xa65-xa-2.3.11/xa
+#    else
+#        wget https://www.floodgap.com/retrotech/xa/dists/xa-${XA_VERSION}.tar.gz
+#        tar -xzf xa-${XA_VERSION}.tar.gz
+#        cd xa-${XA_VERSION}
+#    fi
+#    make mingw install
+#    cp /usr/local/bin/xa.exe /usr/local/bin/xa65.exe
+#    popd
+#fi
 
 
 # Set proper configure options
