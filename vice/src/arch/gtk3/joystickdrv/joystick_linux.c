6fed965c827fa2929e89efb9f0546d73d1f8b0f0
compyx <b.wassink@ziggo.nl>
2023-09-19 06:50:46 +0000

    Clean up and document linux joystick driver, properly close file descriptors
    Try to clean up the code a bit and add comments to make it slightly more
    readable. Properly close any file descriptor of devices we reject.
    
    
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@44534 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/joystickdrv/joystick_linux.c
+++ vice/src/arch/gtk3/joystickdrv/joystick_linux.c
@@ -48,109 +48,164 @@
 #include "types.h"
 #include "lib.h"
 
+
+/** \brief  Number of device nodes to scan */
 #define ANALOG_JOY_NUM 8
 
-static log_t joystick_linux_log = LOG_ERR;
-
+/** \brief  Private, driver-specific data of the device */
 typedef struct linux_joystick_priv_s {
-    int fd;
+    int fd;     /**< file descriptor of the joystick device node */
 } linux_joystick_priv_t;
 
-static void linux_joystick_close(void* priv)
-{
-    close (((linux_joystick_priv_t *)priv)->fd);
-    lib_free(priv);
-}
 
-static void linux_joystick(int joyport, void* priv)
+/** \brief  Linux joystick log */
+static log_t joystick_linux_log = LOG_ERR;
+
+
+/** \brief  Callback for the joystick system to poll a joystick device
+ *
+ * \param[in]   joyport joystick device index
+ * \param[in]   priv    driver-specific data
+ */
+static void linux_joystick_poll(int joyport, void *priv)
 {
     linux_joystick_priv_t *joypriv = priv;
-    struct js_event e;
-    joystick_axis_value_t dir;
+    struct js_event        event;
 
     /* Read all queued events. */
-    while (read(joypriv->fd, &e, sizeof(struct js_event)) == sizeof(struct js_event)) {
-        switch (e.type & ~JS_EVENT_INIT) {
-        case JS_EVENT_BUTTON:
-            joy_button_event(joyport, e.number, e.value);
-            break;
-        case JS_EVENT_AXIS:
-            if (e.value > 16384) {
-                dir = JOY_AXIS_POSITIVE;
-            } else if (e.value < -16384) {
-                dir = JOY_AXIS_NEGATIVE;
-            } else {
-                dir = JOY_AXIS_MIDDLE;
-            }
-            joy_axis_event(joyport, e.number, dir);
-             break;
+    while (read(joypriv->fd, &event, sizeof event) == sizeof event) {
+        joystick_axis_value_t direction;
+
+        switch (event.type & ~JS_EVENT_INIT) {
+
+            case JS_EVENT_BUTTON:
+                joy_button_event((uint8_t)joyport, event.number, (int16_t)event.value);
+                break;
+
+            case JS_EVENT_AXIS:
+                if (event.value > 16384) {
+                    direction = JOY_AXIS_POSITIVE;
+                } else if (event.value < -16384) {
+                    direction = JOY_AXIS_NEGATIVE;
+                } else {
+                    direction = JOY_AXIS_MIDDLE;
+                }
+                joy_axis_event((uint8_t)joyport, event.number, (int16_t)direction);
+                break;
+
+            default:
+                break;
         }
     }
 }
 
-static joystick_driver_t linux_joystick_driver = {
-    .poll = linux_joystick,
+/** \brief  Callback for the joystick system to close a joystick device
+ *
+ * \param[in]   priv    driver-specific data
+ */
+static void linux_joystick_close(void *priv)
+{
+    linux_joystick_priv_t *joypriv = priv;
+
+    close(joypriv->fd);
+    lib_free(joypriv);
+}
+
+
+/** \brief  Object used to register driver for devices */
+static joystick_driver_t driver = {
+    .poll  = linux_joystick_poll,
     .close = linux_joystick_close
 };
 
+
+/** \brief  Initialize linux joysticks
+ *
+ * Scan joystick device nodes for valid devices and register them with the
+ * joystick system.
+ */
 void linux_joystick_init(void)
 {
     int i;
 
-
     if (joystick_linux_log == LOG_ERR) {
         joystick_linux_log = log_open("Joystick");
     }
-
     log_message(joystick_linux_log, "Linux joystick interface initialization...");
 
     /* open analog device files */
-
     for (i = 0; i < ANALOG_JOY_NUM; i++) {
         char dev[256];
-        int fd;
-        int ver = 0;
-        uint8_t axes;
-        uint8_t buttons;
-        char name[256];
-        struct JS_DATA_TYPE js;
+        int  fd;
 
-        memset(dev, 0, sizeof(dev));
-        memset(name, 0, sizeof(name));
-
-        snprintf(dev, sizeof(dev) - 1, "/dev/js%d", i);
+        memset(dev, 0, sizeof dev);
+        snprintf(dev, sizeof dev - 1u, "/dev/js%d", i);
         fd = open(dev, O_RDONLY);
         if (fd < 0) {
-            snprintf(dev, sizeof(dev) - 1, "/dev/input/js%d", i);
+            snprintf(dev, sizeof dev - 1u, "/dev/input/js%d", i);
             fd = open(dev, O_RDONLY);
         }
 
         if (fd >= 0) {
-            if (read (fd, &js, sizeof(struct JS_DATA_TYPE)) < 0) {
-                close (fd);
+            struct JS_DATA_TYPE    js;
+            linux_joystick_priv_t *priv;
+            char                   name[256];
+            uint8_t                axes;
+            uint8_t                buttons;
+            int                    ver = 0;
+
+            /* read joystick data */
+            if (read(fd, &js, sizeof js) < 0) {
+                close(fd);
                 continue;
             }
-            if (ioctl(fd, JSIOCGVERSION, &ver)) {
-                log_message(joystick_linux_log, "%s unknown type", dev);
-                log_message(joystick_linux_log, "Built in driver version: %d.%d.%d", JS_VERSION >> 16, (JS_VERSION >> 8) & 0xff, JS_VERSION & 0xff);
-                log_message(joystick_linux_log, "Kernel driver version  : 0.8 ??");
-                log_message(joystick_linux_log, "Please update your Joystick driver!");
+
+            /* check if the runtime drive is at least 1.0 and thus supports
+             * events: */
+            if (ioctl(fd, JSIOCGVERSION, &ver) != 0) {
+                log_message(joystick_linux_log,
+                            "%s unknown type", dev);
+                log_message(joystick_linux_log,
+                            "Built in driver version: %d.%d.%d",
+                            JS_VERSION >> 16, (JS_VERSION >> 8) & 0xff, JS_VERSION & 0xff);
+                log_message(joystick_linux_log,
+                            "Kernel driver version  : 0.8 ??");
+                log_message(joystick_linux_log,
+                            "Please update your Joystick driver!");
+                close(fd);
                 return;
             }
+
+            /* get number of axis, number of buttons and name of device */
+            memset(name, 0, sizeof name);
             ioctl(fd, JSIOCGAXES, &axes);
             ioctl(fd, JSIOCGBUTTONS, &buttons);
-            ioctl(fd, JSIOCGNAME(sizeof(name) - 1), name);
-            log_message(joystick_linux_log, "%s is %s", dev, name);
-            log_message(joystick_linux_log, "Built in driver version: %d.%d.%d", JS_VERSION >> 16, (JS_VERSION >> 8) & 0xff, JS_VERSION & 0xff);
-            log_message(joystick_linux_log, "Kernel driver version  : %d.%d.%d", ver >> 16, (ver >> 8) & 0xff, ver & 0xff);
-            fcntl(fd, F_SETFL, O_NONBLOCK);
+            ioctl(fd, JSIOCGNAME(sizeof name - 1u), name);
+
+            log_message(joystick_linux_log,
+                        "%s is %s", dev, name);
+            log_message(joystick_linux_log,
+                        "Built in driver version: %d.%d.%d",
+                        JS_VERSION >> 16, (JS_VERSION >> 8) & 0xff, JS_VERSION & 0xff);
+            log_message(joystick_linux_log,
+                        "Kernel driver version  : %d.%d.%d",
+                        ver >> 16, (ver >> 8) & 0xff, ver & 0xff);
+
+            /* check geometry */
             if ((axes == 0) || (buttons == 0)) {
-                log_message(joystick_linux_log, "Joystick with invalid geometry found -- ignoring.");
+                log_message(joystick_linux_log,
+                            "Joystick with invalid geometry found -- ignoring.");
+                close(fd);
                 continue;
             }
-            linux_joystick_priv_t *priv = lib_malloc(sizeof(linux_joystick_priv_t));
+
+            /* set file descriptor to non-blocking */
+            fcntl(fd, F_SETFL, O_NONBLOCK);
+
+            /* register device with the joystick system */
+            priv = lib_malloc(sizeof *priv);
             priv->fd = fd;
-            register_joystick_driver(&linux_joystick_driver, name, priv, axes, buttons, 0);
+            register_joystick_driver(&driver, name, priv, axes, buttons, 0);
         } else {
             log_warning(joystick_linux_log, "Cannot open joystick device `%s'.", dev);
         }
