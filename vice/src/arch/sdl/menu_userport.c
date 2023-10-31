162c0c9b44f6f7b802977a1352b4ed3f68f35aa1
compyx <b.wassink@ziggo.nl>
2023-10-31 11:52:34 +0000

    SDL: Add WiC64 settings to userport menu
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@44623 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/sdl/menu_userport.c
+++ vice/src/arch/sdl/menu_userport.c
@@ -30,11 +30,14 @@
 
 
 #include "lib.h"
+#include "machine.h"
 #include "menu_common.h"
 #include "types.h"
+#include "ui.h"
 #include "uiactions.h"
 #include "uimenu.h"
 #include "userport.h"
+#include "userport_wic64.h"
 
 #include "menu_userport.h"
 
@@ -79,7 +82,67 @@ static UI_MENU_CALLBACK(UserportDevice_dynmenu_callback)
     return MENU_SUBMENU_STRING;
 }
 
+/*
+ * WiC64 menu stuff
+ */
+#ifdef HAVE_LIBCURL
+static ui_menu_entry_t *dyn_menu_wic64;
+
+UI_MENU_DEFINE_TOGGLE(WIC64Logenabled)
+UI_MENU_DEFINE_INT(WIC64Hexdumplines);
+UI_MENU_DEFINE_STRING(WIC64DefaultServer)
+UI_MENU_DEFINE_TOGGLE(WIC64Resetuser)
+UI_MENU_DEFINE_RADIO(WIC64Timezone);
+
+/** \brief  Generate WiC64 runtime timezones menu
+ *
+ * Iterate WiC64 timezones table and create radio buttons for each zone.
+ */
+static void wic64_timezones_menu_new(void)
+{
+    const tzones_t *zones;
+    size_t          num_zones;
+    size_t          z;
+
+    zones          = userport_wic64_get_timezones(&num_zones);
+    dyn_menu_wic64 = lib_malloc((num_zones + 1u) * sizeof *dyn_menu_wic64);
+
+    for (z = 0; z < num_zones; z++) {
+        dyn_menu_wic64[z].action   = ACTION_NONE;
+        dyn_menu_wic64[z].string   = lib_msprintf("%d: %s", zones[z].idx, zones[z].tz_name);
+        dyn_menu_wic64[z].type     = MENU_ENTRY_RESOURCE_RADIO;
+        dyn_menu_wic64[z].callback = radio_WIC64Timezone_callback;
+        dyn_menu_wic64[z].data     = int_to_void_ptr(zones[z].idx);
+    }
+    dyn_menu_wic64[z].string = NULL;
+}
+
+/** \brief  Free WiC64 runtime timezones menu */
+static void wic64_timezones_menu_free(void)
+{
+    int z;
+
+    for (z = 0; dyn_menu_wic64[z].string != NULL; z++) {
+        lib_free(dyn_menu_wic64[z].string);
+    }
+}
+
+static UI_MENU_CALLBACK(custom_wic64_reset_callback)
+{
+    if (activated) {
+        userport_wic64_factory_reset();
+        ui_message("WiC64 has been reset to factory default.");
+    }
+    return NULL;
+}
+
+#endif
+
+#ifdef HAVE_LIBCURL
+ui_menu_entry_t userport_menu[13];
+#else
 ui_menu_entry_t userport_menu[4];
+#endif
 
 UI_MENU_DEFINE_TOGGLE(UserportRTCDS1307Save)
 UI_MENU_DEFINE_TOGGLE(UserportRTC58321aSave)
@@ -104,6 +167,82 @@ void uiuserport_menu_create(int rtc)
         j++;
     }
 
+#ifdef HAVE_LIBCURL
+    if (machine_class == VICE_MACHINE_C64 ||
+        machine_class == VICE_MACHINE_C64SC ||
+        machine_class == VICE_MACHINE_C128 ||
+        machine_class == VICE_MACHINE_VIC20 ||
+        machine_class == VICE_MACHINE_SCPU64) {
+
+        if (dyn_menu_wic64 == NULL) {
+            wic64_timezones_menu_new();
+        }
+
+        userport_menu[j].action   = ACTION_NONE;
+        userport_menu[j].string   = "";
+        userport_menu[j].type     = MENU_ENTRY_TEXT;
+        userport_menu[j].callback = seperator_callback;
+        userport_menu[j].data     = NULL;
+        j++;
+
+        userport_menu[j].action   = ACTION_NONE;
+        userport_menu[j].string   = "WiC64 Settings";
+        userport_menu[j].type     = MENU_ENTRY_TEXT;
+        userport_menu[j].callback = seperator_callback;
+        userport_menu[j].data     = (ui_callback_data_t)1;
+        j++;
+
+        userport_menu[j].action   = ACTION_NONE;
+        userport_menu[j].string   = "WiC64 tracing";
+        userport_menu[j].type     = MENU_ENTRY_RESOURCE_TOGGLE;
+        userport_menu[j].callback = toggle_WIC64Logenabled_callback;
+        userport_menu[j].data     = NULL;
+        j++;
+
+        userport_menu[j].action   = ACTION_NONE;
+        userport_menu[j].string   = "Hexdump lines (0=unlimited):";
+        userport_menu[j].type     = MENU_ENTRY_RESOURCE_INT;
+        userport_menu[j].callback = int_WIC64Hexdumplines_callback;
+        userport_menu[j].data     = NULL;
+        j++;
+
+        userport_menu[j].action   = ACTION_NONE;
+        userport_menu[j].string   = "Default server:";
+        userport_menu[j].type     = MENU_ENTRY_RESOURCE_STRING;
+        userport_menu[j].callback = string_WIC64DefaultServer_callback;
+        userport_menu[j].data     = NULL;
+        j++;
+
+        userport_menu[j].action   = ACTION_NONE;
+        userport_menu[j].string   = "Timezone";
+        userport_menu[j].type     = MENU_ENTRY_DYNAMIC_SUBMENU;
+        userport_menu[j].callback = submenu_radio_callback;
+        userport_menu[j].data     = dyn_menu_wic64;
+        j++;
+
+        userport_menu[j].action   = ACTION_NONE;
+        userport_menu[j].string   = "Reset User when resetting WiC64";
+        userport_menu[j].type     = MENU_ENTRY_RESOURCE_TOGGLE;
+        userport_menu[j].callback = toggle_WIC64Resetuser_callback;
+        userport_menu[j].data     = NULL;
+        j++;
+
+        userport_menu[j].action   = ACTION_NONE;
+        userport_menu[j].string   = "Reset WiC64 to factory default";
+        userport_menu[j].type     = MENU_ENTRY_OTHER;
+        userport_menu[j].callback = custom_wic64_reset_callback;
+        userport_menu[j].data     = NULL;
+        j++;
+    }
+#endif
+
+    userport_menu[j].action   = ACTION_NONE;
+    userport_menu[j].string   = "";
+    userport_menu[j].type     = MENU_ENTRY_TEXT;
+    userport_menu[j].callback = seperator_callback;
+    userport_menu[j].data     = NULL;
+    j++;
+
     userport_menu[j].action   = ACTION_NONE;
     userport_menu[j].string   = "Userport devices";
     userport_menu[j].type     = MENU_ENTRY_DYNAMIC_SUBMENU;
@@ -121,5 +260,8 @@ void uiuserport_menu_shutdown(void)
 {
     if (userport_dyn_menu_init) {
         sdl_menu_userport_free();
+    }
+    if (dyn_menu_wic64 != NULL) {
+        wic64_timezones_menu_free();
     }
 }
