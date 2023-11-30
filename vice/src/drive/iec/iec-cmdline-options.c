f9e729f6e4c81d1091bc9434b85cf262af6e144d
compyx <b.wassink@ziggo.nl>
2023-11-30 15:25:26 +0000

    Fix capitalization of resource name "DosName1541ii"
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@44814 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/drive/iec/iec-cmdline-options.c
+++ vice/src/drive/iec/iec-cmdline-options.c
@@ -42,7 +42,7 @@ static const cmdline_option_t cmdline_options[] =
       NULL, NULL, "DosName1541", NULL,
       "<Name>", "Specify name of 1541 DOS ROM image" },
     { "-dos1541II", SET_RESOURCE, CMDLINE_ATTRIB_NEED_ARGS,
-      NULL, NULL, "DosName1541II", NULL,
+      NULL, NULL, "DosName1541ii", NULL,
       "<Name>", "Specify name of 1541-II DOS ROM image" },
     { "-dos1570", SET_RESOURCE, CMDLINE_ATTRIB_NEED_ARGS,
       NULL, NULL, "DosName1570", NULL,
