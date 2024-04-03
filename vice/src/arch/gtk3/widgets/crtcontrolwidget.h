6dc36e625481456e39105794a74a1215ae683e95
gpz <groepaz@gmx.net>
2024-04-03 23:20:47 +0000

    Hidden feature for the 'set to default' button for the CRT sliders: hold shift or control, or click with right button, to reset to neutral setting instead of whatever tweaks the defaults are. Allows to quickly switch to a setting that reproduces a loaded palette 100%
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@45066 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/arch/gtk3/widgets/crtcontrolwidget.h
+++ vice/src/arch/gtk3/widgets/crtcontrolwidget.h
@@ -35,8 +35,6 @@ GtkWidget * crt_control_widget_create(GtkWidget *parent,
                                       const char *chip,
                                       gboolean minimal);
 
-gboolean    crt_control_widget_reset(GtkWidget *widget);
-
 void        crt_control_widget_shutdown(void);
 
 #endif
