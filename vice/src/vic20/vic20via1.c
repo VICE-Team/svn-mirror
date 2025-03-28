22028e66321bd7524500b71c3fd04119b1334b1b
gpz <groepaz@gmx.net>
2025-03-28 18:10:56 +0000

    swap vic20via1/2.c
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/trunk@45599 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- vice/src/vic20/vic20via1.c
+++ vice/src/vic20/vic20via1.c
@@ -1,5 +1,5 @@
 /*
- * vic20via1.c - VIA2 emulation in the VIC20.
+ * vic20via1.c - VIA1 emulation in the VIC20.
  *
  * Written by
  *  Andre Fachat <fachat@physik.tu-chemnitz.de>
@@ -33,30 +33,45 @@
 #include "datasette.h"
 #include "interrupt.h"
 #include "joyport.h"
+#include "joystick.h"
 #include "keyboard.h"
 #include "lib.h"
 #include "maincpu.h"
 #include "tapeport.h"
 #include "types.h"
+#include "userport.h"
 #include "via.h"
+#include "vic.h"
 #include "vic20.h"
+#include "vic20-resources.h"
 #include "vic20iec.h"
 #include "vic20via.h"
 
+int vic20_vflihack_userport = 0xff;
 
-void via2_store(uint16_t addr, uint8_t data)
+static int userport_pa6 = 1;    /* HACK: pin 8, this is also connected to tape sense */
+static int userport_pb = 0xff;
+
+/* HACK: on the C64, pin 8 is connected to PC2, which automatically generates a pulse
+         when PB is written, which is why both is updated by the same function. */
+static void update_portbits(void)
 {
-    viacore_store(machine_context.via2, addr, data);
+    store_userport_pbx(userport_pb, userport_pa6 ? USERPORT_NO_PULSE : USERPORT_PULSE);
 }
 
-uint8_t via2_read(uint16_t addr)
+void via1_store(uint16_t addr, uint8_t data)
 {
-    return viacore_read(machine_context.via2, addr);
+    viacore_store(machine_context.via1, addr, data);
 }
 
-uint8_t via2_peek(uint16_t addr)
+uint8_t via1_read(uint16_t addr)
 {
-    return viacore_peek(machine_context.via2, addr);
+    return viacore_read(machine_context.via1, addr);
+}
+
+uint8_t via1_peek(uint16_t addr)
+{
+    return viacore_peek(machine_context.via1, addr);
 }
 
 static void set_ca2(via_context_t *via_context, int state)
@@ -65,57 +80,94 @@ static void set_ca2(via_context_t *via_context, int state)
 
 static void set_cb2(via_context_t *via_context, int state, int offset)
 {
+    if (!via_context->cb2_is_input) {
+        /* CB2 goes to userport pin M */
+        store_userport_pa2(state);
+    }
 }
 
-static void set_int(via_context_t *via_context, unsigned int int_num,
-                    int value, CLOCK rclk)
+static void set_int(via_context_t *via_context, unsigned int int_num, int value, CLOCK rclk)
 {
-    interrupt_set_irq(maincpu_int_status, int_num, value, rclk);
+    interrupt_set_nmi(maincpu_int_status, int_num, value, rclk);
 }
 
 static void restore_int(via_context_t *via_context, unsigned int int_num, int value)
 {
-    interrupt_restore_irq(maincpu_int_status, int_num, value);
+    interrupt_restore_nmi(maincpu_int_status, int_num, value);
 }
 
-static void undump_acr(via_context_t *via_context, uint8_t byte)
+static int tape_sense = 0;
+static int tape_write_in = 0;
+static int tape_motor_in = 0;
+
+void via1_set_tape_sense(int v)
 {
+    tape_sense = v;
 }
 
-static void store_acr(via_context_t *via_context, uint8_t byte)
+/* FIXME: find out how to set the write in and motor in lines */
+void via1_set_tape_write_in(int v)
 {
+    tape_write_in = v;
 }
 
-static void store_sr(via_context_t *via_context, uint8_t byte)
+void via1_set_tape_motor_in(int v)
 {
+    tape_motor_in = v;
 }
 
-static void store_t2l(via_context_t *via_context, uint8_t byte)
+static void via1_internal_lightpen_check(uint8_t pa)
 {
+    uint8_t b = read_joyport_dig(JOYPORT_1);
+
+    b &= pa;
+
+    vic_set_light_pen(maincpu_clk, !(b & 0x20));
+}
+
+void via1_check_lightpen(void)
+{
+    uint8_t pa = machine_context.via1->via[VIA_PRA] | ~(machine_context.via1->via[VIA_DDRA]);
+
+    via1_internal_lightpen_check(pa);
 }
 
 static void undump_pra(via_context_t *via_context, uint8_t byte)
 {
+    iec_pa_write(byte);
 }
 
 static void store_pra(via_context_t *via_context, uint8_t byte, uint8_t myoldpa,
                       uint16_t addr)
 {
+    uint8_t joy_bits = 0;
+
+    via1_internal_lightpen_check(byte);
+    iec_pa_write(byte);
+
+    joy_bits = ((byte & 0x20) >> 1) | ((byte & 0x1c) >> 2);
+    store_joyport_dig(JOYPORT_1, joy_bits, 0x17);
+
+    tapeport_set_sense_out(TAPEPORT_PORT_1, byte & 0x40 ? 1 : 0);
+
+    userport_pa6 = byte & 0x40 ? 1 : 0;
+    update_portbits();  /* HACK: see above */
 }
 
 static void undump_prb(via_context_t *via_context, uint8_t byte)
 {
+    userport_pb = byte;
+    update_portbits();  /* HACK: see above */
 }
 
 static void store_prb(via_context_t *via_context, uint8_t byte, uint8_t myoldpb,
                       uint16_t addr)
 {
-    uint8_t joy_bit = (byte & 0x80) >> 4;
+    /* for mike's VFLI hack, PB0-PB3 are used as A10-A13 of the color ram */
+    vic20_vflihack_userport = byte & 0x0f;
 
-    if ((byte ^ myoldpb) & 8) {
-        tapeport_toggle_write_bit(TAPEPORT_PORT_1, (~(via_context->via[VIA_DDRB]) | byte) & 0x8);
-    }
-    store_joyport_dig(JOYPORT_1, joy_bit, 8);
+    userport_pb = byte;
+    update_portbits();  /* HACK: see above */
 }
 
 static void undump_pcr(via_context_t *via_context, uint8_t byte)
@@ -124,12 +176,15 @@ static void undump_pcr(via_context_t *via_context, uint8_t byte)
 
 static void reset(via_context_t *via_context)
 {
-/*iec_pcr_write(0x22);*/
+    userport_pb = 0xff;
+    userport_pa6 = 1;
+    store_userport_pbx(0xff, USERPORT_NO_PULSE);
+    store_userport_pa2(1);
 }
 
 static uint8_t store_pcr(via_context_t *via_context, uint8_t byte, uint16_t addr)
 {
-    /* FIXME: this should use via_set_ca2() and via_set_cb2() */
+    /* FIXME: should use via_set_ca2() and via_set_cb2() */
     if (byte != via_context->via[VIA_PCR]) {
         register uint8_t tmp = byte;
         /* first set bit 1 and 5 to the real output values */
@@ -139,67 +194,86 @@ static uint8_t store_pcr(via_context_t *via_context, uint8_t byte, uint16_t addr
         if ((tmp & 0xc0) != 0xc0) {
             tmp |= 0x20;
         }
-        iec_pcr_write(tmp);
+
+        tapeport_set_motor(TAPEPORT_PORT_1, !(byte & 0x02));
+
+        store_userport_pa2((uint8_t)((byte & 0x20) >> 5));
     }
     return byte;
 }
 
-static uint8_t read_pra(via_context_t *via_context, uint16_t addr)
+static void undump_acr(via_context_t *via_context, uint8_t byte)
+{
+}
+
+inline static void store_acr(via_context_t *via_context, uint8_t byte)
+{
+}
+
+inline static void store_sr(via_context_t *via_context, uint8_t byte)
+{
+}
+
+inline static void store_t2l(via_context_t *via_context, uint8_t byte)
+{
+}
+
+inline static uint8_t read_pra(via_context_t *via_context, uint16_t addr)
 {
     uint8_t byte;
-    /* FIXME: not 100% sure about this... */
-    uint8_t val = ~(via_context->via[VIA_DDRA]);
-    uint8_t msk = via_context->oldpb;
-    uint8_t m;
-    int i;
+    uint8_t joy_bits;
 
-    for (m = 0x1, i = 0; i < 8; m <<= 1, i++) {
-        if (!(msk & m)) {
-            val &= ~rev_keyarr[i];
-        }
-    }
+    /*
+        Port A is connected this way:
 
-    byte = val | (via_context->via[VIA_PRA] & via_context->via[VIA_DDRA]);
+        bit 0  IEC clock
+        bit 1  IEC data
+        bit 2  joystick switch 0 (up)
+        bit 3  joystick switch 1 (down)
+        bit 4  joystick switch 2 (left)
+        bit 5  joystick switch 4 (fire)
+        bit 6  tape sense
+        bit 7  IEC ATN
+    */
+
+    /* Setup joy bits (2 through 5).  Use the `or' of the values
+       of both joysticks so that it works with every joystick
+       setting.  This is a bit slow... we might think of a
+       faster method.  */
+    joy_bits = read_joyport_dig(JOYPORT_1);
+    joy_bits = ((joy_bits & 0x7) << 2) | ((joy_bits & 0x10) << 1);
+
+    joy_bits |= tape_sense ? 0 : 0x40;
+
+    /* We assume `iec_pa_read()' returns the non-IEC bits
+       as zeroes. */
+    byte = ((via_context->via[VIA_PRA] & via_context->via[VIA_DDRA])
+            | ((iec_pa_read() | joy_bits) & ~(via_context->via[VIA_DDRA])));
     return byte;
 }
 
-static uint8_t read_prb(via_context_t *via_context)
+inline static uint8_t read_prb(via_context_t *via_context)
 {
-    uint8_t byte;
-    /* FIXME: not 100% sure about this... */
-    uint8_t val = ~(via_context->via[VIA_DDRB]);
-    uint8_t msk = via_context->oldpa;
-    uint8_t joy = ~read_joyport_dig(JOYPORT_1);
-    int m, i;
+    uint8_t byte = 0xff;
+    byte = via_context->via[VIA_PRB] | ~(via_context->via[VIA_DDRB]);
 
-    for (m = 0x1, i = 0; i < 8; m <<= 1, i++) {
-        if (!(msk & m)) {
-            val &= ~keyarr[i];
-        }
-    }
+    byte = read_userport_pbx(byte);
 
-    /* Bit 7 is mapped to the right direction of the joystick (bit
-       3 in `joystick_value[]'). */
-    if (joy & 0x8) {
-        val &= 0x7f;
-    }
-
-    byte = val | (via_context->via[VIA_PRB] & via_context->via[VIA_DDRB]);
     return byte;
 }
 
-void via2_init(via_context_t *via_context)
+void via1_init(via_context_t *via_context)
 {
-    viacore_init(machine_context.via2, maincpu_alarm_context,
+    viacore_init(machine_context.via1, maincpu_alarm_context,
                  maincpu_int_status);
 }
 
-void vic20via2_setup_context(machine_context_t *machinecontext)
+void vic20via1_setup_context(machine_context_t *machinecontext)
 {
     via_context_t *via;
 
-    machinecontext->via2 = lib_malloc(sizeof(via_context_t));
-    via = machinecontext->via2;
+    machinecontext->via1 = lib_malloc(sizeof(via_context_t));
+    via = machinecontext->via1;
 
     via->prv = NULL;
     via->context = NULL;
@@ -214,7 +288,7 @@ void vic20via2_setup_context(machine_context_t *machinecontext)
 
     via->write_offset = 0;
 
-    via->irq_line = IK_IRQ;
+    via->irq_line = IK_NMI;
 
     via->undump_pra = undump_pra;
     via->undump_prb = undump_prb;
--- vice/src/vic20/vic20via2.c
+++ vice/src/vic20/vic20via2.c
@@ -1,5 +1,5 @@
 /*
- * vic20via2.c - VIA1 emulation in the VIC20.
+ * vic20via2.c - VIA2 emulation in the VIC20.
  *
  * Written by
  *  Andre Fachat <fachat@physik.tu-chemnitz.de>
@@ -33,45 +33,30 @@
 #include "datasette.h"
 #include "interrupt.h"
 #include "joyport.h"
-#include "joystick.h"
 #include "keyboard.h"
 #include "lib.h"
 #include "maincpu.h"
 #include "tapeport.h"
 #include "types.h"
-#include "userport.h"
 #include "via.h"
-#include "vic.h"
 #include "vic20.h"
-#include "vic20-resources.h"
 #include "vic20iec.h"
 #include "vic20via.h"
 
-int vic20_vflihack_userport = 0xff;
 
-static int userport_pa6 = 1;    /* HACK: pin 8, this is also connected to tape sense */
-static int userport_pb = 0xff;
-
-/* HACK: on the C64, pin 8 is connected to PC2, which automatically generates a pulse
-         when PB is written, which is why both is updated by the same function. */
-static void update_portbits(void)
+void via2_store(uint16_t addr, uint8_t data)
 {
-    store_userport_pbx(userport_pb, userport_pa6 ? USERPORT_NO_PULSE : USERPORT_PULSE);
+    viacore_store(machine_context.via2, addr, data);
 }
 
-void via1_store(uint16_t addr, uint8_t data)
+uint8_t via2_read(uint16_t addr)
 {
-    viacore_store(machine_context.via1, addr, data);
+    return viacore_read(machine_context.via2, addr);
 }
 
-uint8_t via1_read(uint16_t addr)
+uint8_t via2_peek(uint16_t addr)
 {
-    return viacore_read(machine_context.via1, addr);
-}
-
-uint8_t via1_peek(uint16_t addr)
-{
-    return viacore_peek(machine_context.via1, addr);
+    return viacore_peek(machine_context.via2, addr);
 }
 
 static void set_ca2(via_context_t *via_context, int state)
@@ -80,94 +65,57 @@ static void set_ca2(via_context_t *via_context, int state)
 
 static void set_cb2(via_context_t *via_context, int state, int offset)
 {
-    if (!via_context->cb2_is_input) {
-        /* CB2 goes to userport pin M */
-        store_userport_pa2(state);
-    }
 }
 
-static void set_int(via_context_t *via_context, unsigned int int_num, int value, CLOCK rclk)
+static void set_int(via_context_t *via_context, unsigned int int_num,
+                    int value, CLOCK rclk)
 {
-    interrupt_set_nmi(maincpu_int_status, int_num, value, rclk);
+    interrupt_set_irq(maincpu_int_status, int_num, value, rclk);
 }
 
 static void restore_int(via_context_t *via_context, unsigned int int_num, int value)
 {
-    interrupt_restore_nmi(maincpu_int_status, int_num, value);
+    interrupt_restore_irq(maincpu_int_status, int_num, value);
 }
 
-static int tape_sense = 0;
-static int tape_write_in = 0;
-static int tape_motor_in = 0;
-
-void via1_set_tape_sense(int v)
+static void undump_acr(via_context_t *via_context, uint8_t byte)
 {
-    tape_sense = v;
 }
 
-/* FIXME: find out how to set the write in and motor in lines */
-void via1_set_tape_write_in(int v)
+static void store_acr(via_context_t *via_context, uint8_t byte)
 {
-    tape_write_in = v;
 }
 
-void via1_set_tape_motor_in(int v)
+static void store_sr(via_context_t *via_context, uint8_t byte)
 {
-    tape_motor_in = v;
 }
 
-static void via1_internal_lightpen_check(uint8_t pa)
+static void store_t2l(via_context_t *via_context, uint8_t byte)
 {
-    uint8_t b = read_joyport_dig(JOYPORT_1);
-
-    b &= pa;
-
-    vic_set_light_pen(maincpu_clk, !(b & 0x20));
-}
-
-void via1_check_lightpen(void)
-{
-    uint8_t pa = machine_context.via1->via[VIA_PRA] | ~(machine_context.via1->via[VIA_DDRA]);
-
-    via1_internal_lightpen_check(pa);
 }
 
 static void undump_pra(via_context_t *via_context, uint8_t byte)
 {
-    iec_pa_write(byte);
 }
 
 static void store_pra(via_context_t *via_context, uint8_t byte, uint8_t myoldpa,
                       uint16_t addr)
 {
-    uint8_t joy_bits = 0;
-
-    via1_internal_lightpen_check(byte);
-    iec_pa_write(byte);
-
-    joy_bits = ((byte & 0x20) >> 1) | ((byte & 0x1c) >> 2);
-    store_joyport_dig(JOYPORT_1, joy_bits, 0x17);
-
-    tapeport_set_sense_out(TAPEPORT_PORT_1, byte & 0x40 ? 1 : 0);
-
-    userport_pa6 = byte & 0x40 ? 1 : 0;
-    update_portbits();  /* HACK: see above */
 }
 
 static void undump_prb(via_context_t *via_context, uint8_t byte)
 {
-    userport_pb = byte;
-    update_portbits();  /* HACK: see above */
 }
 
 static void store_prb(via_context_t *via_context, uint8_t byte, uint8_t myoldpb,
                       uint16_t addr)
 {
-    /* for mike's VFLI hack, PB0-PB3 are used as A10-A13 of the color ram */
-    vic20_vflihack_userport = byte & 0x0f;
+    uint8_t joy_bit = (byte & 0x80) >> 4;
 
-    userport_pb = byte;
-    update_portbits();  /* HACK: see above */
+    if ((byte ^ myoldpb) & 8) {
+        tapeport_toggle_write_bit(TAPEPORT_PORT_1, (~(via_context->via[VIA_DDRB]) | byte) & 0x8);
+    }
+    store_joyport_dig(JOYPORT_1, joy_bit, 8);
 }
 
 static void undump_pcr(via_context_t *via_context, uint8_t byte)
@@ -176,15 +124,12 @@ static void undump_pcr(via_context_t *via_context, uint8_t byte)
 
 static void reset(via_context_t *via_context)
 {
-    userport_pb = 0xff;
-    userport_pa6 = 1;
-    store_userport_pbx(0xff, USERPORT_NO_PULSE);
-    store_userport_pa2(1);
+/*iec_pcr_write(0x22);*/
 }
 
 static uint8_t store_pcr(via_context_t *via_context, uint8_t byte, uint16_t addr)
 {
-    /* FIXME: should use via_set_ca2() and via_set_cb2() */
+    /* FIXME: this should use via_set_ca2() and via_set_cb2() */
     if (byte != via_context->via[VIA_PCR]) {
         register uint8_t tmp = byte;
         /* first set bit 1 and 5 to the real output values */
@@ -194,86 +139,67 @@ static uint8_t store_pcr(via_context_t *via_context, uint8_t byte, uint16_t addr
         if ((tmp & 0xc0) != 0xc0) {
             tmp |= 0x20;
         }
-
-        tapeport_set_motor(TAPEPORT_PORT_1, !(byte & 0x02));
-
-        store_userport_pa2((uint8_t)((byte & 0x20) >> 5));
+        iec_pcr_write(tmp);
     }
     return byte;
 }
 
-static void undump_acr(via_context_t *via_context, uint8_t byte)
-{
-}
-
-inline static void store_acr(via_context_t *via_context, uint8_t byte)
-{
-}
-
-inline static void store_sr(via_context_t *via_context, uint8_t byte)
-{
-}
-
-inline static void store_t2l(via_context_t *via_context, uint8_t byte)
-{
-}
-
-inline static uint8_t read_pra(via_context_t *via_context, uint16_t addr)
+static uint8_t read_pra(via_context_t *via_context, uint16_t addr)
 {
     uint8_t byte;
-    uint8_t joy_bits;
+    /* FIXME: not 100% sure about this... */
+    uint8_t val = ~(via_context->via[VIA_DDRA]);
+    uint8_t msk = via_context->oldpb;
+    uint8_t m;
+    int i;
 
-    /*
-        Port A is connected this way:
+    for (m = 0x1, i = 0; i < 8; m <<= 1, i++) {
+        if (!(msk & m)) {
+            val &= ~rev_keyarr[i];
+        }
+    }
 
-        bit 0  IEC clock
-        bit 1  IEC data
-        bit 2  joystick switch 0 (up)
-        bit 3  joystick switch 1 (down)
-        bit 4  joystick switch 2 (left)
-        bit 5  joystick switch 4 (fire)
-        bit 6  tape sense
-        bit 7  IEC ATN
-    */
-
-    /* Setup joy bits (2 through 5).  Use the `or' of the values
-       of both joysticks so that it works with every joystick
-       setting.  This is a bit slow... we might think of a
-       faster method.  */
-    joy_bits = read_joyport_dig(JOYPORT_1);
-    joy_bits = ((joy_bits & 0x7) << 2) | ((joy_bits & 0x10) << 1);
-
-    joy_bits |= tape_sense ? 0 : 0x40;
-
-    /* We assume `iec_pa_read()' returns the non-IEC bits
-       as zeroes. */
-    byte = ((via_context->via[VIA_PRA] & via_context->via[VIA_DDRA])
-            | ((iec_pa_read() | joy_bits) & ~(via_context->via[VIA_DDRA])));
+    byte = val | (via_context->via[VIA_PRA] & via_context->via[VIA_DDRA]);
     return byte;
 }
 
-inline static uint8_t read_prb(via_context_t *via_context)
+static uint8_t read_prb(via_context_t *via_context)
 {
-    uint8_t byte = 0xff;
-    byte = via_context->via[VIA_PRB] | ~(via_context->via[VIA_DDRB]);
+    uint8_t byte;
+    /* FIXME: not 100% sure about this... */
+    uint8_t val = ~(via_context->via[VIA_DDRB]);
+    uint8_t msk = via_context->oldpa;
+    uint8_t joy = ~read_joyport_dig(JOYPORT_1);
+    int m, i;
 
-    byte = read_userport_pbx(byte);
+    for (m = 0x1, i = 0; i < 8; m <<= 1, i++) {
+        if (!(msk & m)) {
+            val &= ~keyarr[i];
+        }
+    }
 
+    /* Bit 7 is mapped to the right direction of the joystick (bit
+       3 in `joystick_value[]'). */
+    if (joy & 0x8) {
+        val &= 0x7f;
+    }
+
+    byte = val | (via_context->via[VIA_PRB] & via_context->via[VIA_DDRB]);
     return byte;
 }
 
-void via1_init(via_context_t *via_context)
+void via2_init(via_context_t *via_context)
 {
-    viacore_init(machine_context.via1, maincpu_alarm_context,
+    viacore_init(machine_context.via2, maincpu_alarm_context,
                  maincpu_int_status);
 }
 
-void vic20via1_setup_context(machine_context_t *machinecontext)
+void vic20via2_setup_context(machine_context_t *machinecontext)
 {
     via_context_t *via;
 
-    machinecontext->via1 = lib_malloc(sizeof(via_context_t));
-    via = machinecontext->via1;
+    machinecontext->via2 = lib_malloc(sizeof(via_context_t));
+    via = machinecontext->via2;
 
     via->prv = NULL;
     via->context = NULL;
@@ -288,7 +214,7 @@ void vic20via1_setup_context(machine_context_t *machinecontext)
 
     via->write_offset = 0;
 
-    via->irq_line = IK_NMI;
+    via->irq_line = IK_IRQ;
 
     via->undump_pra = undump_pra;
     via->undump_prb = undump_prb;
